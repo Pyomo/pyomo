Pyomo/pyomo

commit 18aec38397afca44883151f83345cc33a64f54d3

Merging the coopr.dae v2 branch onto Pyomo trunk.
git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9315 570ccb8d-5833-0410-9ce8-cbedd0da42eb

pyomo/dae/plugins/finitedifference.py
@@ -1,0 +1,246 @@
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the Pyomo README.txt file.
+#  _________________________________________________________________________
+
+import logging
+import sys
+
+from pyomo.core.base.plugin import alias
+from pyomo.core.base import Transformation
+from pyomo.core. import *
+from pyomo.core.base import Block
+from pyomo.dae import *
+from pyomo.dae.misc import generate_finite_elements
+from pyomo.dae.misc import update_contset_indexed_component
+from pyomo.dae.misc import create_partial_expression
+from pyomo.dae.misc import add_discretization_equations
+from pyomo.dae.misc import block_fully_discretized
+
+logger = logging.getLogger('pyomo.core')
+
+def _central_transform(v,s):
+    """
+    Applies the Central Difference formula of order O(h^2) for first derivatives
+    """
+    def _ctr_fun(i):
+        tmp = sorted(s)
+        idx = tmp.index(i)
+        if idx == 0: # Needed since '-1' is considered a valid index in Python
+            raise IndexError("list index out of range")
+        return 1/(tmp[idx+1]-tmp[idx-1])*(v(tmp[idx+1])-v(tmp[idx-1]))
+    return _ctr_fun
+
+def _central_transform_order2(v,s):
+    """
+    Applies the Central Difference formula of order O(h^2) for second derivatives
+    """
+    def _ctr_fun2(i):
+        tmp = sorted(s)
+        idx = tmp.index(i)
+        if idx == 0: # Needed since '-1' is considered a valid index in Python
+            raise IndexError("list index out of range")
+        return 1/((tmp[idx+1]-tmp[idx])*(tmp[idx]-tmp[idx-1]))*(v(tmp[idx+1])-2*v(tmp[idx])+v(tmp[idx-1]))
+    return _ctr_fun2
+
+def _forward_transform(v,s):
+    """
+    Applies the Forward Difference formula of order O(h) for first derivatives
+    """
+    def _fwd_fun(i):
+        tmp = sorted(s)
+        idx = tmp.index(i)
+        return 1/(tmp[idx+1]-tmp[idx])*(v(tmp[idx+1])-v(tmp[idx]))
+    return _fwd_fun
+
+def _forward_transform_order2(v,s):
+    """
+    Applies the Forward Difference formula of order O(h) for second derivatives
+    """
+    def _fwd_fun(i):
+        tmp = sorted(s)
+        idx = tmp.index(i)
+        return 1/((tmp[idx+2]-tmp[idx+1])*(tmp[idx+1]-tmp[idx]))*(v(tmp[idx+2])-2*v(tmp[idx+1])+v(tmp[idx]))
+    return _fwd_fun
+
+def _backward_transform(v,s):
+    """
+    Applies the Backward Difference formula of order O(h) for first derivatives
+    """
+    def _bwd_fun(i):
+        tmp = sorted(s)
+        idx = tmp.index(i)
+        if idx == 0: # Needed since '-1' is considered a valid index in Python
+            raise IndexError("list index out of range")
+        return 1/(tmp[idx]-tmp[idx-1])*(v(tmp[idx])-v(tmp[idx-1]))
+    return _bwd_fun
+
+def _backward_transform_order2(v,s):
+    """
+    Applies the Backward Difference formula of order O(h) for second derivatives
+    """
+    def _bwd_fun(i):
+        tmp = sorted(s)
+        idx = tmp.index(i)
+        if idx == 0 or idx == 1: # Needed since '-1' is considered a valid index in Python
+            raise IndexError("list index out of range")
+        return 1/((tmp[idx-1]-tmp[idx-2])*(tmp[idx]-tmp[idx-1]))*(v(tmp[idx])-2*v(tmp[idx-1])+v(tmp[idx-2]))
+    return _bwd_fun
+
+class Finite_Difference_Transformation(Transformation):
+    """
+    Transformation that applies finite difference methods to
+    DAE, ODE, or PDE models. 
+    
+    """
+    alias('dae.finite_difference_discretization', doc="TODO")
+
+    def __init__(self):
+        super(Finite_Difference_Transformation, self).__init__()
+        self._nfe = {}
+        self.all_schemes = {
+            'BACKWARD' : (_backward_transform, _backward_transform_order2),
+            'CENTRAL' : (_central_transform, _central_transform_order2),
+            'FORWARD' : (_forward_transform, _forward_transform_order2),
+            }
+
+    def _setup(self, instance):
+        instance = instance.clone()
+        instance.concrete_mode()
+        return instance
+
+    def apply(self, instance, **kwds):
+        """
+        Applies the transformation to a modeling instance
+
+        Keyword Arguments:
+        nfe           The desired number of finite element points to be 
+                      included in the discretization.
+        wrt           Indicates which ContinuousSet the transformation 
+                      should be applied to. If this keyword argument is not
+                      specified then the same scheme will be applied to all
+                      ContinuousSets.
+        scheme        Indicates which finite difference method to apply. 
+                      Options are BACKWARD, CENTRAL, or FORWARD. The default
+                      scheme is the backward difference method
+        clonemodel    Indicates whether the transformation should be applied to
+                      a copy of the model or the model itself.
+        """
+        # The discretization expressions generated by this transformation
+        # are specific to a model instance. Therefore, if the user applies
+        # the transformation to each differentialset individually, we want
+        # to continue modifying the same instance and not a new copy each time
+        tmpclone = kwds.pop('clonemodel',True)
+        if tmpclone:
+            instance = self._setup(instance)  
+        tmpnfe = kwds.pop('nfe',10)
+        tmpds = kwds.pop('wrt',None)
+        tmpscheme = kwds.pop('scheme','BACKWARD')
+        self._scheme_name = tmpscheme.upper()
+
+        if tmpds is not None:
+            if tmpds.type() is not ContinuousSet:
+                raise TypeError("The component specified using the 'wrt' keyword "\
+                     "must be a differential set")
+            elif tmpds.get_discretization_info().has_key('scheme'):
+                raise ValueError("The discretization scheme '%s' has already been applied "\
+                     "to the ContinuousSet '%s'" %(tmpds.get_discretization_info()['scheme'],tmpds.cname(True)))
+
+        if self._nfe.has_key(None):
+            raise ValueError("A general discretization scheme has already been applied to "\
+                    "to every differential set in the model. If you would like to specify a "\
+                    "specific discretization scheme for one of the differential sets you must discretize "\
+                    "each differential set individually. If you would like to apply a different "\
+                    "discretization scheme to all differential sets you must declare a new Implicit_"\
+                    "Euler object")
+
+        if len(self._nfe) == 0 and tmpds is None:
+            # Same discretization on all differentialsets
+            self._nfe[None] = tmpnfe
+            currentds = None
+        else :
+            self._nfe[tmpds.cname(True)]=tmpnfe
+            currentds = tmpds.cname(True)
+
+        self._scheme = self.all_schemes.get(self._scheme_name,None)
+        if self._scheme is None:
+            raise ValueError("Unknown finite difference scheme '%s' specified using the "\
+                     "'scheme' keyword. Valid schemes are 'BACKWARD', 'CENTRAL', and "\
+                     "'FORWARD'" %(tmpscheme))
+
+        for block in instance.all_blocks():
+            self._transformBlock(block,currentds)
+
+        # Taken from bigm
+        # REQUIRED: re-call preprocess()
+        self._applied = True
+        if block_fully_discretized(instance):
+            instance.preprocess()
+        return instance
+
+    def _transformBlock(self, block, currentds):
+
+        self._fe = {}
+        for ds in block.components(ContinuousSet).itervalues():
+            if currentds is None or currentds is ds.cname(True):
+                generate_finite_elements(ds,self._nfe[currentds])
+                if not ds.get_changed():
+                    if len(ds)-1 > self._nfe[currentds]:
+                        print("***WARNING: More finite elements were found in ContinuousSet "\
+                            "'%s' than the number of finite elements specified in apply. "\
+                            "The larger number of finite elements will be used." %(ds.cname(True)))
+                
+                self._nfe[ds.cname(True)]=len(ds)-1
+                self._fe[ds.cname(True)]=sorted(ds)
+                # Adding discretization information to the differentialset object itself
+                # so that it can be accessed outside of the discretization object
+                disc_info = ds.get_discretization_info()
+                disc_info['nfe']=self._nfe[ds.cname(True)]
+                disc_info['scheme']=self._scheme_name + ' Difference'
+                     
+        # Maybe check to see if any of the ContinuousSets have been changed,
+        # if they haven't then the model components need not be updated
+        # or even iterated through
+
+        for c in block.components().itervalues():
+            update_contset_indexed_component(c)
+
+        for d in block.components(DerivativeVar).itervalues():
+            dsets = d.get_continuousset_list()
+            for i in set(dsets):
+                if currentds is None or i.cname(True) is currentds:
+                    oldexpr = d.get_derivative_expression()
+                    loc = d.get_state_var()._contset[i]
+                    count = dsets.count(i)
+                    if count >= 3:
+                        raise DAE_Error(
+                            "Error discretizing '%s' with respect to '%s'. Current implementation "\
+                            "only allows for taking the first or second derivative with respect to "\
+                            "a particular ContinuousSet" %(d.cname(True),i.cname(True)))
+                    scheme = self._scheme[count-1]
+                    newexpr = create_partial_expression(scheme,oldexpr,i,loc)
+                    d.set_derivative_expression(newexpr)
+
+            # Reclassify DerivativeVar if all indexing ContinuousSets have been discretized
+            if d.is_fully_discretized():
+                add_discretization_equations(block,d)
+                block.reclassify_component_type(d,Var)
+
+        # Reclassify StateVars and Integrals if all ContinuousSets have been discretized       
+        if block_fully_discretized(block):
+            for j in block.components(StateVar).itervalues():  
+                block.reclassify_component_type(j,Var)
+            
+            if block.contains_component(Integral):
+                for i in block.components(Integral).itervalues():  
+                    i.reconstruct()
+                    block.reclassify_component_type(i,Expression)
+                # If a model contains integrals they are most likely to appear in the objective
+                # function which will need to be reconstructed after the model is discretized.
+                for k in block.components(Objective).itervalues():
+                    k.reconstruct()

examples/dae/Heat_Conduction.py
@@ -1,0 +1,46 @@
+# Heat Conduction Model
+
+from pyomo.environ import *
+from pyomo.dae import *
+
+m = ConcreteModel()
+m.time = ContinuousSet(bounds=(0,1))
+m.x = ContinuousSet(bounds=(0,10))
+m.y = ContinuousSet(bounds=(0,5))
+m.T = StateVar(m.x,m.y,m.time)
+m.u = Var(m.x,m.y,m.time)
+m.T0 = Param(initialize=5)
+m.TD = Param(m.x,m.y,initialize=25)
+m.Ux0 = Param(initialize=10)
+m.Uy5 = Param(initialize=15)
+
+m.dTdx = DerivativeVar(m.T,wrt=m.x)
+m.d2Tdx2 = DerivativeVar(m.T,wrt=(m.x,m.x))
+m.dTdy = DerivativeVar(m.T,wrt=m.y)
+m.d2Tdy2 = DerivativeVar(m.T,wrt=(m.y,m.y))
+m.dTdt = DerivativeVar(m.T,wrt=m.time)
+
+def _heateq(m,i,j,k):
+    return m.d2Tdx2[i,j,k] + m.d2Tdy2[i,j,k] + m.u[i,j,k] == m.dTdt[i,j,k]
+m.heateq = Constraint(m.x,m.y,m.time,rule=_heateq)
+
+def _initT(m,i,j):
+    return m.T[i,j,0] == m.T0
+m.initT = Constraint(m.x,m.y,rule=_initT)
+
+def _xbound(m,j,k):
+    return m.dTdx[0,j,k] == m.Ux0
+m.xbound = Constraint(m.y,m.time,rule=_xbound)
+
+def _ybound(m,i,k):
+    return m.dTdy[i,5,k] == m.Uy5
+m.ybound = Constraint(m.x,m.time,rule=_ybound)
+
+# def _intExp(m,i,j):
+#     return m.T[i,j,1] - m.TD[i,j]
+# m.intExp = Expression(m.x,m.y,rule=_intExp)
+
+# def _obj(m):
+#     return Integral(Integral(expr=m.intExp,wrt=m.x,bounds=(0,10)),
+#                     wrt=m.y,bounds=(0,5))
+# m.obj = Objective(rule=_obj)

examples/dae/Optimal_Control.py
@@ -1,0 +1,41 @@
+# Sample Problem 1 (Ex 1 from Dynopt Guide)
+#
+#	min X2(tf)
+#	s.t.	X1_dot = u			X1(0) = 1
+#		X2_dot = X1^2 + u^2		X2(0) = 0
+#		tf = 1
+
+from pyomo.environ import *
+from pyomo.dae import *
+
+m = ConcreteModel()
+
+m.t = ContinuousSet(bounds=(0,1)) 
+
+m.x1 = StateVar(m.t, bounds=(0,1))
+m.x2 = StateVar(m.t, bounds=(0,1))
+m.u = Var(m.t, initialize=0)
+
+m.x1dot = DerivativeVar(m.x1)
+m.x2dot = DerivativeVar(m.x2)
+
+m.obj = Objective(expr=m.x2[1])
+
+def _x1dot(M,i):
+	if i == 0:
+		return Constraint.Skip
+	return M.x1dot[i] == M.u[i]
+m.x1dotcon = Constraint(m.t, rule=_x1dot)
+
+def _x2dot(M,i):
+	if i == 0:
+		return Constraint.Skip
+	return M.x2dot[i] == M.x1[i]**2 + M.u[i]**2
+m.x2dotcon = Constraint(m.t, rule=_x2dot)
+
+def _init(M):
+	yield M.x1[0] == 1
+	yield M.x2[0] == 0
+	yield ConstraintList.End
+m.init_conditions = ConstraintList(rule=_init)
+

examples/dae/PDE_example.py
@@ -1,0 +1,84 @@
+# Example 1 from http://www.mathworks.com/help/matlab/ref/pdepe.html
+
+from pyomo.environ import *
+from pyomo.dae import *
+from pyomo.opt import SolverFactory
+from pyomo.dae.plugins.finitedifference import Finite_Difference_Transformation
+from pyomo.dae.plugins.colloc import Collocation_Discretization_Transformation
+import math
+
+m = ConcreteModel()
+m.t = ContinuousSet(bounds=(0,2))
+m.x = ContinuousSet(bounds=(0,1))
+m.u = StateVar(m.x,m.t)
+
+m.dudx = DerivativeVar(m.u,wrt=m.x)
+m.dudx2 = DerivativeVar(m.u,wrt=(m.x,m.x))
+m.dudt = DerivativeVar(m.u,wrt=m.t)
+
+def _pde(m,i,j):
+    if i == 0 or i == 1 or j == 0 :
+        return Constraint.Skip
+    return math.pi**2*m.dudt[i,j] == m.dudx2[i,j]
+m.pde = Constraint(m.x,m.t,rule=_pde)
+
+def _initcon(m,i):
+    if i == 0 or i == 1:
+        return Constraint.Skip
+    return m.u[i,0] == sin(math.pi*i)
+m.initcon = Constraint(m.x,rule=_initcon)
+
+def _lowerbound(m,j):
+    return m.u[0,j] == 0
+m.lowerbound = Constraint(m.t,rule=_lowerbound)
+
+def _upperbound(m,j):
+    return math.pi*exp(-j)+m.dudx[1,j] == 0
+m.upperbound = Constraint(m.t,rule=_upperbound)
+
+m.obj = Objective(expr=1)
+
+# Discretize using Finite Difference Method
+discretize = Finite_Difference_Transformation()
+disc = discretize.apply(m,nfe=25,wrt=m.x,scheme='BACKWARD')
+disc = discretize.apply(disc,nfe=20,wrt=m.t,scheme='BACKWARD',clonemodel=False)
+
+# Discretize using Orthogonal Collocation
+#discretize2 = Collocation_Discretization_Transformation()
+#disc = discretize2.apply(disc,nfe=10,ncp=3,wrt=m.x,clonemodel=False)
+#disc = discretize2.apply(disc,nfe=20,ncp=3,wrt=m.t,clonemodel=False)
+
+
+solver='ipopt'
+opt=SolverFactory(solver)
+
+results = opt.solve(disc,tee=True)
+disc.load(results)
+
+#disc.u.pprint()
+
+x = []
+t = []
+u = []
+
+for i in sorted(disc.x):
+    temp=[]
+    tempx = []
+    for j in sorted(disc.t):
+        tempx.append(i)
+        temp.append(value(disc.u[i,j]))
+    x.append(tempx)
+    t.append(sorted(disc.t))
+    u.append(temp)
+
+
+import numpy
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1,projection='3d')
+ax.set_xlabel('Distance x')
+ax.set_ylabel('Time t')
+ax.set_title('Numerical Solution Using Backward Difference Method')
+p = ax.plot_wireframe(x,t,u,rstride=1,cstride=1)
+fig.show()

examples/dae/Parameter_Estimation.py
@@ -1,0 +1,53 @@
+# Sample Problem 2: Parameter Estimation
+# (Ex 5 from Dynopt Guide)
+#
+#	min sum((X1(ti)-X1_meas(ti))^2)
+#	s.t.	X1_dot = X2				X1(0) = p1
+#			X2_dot = 1-2*X2-X1		X2(0) = p2
+#			-1.5 <= p1,p2 <= 1.5
+#			tf = 6
+#
+
+from pyomo.environ import *
+from pyomo.dae import *
+
+model = AbstractModel()
+model.t = ContinuousSet()	
+model.MEAS_t = Set(within=model.t)	# Measurement times, must be subset of t
+model.x1_meas = Param(model.MEAS_t)
+
+model.x1 = StateVar(model.t)
+model.x2 = StateVar(model.t)
+
+model.p1 = Var(bounds=(-1.5,1.5))
+model.p2 = Var(bounds=(-1.5,1.5))
+
+model.x1dot = DerivativeVar(model.x1,wrt=model.t)
+model.x2dot = DerivativeVar(model.x2)
+
+def _init_conditions(model):
+	yield model.x1[0] == model.p1
+	yield model.x2[0] == model.p2
+	yield ConstraintList.End
+model.init_conditions = ConstraintList(rule=_init_conditions)
+
+# Alternate way to declare initial conditions
+#def _initx1(model):
+#	return model.x1[0] == model.p1		
+#model.initx1 = Constraint(rule=_initx1)
+
+#def _initx2(model):
+#	return model.x2[0] == model.p2
+#model.initx2 = Constraint(rule=_initx2)
+
+def _x1dot(model,i):
+	return model.x1dot[i] == model.x2[i]
+model.x1dotcon = Constraint(model.t, rule=_x1dot)
+
+def _x2dot(model,i):
+	return model.x2dot[i] == 1-2*model.x2[i]-model.x1[i]
+model.x2dotcon = Constraint(model.t, rule=_x2dot)
+
+def _obj(model):
+	return sum((model.x1[i]-model.x1_meas[i])**2 for i in model.MEAS_t)
+model.obj = Objective(rule=_obj)

examples/dae/Path_Constraint.py
@@ -1,0 +1,52 @@
+# Sample Problem 3: Inequality State Path Constraint
+# (Ex 4 from Dynopt Guide)
+#
+#   min x3(tf)
+#   s.t.    X1_dot = X2                     X1(0) =  0
+#           X2_dot = -X2+u                  X2(0) = -1
+#           X3_dot = X1^2+x2^2+0.005*u^2    X3(0) =  0
+#           X2-8*(t-0.5)^2+0.5 <= 0
+#           tf = 1
+#
+
+from pyomo.environ import *
+from pyomo.dae import *
+
+m = ConcreteModel()
+
+m.t = ContinuousSet(bounds=(0,1))
+
+m.x1 = StateVar(m.t)
+m.x2 = StateVar(m.t)
+m.x3 = StateVar(m.t)
+m.u = Var(m.t, initialize=0)
+
+m.dx1 = DerivativeVar(m.x1, wrt=m.t)
+m.dx2 = DerivativeVar(m.x2, wrt=m.t)
+m.dx3 = DerivativeVar(m.x3)
+
+m.obj = Objective(expr=m.x3[1])
+
+def _init(m):
+    yield m.x1[0] == 0
+    yield m.x2[0] == -1
+    yield m.x3[0] == 0
+    yield ConstraintList.End
+m.init_conditions = ConstraintList(rule=_init)
+
+def _x1dot(m, t):
+    return m.dx1[t] == m.x2[t]
+m.x1dotcon = Constraint(m.t, rule=_x1dot)
+
+def _x2dot(m, t):
+    return m.dx2[t] ==  -m.x2[t]+m.u[t]
+m.x2dotcon = Constraint(m.t, rule=_x2dot)
+
+def _x3dot(m, t):
+    return m.dx3[t] == m.x1[t]**2+m.x2[t]**2+0.005*m.u[t]**2
+m.x3dotcon = Constraint(m.t, rule=_x3dot)
+
+def _con(m, t):
+    return m.x2[t]-8*(t-0.5)**2+0.5 <= 0
+m.con = Constraint(m.t, rule=_con)
+

examples/dae/data_set2.dat
@@ -1,0 +1,8 @@
+set t := 0 1 2 3 5 6 ;
+set MEAS_t := 1 2 3 5 ;
+param x1_meas :=
+1 0.264
+2 0.594
+3 0.801
+5 0.959
+;

examples/dae/distill.dat
@@ -1,0 +1,12 @@
+set S_TRAYS :=
+1234567891011121314151617181920212223242526272829303132;
+
+set S_RECTIFICATION :=
+2345678910111213141516;
+
+set S_STRIPPING :=
+1819202122232425262728293031;
+
+param x0 := 
+1	0.9354194162	0.9005255373	0.8622964514	0.8216994035	0.7799907966	0.7385716867	0.6988049098	0.6618425349	0.62850777610	0.599252711	0.5741856812	0.55314422713	0.53578454414	0.5216655115	0.51031495116	0.50127509217	0.49412891718	0.4854499219	0.47420248120	0.45980349921	0.44164297322	0.41919109823	0.39205549224	0.36024592625	0.3240799326	0.28467681627	0.24320921328	0.20181568329	0.1617726930	0.1251497131	0.09245832632	0.064583177;
+

commit 40ecd80928c455b8e504afd694ad41e31f047533

Fixing a typo introduced in r9315.
git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9316 570ccb8d-5833-0410-9ce8-cbedd0da42eb

pyomo/dae/plugins/finitedifference.py
@@ -13,7 +13,7 @@ import sys
 
 from pyomo.core.base.plugin import alias
 from pyomo.core.base import Transformation
-from pyomo.core. import *
+from pyomo.core import *
 from pyomo.core.base import Block
 from pyomo.dae import *
 from pyomo.dae.misc import generate_finite_elements