4a9be7de46470f5928a9ead737b5e6ac8c304558
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2014-11-16 08:26:30 +0000

    Another fix so that help will be printed when no arguments are given to "pyomo solve". The a module was being supplied as the function argument rather than a parser object.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9443 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/scripting/plugins/solve.py
+++ pyomo/scripting/plugins/solve.py
@@ -1,18 +1,19 @@
-from pyomo.scripting import pyomo_parser
+from pyomo.scripting.pyomo_parser import add_subparser
 from pyomo.scripting.pyomo_command import create_parser
-
 
 def solve_exec(args=None):
     import pyomo.scripting.util
     from pyomo.scripting.pyomo_command import run_pyomo
-    return pyomo.scripting.util.run_command(command=run_pyomo, parser=pyomo_parser, args=args, name='solve')
+    return pyomo.scripting.util.run_command(command=run_pyomo,
+                                            parser=solve_parser,
+                                            args=args,
+                                            name='solve')
 
 #
 # Add a subparser for the solve command
 #
-solve_parser = create_parser(pyomo_parser.add_subparser('solve',
-        func=solve_exec,
-        help='Analyze a generic optimation model',
-        description='This pyomo subcommand is used to analyze optimization models.'
-        ))
+solve_parser = create_parser(add_subparser('solve',
+    func=solve_exec,
+    help='Analyze a generic optimation model',
+    description='This pyomo subcommand is used to analyze optimization models.'))
 
