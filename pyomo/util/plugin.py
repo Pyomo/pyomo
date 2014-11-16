0b78b33ccb5e34ef2f2e4353d032ae6bd33908ac
William Hart <whart222@users.noreply.github.com>
2014-11-16 16:13:08 +0000

    Adding copyright assertion
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9447 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/util/plugin.py
+++ pyomo/util/plugin.py
@@ -1,3 +1,12 @@
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the Pyomo README.txt file.
+#  _________________________________________________________________________
 
 __all__ = ['Plugin', 'implements', 'Interface', 'CreatePluginFactory', 'PluginMeta', 'alias', 'ExtensionPoint', 'SingletonPlugin', 'PluginFactory', 'PluginError']
 
