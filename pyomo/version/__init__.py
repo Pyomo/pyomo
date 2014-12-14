28c7d5ef3ac79bf363f0eddf71b366e54fe77d1d
William Hart <whart222@users.noreply.github.com>
2014-12-14 15:08:44 +0000

    Changes to improve coverage
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9478 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/version/__init__.py
+++ pyomo/version/__init__.py
@@ -34,19 +34,18 @@ if '/trunk/' in _init_url:
     # sys.argv[0] also fails because it doesn't not always contains the path
     from os.path import abspath, dirname, exists, join
     from inspect import getfile, currentframe
-    if exists(join( dirname( abspath( getfile( currentframe() ) ) ), 
-                    '..', '..', '.svn' )):
-        _releaselevel = 'trunk'
+    if exists(join( dirname( abspath( getfile( currentframe() ) ) ), '..', '..', '.svn' )):
+        _releaselevel = 'trunk'             #pragma:nocover
     else:
-        _releaselevel = 'VOTD'
-elif '/tags/' in _init_url:
+        _releaselevel = 'VOTD'              #pragma:nocover
+elif '/tags/' in _init_url:                 #pragma:nocover
     _releaselevel = 'final'
 
 version_info = (_major, _minor, _micro, _releaselevel, _serial)
 
 if _micro:
-    version = '.'.join(str(x) for x in version_info[:3])
+    version = '.'.join(str(x) for x in version_info[:3])        #pragma:nocover
 else:
-    version = '.'.join(str(x) for x in version_info[:2])
+    version = '.'.join(str(x) for x in version_info[:2])        #pragma:nocover
 if _releaselevel != 'final':
     version += ' ('+_releaselevel+')'
