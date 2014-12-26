0dd7cbafd2705e88562e66a44a7cb7df0b0c6aff
William Hart <whart222@users.noreply.github.com>
2014-12-26 07:57:58 +0000

    Misc cleanup
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9546 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/mpec/mcplib/billups.py
+++ examples/mpec/mcplib/billups.py
@@ -13,14 +13,13 @@
 # http://ftp.cs.wisc.edu/pub/mcplib/gams/
 #
 
-import pyomo.environ
-from pyomo.core import *
-from pyomo.mpec import Complementarity
+from pyomo.environ import *
+from pyomo.mpec import *
 
 
 def pyomo_create_model(**kwargs):
     M = ConcreteModel()
     M.x = Var()
-    M.c = Complementarity(expr=((M.x - 1.0)*(M.x - 1.0) - 1.01 >= 0, M.x >= 0))
+    M.c = Complementarity(expr=complements((M.x - 1.0)*(M.x - 1.0) - 1.01 >= 0, M.x >= 0))
     return M
 
---
+++ examples/mpec/mcplib/munson1.py
@@ -0,0 +1,37 @@
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2014 Sandia Corporation.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  This software is distributed under the BSD License.
+#  _________________________________________________________________________
+#
+# munson1.py
+#
+# Problem adapted from munson1.mod
+# http://www.ampl.com/NEW/COMPLEMENT/munson1.mod
+#
+
+from pyomo.environ import *
+from pyomo.mpec import *
+
+
+model = ConcreteModel()
+
+model.x1 = Var()
+model.x2 = Var()
+model.x3 = Var()
+
+model.f1 = Complementarity(expr=\
+                complements(model.x1 >= 0, \
+                            model.x1 + 2*model.x2 + 3*model.x3 >= 1))
+
+model.f2 = Complementarity(expr=\
+                complements(model.x2 >= 0, \
+                            model.x2 - model.x3 >= -1))
+
+model.f3 = Complementarity(expr=\
+                complements(model.x3 >= 0, \
+                            model.x1 + model.x2 >= -1))
+
