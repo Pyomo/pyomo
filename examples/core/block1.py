1495b730a467c1304d8ef0b55477dff8e266345a
William Hart <whart222@users.noreply.github.com>
2014-12-28 07:34:05 +0000

    Adding an example that illustrates the use of Block components.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9562 570ccb8d-5833-0410-9ce8-cbedd0da42eb

---
+++ examples/core/block1.py
@@ -0,0 +1,19 @@
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2014 Sandia Corporation.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  This software is distributed under the BSD License.
+#  _________________________________________________________________________
+
+from pyomo.environ import *
+
+model = ConcreteModel()
+model.x = Var()
+
+model.b = Block()
+model.b.x = Var()
+
+model.o = Objective(expr=(model.x-1.0)**2 + (model.b.x - 2.0)**2)
+
