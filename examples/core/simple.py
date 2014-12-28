1a3216783df2baf4e93e37506112b1400d206d26
William Hart <whart222@users.noreply.github.com>
2014-12-28 19:36:42 +0000

    Misc update
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9568 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/core/simple.py
+++ examples/core/simple.py
@@ -15,9 +15,13 @@ model.x1 = Var()
 model.x2 = Var(bounds=(-1,1))
 model.x3 = Var(bounds=(1,2))
 
+model.o = Objective(
+         expr=m.x1**2 + (m.x2*m.x3)**4 + \
+              m.x1*m.x3 + \
+              m.x2*sin(m.x1+m.x3) + m.x2)
 def obj_rule(m):
     return m.x1**2 + (m.x2*m.x3)**4 + \
           m.x1*m.x3 + \
           m.x2*sin(m.x1+m.x3) + m.x2
-model.obj = Objective(rule=obj_rule)
+#model.obj = Objective(rule=obj_rule)
 
