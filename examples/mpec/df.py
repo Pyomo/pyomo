04730c94ba241bdf7792d5fef52b95e820ccc78e
William Hart <whart222@users.noreply.github.com>
2014-12-29 05:50:42 +0000

    Misc editorial changes
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9573 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/mpec/df.py
+++ examples/mpec/df.py
@@ -17,18 +17,17 @@
 # Editors Masao Fukushima and Liqun Qi
 #
 
+# df.py
 from pyomo.environ import *
 from pyomo.mpec import *
-
 
 M = ConcreteModel()
 M.x = Var(bounds=(-1,2))
 M.y = Var()
 
-M.o = Objective(expr=(M.x - 1 - M.y)**2)
+M.o  = Objective(expr=(M.x - 1 - M.y)**2)
 M.c1 = Constraint(expr=M.x**2 <= 2)
 M.c2 = Constraint(expr=(M.x - 1)**2 + (M.y - 1)**2 <= 3)
 M.c3 = Complementarity(expr=complements(M.y - M.x**2 + 1 >= 0, M.y >= 0))
-
 
 model = M
