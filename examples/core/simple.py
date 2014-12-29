c6b99fe9c36fc063be938929dd48a8e8eb0ab115
William Hart <whart222@users.noreply.github.com>
2014-12-29 05:09:07 +0000

    Misc editorial updates
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9572 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/core/simple.py
+++ examples/core/simple.py
@@ -6,16 +6,14 @@
 #  the U.S. Government retains certain rights in this software.
 #  This software is distributed under the BSD License.
 #  _________________________________________________________________________
-
+# simple.py
 from pyomo.environ import *
 
 M = ConcreteModel()
-
 M.x1 = Var()
 M.x2 = Var(bounds=(-1,1))
 M.x3 = Var(bounds=(1,2))
-
-M.o = Objective(
+M.o  = Objective(
          expr=M.x1**2 + (M.x2*M.x3)**4 + \
               M.x1*M.x3 + \
               M.x2*sin(M.x1+M.x3) + M.x2)
