2fdf6e64dbd00156c90e70aa50466c4c24635f8a
William Hart <whart222@users.noreply.github.com>
2015-01-03 23:23:32 +0000

    Bug fix.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9582 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/bilevel/bard_5_1_1.py
+++ examples/bilevel/bard_5_1_1.py
@@ -12,8 +12,7 @@
 # Practical Bilevel Optimization: Algorithms and Applications
 #   Jonathan Bard
 
-import pyomo.environ
-from pyomo.core import *
+from pyomo.environ import *
 from pyomo.bilevel import *
 
 M = ConcreteModel()
@@ -23,10 +22,10 @@ M.o = Objective(expr=M.x - 4*M.y)
 
 M.sub = SubModel(fixed=M.x)
 M.sub.o = Objective(expr=M.y)
-M.sub.c1 = Constraint(expr=-M.x - M.y <= -3)
-M.sub.c2 = Constraint(expr=-2*M.x + M.y <= 0)
-M.sub.c3 = Constraint(expr=2*M.x + M.y <= 12)
-M.sub.c4 = Constraint(expr=-3*M.x + 27 <= -4)
+M.sub.c1 = Constraint(expr=-  M.x -  M.y <= -3)
+M.sub.c2 = Constraint(expr=-2*M.x +  M.y <=  0)
+M.sub.c3 = Constraint(expr= 2*M.x +  M.y <= 12)
+M.sub.c4 = Constraint(expr=-3*M.x + 2*y  <= -4)
 
 M.pprint()
 
