30269a44fe78b8dfdaac4d55c5b4396361f2e0b9
William Hart <whart222@users.noreply.github.com>
2015-01-11 12:10:59 +0000

    Bug fix
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9645 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/bilevel/bard_5_1_1.py
+++ examples/bilevel/bard_5_1_1.py
@@ -22,10 +22,10 @@ M.o = Objective(expr=M.x - 4*M.y)
 
 M.sub = SubModel(fixed=M.x)
 M.sub.o = Objective(expr=M.y)
-M.sub.c1 = Constraint(expr=-  M.x -  M.y <= -3)
-M.sub.c2 = Constraint(expr=-2*M.x +  M.y <=  0)
-M.sub.c3 = Constraint(expr= 2*M.x +  M.y <= 12)
-M.sub.c4 = Constraint(expr=-3*M.x + 2*y  <= -4)
+M.sub.c1 = Constraint(expr=-  M.x -   M.y <= -3)
+M.sub.c2 = Constraint(expr=-2*M.x +   M.y <=  0)
+M.sub.c3 = Constraint(expr= 2*M.x +   M.y <= 12)
+M.sub.c4 = Constraint(expr=-3*M.x + 2*M.y <= -4)
 
 M.pprint()
 
