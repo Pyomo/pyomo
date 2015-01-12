87041a34a6184470e98a16bf75183deab70f4d4c
William Hart <whart222@users.noreply.github.com>
2015-01-12 16:16:36 +0000

    Removing the bilevel subcommand, which is no longer used.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9650 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/bilevel/plugins/__init__.py
+++ pyomo/bilevel/plugins/__init__.py
@@ -8,7 +8,6 @@
 #  _________________________________________________________________________
 
 def load():
-    #import pyomo.bilevel.plugins.driver
     import pyomo.bilevel.plugins.dual
     import pyomo.bilevel.plugins.lcp
     import pyomo.bilevel.plugins.solver1
