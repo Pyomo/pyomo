70f77710599a25b2cb351dcf594c19dbea7ef05d
William Hart <whart222@users.noreply.github.com>
2015-02-21 05:14:41 +0000

    Removing the deprecated multisum function.  Deprecating the xsequence function.  Changed the sequence function to be a generator.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9807 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/core/base/util.py
+++ pyomo/core/base/util.py
@@ -11,17 +11,15 @@
 # Utility functions
 #
 
-__all__ = ['summation', 'dot_product', 'sequence', 'xsequence', 'prod']
+__all__ = ['summation', 'dot_product', 'sequence', 'prod', 'xsequence']
 
 try:
     xrange = xrange
 except:
     xrange = range
-    __all__.append('xrange')
 
 import pyomo.core.base.var
 import inspect
-#import pyomo.core.base.expr
 from functools import reduce
 import operator
 
@@ -92,111 +90,40 @@ def summation(*args, **kwds):
         ans += item
     return ans
 
+
 def dot_product(*args, **kwds):
     """
     A synonym for the summation() function.
     """
     return summation(*args, **kwds)
 
-# This is deprecated.  The same functionality is provided by
-# itertools.product
-def multisum(*args):
-    """
-    Returns a generator that produces all possible permutations from an
-    arbitrary set of containers.
-
-    Example:
-
-    >>> a = [1,2,3]
-    >>> b = ['foo','bar']
-    >>> c = [4,5,6]
-
-    >>> for (i,j,k) in multisum(a,b,c):
-            print i,j,k
-
-    1 foo 4
-    2 foo 4
-    3 foo 4
-    1 bar 4
-    2 bar 4
-    3 bar 4
-    1 foo 5
-    2 foo 5
-    3 foo 5
-    1 bar 5
-    2 bar 5
-    3 bar 5
-    1 foo 6
-    2 foo 6
-    3 foo 6
-    1 bar 6
-    2 bar 6
-    3 bar 6
-
-    """
-    nargs = len(args)
-
-    # Generators
-    gens = [arg.__iter__() for arg in args]
-
-    # Current value list; note that this will fail if any container is empty,
-    # which is the desired behavior
-    current = [g.next() for g in gens]
-
-    i = 0
-    while i < nargs:
-        yield current
-
-        # Find the next legal iterator, or break if we run out
-        while True:
-            if i >= nargs:
-                break
-            try:
-                current[i] = gens[i].next()
-                i = 0
-                break
-            except StopIteration:
-                gens[i] = args[i].__iter__()
-                current[i] = gens[i].next()
-                i += 1
-
 
 def sequence(*args):
     """
-    sequence([start,] stop[, step]) -> list of integers
+    sequence([start,] stop[, step]) -> generator for a list of integers
 
-    Return a list containing an arithmetic progression of integers.
-    sequence(i, j) returns [i, i+1, i+2, ..., j]; start (!) defaults to 1.
-    When step is given, it specifies the increment (or decrement).
+    Return a generator that creates a list containing an arithmetic
+    progression of integers.  
+       sequence(i, j) returns [i, i+1, i+2, ..., j]; 
+       start defaults to 1.  
+       step specifies the increment (or decrement)
     For example, sequence(4) returns [1, 2, 3, 4].
     """
     if len(args) == 0:
-        raise TypeError('sequence expected at least 1 arguments, got 0')
+        raise ValueError('sequence expected at least 1 arguments, got 0')
     if len(args) > 3:
-        raise TypeError('sequence expected at most 3 arguments, got %d' % len(args))
-    if len(args) == 1:
-        return range(1,args[0]+1)
-    if len(args) == 2:
-        return range(args[0],args[1]+1)
-    return range(args[0],args[1]+1,args[2])
-
-def xsequence(*args):
-    """
-    xsequence([start,] stop[, step]) -> xrange object
-
-    Like sequence(), but instead of returning a list, returns an object that
-    generates the numbers in the sequence on demand.  For looping, this is
-    slightly faster than sequence() and more memory efficient.
-    """
-    if len(args) == 0:
-        raise TypeError('xsequence expected at least 1 arguments, got 0')
-    if len(args) > 3:
-        raise TypeError('xsequence expected at most 3 arguments, got %d' % len(args))
+        raise ValueError('sequence expected at most 3 arguments, got %d' % len(args))
     if len(args) == 1:
         return xrange(1,args[0]+1)
     if len(args) == 2:
         return xrange(args[0],args[1]+1)
     return xrange(args[0],args[1]+1,args[2])
+
+
+def xsequence(*args):
+    print("WARNING: The xsequence function is deprecated.  Use the sequence function, which returns a generator.") 
+    return sequence(*args)
+
 
 def is_functor(obj):
     """
