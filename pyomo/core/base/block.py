8fe1f4620d5c7ca86a1aba9ee5919ccd75c9ce5f
William Hart <whart222@users.noreply.github.com>
2015-02-21 06:11:59 +0000

    Bug fix in the test used to see if we have a Block or Model.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9816 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/core/base/block.py
+++ pyomo/core/base/block.py
@@ -1235,7 +1235,8 @@ Components must now specify their rules explicitly using 'rule=' keywords.""" %
             return
         if ostream is None:
             ostream = sys.stdout
-        if self.parent_component() is not None:
+        if (self._parent is not None) and (self._parent() is not None): 
+        #if self.parent_component() is not None:
             ostream.write(prefix+"Block "+self.cname()+'\n')
         else:
             ostream.write(prefix+"Model "+self.cname()+'\n')
