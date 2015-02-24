e355dc7d03cfc57417e22e24cae16cd2da737df1
Bethany Nicholson <blnicho@users.noreply.github.com>
2015-02-24 19:14:51 +0000

    Cleaning up the ContinuousSet class
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9829 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/dae/contset.py
+++ pyomo/dae/contset.py
@@ -18,27 +18,41 @@ from pyomo.core.base.numvalue import native_numeric_types
 logger = logging.getLogger('pyomo.core')
 
 class ContinuousSet(OrderedSimpleSet):
-    """ ContinuousSet objects are used to index DAE Pyomo objects
+    """ ContinuousSet objects are used to represent bounded continuous domains
     
-        This is a set that represents a list of numeric values. Minimally, 
-        this set must contain two values defining a range over which a differential
-        equations will be discretized and solved. If additional values are specified
-        they will be used as finite elements locations in the discretization. 
+        Minimally, this set must contain two numeric values defining the
+        bounds of a continuous range. Discrete points of interest may
+        be added to the continuous set. A continuous set is one
+        dimensional and may only contain numerical values.
 
-        Constructor
-            Arguments:
-                name        The name of the continuous set
-                rule
-                initialize  Default set members, which may be overridden
-                            when setting up this set
-                doc         Documentation for this object
+        Constructor Arguments:
+            initialize  Default discretization points to be included
+
+            bounds      Specify bounding points for the continuous domain. 
+                        The bounds will be included as discrete points in 
+                        the continuous set but will not be used to restrict
+                        points added to the continuous set through the 
+                        'initialize' argument, a data file, or the add() method
+
         Private Attributes:
             _changed   This keeps track of whether or not the ContinuousSet was
                        changed during discretization. If the user specifies 
                        all of the needed discretization points before the 
                        discretization then there is no need to go back through
                        the model and reconstruct things indexed by the ContinuousSet
+                      
+            _fe        This is a sorted list of the finite element points in the
+                       ContinuousSet. i.e. this list contains all the discrete 
+                       points in the ContinuousSet that are not collocation points.
+                       Points that are both finite element points and collocation
+                       points will be included in this list.
+
+            _discretization_info
+                       This is a dictionary which contains information on the 
+                       discretization transformation which has been applied to 
+                       the ContinuousSet.
     """
+
     def __init__(self, *args, **kwds):
         """ Constructor """
         if kwds.pop("filter", None) is not None:
@@ -131,11 +145,6 @@ class ContinuousSet(OrderedSimpleSet):
                 raise ValueError("ContinuousSet can only contain numeric values")
 
         if self._bounds is None:
-            # This is needed because if a set is initialized using a function
-            # the bounds are not updated automatically
-            #if self.dimen == 1 and len(self>2):
-            #    self._bounds[0] == min(self)
-            #    self._bounds[1] == max(self)
             raise ValueError("ContinuousSet '%s' must have at least two values indicating "\
                 "the range over which a differential equation is to be discretized" % (self.name))
         if self._bounds[0].__class__ not in native_numeric_types:
@@ -151,12 +160,14 @@ class ContinuousSet(OrderedSimpleSet):
         # with how 'Set' treats this situation. 
         if self._bounds[0] not in self.value:
             self.add(self._bounds[0])
+            self._sort()
         if self._bounds[1] not in self.value:
             self.add(self._bounds[1])
+            self._sort()
         
         if len(self) < 2:
             raise ValueError("ContinuousSet '%s' must have at least two values indicating "\
                 "the range over which a differential equation is to be discretized" % (self.name))
         self._fe = sorted(self)
 
-register_component(ContinuousSet, "A sequence of numeric values defining the range over which a differential equation is to be discretized and solved.")
+register_component(ContinuousSet, "A bounded continuous numerical range optionally containing discrete points of interest.")
