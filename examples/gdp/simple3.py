ef74381ba2d27e3a4ea24a03a1082ec485fa3640
William Hart <whart222@users.noreply.github.com>
2015-03-24 11:56:01 +0000

    Adding several simple examples that illustrate the model variants that work with existing transformations.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9921 570ccb8d-5833-0410-9ce8-cbedd0da42eb

---
+++ examples/gdp/simple3.py
@@ -0,0 +1,36 @@
+# Example: modeling a complementarity condition as a 
+#   disjunction
+#
+# Specifying BigM suffix values for the gdp.bigm transformation
+
+from pyomo.core import *
+from pyomo.gdp import *
+
+model = ConcreteModel()
+
+# x >= 0 _|_ y>=0
+model.x = Var(bounds=(0,None))
+model.y = Var(bounds=(0,None))
+
+# Two conditions
+def _d(disjunct, flag):
+    model = disjunct.model()
+    if flag:
+        # x == 0
+        disjunct.c = Constraint(expr=model.x == 0)
+    else:
+        # y == 0
+        disjunct.c = Constraint(expr=model.y == 0)
+    disjunct.BigM = Suffix()
+    disjunct.BigM[disjunct.c] = 1
+model.d = Disjunct([0,1], rule=_d)
+
+# Define the disjunction
+def _c(model):
+    return [model.d[0], model.d[1]]
+model.c = Disjunction(rule=_c)
+
+model.C = Constraint(expr=model.x+model.y <= 1)
+
+model.o = Objective(expr=2*model.x+3*model.y, sense=maximize)
+
