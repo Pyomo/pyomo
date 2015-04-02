3f0ea138ad9988fffa6c1e465a3affd9b4f9051a
John Siirola <jsiirola@users.noreply.github.com>
2015-04-02 19:55:39 +0000

    Suppress the PIP cache when getting Pyomo
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@9967 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- scripts/get-pyomo.py
+++ scripts/get-pyomo.py
@@ -13,10 +13,30 @@
 import sys
 try:
     import pip
+    pip_version = pip.__version__.split('.')
+    for i,s in enumerate(pip_version):
+        try:
+            pip_version[i] = int(s)
+        except:
+            pass
+    pip_version = tuple(pip_version)
 except ImportError:
     print("You must have 'pip' installed to run this script.")
     raise SystemExit
 
 
 print("Installing Pyomo ...")
-pip.main(['install']+sys.argv[1:]+['Pyomo'])
+
+cmd = ['install',]
+# Disable the PIP download cache
+if pip_version[0] >= 6:
+    cmd.append('--no-cache-dir')
+else:
+    cmd.append('--download-cache')
+    cmd.append('')
+# Allow the user to provide extra options
+cmd.extend( sys.argv[1:] )
+# install Pyomo
+cmd.append('Pyomo')
+
+pip.main(cmd)
