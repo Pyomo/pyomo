9403da8c19e0772148ebc3008dd3fb20f6f0959a
William Hart <whart222@users.noreply.github.com>
2015-04-23 19:42:18 +0000

    Initial cleanup of mpec_lg solver.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10028 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/mpec/plugins/solver2.py
+++ pyomo/mpec/plugins/solver2.py
@@ -30,17 +30,13 @@ class MPEC_Solver2(pyomo.opt.OptSolver):
         # Transform instance
         #
         instance = self._instance.transform('mpec.simple_disjunction')
-        # TODO: Handle condition where the M value is not defined
-        if self.options.transform:
-            instance2 = instance.transform(self.options.transform)
-        else:
-            instance2 = instance.transform('gdp.bigm')
+        instance2 = instance.transform('gdp.bigm', default_bigM=self.options.get('bigM',10^6))
         #
         # Solve with a specified solver
         #
         solver = self.options.solver
         if not self.options.solver:
-            solver = 'glpk'
+            self.options.solver = solver = 'glpk'
         opt = pyomo.opt.SolverFactory(solver)
         self.results = opt.solve(instance2,
                                 tee=self.tee,
@@ -48,9 +44,8 @@ class MPEC_Solver2(pyomo.opt.OptSolver):
         #
         # Transform the result back into the original model
         #
+        self._instance.load(self.results)
         self.results = instance2.update_results(self.results)
-        # TODO: This doesn't work yet.
-
         #
         # Return the sub-solver return condition value and log
         #
@@ -64,58 +59,5 @@ class MPEC_Solver2(pyomo.opt.OptSolver):
         #
         # Return the results
         #
-        # TODO: initialize the solver results
-        #
         return self.results
-
-    def X_postsolve(self):
-        results = pyomo.opt.SolverResults()
-        solv = results.solver
-        solv.name = self.options.subsolver
-        #solv.status = self._glpk_get_solver_status()
-        #solv.memory_used = "%d bytes, (%d KiB)" % (peak_mem, peak_mem/1024)
-        solv.wallclock_time = self._ans.elapsed['solver_time']
-        solv.cpu_time = self._ans.elapsed['solver_cputime']
-        solv.termination_condition = pyomo.opt.TerminationCondition.maxIterations
-        prob = results.problem
-        prob.name = self._instance.name
-        prob.number_of_constraints = self._instance.statistics.number_of_constraints
-        prob.number_of_variables = self._instance.statistics.number_of_variables
-        prob.number_of_binary_variables = self._instance.statistics.number_of_binary_variables
-        prob.number_of_integer_variables = self._instance.statistics.number_of_integer_variables
-        prob.number_of_continuous_variables = self._instance.statistics.number_of_continuous_variables
-        prob.number_of_objectives = self._instance.statistics.number_of_objectives
-
-        from pyomo.core import maximize
-        if self.problem.sense == maximize:
-            prob.sense = pyomo.opt.ProblemSense.maximize
-        else:
-            prob.sense = pyomo.opt.ProblemSense.minimize
-
-        sstatus = pyomo.opt.SolutionStatus.unknown
-
-        if not sstatus in ( pyomo.opt.SolutionStatus.error, ):
-            soln = pyomo.opt.Solution()
-            soln.status = sstatus
-
-            if type(self._ans.ff) in (list, tuple):
-                oval = float(self._ans.ff[0])
-            else:
-                oval = float(self._ans.ff)
-            if self.problem.sense == maximize:
-                soln.objective[ self.problem._f_name[0] ].value = - oval
-            else:
-                soln.objective[ self.problem._f_name[0] ].value = oval
-
-            id = 0
-            for var_label in self._ans.xf.keys():
-                if self._ans.xf[var_label].is_integer():
-                    soln.variable[ var_label.name ] = {'Value': int(self._ans.xf[var_label]), 'Id':id}
-                else:
-                    soln.variable[ var_label.name ] = {'Value': float(self._ans.xf[var_label]), 'Id':id}
-                id += 1
-
-            results.solution.insert( soln )
-
-        return results
 
