8242f9c8d6139c70511986c17237295a1852b9c8
William Hart <whart222@users.noreply.github.com>
2015-06-27 19:21:15 +0000

    Testing skip logic.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10321 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/core/tests/unit/test_sos.py
+++ pyomo/core/tests/unit/test_sos.py
@@ -177,6 +177,19 @@ class TestExamples(unittest.TestCase):
         self.assertEqual(set((v.cname(True),w) for v,w in M.c[0].get_items()), set((M.x[i].cname(True), i) for i in I[0]))
         self.assertEqual(set((v.cname(True),w) for v,w in M.c[1].get_items()), set((M.x[i].cname(True), i-1) for i in I[1]))
 
+    def test14(self):
+        def rule(model, i):
+            if i == 0:
+                return SOSConstraint.Skip
+            else:
+                return list(M.x[i] for i in M.x), [1, 20, 3]
+        w = {0:{1:10, 2:2, 3:30}, 1:{1:1, 2:20, 3:3}}
+        M = ConcreteModel()
+        M.x = Var([1,2,3])
+        M.c = SOSConstraint([0,1], rule=rule, sos=1)
+        self.assertEqual(list(M.c.keys()), [1])
+        self.assertEqual(set((v.cname(True),w) for v,w in M.c[1].get_items()), set((M.x[i].cname(True), w[1][i]) for i in [1,2,3]))
+
 
 if __name__ == "__main__":
     unittest.main()
