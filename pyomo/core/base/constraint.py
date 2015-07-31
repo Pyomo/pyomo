cdada74bd6d583100b1be278b0e54b6b275b5ee0
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2015-07-31 06:48:51 +0000

    backing out r10551 in preparation for next round of commits (easier to merge)
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10552 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/core/base/constraint.py
+++ pyomo/core/base/constraint.py
@@ -145,7 +145,7 @@ class _ConstraintData(ActiveComponentData):
                             equality constraint
     """
 
-    __slots__ = ('body', 'lower', 'upper', '_equality')
+    __slots__ = ('_body', '_lower', '_upper', '_equality')
 
     def __init__(self, owner):
         # the following lines represent in-lining of the ActiveComponentData
@@ -692,21 +692,22 @@ always "lb <= expr <= ub"."""
                                        ] )
 
 
-class SimpleConstraint(_ConstraintData, Constraint):
+class SimpleConstraint(Constraint, _ConstraintData):
     """
     SimpleConstraint is the implementation representing a single,
     non-indexed constraint.
     """
 
     def __init__(self, *args, **kwds):
-        _ConstraintData.__init__(self, self)
+        _ConstraintData.__init__(self, self, None)
         Constraint.__init__(self, *args, **kwds)
 
     def __call__(self, exception=True):
         """Compute the value of the constraint body"""
 
         if self._constructed:
-            return _ConstraintData.__call__(self, exception=exception)
+            return _ConstraintData.__call__(self,
+                                            exception=exception)
         if exception:
             raise ValueError(
                 "Evaluating the numeric value of constraint '%s' "
