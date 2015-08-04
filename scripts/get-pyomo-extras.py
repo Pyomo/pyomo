3dca994a542f72656178c89e1640b03ef4e127e5
William Hart <whart222@users.noreply.github.com>
2015-08-04 19:29:32 +0000

    Updating script to install conditional dependencies.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10607 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- scripts/get-pyomo-extras.py
+++ scripts/get-pyomo-extras.py
@@ -7,16 +7,65 @@
 #  This software is distributed under the BSD License.
 #  _________________________________________________________________________
 #
-# A script to install pyomo.extras
+# A script to optionally install packages that Pyomo could leverage.
 #
 
+packages = {'xlrd':None, 'openpyxl':None, 'suds-jurko':'suds', 'PyYAML':'yaml', 'pypyodbc':None, 'pymysql':None, 'openopt':None, 'FuncDesigner':None, 'DerApproximator':None, 'sympy':None}
+
+#
+# Verify that pip is installed
+#
 import sys
 try:
     import pip
+    pip_version = pip.__version__.split('.')
+    for i,s in enumerate(pip_version):
+        try:
+            pip_version[i] = int(s)
+        except:
+            pass
+    pip_version = tuple(pip_version)
 except ImportError:
     print("You must have 'pip' installed to run this script.")
     raise SystemExit
 
+cmd = ['install','--upgrade']
+# Disable the PIP download cache
+if pip_version[0] >= 6:
+    cmd.append('--no-cache-dir')
+else:
+    cmd.append('--download-cache')
+    cmd.append('')
 
-print("Installing pyomo.extras ...")
-pip.main(['install']+sys.argv[1:]+['pyomo.extras'])
+print(' ')
+print('-'*60)
+print("Installation Output Logs")
+print("  (A summary will be printed below)")
+print('-'*60)
+print(' ')
+
+results = {}
+for package in packages:
+    try:
+        # Allow the user to provide extra options
+        pip.main(cmd + sys.argv[1:] + [package])
+        if packages[package]:
+            __import__(packages[package])
+        else:
+            __import__(package)
+        results[package] = True
+    except:
+        results[package] = False
+    pip.logger.consumers = []
+
+print(' ')
+print(' ')
+print('-'*60)
+print("Installation Summary")
+print('-'*60)
+print(' ')
+for package in sorted(packages):
+    if results[package]:
+        print("YES %s" % package)
+    else:
+        print("NO  %s" % package)
