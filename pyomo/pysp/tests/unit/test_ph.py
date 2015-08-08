5dc85def3e74afd6722c072d06f3f1ab55f21140
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2015-08-08 02:53:56 +0000

    adding test categories pysp unit test class
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10627 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/pysp/tests/unit/test_ph.py
+++ pyomo/pysp/tests/unit/test_ph.py
@@ -1901,8 +1901,8 @@ class TestPHParallel(unittest.TestCase):
         self.assertFileEqualsBaseline(this_test_file_directory+"networkflow1ef10_linearized_cplex_with_bundles_with_phpyro.out",this_test_file_directory+"networkflow1ef10_linearized_cplex_with_bundles_with_phpyro.baseline", filter=filter_pyro)
         time.sleep(5) # wait for the nameserver and related processes to clean up
 
+TestInstanceFactory = unittest.category('smoke','nightly','expensive')(TestInstanceFactory)
 TestPH = unittest.category('nightly','performance')(TestPH)
-
 TestPHParallel = unittest.category('parallel','performance')(TestPHParallel)
 
 if __name__ == "__main__":
