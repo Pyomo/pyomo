3f115516f4f7bf327124cf63da4ee50c4a5f348e
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2015-08-25 21:06:58 +0000

    forgot to unfix variables after xhat solves
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10682 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/pysp/ph.py
+++ pyomo/pysp/ph.py
@@ -1644,6 +1644,11 @@ class ProgressiveHedging(_PHBase):
 
         else:
 
+            # unfix
+            assert ph_bound_base._stack[-1][0] == \
+                ph_bound_base.TREE_VARIABLE_FIXING
+            ph_bound_base.RestoreLastPHChange(self)
+
             if isinstance(self._solver_manager,
                           pyomo.solvers.plugins.smanager.phpyro.SolverManager_PHPyro):
                 phsolverserverutils.collect_full_results(
