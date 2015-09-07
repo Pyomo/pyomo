74bafb5868cd486726ec336d70b1acf674ee8fbd
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2015-09-07 22:46:10 +0000

    Adding documentation for the PySPConfigureObject methods
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10706 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/pysp/util/configured_object.py
+++ pyomo/pysp/util/configured_object.py
@@ -18,10 +18,15 @@ class PySPConfiguredObject(object):
         self.set_options(options)
 
     def set_options(self, options):
+        """Assign an options block to this class instance after
+        validating that all registered options for this class exist on
+        the block."""
         self.validate_options(options)
         self._options = options
 
     def display_options(self, ostream=sys.stdout):
+        """Display the options block currently assigned to this class
+        instance."""
         max_len = max(len(name) for name in self._options)
         line_template = " %s %"+str(max_len)+"s: %s\n"
         for name in self._options:
@@ -33,14 +38,26 @@ class PySPConfiguredObject(object):
                           % (flag, name,configval.value(accessValue=False)))
 
     @classmethod
-    def register_options(cls, options=None, include_bases=True):
-        if options is None:
+    def register_options(cls, *args):
+        """Cls.register_options([options]) -> options.
+        Fills an options block will all registered options for this
+        class. The optional argument 'options' can be a previously
+        existing options block, which would be both updated and
+        returned by this function."""
+
+        if len(args) == 0:
             options = ConfigBlock()
-        if include_bases:
-            bases = inspect.getmro(cls)
-            assert bases[-1] is object
         else:
-            bases = (cls,)
+            if len(args) != 1:
+                raise TypeError("register_options(...) takes at most 1 argument (%s given)"
+                                % (len(args)))
+            options = args[0]
+            if not isinstance(options, ConfigBlock):
+                raise TypeError("register_options(...) argument must be of type ConfigBlock, "
+                                "not %s" % (type(options).__name__))
+
+        bases = inspect.getmro(cls)
+        assert bases[-1] is object
         for cls in reversed(bases):
             if issubclass(cls, PySPConfiguredObject):
                 for name in cls._registered_options:
@@ -52,13 +69,15 @@ class PySPConfiguredObject(object):
         return options
 
     @classmethod
-    def extract_options(cls,
-                        options,
-                        error_if_missing=True,
-                        include_bases=True):
+    def extract_options(cls, options, error_if_missing=True):
+        """Copy the set of registered options for this class from an
+        existing options block and return a new options block with
+        only those values. This method will preserve the _userSet
+        status of all options. The optional flag 'error_if_missing'
+        controls whether or not an exception is raised when registered
+        options are missing."""
 
-        these_options = ConfigBlock()
-        cls.register_options(these_options, include_bases=include_bases)
+        these_options = cls.register_options()
         for name in these_options.keys():
             configval = None
             try:
@@ -84,29 +103,36 @@ class PySPConfiguredObject(object):
     def extract_user_options_to_dict(cls,
                                      options,
                                      error_if_missing=True,
-                                     include_bases=True):
+                                     sparse=False):
+        """Copy the set of registered options for this class from an
+        existing options block and return a dictionary of options
+        (name -> value) with those values. This method will preserve
+        the _userSet status of all options. The optional flag
+        'error_if_missing' controls whether or not an exception is
+        raised when registered options are missing. The optional flag
+        'sparse' controls whether non user-set values should be
+        included in the returned dictionary."""
+
         these_options = \
             cls.extract_options(options,
-                                error_if_missing=error_if_missing,
-                                include_bases=include_bases)
+                                error_if_missing=error_if_missing)
         options_dict = {}
         for name in these_options:
             configval = these_options.get(name)
-            if configval._userSet:
+            if (not sparse) or configval._userSet:
                 options_dict[name] = configval.value()
         return options_dict
 
     @classmethod
-    def validate_options(cls,
-                         options,
-                         error_if_missing=True,
-                         include_bases=True):
+    def validate_options(cls, options, error_if_missing=True):
+        """Validate that all registered can be found in the options
+        block and that their option definitions are the same. The
+        options flag 'error_if_missing' can be used to control whether
+        or not an exception is raised when registered options are
+        missing."""
 
-        if include_bases:
-            bases = inspect.getmro(cls)
-            assert bases[-1] is object
-        else:
-            bases = (cls,)
+        bases = inspect.getmro(cls)
+        assert bases[-1] is object
         for cls in reversed(bases):
             if issubclass(cls, PySPConfiguredObject):
                 for name in cls._registered_options:
