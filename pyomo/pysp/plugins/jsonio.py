5f1f5991d88790009f84f48a75c42031b6696fde
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2015-09-24 23:47:31 +0000

    Splitting the jsonio plugin into separate plugins for each interface. Allows for cleaner option registration when only one interface is requested
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10802 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/pysp/plugins/jsonio.py
+++ pyomo/pysp/plugins/jsonio.py
@@ -7,7 +7,7 @@
 #  This software is distributed under the BSD License.
 #  _________________________________________________________________________
 
-__all__ = ('JSONSolutionIOExtension',)
+__all__ = ()
 
 import itertools
 import json
@@ -15,21 +15,12 @@ import json
 from pyomo.pysp.solutionioextensions import \
     (IPySPSolutionSaverExtension,
      IPySPSolutionLoaderExtension)
+from pyomo.pysp.phutils import indexToString
 from pyomo.util.plugin import implements, SingletonPlugin
 from pyomo.pysp.util.config import (PySPConfigBlock,
                                     safe_register_common_option)
 from pyomo.pysp.util.configured_object import (PySPConfiguredObject,
                                                PySPConfiguredExtension)
-
-
-def extract_node_solution(tree_node):
-    solution = {}
-    for variable_id in tree_node._standard_variable_ids:
-        varname, index = tree_node._variable_ids[variable_id]
-        if varname not in solution:
-            solution[varname] = {}
-        solution[varname][index] = tree_node._solution[variable_id]
-    return solution
 
 def load_node_solution(tree_node, solution):
     for varname in solution:
@@ -38,27 +29,22 @@ def load_node_solution(tree_node, solution):
             variable_id = tree_node._name_index_to_id[(varname, index)]
             tree_node._solution[variable_id] = varsol[index]
 
-class JSONSolutionIOExtension(PySPConfiguredExtension,
-                              PySPConfiguredObject,
-                              SingletonPlugin):
+class JSONSolutionLoaderExtension(PySPConfiguredExtension,
+                                  PySPConfiguredObject,
+                                  SingletonPlugin):
 
-    implements(IPySPSolutionSaverExtension)
     implements(IPySPSolutionLoaderExtension)
 
     _registered_options = \
         PySPConfigBlock("Options registered for the "
-                        "JSONSolutionIOExtension class")
+                        "JSONSolutionLoaderExtension class")
 
-    safe_register_common_option(_registered_options,
-                                "output_name")
     safe_register_common_option(_registered_options,
                                 "input_name")
     safe_register_common_option(_registered_options,
-                                "store_stages")
-    safe_register_common_option(_registered_options,
                                 "load_stages")
 
-    _default_prefix = "jsonio_"
+    _default_prefix = "jsonloader_"
 
     #
     # Note: Do not try to user super() or access the
@@ -78,52 +64,126 @@ class JSONSolutionIOExtension(PySPConfiguredExtension,
             stage_solutions = None
             with open(self.get_option("input_name"), 'rb') as f:
                 stage_solutions = json.load(f)
-            for cnt, (stage, stage_solution) in \
-                enumerate(itertools.izip_longest(manager.scenario_tree.stages,
-                                                 stage_solutions), 1):
+            cntr = 0
+            if self.get_option('load_stages') > len(manager.scenario_tree.stages):
+                raise ValueError("The value of the %s option (%s) can not be greater than "
+                                 "the number of time stages in the local scenario tree (%s)"
+                                 % (self.get_full_option_name('load_stages'),
+                                    self.get_option('load_stages'),
+                                    len(manager.scenario_tree.stages)))
+            if self.get_option('load_stages') > len(stage_solutions):
+                raise ValueError("The value of the %s option (%s) can not be greater than "
+                                 "the number of time stages in the scenario tree solution "
+                                 "stored in %s (%s)"
+                                 % (self.get_full_option_name('load_stages'),
+                                    self.get_option('load_stages'),
+                                    self.get_option('input_name'),
+                                    len(stage_solutions)))
+            for stage, stage_solution in \
+                   itertools.izip_longest(manager.scenario_tree.stages,
+                                          stage_solutions):
                 if stage_solution is None:
                     break
-                if (self.get_option('store_stages') <= 0) or \
-                   (cnt <= self.get_option('store_stages')):
+                if (self.get_option('load_stages') <= 0) or \
+                   (cntr+1 <= self.get_option('load_stages')):
                     if stage is None:
                         raise RuntimeError(
-                            "Local scenario tree has fewer stages than what is "
+                            "Local scenario tree has fewer stages (%s) than what is "
                             "held by the solution loaded from file %s. Use the "
-                            "%s option 'load_stages' to limit the number of "
-                            "stages that are loaded.")
+                            "option %s to limit the number of stages that "
+                            "are loaded." % (cntr,
+                                             self.get_option('input_name'),
+                                             self.get_full_option_name('load_stages')))
+                    cntr += 1
                     for tree_node in stage.nodes:
                         try:
-                            node_solution = stage_solution[tree_node._name]
+                            node_solution = stage_solution[tree_node.name]
                         except KeyError:
                             raise KeyError("Local scenario tree contains a tree node "
                                            "that was not found in the solution at time"
-                                           "-stage %s: %s" % (cnt, tree_node._name))
+                                           "-stage %s: %s" % (cntr, tree_node.name))
                         load_node_solution(tree_node, node_solution)
                 else:
                     break
-                print("Loaded scenario tree solution for %s time stages" % (cnt))
+            print("Loaded scenario tree solution for %s time stages "
+                  "from file %s" % (cntr, self.get_option('input_name')))
             return True
 
         print("No value was set for %s option 'input_name'. "
               "Nothing will be saved." % (type(self).__name__))
         return False
 
+def extract_node_solution(tree_node):
+    solution = {}
+    for variable_id in tree_node._standard_variable_ids:
+        varname, index = tree_node._variable_ids[variable_id]
+        if varname not in solution:
+            solution[varname] = {}
+        if variable_id in tree_node._solution:
+            solution[varname][index] = tree_node._solution[variable_id]
+        else:
+            name, index = tree_node._variable_ids[variable_id]
+            full_name = name+indexToString(index)
+            print("%s: node solution missing for variable with scenario tree "
+                  "id %s (%s)"
+                  % (tree_node.name, variable_id, full_name))
+            return None
+    return solution
+
+class JSONSolutionSaverExtension(PySPConfiguredExtension,
+                                 PySPConfiguredObject,
+                                 SingletonPlugin):
+
+    implements(IPySPSolutionSaverExtension)
+
+    _registered_options = \
+        PySPConfigBlock("Options registered for the "
+                        "JSONSolutionSaverExtension class")
+
+    safe_register_common_option(_registered_options,
+                                "output_name")
+    safe_register_common_option(_registered_options,
+                                "store_stages")
+
+    _default_prefix = "jsonsaver_"
+
+    #
+    # Note: Do not try to user super() or access the
+    #       class name inside the __init__ method when
+    #       a class derives from a SingletonPlugin. Due to
+    #       how Pyutilib implements its Singleton type,
+    #       the __class__ cell will be empty.
+    #       (See: https://stackoverflow.com/questions/
+    #             13126727/how-is-super-in-python-3-implemented)
+    #
+    def __init__(self):
+        PySPConfiguredExtension.__init__(self)
+
     def save(self, manager):
 
         if self.get_option("output_name") is not None:
             stage_solutions = []
-            for cnt, stage in enumerate(manager.scenario_tree.stages, 1):
-                if (self.get_option('load_stages') <= 0) or \
-                   (cnt <= self.get_option('load_stages')):
-                    node_solutions = {}
-                    for tree_node in stage.nodes:
-                        node_solutions[tree_node.name] = \
-                            extract_node_solution(tree_node)
-                    stage_solutions.append(node_solutions)
-                else:
-                    break
             with open(self.get_option('output_name'), 'wb') as f:
+                cntr = 0
+                for stage in manager.scenario_tree.stages:
+                    if (self.get_option('store_stages') <= 0) or \
+                       (cntr+1 <= self.get_option('store_stages')):
+                        cntr += 1
+                        node_solutions = {}
+                        for tree_node in stage.nodes:
+                            _node_solution = extract_node_solution(tree_node)
+                            if _node_solution is None:
+                                print("No solution appears to be stored in node with "
+                                      "name %s. No solution will be saved."
+                                      % (tree_node.name))
+                                return False
+                            node_solutions[tree_node.name] = _node_solution
+                        stage_solutions.append(node_solutions)
+                    else:
+                        break
                 json.dump(stage_solutions, f, indent=2)
+            print("Saved scenario tree solution for %s time stages "
+                  "to file %s" % (cntr, self.get_option('output_name')))
             return True
 
         print("No value was set for %s option 'output_name'. "
