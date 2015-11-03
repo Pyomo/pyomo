42a52291b6319f91bd9fd4f4218468060abc02f3
John Siirola <jsiirola@users.noreply.github.com>
2015-11-03 20:59:22 +0000

    Adding UnknownSolver to the main list of components that are exported by pyomo.environ.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10876 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/environ/__init__.py
+++ pyomo/environ/__init__.py
@@ -13,9 +13,10 @@
 import pyomo.core
 __all__ = [d for d in dir(pyomo.core) if not d.startswith('_')]
 from pyomo.core import *
+__all__.append('UnknownSolver')
 __all__.append('SolverFactory')
 __all__.append('SolverManagerFactory')
-from pyomo.opt import SolverFactory, SolverManagerFactory
+from pyomo.opt import SolverFactory, SolverManagerFactory, UnknownSolver
 
 
 import sys
