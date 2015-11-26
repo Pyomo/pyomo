5674011e0f809898be24a874a07e431828de99da
William Hart <whart222@users.noreply.github.com>
2015-11-26 00:12:35 +0000

    Revising the get_pyomo_extras logic so it can be called as a script.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@10907 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- scripts/get_pyomo_extras.py
+++ scripts/get_pyomo_extras.py
@@ -12,60 +12,68 @@
 
 packages = {'xlrd':None, 'openpyxl':None, 'suds-jurko':'suds', 'PyYAML':'yaml', 'pypyodbc':None, 'pymysql':None, 'openopt':None, 'FuncDesigner':None, 'DerApproximator':None, 'sympy':None, 'ipython[notebook]':'IPython'}
 
-#
-# Verify that pip is installed
-#
-import sys
-try:
-    import pip
-    pip_version = pip.__version__.split('.')
-    for i,s in enumerate(pip_version):
-        try:
-            pip_version[i] = int(s)
-        except:
-            pass
-    pip_version = tuple(pip_version)
-except ImportError:
-    print("You must have 'pip' installed to run this script.")
-    raise SystemExit
-
-cmd = ['install','--upgrade']
-# Disable the PIP download cache
-if pip_version[0] >= 6:
-    cmd.append('--no-cache-dir')
-else:
-    cmd.append('--download-cache')
-    cmd.append('')
-
-print(' ')
-print('-'*60)
-print("Installation Output Logs")
-print("  (A summary will be printed below)")
-print('-'*60)
-print(' ')
-
-results = {}
-for package in packages:
+def main():
+    #
+    # Verify that pip is installed
+    #
+    import sys
     try:
-        # Allow the user to provide extra options
-        pip.main(cmd + sys.argv[1:] + [package])
-        if packages[package]:
-            __import__(packages[package])
-        else:
-            __import__(package)
-        results[package] = True
-    except:
-        results[package] = False
-    pip.logger.consumers = []
+        import pip
+        pip_version = pip.__version__.split('.')
+        for i,s in enumerate(pip_version):
+            try:
+                pip_version[i] = int(s)
+            except:
+                pass
+        pip_version = tuple(pip_version)
+    except ImportError:
+        print("You must have 'pip' installed to run this script.")
+        raise SystemExit
 
-print(' ')
-print(' ')
-print('-'*60)
-print("Installation Summary")
-print('-'*60)
-print(' ')
-for package in sorted(packages):
-    if results[package]:
-        print("YES %s" % package)
+    cmd = ['install','--upgrade']
+    # Disable the PIP download cache
+    if pip_version[0] >= 6:
+        cmd.append('--no-cache-dir')
     else:
-        print("NO  %s" % package)
+        cmd.append('--download-cache')
+        cmd.append('')
+
+    print(' ')
+    print('-'*60)
+    print("Installation Output Logs")
+    print("  (A summary will be printed below)")
+    print('-'*60)
+    print(' ')
+
+    results = {}
+    for package in packages:
+        try:
+            # Allow the user to provide extra options
+            pip.main(cmd + sys.argv[1:] + [package])
+            if packages[package]:
+                __import__(packages[package])
+            else:
+                __import__(package)
+            results[package] = True
+        except:
+            results[package] = False
+        pip.logger.consumers = []
+
+    print(' ')
+    print(' ')
+    print('-'*60)
+    print("Installation Summary")
+    print('-'*60)
+    print(' ')
+    for package in sorted(packages):
+        if results[package]:
+            print("YES %s" % package)
+        else:
+            print("NO  %s" % package)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        print("Error running get-pyomo-extras.py")
