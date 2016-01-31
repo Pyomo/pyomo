8d32efcfd64a8ade7097b885d1d3b9a187d5a604
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2016-01-31 10:13:40 +0000

    tracking changes to var: relax integrality transform can be simplified
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11068 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/core/plugins/transform/relax_integrality.py
+++ pyomo/core/plugins/transform/relax_integrality.py
@@ -31,45 +31,10 @@ class RelaxIntegrality(NonIsomorphicTransformation):
         # Iterate over all variables, replacing the domain with a real-valued domain
         # and setting appropriate bounds.
         #
-        # Note: this technique is not extensible.  If a user creates a new integer variable
-        # class then we'd need to add it here (unless it was a subset of one of the sets we
-        # detect here.  Alternatively, we could make the variables responsible for managing
-        # the generation of a relaxed variable.  But that would pollute the variable objects
-        # with logic about generating a specific relaxation.  That's probably a worse alternative.
-        #
-        comp = model.component_map(Var)
-        for var in comp.values():
-            if isinstance(var.domain, BooleanSet):
-                var.domain=Reals
-                dbnd = ( 0.0, 1.0 )
-            elif isinstance(var.domain, IntegerSet):
-                dbnd = var.domain.bounds()
-                if dbnd is None:
-                    bnd = ( None, None )
-                var.domain=Reals
-            elif isinstance(var.domain, pyomo.core.base.RangeSet):
-                dbnd = var.domain.bounds()
-                if dbnd is None:
-                    bnd = ( None, None )
-                var.domain=Reals
-            else:
-                continue
-
-            bnd = var.domain.bounds()
-            if bnd is None:
-                bnd = ( None, None )
-            bnd = ( self._tightenBound(bnd[0], dbnd[0], max),
-                    self._tightenBound(bnd[1], dbnd[1], min) )
-
-            if bnd == (None, None):
-                var.domain = Reals
-            else:
-                var.domain = RealInterval(bounds=bnd)
-            var._initialize_members(var._index)
-
-    def _tightenBound(self, a, b, comp):
-        if a is None:
-            return b
-        if b is None:
-            return a
-        return comp(a,b)
+        for var in model.component_data_objects(Var):
+            # var.bounds returns the tightest of the domain
+            # vs user-supplied lower and upper bounds
+            lb, ub = var.bounds
+            var.domain = Reals
+            var.setlb(lb)
+            var.setub(ub)
