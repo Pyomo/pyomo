bac04fa0cbad46d13e1b09aacbeca7c30c91ce2b
William Hart <whart222@users.noreply.github.com>
2016-02-14 21:33:20 +0000

    Execute 'pyomo install-extras' as a shell commend.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11142 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- admin/jenkins.py
+++ admin/jenkins.py
@@ -76,7 +76,7 @@ elif config == "core":
     import hudson.driver
     # Install
     hudson.driver.perform_install('pyomo', config='pyomo_all.ini')
-    subprocess.call(["python/bin/pyomo", "install-extras"])
+    subprocess.call(["python/bin/pyomo", "install-extras"], shell=True)
     # Test
     os.environ['TEST_PACKAGES'] = 'checker core environ opt repn scripting solvers util version'
     pyutilib=os.sep.join([os.environ['WORKSPACE'], 'src', 'pyutilib.*'])+',pyutilib.*'
