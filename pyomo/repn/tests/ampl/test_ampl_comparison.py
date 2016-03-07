f41031dbfd1072b13d855afee019ba07058ec584
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2016-03-07 09:02:28 +0000

    Activate the test case that uses the Expr_if component. Not sure why this test was not active in the first place
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11194 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/repn/tests/ampl/test_ampl_comparison.py
+++ pyomo/repn/tests/ampl/test_ampl_comparison.py
@@ -24,22 +24,23 @@ import pyomo.scripting.pyomo_main as main
 has_asl_test = False
 if os.system('asl_test -v') == 0:
     has_asl_test = True
-    
+
 class Tests(unittest.TestCase):
 
     def pyomo(self, cmd):
         os.chdir(currdir)
         output = main.main(['convert', '--logging=quiet', '-c']+cmd)
-        return output 
+        return output
 
 class BaselineTests(Tests):
     def __init__(self, *args, **kwds):
         Tests.__init__(self, *args, **kwds)
 BaselineTests = unittest.category('smoke', 'nightly','expensive')(BaselineTests)
-"""
-The following test generates an nl file for the test case
-and checks that it matches the current pyomo baseline nl file
-"""
+
+#
+#The following test generates an nl file for the test case
+#and checks that it matches the current pyomo baseline nl file
+#
 @unittest.nottest
 def nlwriter_baseline_test(self, name):
     if os.path.exists(currdir+name+'.dat'):
@@ -50,7 +51,7 @@ def nlwriter_baseline_test(self, name):
         self.pyomo(['--output='+currdir+name+'.test.nl',
                     currdir+name+'_testCase.py'])
 
-    # Check that the pyomo nl file matches its own baseline    
+    # Check that the pyomo nl file matches its own baseline
     self.assertFileEqualsBaseline(currdir+name+'.test.nl', currdir+name+'.pyomo.nl', tolerance=1e-7)
 
 
@@ -59,17 +60,18 @@ class ASLTests(Tests):
     def __init__(self, *args, **kwds):
         Tests.__init__(self, *args, **kwds)
 ASLTests = unittest.category('smoke','nightly','expensive')(ASLTests)
-"""
-The following test calls the asl_test executable to generate JSON
-files corresponding to both the AMPL-generated nl file and the 
-Pyomo-generated nl file. The JSON files are then diffed using
-the pyutilib.th test class method assertMatchesJsonBaseline()
-"""
+
+#
+# The following test calls the asl_test executable to generate JSON
+# files corresponding to both the AMPL-generated nl file and the
+# Pyomo-generated nl file. The JSON files are then diffed using
+# the pyutilib.th test class method assertMatchesJsonBaseline()
+#
 @unittest.nottest
 def nlwriter_asl_test(self, name):
     if has_asl_test is False:
         self.skipTest('asl_test executable not available')
-        return        
+        return
     if os.path.exists(currdir+name+'.dat'):
         self.pyomo(['--output='+currdir+name+'.test.nl',
                     '--file-determinism=3',
@@ -101,18 +103,17 @@ def nlwriter_asl_test(self, name):
     self.assertTrue(p[0] == 0, msg=p[1])
     self.assertMatchesJsonBaseline(currdir+'stub.test.json', currdir+'stub.json', tolerance=1e-8)
     os.remove(currdir+'stub.json')
-    
+
     # delete temporary test files
     os.remove(currdir+name+'.test.col')
     os.remove(currdir+name+'.test.row')
     os.remove(currdir+name+'.test.nl')
 
-
+# add test methods to classes
 for f in glob.glob(currdir+'*_testCase.py'):
     name = re.split('[._]',os.path.basename(f))[0]
-    if name != 'small12':
-        BaselineTests.add_fn_test(fn=nlwriter_baseline_test, name=name)
-        ASLTests.add_fn_test(fn=nlwriter_asl_test, name=name)
+    BaselineTests.add_fn_test(fn=nlwriter_baseline_test, name=name)
+    ASLTests.add_fn_test(fn=nlwriter_asl_test, name=name)
 
 if __name__ == "__main__":
     unittest.main()
