5d27738f4bb5346b98a3d8ac2433796ff858396f
William Hart <whart222@users.noreply.github.com>
2016-03-12 03:30:16 +0000

    Updating model.  Added script to generate a figure.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11209 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/pyomo/concrete/sodacan.py
+++ examples/pyomo/concrete/sodacan.py
@@ -6,7 +6,7 @@ M = ConcreteModel()
 M.r = Var(bounds=(0,None))
 M.h = Var(bounds=(0,None))
 M.o = Objective(expr=\
-        2*pi*M.r**2 + 2*pi*M.r*M.h)
+        2*pi*M.r*(M.r + M.h))
 M.c = Constraint(expr=\
         pi*M.h*M.r**2 == 355)
 
---
+++ examples/pyomo/concrete/sodacan_fig.py
@@ -0,0 +1,31 @@
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import numpy as np
+from math import pi, sqrt
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+R_ = np.arange(0.25, 10, 0.25)
+H_ = np.arange(0.25, 10, 0.25)
+R, H = np.meshgrid(R_, H_)
+Z = 2*pi*R*(R+H)
+surf = ax.plot_surface(R, H, Z, rstride=1, cstride=1, cmap=cm.hot,
+                       linewidth=0, antialiased=False)
+ax.set_xlabel("r")
+ax.set_ylabel("h")
+ax.set_zlim(0, 1200)
+
+ax.zaxis.set_major_locator(LinearLocator(10))
+#ax.zaxis.set_major_formatter(FormatStrFormatter(' %.02f'))
+
+#fig.colorbar(surf, shrink=0.5, aspect=5)
+
+H_ = 355/(pi*R_*R_)
+valid = np.where(H_ < 10.1)
+Z_ = R_+H_
+Z_ = 2*pi*R_*Z_
+ax.plot(R_[valid], H_[valid], Z_[valid], label='parametric curve')
+
+plt.show()
