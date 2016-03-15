adfaad065fcd545dd9622dba9e8bdfe8416a5153
Gabriel Hackebeil <ghackebeil@users.noreply.github.com>
2016-03-15 05:14:20 +0000

    cleaning up csvsolutionwriter:  - make sure it works with parallel code (don't try to access instance objects)  - Removing duplicate output of stage cost information. Leaving stage costs    out of the first file because it assumes convergence. Just output individual    scenario stage costs in the second file (that is no longer super secret)  - Renaming CostVarDetail.csv to prefix+"_StageCostDetail.csv" so that    the one created during --solve-ef does not overwrite the one created    by the PH solve (also removing Var from the name).
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11226 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- pyomo/pysp/plugins/csvsolutionwriter.py
+++ pyomo/pysp/plugins/csvsolutionwriter.py
@@ -13,9 +13,11 @@ from pyomo.pysp.scenariotree.tree_structure import \
     ScenarioTree
 
 #
-# a simple utility to munge the index name into something a bit more csv-friendly and
-# in general more readable. at the current time, we just eliminate any leading and trailing
-# parentheses and change commas to colons - the latter because it's a csv file!
+# a simple utility to munge the index name into something a
+# bit more csv-friendly and in general more readable. at the
+# current time, we just eliminate any leading and trailing
+# parentheses and change commas to colons - the latter
+# because it's a csv file!
 #
 
 def index_to_string(index):
@@ -29,43 +31,58 @@ def index_to_string(index):
 
 class CSVSolutionWriter(pyomo.util.plugin.SingletonPlugin):
 
-    pyomo.util.plugin.implements(solutionwriter.ISolutionWriterExtension)
+    pyomo.util.plugin.implements(
+        solutionwriter.ISolutionWriterExtension)
 
     def write(self, scenario_tree, output_file_prefix):
 
         if not isinstance(scenario_tree, ScenarioTree):
-            raise RuntimeError("CSVSolutionWriter write method expects ScenarioTree object - type of supplied object="+str(type(scenario_tree)))
+            raise RuntimeError(
+                "CSVSolutionWriter write method expects "
+                "ScenarioTree object - type of supplied "
+                "object="+str(type(scenario_tree)))
 
-        output_filename = output_file_prefix + ".csv"
-        output_file = open(output_filename,"w")
+        solution_filename = output_file_prefix + ".csv"
+        with open(solution_filename, "w") as f:
+            for stage in scenario_tree.stages:
+                for tree_node in sorted(stage.nodes,
+                                        key=lambda x: x.name):
+                    for variable_id in sorted(tree_node._variable_ids):
+                        var_name, index = \
+                            tree_node._variable_ids[variable_id]
+                        f.write("%s, %s, %s, %s, %s\n"
+                                % (stage.name,
+                                   tree_node.name,
+                                   var_name,
+                                   index_to_string(index),
+                                   tree_node._solution[variable_id]))
 
-        for stage in scenario_tree._stages:
-            stage_name = stage._name
-            for tree_node in stage._tree_nodes:
-                tree_node_name = tree_node._name
-                for variable_id in sorted(tree_node._variable_ids):
-                    var_name, index = tree_node._variable_ids[variable_id]
-                    output_file.write(str(stage_name)+" , "+str(tree_node_name)+" , "+str(var_name)+" , "+str(index_to_string(index))+" , "+str(tree_node._solution[variable_id])+"\n")
+                # GH: I am removing this code because
+                #     it assumes convergence and stage costs
+                #     are written in the CostVarDetail.csv file
+                #     in the next loop anyway.
+                #stage_cost_vardata = tree_node._cost_variable_datas[0][0]
+                #f.write(str(stage.name)+" , "+
+                #        str(tree_node.name)+" , "+
+                #        str(stage._cost_variable[0])+" , "+
+                #        str(index_to_string(stage._cost_variable[1]))+" , "+
+                #        str(stage_cost)+"\n")
+        print("Scenario tree solution written to file="+solution_filename)
 
-                stage_cost_vardata = tree_node._cost_variable_datas[0][0]
-                output_file.write(str(stage_name)+" , "+str(tree_node_name)+" , "+str(stage_cost_vardata.parent_component().name)+" , "+str(index_to_string(stage_cost_vardata.index()))+" , "+str(stage_cost_vardata())+"\n")
-
-        output_file.close()
-
-
-
-        print("Scenario tree solution written to file="+output_filename)
-
-        # special, double-secret probationary code to write a file of cost variables so the user
-        # can make sure that the cost variables are doing what they are supposed to be doing
-         
-        output_filename = "CostVarDetail" + ".csv"
-        output_file = open(output_filename,"w")
-        for stage in scenario_tree._stages:
-            stage_name = stage._name
-            for tree_node in stage._tree_nodes:
-                for cost_var, scenprob in tree_node._cost_variable_datas:
-                    output_file.write(str(stage_name)+" , "+str(tree_node._name)+" , "+str(cost_var.parent_component().name)+" , "+str(cost_var.parent_component().name)+" , "+str(index_to_string(cost_var.index()))+" , "+str(cost_var())+"\n")
-
-        print(output_filename+" written for modeling checking.")
-        output_file.close()
+        cost_filename = output_file_prefix + "_StageCostDetail.csv"
+        with open(cost_filename, "w") as f:
+            for stage in scenario_tree.stages:
+                cost_name, cost_index = stage._cost_variable
+                for tree_node in sorted(stage.nodes,
+                                        key=lambda x: x.name):
+                    for scenario in sorted(tree_node.scenarios,
+                                           key=lambda x: x.name):
+                        stage_cost = scenario._stage_costs[stage.name]
+                        f.write("%s, %s, %s, %s, %s, %s\n"
+                                % (stage.name,
+                                   tree_node.name,
+                                   scenario.name,
+                                   cost_name,
+                                   index_to_string(cost_index),
+                                   stage_cost))
+        print("Scenario stage costs written to file="+cost_filename)
