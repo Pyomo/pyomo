9c179e254b6754eded0b1e59c11fd449bf03b972
William Hart <whart222@users.noreply.github.com>
2016-03-16 00:50:03 +0000

    Update
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11233 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- examples/pyomo/concrete/sp.py
+++ examples/pyomo/concrete/sp.py
@@ -1,6 +1,8 @@
 # sp.py
 from pyomo.environ import *
-from sp_data import *
+from sp_data import *      # define c, b, h, and d
+
+scenarios = range(1,6)
 
 M = ConcreteModel()
 M.x = Var(within=NonNegativeReals)
@@ -10,11 +12,10 @@ def b_rule(B, i):
   B.l = Constraint(expr=B.y >= (c-b)*M.x + b*d[i])
   B.u = Constraint(expr=B.y >= (c+h)*M.x + h*d[i])
   return B
-M.B = Block(range(1,6), rule=b_rule)
+M.B = Block(scenarios, rule=b_rule)
 
 def o_rule(M):
-    return sum(M.B[i].y for i in range(1,6))/5.0
+    return sum(M.B[i].y for i in scenarios)/5.0
 M.o = Objective(rule=o_rule)
 
 model = M
-
