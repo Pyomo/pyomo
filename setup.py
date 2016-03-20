b6ee975e909dd628ccb6ff2fa4e1199c281916d8
John Siirola <jsiirola@users.noreply.github.com>
2016-03-20 17:37:11 +0000

    Cleaning up setup.py for PEP8 compliance.
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11248 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- setup.py
+++ setup.py
@@ -14,11 +14,12 @@ Script to generate the installer for pyomo.
 import sys
 import os
 
+
 def _find_packages(path):
     """
     Generate a list of nested packages
     """
-    pkg_list=[]
+    pkg_list = []
     if not os.path.exists(path):
         return []
     if not os.path.exists(path+os.sep+"__init__.py"):
@@ -30,22 +31,23 @@ def _find_packages(path):
             for name in dirs:
                 if os.path.exists(root+os.sep+name+os.sep+"__init__.py"):
                     pkg_list.append(root+os.sep+name)
-    return [pkg for pkg in map(lambda x:x.replace(os.sep,"."), pkg_list)]
+    return [pkg for pkg in map(lambda x:x.replace(os.sep, "."), pkg_list)]
+
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-requires=[
-            'PyUtilib>=5.2.3601',
-            'appdirs',
-            'ply',
-            'nose',
-            'six>=1.4',
-            ]
-if sys.version_info < (2,7):
-        requires.append('argparse')
-        requires.append('unittest2')
-        requires.append('ordereddict')
+requires = [
+    'PyUtilib>=5.2.3601',
+    'appdirs',
+    'ply',
+    'nose',
+    'six>=1.4',
+    ]
+if sys.version_info < (2, 7):
+    requires.append('argparse')
+    requires.append('unittest2')
+    requires.append('ordereddict')
 
 from setuptools import setup
 packages = _find_packages('pyomo')
@@ -55,7 +57,7 @@ setup(name='Pyomo',
       # Note: trunk should have *next* major.minor
       #     VOTD and Final releases will have major.minor.revnum
       #
-      # When cutting a release, ALSO update _major/_minor/_revnum in 
+      # When cutting a release, ALSO update _major/_minor/_revnum in
       #
       #     pyomo/pyomo/version/__init__.py
       #     pyomo/RELEASE.txt
@@ -63,24 +65,24 @@ setup(name='Pyomo',
       version='4.3',
       maintainer='William E. Hart',
       maintainer_email='wehart@sandia.gov',
-      url = 'http://pyomo.org',
-      license = 'BSD',
-      platforms = ["any"],
-      description = 'Pyomo: Python Optimization Modeling Objects',
-      long_description = read('README.txt'),
-      classifiers = [
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: End Users/Desktop',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: BSD License',
-            'Natural Language :: English',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: Unix',
-            'Programming Language :: Python',
-            'Programming Language :: Unix Shell',
-            'Topic :: Scientific/Engineering :: Mathematics',
-            'Topic :: Software Development :: Libraries :: Python Modules'
-        ],
+      url='http://pyomo.org',
+      license='BSD',
+      platforms=["any"],
+      description='Pyomo: Python Optimization Modeling Objects',
+      long_description=read('README.txt'),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Unix Shell',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
       packages=packages,
       keywords=['optimization'],
       namespace_packages=['pyomo', 'pyomo.data'],
