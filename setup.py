b71fc2cc311abbcc9f8bc1c9da6250e4f7963d79
William Hart <whart222@users.noreply.github.com>
2016-04-24 17:14:06 +0000

    Tagging Pyomo 4.3.11377
    git-svn-id: https://software.sandia.gov/svn/pyomo/pyomo/trunk@11378 570ccb8d-5833-0410-9ce8-cbedd0da42eb

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 requires = [
-    'PyUtilib>=5.3.2',
+    'PyUtilib>=5.3.5',
     'appdirs',
     'ply',
     'nose',
