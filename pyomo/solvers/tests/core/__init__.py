1bd96be091e6c54931c9c3391a9b4330be610f5a
Hart <whart222@gmail.com>
2016-05-28 09:17:55 -0400

    Re-adding the __init__.py file
    Omitting this causes errors on earlier versions of Python,
    when running 'coverage'.

---
+++ pyomo/solvers/tests/core/__init__.py
@@ -0,0 +1,9 @@
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  For more information, see the Pyomo README.txt file.
+#  _________________________________________________________________________
