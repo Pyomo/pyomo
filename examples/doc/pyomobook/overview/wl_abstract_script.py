ebf90db0110e3b7326889113fba9bfeabc8cee2c
Carl Laird <cdlaird@sandia.gov>
2016-07-22 07:11:27 -0600

    updates to example for whitespace improvement

--- examples/doc/pyomobook/overview/wl_abstract_script.py
+++ examples/doc/pyomobook/overview/wl_abstract_script.py
@@ -28,6 +28,7 @@ model.warehouse_active = Constraint(model.N, model.M, rule=warehouse_active_rule
 def num_warehouses_rule(model):
     return sum(model.y[n] for n in model.N) <= model.P
 model.num_warehouses = Constraint(rule=num_warehouses_rule)
+
 # @abstractsolve:
 instance = model.create_instance('wl_data.dat')
 solver = SolverFactory('glpk')
