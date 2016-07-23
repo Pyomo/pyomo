ba88b30e1460f6e066f9d511368e618ed682825c
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2016-07-23 03:43:49 -0400

    Adding missing __init__.py files from tests subdirectories. These are required for discovery by the nosetests script.

---
+++ pyomo/pysp/tests/benders/__init__.py
@@ -0,0 +1,11 @@
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2014 Sandia Corporation.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  This software is distributed under the BSD License.
+#  _________________________________________________________________________
+#
+# Unit tests for PySP benders solver
+#
