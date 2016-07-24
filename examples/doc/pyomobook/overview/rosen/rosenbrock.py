6136658c6fa3f4285ef65f6f65bdc1d3279275c2
Hart <whart222@gmail.com>
2016-07-24 12:25:54 -0400

    Documentation updates for the Overview chapter.

--- examples/doc/pyomobook/overview/rosen/rosenbrock.py
+++ examples/doc/pyomobook/overview/rosen/rosenbrock.py
@@ -1,15 +1,11 @@
-# rosenbrock.py: A Pyomo model for minimizing the Rosenbrock function
-# @intro:
+# rosenbrock.py: A Pyomo model for the Rosenbrock problem
 from pyomo.environ import *
 model = AbstractModel()
-# @:intro
-# @vars:
+
 model.x = Var(initialize=1.5)
 model.y = Var(initialize=1.5)
-# @:vars
-# @obj:
+
 def rosenbrock(model):
     return (1.0-model.x)**2 \
         + 100.0*(model.y - model.x**2)**2
 model.obj = Objective(rule=rosenbrock, sense=minimize)
-# @:obj
