d1bff103f9bb0f80ac8db3410783bb4e54db37cf
William Hart <whart222@gmail.com>
2016-08-22 22:18:32 -0400

    Update to use PyUtilib 5.4

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 requires = [
-    'PyUtilib>=5.3.5',
+    'PyUtilib>=5.4',
     'appdirs',
     'ply',
     'six>=1.4',
