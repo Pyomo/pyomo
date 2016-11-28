6f78088ab32be8469de120944fd8b9c6fa72c35c
Bethany Nicholson <blnicho@sandia.gov>
2016-11-28 14:37:24 -0700

    Cleaning up book examples for pyomo command chapter

--- examples/doc/pyomobook/command-ch/pyomo.model2.sh
+++ examples/doc/pyomobook/command-ch/pyomo.model2.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cd ../overview
 # @cmd:
 pyomo convert --output=concrete1.lp concrete1.py
 # @:cmd
-diff concrete1.lp ../command/concrete1.lp
+diff concrete1.lp ../command-ch/concrete1-ref.lp
--- examples/doc/pyomobook/command-ch/pyomo.model3.sh
+++ examples/doc/pyomobook/command-ch/pyomo.model3.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cd ../overview
 # @cmd:
 pyomo convert --output=concrete1.nl concrete1.py
 # @:cmd
-diff concrete1.nl ../command/concrete1.nl
+diff concrete1.nl ../command-ch/concrete1-ref.nl
