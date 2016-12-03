c40ee8a388683dc6e6584065b25ae7d61e868880
William Hart <whart222@gmail.com>
2016-12-03 11:58:03 -0500

    Remove results file.

--- examples/doc/pyomobook/gdp-ch/gdp_uc.sh
+++ examples/doc/pyomobook/gdp-ch/gdp_uc.sh
@@ -3,3 +3,5 @@
 # @cmd:
 pyomo solve gdp_uc.py gdp_uc.dat --transform gdp.bigm --solver=glpk
 # @:cmd
+cat results.yml
+rm results.yml
--- examples/doc/pyomobook/gdp-ch/scont.sh
+++ examples/doc/pyomobook/gdp-ch/scont.sh
@@ -4,3 +4,4 @@
 pyomo solve scont.py --transform gdp.bigm --solver=glpk
 # @:cmd
 cat results.yml
+rm results.yml
