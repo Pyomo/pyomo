067227c3cb53ffacfb4943e7a7c94fe5f279d4d5
William Hart <whart222@gmail.com>
2016-12-09 20:49:30 -0500

    Adding an 'aux' subpackage to Pyomo

---
+++ pyomo/aux/__init__.py
@@ -0,0 +1,16 @@
+#  _________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2014 Sandia Corporation.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  This software is distributed under the BSD License.
+#  _________________________________________________________________________
+
+# this is a namespace package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
