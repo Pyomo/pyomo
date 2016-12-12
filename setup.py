36c227355afe05d11eb2b29cb2f873696ad9eaf2
William Hart <whart222@gmail.com>
2016-12-12 13:46:04 -0500

    Removing pyomo.aux

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(name='Pyomo',
           ],
       packages=packages,
       keywords=['optimization'],
-      namespace_packages=['pyomo', 'pyomo.data', 'pyomo.aux'],
+      namespace_packages=['pyomo', 'pyomo.data'],
       install_requires=requires,
       entry_points="""
         [console_scripts]
