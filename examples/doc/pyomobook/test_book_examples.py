cc3a02c522065f1c29c2f9a4a68cedc7e2676d31
Bethany Nicholson <blnicho@sandia.gov>
2016-12-13 11:20:47 -0700

    Fixing, simplifying, and renaming the script for testing the book examples

--- examples/doc/pyomobook/test_book_examples.py
+++ examples/doc/pyomobook/test_book_examples.py
@@ -12,7 +12,7 @@ datadir = currdir
 testdirs = [currdir, ]
 
 solver_dependencies =   {
-                        'Test_nonlinear': 
+                        'Test_nonlinear_ch': 
                             {'test_rosen_pyomo_rosen': 'ipopt',
                             'test_react_design_run_pyomo_reactor_table': 'ipopt',
                             'test_react_design_run_pyomo_reactor': 'ipopt',
@@ -23,10 +23,14 @@ solver_dependencies =   {
                             'test_deer_run_deer': 'ipopt'}
                         }
 package_dependencies =  {
-                        'Test_data':
-                            {'test_data_ABCD9': 'pyodbc',
-                            'test_data_ABCD8': 'pyodbc',
-                            'test_data_ABCD7': 'win32com'}
+                        'Test_data_abstract_ch':
+                            {'test_data_abstract_ch_ABCD9': ['pyodbc',],
+                            'test_data_abstract_ch_ABCD8': ['pyodbc',],
+                             'test_data_abstract_ch_ABCD7': ['win32com',]},
+                        'Test_overview_ch':
+                            {'test_overview_ch_pyomo_wl_excel': ['numpy','pandas','xlrd',]},
+                        'Test_scripts_ch':
+                            {'test_scripts_ch_warehouse_function_cuts': ['numpy','matplotlib',]},
                         }
 solver_available = {}
 package_available = {}
@@ -44,15 +48,14 @@ def check_skip(tfname_, name):
                     solver_available[solver_] = opt.available()
         for tf_ in package_dependencies:
             for n_ in package_dependencies[tf_]:
-                package_ = package_dependencies[tf_][n_]
-                if not package_ in package_available:
-                    try:
-                        __import__()
-                        package_available[package_] = True
-                    except:
-                        package_available[package_] = False
-        #print solver_available
-        #print package_available
+                packages_ = package_dependencies[tf_][n_]
+                for package_ in packages_:
+                    if not package_ in package_available:
+                        try:
+                            __import__(package_)
+                            package_available[package_] = True
+                        except:
+                            package_available[package_] = False
     #
     # Return a boolean if the test should be skipped
     #
@@ -60,16 +63,20 @@ def check_skip(tfname_, name):
         if name in solver_dependencies[tfname_] and \
            not solver_available[solver_dependencies[tfname_][name]]:
             # Skip the test because a solver is not available
+            # print('Skipping %s because of missing solver' %(name)) 
             return True
     if tfname_ in package_dependencies:
-        if name in package_dependencies[tfname_] and \
-           not package_available[package_dependencies[tfname_][name]]:
-            # Skip the test because a package is not available
-            return True
+        if name in package_dependencies[tfname_]:
+            packages_ = package_dependencies[tfname_][name]
+            if not all([package_available[i] for i in packages_]):
+                # Skip the test because a package is not available
+                # print('Skipping %s because of missing package' %(name))
+                return True
     return False
 
 
 def filter(line):
+    # Ignore certain text when comparing output with baseline
     line.strip()
     if line.startswith('password:') or line.startswith('http:') or line.startswith('Job '):
         return True
@@ -83,87 +90,84 @@ def filter(line):
         return True
     if 'Status: optimal' in line or 'Status: feasible' in line:
         return True
-    status = 'time:' in line or 'Time:' in line or line.startswith('[') or 'with format cpxlp' in line or 'usermodel = <module' in line or line.startswith('File') or 'execution time=' in line or 'Solver results file:' in line
+    status = 'time:' in line or 'Time:' in line or \
+             line.startswith('[') or 'with format cpxlp' in line or \
+             'usermodel = <module' in line or line.startswith('File') or \
+             'execution time=' in line or 'Solver results file:' in line
     return status
 
 for tdir in testdirs:
-  #print(tdir)
+
   for fname in glob.glob(os.path.join(tdir,'*')):
     if not os.path.isdir(fname):
         continue
-
+    # Only test files in directories ending in -ch. These directories
+    # contain the updated python and scripting files corresponding to
+    # each chapter in the book.
     if '-ch' not in fname:
         continue
 
-    print("Testing ",fname)
-    #print("HERE - DIRECTORY")
+    # print("Testing ",fname)
 
     # Declare an empty TestCase class
     fname_ = fname.replace('-','_')
-    if 'pyomobook' in fname_:
-        tfname_ = 'Test_'+fname_.split("pyomobook"+os.sep)[1]
-        tfname2_ = 'Test2_'+fname_.split("pyomobook"+os.sep)[1]
-    else:
-        tfname_ = 'Test_'+fname_.split("examples"+os.sep)[1]
-        tfname2_ = 'Test2_'+fname_.split("examples"+os.sep)[1]
+    tfname_ = 'Test_'+fname_.split("pyomobook"+os.sep)[1]
     Test = globals()[tfname_] = type(tfname_, (unittest.TestCase,), {})
     Test = unittest.category("book")(Test)
-    Test2 = globals()[tfname2_] = type(tfname2_, (unittest.TestCase,), {})
-    Test2 = unittest.category("book2")(Test2)
     
-    #
+    # Find all .py files in the test directory
     for file in list(glob.glob(fname+'/*.py')) + list(glob.glob(fname+'/*/*.py')):
-        #print(file)
+    
         bname = os.path.basename(file)
         dir_ = os.path.dirname(os.path.abspath(file))+os.sep
         name='.'.join(bname.split('.')[:-1])
         tname = os.path.basename(os.path.dirname(dir_))+'_'+name
-        #
+    
         suffix = None
-        for suffix_ in ['.txt', '.yml', '.txt2', '.yml2']:
+        # Look for txt and yml file names matching py file names. Add
+        # a test for any found
+        for suffix_ in ['.txt', '.yml']:
             if os.path.exists(dir_+name+suffix_):
                 suffix = suffix_
                 break
-        #print([dir_, name, suffix])
-        #
         if not suffix is None:
-            print(tname)
             cwd = os.getcwd()
             os.chdir(dir_)
-            if suffix_ in ['.txt2', '.yml2']:
-                forceskip = check_skip(tfname2_, 'test_'+tname.replace('.','_'))
-                Test2.add_baseline_test(cmd='cd %s; %s %s' % (dir_, sys.executable, os.path.abspath(bname)),  baseline=dir_+name+suffix, name=tname, filter=filter, tolerance=1e-3, forceskip=forceskip)
-            else:
-                forceskip = check_skip(tfname_, 'test_'+tname.replace('.','_'))
-                Test.add_baseline_test(cmd='cd %s; %s %s' % (dir_, sys.executable, os.path.abspath(bname)),  baseline=dir_+name+suffix, name=tname, filter=filter, tolerance=1e-3, forceskip=forceskip)
+            tname = tname.replace('-','_')
+            tname = tname.replace('.','_')
+            # print(tname)
+            forceskip = check_skip(tfname_, 'test_'+tname)
+            Test.add_baseline_test(cmd='cd %s; %s %s' % (dir_,
+                                                         sys.executable, os.path.abspath(bname)),
+                                   baseline=dir_+name+suffix, name=tname, filter=filter,
+                                   tolerance=1e-3, forceskip=forceskip)
             os.chdir(cwd)
 
-    #
+    # Find all .sh files in the test directory
     for file in list(glob.glob(fname+'/*.sh')) + list(glob.glob(fname+'/*/*.sh')):
         bname = os.path.basename(file)
         dir_ = os.path.dirname(os.path.abspath(file))+os.sep
         name='.'.join(bname.split('.')[:-1])
         tname = os.path.basename(os.path.dirname(dir_))+'_'+name
-        #
-        #
         suffix = None
-        for suffix_ in ['.txt', '.yml', '.txt2', '.yml2']:
+        # Look for txt and yml file names matching sh file names. Add
+        # a test for any found
+        for suffix_ in ['.txt', '.yml']:
             if os.path.exists(dir_+name+suffix_):
                 suffix = suffix_
                 break
-        #
         if not suffix is None:
-            print(tname)
             cwd = os.getcwd()
             os.chdir(dir_)
-            if suffix_ in ['.txt2', '.yml2']:
-                forceskip = check_skip(tfname2_, 'test_'+tname.replace('.','_'))
-                Test2.add_baseline_test(cmd='cd %s; %s' % (dir_, os.path.abspath(bname)),  baseline=dir_+name+suffix, name=tname, filter=filter, tolerance=1e-3, forceskip=forceskip)
-            else:
-                forceskip = check_skip(tfname_, 'test_'+tname.replace('.','_'))
-                Test.add_baseline_test(cmd='cd %s; %s' % (dir_, os.path.abspath(bname)),  baseline=dir_+name+suffix, name=tname, filter=filter, tolerance=1e-3, forceskip=forceskip)
+            tname = tname.replace('-','_')
+            tname = tname.replace('.','_')
+            # print(tname)
+            forceskip = check_skip(tfname_, 'test_'+tname)
+            Test.add_baseline_test(cmd='cd %s; %s' % (dir_,
+                                                      os.path.abspath(bname)), baseline=dir_+name+suffix,
+                                   name=tname, filter=filter, tolerance=1e-3,
+                                   forceskip=forceskip)
             os.chdir(cwd)
-    #
     Test = None
 
 # Execute the tests
