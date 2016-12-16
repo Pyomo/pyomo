b1d7133d9faf2c28d7dd4b779a2f7c25bc41e88d
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2016-12-16 05:20:52 -0500

    adding to sos example

--- examples/kernel/special_ordered_sets.py
+++ examples/kernel/special_ordered_sets.py
@@ -84,3 +84,35 @@ assert len(s.weights) == 2
 d[0].expr = p[0] + 1
 d[1].expr = p[0] + p[1]
 assert tuple(pk.value(w) for w in s.weights) == (2, 3)
+
+#
+# Example model (piecewise linear objective)
+#
+import pyomo.environ
+
+breakpoints = [-1.0, 0.0, 1.0, 2.0]
+function_points = [2.0, -2.5, 3.0, 1.0]
+
+m = pk.block()
+
+m.z = pk.variable_list(
+    pk.variable(domain=pk.NonNegativeReals)
+    for i in range(4))
+m.x = pk.variable()
+m.y = pk.variable()
+
+m.o = pk.objective(m.y)
+
+m.c1 = pk.constraint(
+    m.y == sum(f*z for f,z in zip(m.z, function_points)))
+m.c2 = pk.constraint(
+    m.x == sum(w*z for w,z in zip(m.z, breakpoints)))
+m.c3 = pk.constraint(sum(m.z) == 1)
+m.s = pk.sos2(m.z)
+
+opt = pk.SolverFactory("cplex")
+status = opt.solve(m)
+assert status.solver.status == \
+    pk.SolverStatus.ok
+assert status.solver.termination_condition == \
+    pk.TerminationCondition.optimal
