b0e2247d1c9b3cce5ee2994bd14aa4112fa79007
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2017-01-05 18:13:38 -0500

    cleanup whitespace, fix comment

--- pyomo/repn/tests/ampl/small11_testCase.py
+++ pyomo/repn/tests/ampl/small11_testCase.py
@@ -11,11 +11,11 @@
 # Purpose: For regression testing to ensure that the Pyomo
 #          NL writer properly labels constraint ids in the "J"
 #          section of the NL file when trivial constraints exist.
-#          At the creation of this test, trivial constraints 
-#          (constraints with no variables) are being written to 
+#          At the creation of this test, trivial constraints
+#          (constraints with no variables) are being written to
 #          the nl file as a feasibility check for the user.
 #
-#          This test model relies on the asl_test executable. It
+#          This test model relies on the gjh_asl_json executable. It
 #          will not solve if sent to a real optimizer.
 #
 
