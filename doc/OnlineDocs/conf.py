4287655515997b39fc8ab972e780c182b340ba44
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2017-03-26 19:32:40 -0400

    do not document private members

--- doc/OnlineDocs/conf.py
+++ doc/OnlineDocs/conf.py
@@ -44,7 +44,7 @@ extensions = ['sphinx.ext.autodoc',
     #'sphinx.ext.githubpages']
 
 viewcode_import = True
-napoleon_include_private_with_doc = True
+#napoleon_include_private_with_doc = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
