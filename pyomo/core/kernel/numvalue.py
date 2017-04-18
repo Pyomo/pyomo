06d362fbfdbe03a3fddf360f07421f2ca067991e
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2017-04-18 15:37:17 -0400

    Adding some documentation for the value function

--- pyomo/core/kernel/numvalue.py
+++ pyomo/core/kernel/numvalue.py
@@ -129,9 +129,20 @@ def value(obj, exception=True):
     A utility function that returns the value of a Pyomo object or
     expression.
 
-    If the argument is None, a numeric value or a string, then this
-    function simply returns the argument.  Otherwise, if the argument
-    is a NumericValue then the __call__ method is executed.
+    Args:
+        obj: The argument to evaluate. If it is None, a
+            string, or any other primative numeric type,
+            then this function simply returns the argument.
+            Otherwise, if the argument is a NumericValue
+            then the __call__ method is executed.
+        exception (bool): Indicates if an exception should
+            be raised when instances of NumericValue fail to
+            evaluate due to one or more objects not being
+            initialized to a numeric value (e.g, one or more
+            variables in an algebraic expression having the
+            value None). Default is True.
+
+    Returns: A numeric value or None.
     """
     if obj.__class__ in native_types:
         return obj
