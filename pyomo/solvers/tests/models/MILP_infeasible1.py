22b3a012b002e6e9ec9211f22f6beb36d4d7905c
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2017-05-18 16:43:45 -0400

    Adding more solver tests for kernel models

--- pyomo/solvers/tests/models/MILP_infeasible1.py
+++ pyomo/solvers/tests/models/MILP_infeasible1.py
@@ -8,10 +8,10 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
+import pyomo.kernel as pmo
 from pyomo.core import ConcreteModel, Param, Var, Expression, Objective, Constraint, Integers, Binary, NonNegativeReals
 from pyomo.opt import TerminationCondition
 from pyomo.solvers.tests.models.base import _BaseTestModel, register_model
-
 
 @register_model
 class MILP_infeasible1(_BaseTestModel):
@@ -45,10 +45,29 @@ class MILP_infeasible1(_BaseTestModel):
     def warmstart_model(self):
         assert self.model is not None
         model = self.model
-        model.x = None
-        model.y = None
-        model.z = None
+        model.x.value = None
+        model.y.value = None
+        model.z.value = None
 
     def post_solve_test_validation(self, tester, results):
         tester.assertEqual(results['Solver'][0]['termination condition'],
                            TerminationCondition.infeasible)
+
+@register_model
+class MILP_infeasible1_kernel(MILP_infeasible1):
+
+    def _generate_model(self):
+        self.model = pmo.block()
+        model = self.model
+        model._name = self.description
+
+        model.x = pmo.variable(domain=Binary)
+        model.y = pmo.variable(domain=Binary)
+        model.z = pmo.variable(domain=Binary)
+
+        model.o = pmo.objective(-model.x-model.y-model.z)
+
+        model.c1 = pmo.constraint(model.x+model.y <= 1)
+        model.c2 = pmo.constraint(model.x+model.z <= 1)
+        model.c3 = pmo.constraint(model.y+model.z <= 1)
+        model.c4 = pmo.constraint(model.x+model.y+model.z >= 1.5)
--- pyomo/solvers/tests/models/MIQCP_simple.py
+++ pyomo/solvers/tests/models/MIQCP_simple.py
@@ -8,9 +8,9 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
+import pyomo.kernel as pmo
 from pyomo.core import ConcreteModel, Param, Var, Expression, Objective, Constraint, Binary, maximize
 from pyomo.solvers.tests.models.base import _BaseTestModel, register_model
-
 
 @register_model
 class MIQCP_simple(_BaseTestModel):
@@ -43,7 +43,23 @@ class MIQCP_simple(_BaseTestModel):
     def warmstart_model(self):
         assert self.model is not None
         model = self.model
-        model.x = None
-        model.y = None
-        model.z = None
+        model.x.value = None
+        model.y.value = None
+        model.z.value = None
 
+@register_model
+class MIQCP_simple_kernel(MIQCP_simple):
+
+    def _generate_model(self):
+        self.model = pmo.block()
+        model = self.model
+        model._name = self.description
+
+        model.x = pmo.variable(domain=Binary)
+        model.y = pmo.variable(domain=Binary)
+        model.z = pmo.variable(domain=Binary)
+
+        model.obj = pmo.objective(model.x,sense=maximize)
+        model.c0 = pmo.constraint(model.x+model.y+model.z == 1)
+        model.qc0 = pmo.constraint(model.x**2 + model.y**2 <= model.z**2)
+        model.qc1 = pmo.constraint(model.x**2 <= model.y*model.z)
