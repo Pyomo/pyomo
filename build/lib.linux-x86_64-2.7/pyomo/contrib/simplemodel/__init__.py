b56859baa1ebc283b7902e2814e110c906f2e9c1
Grant Seastream <gseastre@andrew.cmu.edu>
2017-06-08 17:11:52 -0400

    Removed build/ and dist/

--- build/lib.linux-x86_64-2.7/pyomo/contrib/simplemodel/__init__.py
+++
@@ -1,4 +0,0 @@
-try:
-    from pyomocontrib_simplemodel import *
-except:
-    raise RuntimeError("The pyomocontrib_simplemodel package is not installed.")
--- build/lib.linux-x86_64-2.7/pyomo/solvers/tests/core/plugins/__init__.py
+++
@@ -1,2 +0,0 @@
-#from pyomo.data.core.plugins.lp import *
-#from pyomo.data.core.plugins.mip import *
