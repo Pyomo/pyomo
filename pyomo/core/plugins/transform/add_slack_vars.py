167b84adc2d1c8e64c49ab9293906980204ef75f
Emma Johnson <emma58@users.noreply.github.com>
2017-06-13 17:09:53 -0600

    moves tests for add slacks to their own file, starts testing the expressions by digging into the _SumExpression objects

--- pyomo/core/plugins/transform/add_slack_vars.py
+++ pyomo/core/plugins/transform/add_slack_vars.py
@@ -6,6 +6,8 @@ from pyomo.opt import SolverFactory
 from pyomo.util.plugin import alias
 from pyomo.core.plugins.transform.hierarchy import NonIsomorphicTransformation
 
+from random import randint
+
 #DEBUG
 import pdb
 
@@ -21,10 +23,30 @@ class AddSlackVariables(NonIsomorphicTransformation):
         kwds['name'] = "add_slack_vars"
         super(AddSlackVariables, self).__init__(**kwds)
 
+    def _get_unique_name(self, instance, name):
+        # test if this name already exists in model. If not, we're good. Else, we add random numbers until it doesn't
+        # TODO: is this good enough?
+        while True:
+            if not instance.component(name):
+                return name
+            else:
+                name += str(randint(0,9))
+
+    # JDS: TODOs:
+    #  - support targets= argument to specify the constraints to relax
+    #    - make sure that works for apply_to and crate_using
+    #    - make sure that works for Constraint, IndexedConstraint, and _ConstraintData
+    #  - make sure no bogus kwds are passed!
+
     def _apply_to(self, instance, **kwds):
         # deactivate the objective
         for o in instance.component_data_objects(Objective):
             o.deactivate()
+
+        # create block where we can add slack variables safely
+        xblockname = self._get_unique_name(instance, "_core_add_slack_variables")
+        instance.add_component(xblockname, Block())
+        xblock = instance.component(xblockname)
 
         obj_expr = 0
         for cons in instance.component_data_objects(Constraint, descend_into=(Block, Disjunct)):
@@ -39,7 +61,7 @@ class AddSlackVariables(NonIsomorphicTransformation):
                 posSlack = Var(within=NonNegativeReals)
                 # TODO: Should I be making my own block and adding variables to that?
                 # Did we talk about that??
-                instance.add_component(varName, posSlack)
+                xblock.add_component(varName, posSlack)
                 # add positive slack to body expression
                 cons._body += posSlack
                 # penalize slack in objective
@@ -49,11 +71,11 @@ class AddSlackVariables(NonIsomorphicTransformation):
                 # declare slack
                 varName = "_slack_minus_" + cons.name
                 negSlack = Var(within=NonNegativeReals)
-                instance.add_component(varName, negSlack)
+                xblock.add_component(varName, negSlack)
                 # add negative slack to body expression
                 cons._body -= negSlack
                 # add slack to objective
                 obj_expr += negSlack
 
         # make a new objective that minimizes sum of slack variables
-        instance._slack_objective = Objective(expr=obj_expr)
+        xblock._slack_objective = Objective(expr=obj_expr)
