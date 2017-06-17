6f8b5ab4c88fe91f4e0b1afb531870a34d5af6e6
Emma Johnson <emma58@users.noreply.github.com>
2017-06-17 17:16:45 -0600

    Breaks bigm tests into smaller tests and starts to correct them for new structure of transformed model

--- pyomo/gdp/tests/test_bigm.py
+++ pyomo/gdp/tests/test_bigm.py
@@ -4,11 +4,22 @@ from pyomo.environ import *
 from pyomo.gdp import *
 from pyomo.core.base import expr_common, expr as EXPR
 
+import random
+
 # DEBUG
 from nose.tools import set_trace
 
-class TestBigM_2TermDisj(unittest.TestCase):
-    def test_twoTerm_disjunction(self):
+class TestBigM_2TermDisj_coopr3(unittest.TestCase):
+    def setUp(self):
+        EXPR.set_expression_tree_format(expr_common.Mode.coopr3_trees)
+        # set seed so we can test name collisions predictably
+        random.seed(666)
+
+    def tearDown(self):
+        EXPR.set_expression_tree_format(expr_common._default_mode)
+    
+    @staticmethod
+    def makeModel():
         m = ConcreteModel()
         # m.BigM = Suffix(direction=Suffix.LOCAL)
         # m.BigM[None] = 78
@@ -23,24 +34,51 @@ class TestBigM_2TermDisj(unittest.TestCase):
         def disj_rule(m):
             return [m.d[0], m.d[1]]
         m.disj = Disjunction(rule=disj_rule)
-
-        TransformationFactory('gdp.bigm').apply_to(m)#, default_bigM=78)
-        
+        return m
+    
+    def test_new_transformation_block(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
         # we have a transformation block
-        gdpblock = m.component("_gdp_relax")
+        gdpblock = m.component("_pyomo_gdp_relaxation")
         self.assertIsInstance(gdpblock, Block)
-        # have indicator variables
+        # it has the disjunct on it
+        self.assertIsInstance(m._pyomo_gdp_relaxation.component("d"), Block)
+
+    def test_new_trans_block_nameCollision(self):
+        m = self.makeModel()
+        m._pyomo_gdp_relaxation = Block()
+        TransformationFactory('gdp.bigm').apply_to(m)
+        gdpblock = m.component("_pyomo_gdp_relaxation4")
+        self.assertIsInstance(gdpblock, Block)
+        # it has the disjunct on it
+        self.assertIsInstance(m._pyomo_gdp_relaxation.component("d"), Block)
+
+    def test_still_have_indVars(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
         dblock = m._gdp_relax.component("d")
-        self.assertIsInstance(dblock, Block)
         self.assertIsInstance(dblock[0].indicator_var, Var)
         self.assertTrue(dblock[0].indicator_var.is_binary())
         self.assertIsInstance(dblock[1].indicator_var, Var)
         self.assertTrue(dblock[1].indicator_var.is_binary())
+    
+    def test_deactivated_constraints(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
+        dblock = m._gdp_relax.component("d")
 
         # old constraint there, deactivated, new constraint active
         oldc = dblock[0].component("c")
         self.assertIsInstance(oldc, Constraint)
         self.assertFalse(oldc.active)
+
+    def test_transformed_constraints(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
+        dblock = m._gdp_relax.component("d")
+
+        oldc = dblock[0].component("c")
         newc = dblock[0].component("c_eq")
         self.assertIsInstance(newc, Constraint)
         self.assertTrue(newc.active)
