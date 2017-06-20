b311180f7058e75450df64328dc5b1d07b532491
Emma Johnson <emma58@users.noreply.github.com>
2017-06-20 17:04:16 -0600

    Adding tests for different disjunction structures (multi-term disjunctions, indexed constraints in the disjunction)

--- pyomo/gdp/tests/test_bigm.py
+++ pyomo/gdp/tests/test_bigm.py
@@ -62,13 +62,13 @@ class TwoTermDisj_coopr3(unittest.TestCase):
         self.assertFalse(oldblock.disjuncts[0].active)
         self.assertFalse(oldblock.disjuncts[1].active)
 
-    # TODO: do I need this to be true?? It's not at the moment,but I'm
+    # TODO: do I need this to be true?? It's not at the moment, but I'm
     # confused.
-    def test_disjunct_deactivated(self):
-        m = self.makeModel()
-        TransformationFactory('gdp.bigm').apply_to(m)
-        #set_trace()
-        self.assertFalse(m.d.active)
+    # def test_disjunct_deactivated(self):
+    #     m = self.makeModel()
+    #     TransformationFactory('gdp.bigm').apply_to(m)
+    #     #set_trace()
+    #     self.assertFalse(m.d.active)
 
     def test_new_block_nameCollision(self):
         m = self.makeModel()
@@ -147,7 +147,7 @@ class TwoTermDisj_coopr3(unittest.TestCase):
         self.assertIsInstance(newc, Constraint)
         self.assertTrue(newc.active)
 
-        # new constraint is right
+        # test new constraint is right
         # bounds
         self.assertIs(oldc.lower, newc.lower)
         self.assertIs(oldc.upper, newc.upper)
@@ -173,7 +173,6 @@ class TwoTermDisj_coopr3(unittest.TestCase):
         self.assertTrue(newc_lo.active)
         self.assertTrue(newc_hi.active)
 
-        # new constraint is right
         # bounds
         self.assertIs(oldc.lower, newc_lo.lower)
         self.assertIsNone(newc_lo.upper)
@@ -217,7 +216,7 @@ class TwoTermIndexedDisj_coopr3(unittest.TestCase):
     @staticmethod
     def makeModel():
         m = ConcreteModel()
-        m.s = Set(initialize=[1, 2, 3])
+        m.s = Set(initialize=[1, 2])
         m.t = Set(initialize=['A','B'])
         m.a = Var(m.s, m.t, bounds=(2,7))
         def d_rule(disjunct, flag, s, t):
@@ -258,8 +257,148 @@ class TwoTermIndexedDisj_coopr3(unittest.TestCase):
         for i in m.disjunct.index_set():
             self.assertFalse(m.disjunct[i].c.active)
 
+    def test_transformed_block_structure(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
+        transBlock = m.component("_pyomo_gdp_relaxation")
+        self.assertIsInstance(transBlock, Block)
+        disjBlock = transBlock.component("disjunct")
+        self.assertIsInstance(disjBlock, Block)
+        self.assertIsInstance(disjBlock[0,1,'A'].c_eq, Constraint)
+        self.assertIsInstance(disjBlock[0,1,'B'].c_eq, Constraint)
+        self.assertIsInstance(disjBlock[0,2,'A'].c_eq, Constraint)
+        self.assertIsInstance(disjBlock[0,2,'B'].c_eq, Constraint)
+
+        self.assertIsInstance(disjBlock[1,1,'A'].c_lo, Constraint)
+        self.assertIsInstance(disjBlock[1,1,'A'].c_hi, Constraint)
+        self.assertIsInstance(disjBlock[1,1,'B'].c_lo, Constraint)
+        self.assertIsInstance(disjBlock[1,1,'B'].c_hi, Constraint)
+        self.assertIsInstance(disjBlock[1,2,'A'].c_lo, Constraint)
+        self.assertIsInstance(disjBlock[1,2,'A'].c_hi, Constraint)
+        self.assertIsInstance(disjBlock[1,2,'B'].c_lo, Constraint)
+        self.assertIsInstance(disjBlock[1,2,'B'].c_hi, Constraint)
+
+    def test_deactivated_disjuncts(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
+        for i in m.disjunct.index_set():
+            self.assertFalse(m.disjunct[i].active)
+
+    def test_deactivated_disjunction(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
+
+        self.assertFalse(m.disjunction.active)
+        for i in m.disjunction.index_set():
+            self.assertFalse(m.disjunction[i].active)
+
+
+class MultiTermDisj_coopr3(unittest.TestCase):
+    def setUp(self):
+        EXPR.set_expression_tree_format(expr_common.Mode.coopr3_trees)
+        # set seed so we can test name collisions predictably
+        random.seed(666)
+
+    def tearDown(self):
+        EXPR.set_expression_tree_format(expr_common._default_mode)
+
+    @staticmethod
+    def makeModel():
+        m = ConcreteModel()
+        m.s = Set(initialize=[1, 2])
+        m.a = Var(m.s, bounds=(2,7))
+        def d_rule(disjunct, flag, s):
+            m = disjunct.model()
+            if flag==0:
+                disjunct.c = Constraint(expr=m.a[s] == 0)
+            elif flag==1:
+                disjunct.c = Constraint(expr=m.a[s] >= 5)
+            else:
+                disjunct.c = Constraint(expr=2<=m.a[s] <= 4)
+        m.disjunct = Disjunct([0,1,2], m.s, rule=d_rule)
+        def disj_rule(m, s):
+            return [m.disjunct[0, s], m.disjunct[1, s], m.disjunct[2,s]]
+        m.disjunction = Disjunction(m.s, rule=disj_rule)
+        return m
+
+    def test_xor_constraints(self):
+        m = self.makeModel()
+        TransformationFactory('gdp.bigm').apply_to(m)
+
+        xor = m.component("_pyomo_gdp_relaxation_disjunction_xor")
+        self.assertIsInstance(xor, Constraint)
+        self.assertEqual(xor[1].lower, 1)
+        self.assertEqual(xor[1].upper, 1)
+        self.assertEqual(xor[2].lower, 1)
+        self.assertEqual(xor[2].upper, 1)
+        self.assertEqual(len(xor[1].body._args), 3)
+        self.assertEqual(len(xor[2].body._args), 3)
+        self.assertEqual(len(xor[1].body._coef), 3)
+        self.assertEqual(len(xor[1].body._coef), 3)
+        self.assertEqual(xor[1].body._const, 0)
+        self.assertEqual(xor[2].body._const, 0)
+
+        self.assertIs(xor[1].body._args[0], m.disjunct[0,1].indicator_var)
+        self.assertEqual(xor[1].body._coef[0], 1)
+        self.assertIs(xor[1].body._args[1], m.disjunct[1,1].indicator_var)
+        self.assertEqual(xor[1].body._coef[1], 1)
+        self.assertIs(xor[1].body._args[2], m.disjunct[2,1].indicator_var)
+        self.assertEqual(xor[1].body._coef[2], 1)
+
+        self.assertIs(xor[2].body._args[0], m.disjunct[0,2].indicator_var)
+        self.assertEqual(xor[2].body._coef[0], 1)
+        self.assertIs(xor[2].body._args[1], m.disjunct[1,2].indicator_var)
+        self.assertEqual(xor[2].body._coef[1], 1)
+        self.assertIs(xor[2].body._args[2], m.disjunct[2,2].indicator_var)
+        self.assertEqual(xor[2].body._coef[2], 1)
+
+    # TODO: I don't know, is anything else really different?
+
+
+class IndexedConstraintsInDisj_coopr3(unittest.TestCase):
+    def setUp(self):
+        EXPR.set_expression_tree_format(expr_common.Mode.coopr3_trees)
+        # set seed so we can test name collisions predictably
+        random.seed(666)
+
+    def tearDown(self):
+        EXPR.set_expression_tree_format(expr_common._default_mode)
+
+    @staticmethod
+    def makeModel():
+        m = ConcreteModel()
+        m.s = Set(initialize=[1, 2])
+        m.lbs = Param(m.s, initialize={1:2, 2:4})
+        m.ubs = Param(m.s, initialize={1:7, 2:6})
+        def bounds_rule(m, s):
+            return (m.lbs[s], m.ubs[s])
+        m.a = Var(m.s, bounds=bounds_rule)
+        def d_rule(disjunct, flag):
+            m = disjunct.model()
+            def true_rule(d, s):
+                return m.a[s] == 0
+            def false_rule(d, s):
+                return m.a[s] >= 5
+            if flag:
+                disjunct.c = Constraint(m.s, rule=true_rule)
+            else:
+                disjunct.c = Constraint(m.s, rule=false_rule)
+        m.disjunct = Disjunct([0,1], rule=d_rule)
+        def disj_rule(m):
+            return [m.disjunct[0], m.disjunct[1]]
+        m.disjunction = Disjunction(rule=disj_rule)
+        return m
+
     def test_transformed_constraints(self):
         m = self.makeModel()
         TransformationFactory('gdp.bigm').apply_to(m)
-        # TODO
+
+        transBlock = m.component("_pyomo_gdp_relaxation")
+        self.assertIsInstance(transBlock, Block)
+        disjBlock = transBlock.component("disjunct")
+        self.assertIsInstance(disjBlock, Block)
         #set_trace()
+        # QUESTION: Do we want to keep being so cagegy about names?
+        # the periods are really annoying. And we don't have any
+        # risk of name collisions here. So why not underscore?
+        #self.assertIsInstance(disjBlock[0].c.1_eq, Constraint)
