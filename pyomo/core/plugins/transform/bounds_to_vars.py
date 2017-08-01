61522dea60e4bcfc8e5f5b339c3c1f09b11c4242
Qi Chen <qichen@andrew.cmu.edu>
2017-08-01 11:02:31 -0400

    Bugfix and simplification

--- pyomo/core/plugins/transform/bounds_to_vars.py
+++ pyomo/core/plugins/transform/bounds_to_vars.py
@@ -38,38 +38,8 @@ class ConstraintToVarBoundTransform(IsomorphicTransformation):
         for constr in m.component_data_objects(ctype=Constraint,
                                                active=True,
                                                descend_into=True):
-            body = constr.body
-            # See if the constraint is a simple x <= b
-            try:
-                if body.type() is Var:
-                    var = body
-                    if constr.upper is not None:
-                        var.setub(min(var.ub, value(constr.upper))
-                                  if var.ub is not None
-                                  else value(constr.upper))
-                    if constr.lower is not None:
-                        var.setlb(max(var.lb, value(constr.lower))
-                                  if var.lb is not None
-                                  else value(constr.lower))
-                constr.deactivate()
-                # Sometimes deactivating the constraint will remove the
-                # variable from all active constraints, so that it won't be
-                # updated during the optimization. Therefore, we need to shift
-                # the value of var as necessary in order to keep it within the
-                # constraints.
-                if var.lb is not None and var.value < var.lb:
-                    var.set_value(var.lb)
-                if var.ub is not None and var.value > var.ub:
-                    var.set_value(var.ub)
-                continue
-            except AttributeError as err:
-                if "object has no attribute 'type'" in err.message:
-                    pass
-                else:
-                    raise
             # Check if the constraint is k * x + c1 <= c2 or c2 <= k * x + c1
-            if (isinstance(body, EXPR._SumExpression) and
-                    body.polynomial_degree() in (0, 1)):
+            if constr.body.polynomial_degree() == 1:
                 repn = generate_canonical_repn(constr.body)
                 if repn.variables is not None and len(repn.variables) == 1:
                     var = repn.variables[0]
