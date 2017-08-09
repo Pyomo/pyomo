a513fcd5ad0ea465dd2a7936ec3be6d577752482
Grant Seastream <gseastre@andrew.cmu.edu>
2017-08-09 11:13:32 -0400

    Change NLP default back to CONOPT

--- .travis_install_gams.sh
+++ .travis_install_gams.sh
@@ -5,10 +5,6 @@
 wget https://d37drm4t2jghv5.cloudfront.net/distributions/24.8.5/linux/linux_x64_64_sfx.exe
 chmod u+x linux_x64_64_sfx.exe
 ./linux_x64_64_sfx.exe > /dev/null
-# Change default NLP solver to baron for testing, performs better than CONOPT
-pushd $PWD/gams24.8_linux_x64_64_sfx
-sed -i 's/NLP CONOPT/NLP BARON/g' gmscmpun.txt
-popd
 if [ "${TRAVIS_PYTHON_VERSION}" = "2.6" ]; then
    pushd $PWD/gams24.8_linux_x64_64_sfx/apifiles/Python/api_26;
    python setup.py install;
