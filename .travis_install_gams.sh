de327d52fc0d45391251260e56a9135452de3987
Grant Seastream <gseastre@andrew.cmu.edu>
2017-08-09 02:37:37 -0400

    Move PATH setting to .travis.yml

--- .travis_install_gams.sh
+++ .travis_install_gams.sh
@@ -5,7 +5,6 @@
 wget https://d37drm4t2jghv5.cloudfront.net/distributions/24.8.5/linux/linux_x64_64_sfx.exe
 chmod u+x linux_x64_64_sfx.exe
 ./linux_x64_64_sfx.exe > /dev/null
-PATH=${PATH}:$PWD/gams24.8_linux_x64_64_sfx
 # Change default NLP solver to baron for testing, performs better than CONOPT
 pushd $PWD/gams24.8_linux_x64_64_sfx
 sed -i 's/NLP CONOPT/NLP BARON/g' gmscmpun.txt
