e5cfa0eca1429e3a958078d6c8f38aa1fed9392a
Qi Chen <qichen@andrew.cmu.edu>
2017-08-14 14:49:04 -0400

    Add reversion testing

--- pyomo/core/tests/transform/test_fixed_var_detection.py
+++ pyomo/core/tests/transform/test_fixed_var_detection.py
@@ -1,7 +1,7 @@
 """Tests detection of fixed variables."""
 import pyutilib.th as unittest
 from pyomo.environ import (ConcreteModel, TransformationFactory,
-                           Var)
+                           value, Var)
 
 __author__ = "Qi Chen <qichen at andrew.cmu.edu>"
 
@@ -10,7 +10,7 @@ class TestConstraintToVarBoundTransform(unittest.TestCase):
     """Tests detection of fixed variables."""
 
     def test_fixed_var_propagate(self):
-        """Test for transitivity in a variable equality set."""
+        """Test for detecting de-facto fixed variables and fixing them."""
         m = ConcreteModel()
         m.v1 = Var(initialize=1)
         m.v2 = Var(initialize=2)
@@ -20,6 +20,22 @@ class TestConstraintToVarBoundTransform(unittest.TestCase):
         TransformationFactory('core.detect_fixed_vars').apply_to(m)
         self.assertTrue(m.v1.fixed)
         self.assertFalse(m.v2.fixed)
+
+    def test_fixed_var_revert(self):
+        """Test for reversion of fixed variables."""
+        m = ConcreteModel()
+        m.v1 = Var(initialize=1)
+        m.v2 = Var(initialize=2)
+        m.v1.setub(2)
+        m.v1.setlb(2)
+
+        xfrm = TransformationFactory('core.detect_fixed_vars')
+        xfrm.apply_to(m, tmp=True)
+        self.assertTrue(m.v1.fixed)
+        self.assertFalse(m.v2.fixed)
+        xfrm.revert()
+        self.assertFalse(m.v1.fixed)
+        self.assertEquals(value(m.v1), 1)
 
 
 if __name__ == '__main__':
