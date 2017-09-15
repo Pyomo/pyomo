b1a29ab275d482201e8bbd286291cc3b572c354b
John Siirola <jsiirola@users.noreply.github.com>
2017-09-15 14:48:55 -0600

    Adding a custom (stable) implementation of randint.

--- pyomo/util/modeling.py
+++ pyomo/util/modeling.py
@@ -8,7 +8,17 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
-from random import randint
+from random import random
+
+def randint(a,b):
+    """Our implementation of random.randint.
+
+    The Python random.randint is not consistent between python versions
+    and produces a series that is different in 3.x than 2.x.  So that we
+    can support deterministic testing (i.e., setting the random.seed and
+    expecting the same sequence), we will implement a simple, but stable
+    version of randint()."""
+    return int((b-a+1)*random())
 
 def unique_component_name(instance, name):
     # test if this name already exists in model. If not, we're good. 
