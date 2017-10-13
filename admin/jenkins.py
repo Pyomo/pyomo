66dc630b18e55ea354c017aafae0487971fdfe26
John Siirola <jsiirola@users.noreply.github.com>
2017-10-13 14:47:37 -0600

    Tracking changes in the test.pyomo API
    This fixes problems with the core nd nonPySP tests, along with making
    the driver more robust to problems with coverage generation.

--- admin/jenkins.py
+++ admin/jenkins.py
@@ -67,6 +67,11 @@ coverage_omit=','.join([
     'pyutilib.*',
 ])
 
+pyomo_packages = [
+    'pyomo.%s' % os.path.basename(x) for x in
+    glob.glob(os.path.join(os.environ['WORKSPACE'], 'src', 'pyomo', '*'))
+    if os.path.isdir(x) ]
+
 if config == "notests":
     driver.perform_install('pyomo', config='pyomo_all.ini')
 
@@ -90,14 +95,18 @@ elif config == "core":
     else:
         assert False
     # Test
-    os.environ['TEST_PACKAGES'] = 'checker core environ opt repn scripting solvers util version'
+    os.environ['TEST_PACKAGES'] = ' '.join([
+            'pyomo.checker','pyomo.core','pyomo.environ','pyomo.opt',
+            'pyomo.repn','pyomo.scripting','pyomo.solvers','pyomo.util',
+            'pyomo.version'])
     print("-" * 60)
     print("Performing tests")
     print("-" * 60)
     driver.perform_tests('pyomo', coverage=True, omit=coverage_omit)
 
 elif config == "nonpysp":
-    os.environ['TEST_PACKAGES'] = '-e pysp'
+    os.environ['TEST_PACKAGES'] = ' '.join(
+        x for x in pyomo_packages if x != 'pyomo.pysp' )
     driver.perform_build('pyomo', coverage=True, omit=coverage_omit, config='pyomo_all.ini')
 
 elif config == "parallel":
