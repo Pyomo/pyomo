c98c49dddd9091169c31e17fd96930f276ee2efc
William Hart <whart222@gmail.com>
2017-11-18 10:59:13 -0700

    First stab at using the replacement visitor.
    NOTE:  This should be faster since you don't need to clone
    and then replace intrinsic functions.  These can be done together.

--- pyomo/core/base/symbolic.py
+++ pyomo/core/base/symbolic.py
@@ -10,8 +10,8 @@
 
 from six import StringIO
 from pyomo import core
-from pyomo.core.base import expr_common, expr as EXPR
-from pyomo.core.base.numvalue import native_types
+from pyomo.core.expr import current as EXPR
+from pyomo.core.expr.numvalue import native_types
 from pyomo.util import DeveloperError
 
 _sympy_available = True
@@ -25,7 +25,7 @@ try:
         return ans
 
     def _sum(*x):
-        return sum(i for i in x)
+        return sum(x_ for x_ in x)
 
     def _nondifferentiable(*x):
         raise NondifferentiableError(
@@ -83,22 +83,28 @@ def differentiate(expr, wrt=None, wrt_list=None):
         raise RuntimeError(
             "The sympy module is not available.  "
             "Cannot perform automatic symbolic differentiation.")
-
     if not (( wrt is None ) ^ ( wrt_list is None )):
         raise ValueError(
             "differentiate(): Must specify exactly one of wrt and wrt_list")
+    #
+    # Setup the WRT list
+    #
     if wrt is not None:
         wrt_list = [ wrt ]
     else:
         # Copy the list because we will normalize things in place below
         wrt_list = list(wrt_list)
-
+    #
+    # Setup mapping dictionaries
+    #
     pyomo_vars = list(EXPR.identify_variables(expr))
     sympy_vars = [sympy.var('x%s'% i) for i in range(len(pyomo_vars))]
     sympy2pyomo = dict( zip(sympy_vars, pyomo_vars) )
     pyomo2sympy = dict( (id(pyomo_vars[i]), sympy_vars[i])
                          for i in range(len(pyomo_vars)) )
-
+    #
+    # Process WRT information
+    #
     ans = []
     for i, target in enumerate(wrt_list):
         if target.__class__ is not tuple:
@@ -110,15 +116,18 @@ def differentiate(expr, wrt=None, wrt_list=None):
                 break
         wrt_list[i] = tuple( pyomo2sympy.get(id(var),None) for var in target )
         ans.append(0 if mismatch_target else None)
-
+    #
     # If there is nothing to do, do nothing
+    #
     if all(i is not None for i in ans):
         return ans if wrt is None else ans[0]
-
-    tmp_expr = EXPR.clone_expression( expr, memo=pyomo2sympy )
-    tmp_expr = _map_intrinsic_functions(tmp_expr, sympy2pyomo)
     #
-    # Call to_string() directly to create a string version of this expression
+    # Clone the expression and map intrinsic functions
+    #
+    #tmp_expr = EXPR.clone_expression( expr, memo=pyomo2sympy )
+    tmp_expr = _map_intrinsic_functions(expr, sympy2pyomo, pyomo2sympy)
+    #
+    # Call to_string() to create a string version of this expression
     #
     buf = StringIO()
     if isinstance(tmp_expr, EXPR._ExpressionBase):
@@ -127,19 +136,78 @@ def differentiate(expr, wrt=None, wrt_list=None):
         buf.close()
     else:
         tmp_expr = str(tmp_expr)
-
+    #
+    # Sympify the string representation of the expression
+    #
     sympy_expr = sympy.sympify(
         tmp_expr, locals=dict((str(x), x) for x in sympy_vars) )
-
+    #
+    # Differentiate for each WRT variable, and map the 
+    # result back into a Pyomo expression tree.
+    #
     for i, target in enumerate(wrt_list):
         if ans[i] is None:
             sympy_ans = sympy_expr.diff(*target)
             ans[i] = _map_sympy2pyomo(sympy_ans, sympy2pyomo)
-
+    #
+    # Return the answer
+    #
     return ans if wrt is None else ans[0]
 
 
-def _map_intrinsic_functions(expr, sympySymbols):
+class ReplaceIntrinsicFunctions(EXPR.ExpressionReplacementVisitor):
+
+    def __init__(self, native_or_sympy_types, pyomo2sympy):
+        self.native_or_sympy_types = native_or_sympy_types
+        self.pyomo2sympy = pyomo2sympy
+
+    def visit_potential_leaf(self, node):
+        #
+        # Don't replace native or sympy types
+        #
+        if type(node) in self.native_or_sympy_types:
+            return False, None
+        #
+        # Replace pyomo variables with sympy variables
+        #
+        if id(node) in self.pyomo2sympy:
+            return True, self.pyomo2sympy[id(node)]
+        #
+        # Replace some intrinsic functions whose name or
+        # definition change.
+        #
+        if isinstance(node, EXPR._UnaryFunctionExpression):
+            if node._name == 'ceil':
+                node_ = node.__class__(list(node.args))
+                node_._name = 'ceiling'
+                return True, node_
+                
+            if node._name == 'log10':
+                node_ = node.__class__([node/EXPR.log(10)])
+                node_._name = 'log'
+                return True, node_
+        #
+        # Don't replace anything else
+        #
+        return False, None
+
+
+def _map_intrinsic_functions(expr, sympySymbols, pyomo2sympy):
+    #
+    # Handle simple cases
+    #
+    if expr.__class__ in native_numeric_types:
+        return expr
+    if not expr.is_expression():
+        return expr
+    #
+    # Create the visitor and call it.
+    #
+    visitor = ReplaceIntrinsicFunctions(sympySymbols, pyomo2sympy)
+    return visitor.dfs_postorder_stack(expr)
+
+
+def X_map_intrinsic_functions(expr, sympySymbols):
     coopr3_mode = expr_common.mode is expr_common.Mode.coopr3_trees
     native_or_sympy_types = set(native_types)
     native_or_sympy_types.add( type(list(sympySymbols)[0]) )
@@ -188,6 +256,40 @@ def _map_intrinsic_functions(expr, sympySymbols):
                 _idx = 0
                 _len = _sub.nargs()
     return _argList[0]
+
+
+# =====================================================
+# _map_sympy2pyomo
+# =====================================================
+
+class Sympy2PyomoVisitor(EXPR.ExpressionValueVisitor):
+
+    def visit(self, node, values):
+        """ Visit nodes that have been expanded """
+        _sympyOp = node
+        _op = _operatorMap.get( type(_sympyOp), None )
+        if _op is None:
+            raise DeveloperError(
+                "sympy expression type '%s' not found in the operator "
+                "map" % type(_sympyOp) )
+        return _op(*tuple(values))
+
+    def visiting_potential_leaf(self, node, _values):
+        """ 
+        Visiting a potential leaf.
+
+        Return True if the node is not expanded.
+        """
+        if node.nargs() > 0:
+            return False
+        if node in sympy2pyomo:
+            _values.append( sympy2pyomo[node] )
+        else:
+            _values.append( node.evalf() )
+        return True
+
+    def finalize(self, ans):
+        return ans
 
 
 def _map_sympy2pyomo(expr, sympy2pyomo):
