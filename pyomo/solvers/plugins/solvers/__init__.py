47e291dfecda5c1af3f2b5c1f16c228b32c4e6a6
Sunjeev Kale <sjkale@andrew.cmu.edu>
2017-11-26 14:46:24 -0500

    edited init file

--- pyomo/solvers/plugins/solvers/__init__.py
+++ pyomo/solvers/plugins/solvers/__init__.py
@@ -2,8 +2,8 @@
 #
 #  Pyomo: Python Optimization Modeling Objects
 #  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
-#  Under the terms of Contract DE-NA0003525 with National Technology and 
-#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain 
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
 #  rights in this software.
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
@@ -29,3 +29,4 @@ import pyomo.solvers.plugins.solvers.gurobi_persistent
 import pyomo.solvers.plugins.solvers.cplex_direct
 import pyomo.solvers.plugins.solvers.cplex_persistent
 import pyomo.solvers.plugins.solvers.GAMS
+import pyomo.solvers.plugins.solvers.multi
