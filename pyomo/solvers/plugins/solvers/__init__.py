ade6b05bd9857a8a30e4373ffd10894a905465d9
Sunjeev Kale <sjkale@andrew.cmu.edu>
2017-11-26 13:48:17 -0500

    revert all multistart commits

--- pyomo/solvers/plugins/solvers/__init__.py
+++ pyomo/solvers/plugins/solvers/__init__.py
@@ -2,8 +2,8 @@
 #
 #  Pyomo: Python Optimization Modeling Objects
 #  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
-#  Under the terms of Contract DE-NA0003525 with National Technology and
-#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  Under the terms of Contract DE-NA0003525 with National Technology and 
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain 
 #  rights in this software.
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
@@ -28,4 +28,3 @@ import pyomo.solvers.plugins.solvers.CONOPT
 import pyomo.solvers.plugins.solvers.XPRESS
 import pyomo.solvers.plugins.solvers.IPOPT
 import pyomo.solvers.plugins.solvers.GAMS
-import pyomo.solvers.plugins.solvers.multi
