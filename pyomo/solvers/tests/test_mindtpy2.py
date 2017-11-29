748b3e5358bc58ad295072380270911294166d05
bernalde <bernalde@cmu.edu>
2017-11-29 18:54:07 -0500

    we have found the bug, and hopefully are about to kill it!

--- pyomo/solvers/tests/test_mindtpy2.py
+++ pyomo/solvers/tests/test_mindtpy2.py
@@ -17,34 +17,34 @@ __author__ = "David Bernal <https://github.com/bernalde>"
 
 class TestMindtPy(unittest.TestCase):
     """Tests for the MINDT solver plugin."""
-
-    @unittest.skipIf(not subsolvers_available,
-                     "Required subsolvers {} are not available"
-                     .format(required_solvers))
-    def test_OA(self):
-        """Test the outer approximation decomposition algorithm."""
-        with SolverFactory('mindtpy') as opt:
-            model = SimpleMINLP()
-            print('\n Solving problem with Outer Approximation')
-            opt.solve(model, strategy='OA', init_strategy = 'rNLP')
-
-            # self.assertIs(results.solver.termination_condition,
-            #               TerminationCondition.optimal)
-            self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
-
-    @unittest.skipIf(not subsolvers_available,
-                     "Required subsolvers {} are not available"
-                     .format(required_solvers))
-    def test_PSC(self):
-        """Test the partial surrogate cuts decomposition algorithm."""
-        with SolverFactory('mindtpy') as opt:
-            model = SimpleMINLP()
-            print('\n Solving problem with Partial Surrogate Cuts')
-            opt.solve(model, strategy='PSC', init_strategy = 'rNLP')
-
-            # self.assertIs(results.solver.termination_condition,
-            #               TerminationCondition.optimal)
-            self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
+    #
+    # @unittest.skipIf(not subsolvers_available,
+    #                  "Required subsolvers {} are not available"
+    #                  .format(required_solvers))
+    # def test_OA(self):
+    #     """Test the outer approximation decomposition algorithm."""
+    #     with SolverFactory('mindtpy') as opt:
+    #         model = SimpleMINLP()
+    #         print('\n Solving problem with Outer Approximation')
+    #         opt.solve(model, strategy='OA', init_strategy = 'rNLP')
+    #
+    #         # self.assertIs(results.solver.termination_condition,
+    #         #               TerminationCondition.optimal)
+    #         self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
+    #
+    # @unittest.skipIf(not subsolvers_available,
+    #                  "Required subsolvers {} are not available"
+    #                  .format(required_solvers))
+    # def test_PSC(self):
+    #     """Test the partial surrogate cuts decomposition algorithm."""
+    #     with SolverFactory('mindtpy') as opt:
+    #         model = SimpleMINLP()
+    #         print('\n Solving problem with Partial Surrogate Cuts')
+    #         opt.solve(model, strategy='PSC', init_strategy = 'rNLP')
+    #
+    #         # self.assertIs(results.solver.termination_condition,
+    #         #               TerminationCondition.optimal)
+    #         self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
 
     @unittest.skipIf(not subsolvers_available,
                      "Required subsolvers {} are not available"
@@ -54,26 +54,26 @@ class TestMindtPy(unittest.TestCase):
         with SolverFactory('mindtpy') as opt:
             model = SimpleMINLP()
             print('\n Solving problem with Generalized Benders Decomposition')
-            opt.solve(model, strategy='GBD', init_strategy = 'rNLP')
+            opt.solve(model, strategy='GBD', init_strategy = 'initial_binary')
 
             # self.assertIs(results.solver.termination_condition,
             #               TerminationCondition.optimal)
             self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
 
-    @unittest.skipIf(not subsolvers_available,
-                     "Required subsolvers {} are not available"
-                     .format(required_solvers))
-    def test_ECP(self):
-        """Test the Extended Cutting Planes algorithm."""
-        with SolverFactory('mindtpy') as opt:
-            model = SimpleMINLP()
-            print('\n Solving problem with Extended Cutting Planes')
-            opt.solve(model, strategy='ECP', init_strategy = 'rNLP')
-
-            # self.assertIs(results.solver.termination_condition,
-            #               TerminationCondition.optimal)
-            self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
-
+    # @unittest.skipIf(not subsolvers_available,
+    #                  "Required subsolvers {} are not available"
+    #                  .format(required_solvers))
+    # def test_ECP(self):
+    #     """Test the Extended Cutting Planes algorithm."""
+    #     with SolverFactory('mindtpy') as opt:
+    #         model = SimpleMINLP()
+    #         print('\n Solving problem with Extended Cutting Planes')
+    #         opt.solve(model, strategy='ECP', init_strategy = 'rNLP')
+    #
+    #         # self.assertIs(results.solver.termination_condition,
+    #         #               TerminationCondition.optimal)
+    #         self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
+    #
 
 if __name__ == "__main__":
     unittest.main()
