70b28182e5f594f5368fd669c3b6b555c259a5a1
bernalde <bernalde@cmu.edu>
2017-12-04 17:18:07 -0500

    final details for the solvers. already working for pse 2018 results

--- pyomo/solvers/tests/test_mindtpy2.py
+++ pyomo/solvers/tests/test_mindtpy2.py
@@ -46,34 +46,34 @@ class TestMindtPy(unittest.TestCase):
     #         #               TerminationCondition.optimal)
     #         self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
     #
-    # @unittest.skipIf(not subsolvers_available,
-    #                  "Required subsolvers {} are not available"
-    #                  .format(required_solvers))
-    # def test_GBD(self):
-    #     """Test the generalized Benders Decomposition algorithm."""
-    #     with SolverFactory('mindtpy') as opt:
-    #         model = SimpleMINLP()
-    #         print('\n Solving problem with Generalized Benders Decomposition')
-    #         opt.solve(model, strategy='GBD', init_strategy = 'initial_binary')
-    #
-    #         # self.assertIs(results.solver.termination_condition,
-    #         #               TerminationCondition.optimal)
-    #         self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
-
     @unittest.skipIf(not subsolvers_available,
                      "Required subsolvers {} are not available"
                      .format(required_solvers))
-    def test_ECP(self):
-        """Test the Extended Cutting Planes algorithm."""
+    def test_GBD(self):
+        """Test the generalized Benders Decomposition algorithm."""
         with SolverFactory('mindtpy') as opt:
             model = SimpleMINLP()
-            print('\n Solving problem with Extended Cutting Planes')
-            opt.solve(model, strategy='ECP', init_strategy = 'initial_binary',
-            ECP_tolerance=1E-4)
+            print('\n Solving problem with Generalized Benders Decomposition')
+            opt.solve(model, strategy='GBD', init_strategy = 'initial_binary')
 
             # self.assertIs(results.solver.termination_condition,
             #               TerminationCondition.optimal)
             self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
+
+    # @unittest.skipIf(not subsolvers_available,
+    #                  "Required subsolvers {} are not available"
+    #                  .format(required_solvers))
+    # def test_ECP(self):
+    #     """Test the Extended Cutting Planes algorithm."""
+    #     with SolverFactory('mindtpy') as opt:
+    #         model = SimpleMINLP()
+    #         print('\n Solving problem with Extended Cutting Planes')
+    #         opt.solve(model, strategy='ECP', init_strategy = 'initial_binary',
+    #         ECP_tolerance=1E-4)
+    #
+    #         # self.assertIs(results.solver.termination_condition,
+    #         #               TerminationCondition.optimal)
+    #         self.assertTrue(abs(value(model.cost.expr) - 3.5) <= 1E-2)
     #
 
 if __name__ == "__main__":
