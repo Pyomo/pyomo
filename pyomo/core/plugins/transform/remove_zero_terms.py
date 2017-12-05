7122ede00b6ac9a015ab6d73a7063747963d6741
Qi Chen <qichen@andrew.cmu.edu>
2017-12-05 15:02:16 -0500

    remove author field

--- pyomo/core/plugins/transform/remove_zero_terms.py
+++ pyomo/core/plugins/transform/remove_zero_terms.py
@@ -9,8 +9,6 @@ from pyomo.core.plugins.transform.hierarchy import IsomorphicTransformation
 from pyomo.repn.canonical_repn import generate_canonical_repn
 from pyomo.util.plugin import alias
 
-__author__ = "Qi Chen <https://github.com/qtothec>"
-
 
 class RemoveZeroTerms(IsomorphicTransformation):
     """Looks for 0 * var in a constraint and removes it.
@@ -22,8 +20,9 @@ class RemoveZeroTerms(IsomorphicTransformation):
 
     """
 
-    alias('core.remove_zero_terms',
-          doc=textwrap.fill(textwrap.dedent(__doc__.strip())))
+    alias(
+        'core.remove_zero_terms',
+        doc=textwrap.fill(textwrap.dedent(__doc__.strip())))
 
     def __init__(self, *args, **kwargs):
         """Initialize the transformation."""
@@ -33,32 +32,32 @@ class RemoveZeroTerms(IsomorphicTransformation):
         """Apply the transformation."""
         m = model
 
-        for constr in m.component_data_objects(ctype=Constraint,
-                                               active=True,
-                                               descend_into=True):
+        for constr in m.component_data_objects(
+                ctype=Constraint, active=True, descend_into=True):
             if not constr.body.polynomial_degree() == 1:
                 continue  # we currently only process linear constraints
             repn = generate_canonical_repn(constr.body)
-            nonzero_vars_indx = [i for i in range(len(repn.variables))
-                                 if not repn.linear[i] == 0]
+            nonzero_vars_indx = [
+                i for i in range(len(repn.variables))
+                if not repn.linear[i] == 0
+            ]
             const = repn.constant if repn.constant is not None else 0
 
             if constr.equality:
-                constr.set_value(sum(repn.linear[i] * repn.variables[i]
-                                     for i in nonzero_vars_indx) + const ==
-                                 constr.upper)
+                constr.set_value(
+                    sum(repn.linear[i] * repn.variables[i]
+                        for i in nonzero_vars_indx) + const == constr.upper)
             elif constr.has_lb() and not constr.has_ub():
-                constr.set_value(sum(repn.linear[i] * repn.variables[i]
-                                     for i in nonzero_vars_indx) + const >=
-                                 constr.lower)
+                constr.set_value(
+                    sum(repn.linear[i] * repn.variables[i]
+                        for i in nonzero_vars_indx) + const >= constr.lower)
             elif constr.has_ub() and not constr.has_lb():
-                constr.set_value(sum(repn.linear[i] * repn.variables[i]
-                                     for i in nonzero_vars_indx) + const <=
-                                 constr.upper)
+                constr.set_value(
+                    sum(repn.linear[i] * repn.variables[i]
+                        for i in nonzero_vars_indx) + const <= constr.upper)
             else:
                 # constraint is a bounded inequality of form a <= x <= b.
                 # I don't think this is a great idea, but ¯\_(ツ)_/¯
-                constr.set_value(constr.lower <=
-                                 sum(repn.linear[i] * repn.variables[i]
-                                     for i in nonzero_vars_indx) + const <=
-                                 constr.upper)
+                constr.set_value(constr.lower <= sum(
+                    repn.linear[i] * repn.variables[i]
+                    for i in nonzero_vars_indx) + const <= constr.upper)
