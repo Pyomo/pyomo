ef1cc60be7c7b4718aebb5181822b6ad7592e86b
Qi Chen <qichen@andrew.cmu.edu>
2017-12-05 14:24:35 -0500

    Clean up code for deactivate_trivial_constraints.

--- pyomo/core/plugins/transform/deactivate_trivial_constraints.py
+++ pyomo/core/plugins/transform/deactivate_trivial_constraints.py
@@ -9,64 +9,77 @@ from pyomo.core.kernel.numvalue import value
 from pyomo.core.plugins.transform.hierarchy import IsomorphicTransformation
 from pyomo.util.plugin import alias
 
-__author__ = "Qi Chen <https://github.com/qtothec>"
-
 
 class TrivialConstraintDeactivator(IsomorphicTransformation):
     """Deactivates trivial constraints of form constant = constant."""
 
-    alias('core.deactivate_trivial_constraints',
-          doc=textwrap.fill(textwrap.dedent(__doc__.strip())))
+    alias(
+        'core.deactivate_trivial_constraints',
+        doc=textwrap.fill(textwrap.dedent(__doc__.strip())))
 
     def __init__(self):
         """Initialize the transformation."""
         super(TrivialConstraintDeactivator, self).__init__()
         self.tolerance = 1E-13
 
-    def _apply_to(self, instance, tmp=False, ignore_infeasible=False):
-        """Apply the transformation."""
+    def _apply_to(self, instance, tmp=False, ignore_infeasible=False, **kwds):
+        """Apply the transformation.
+
+        Kwds:
+            instance: Pyomo model object to transform.
+            tmp: True to store a set of transformed constraints for future
+                reversion of the transformation
+            ignore_infeasible: True to skip over trivial constraints that are
+                infeasible instead of raising a ValueError.
+            tol: tolerance on constraint violations
+        """
+        tmp = kwds.pop('tmp', False)
+        ignore_infeasible = kwds.pop('ignore_infeasible', False)
+        tol = kwds.pop('tolerance', 1E-13)
         if tmp and not hasattr(instance, '_tmp_trivial_deactivated_constrs'):
             instance._tmp_trivial_deactivated_constrs = ComponentSet()
 
-        for constr in instance.component_data_objects(
-                ctype=Constraint, active=True, descend_into=True):
-            if constr.body.polynomial_degree() == 0:
-                # Check to make sure constraint not violated.
-                if (constr.has_lb() and
-                        value(constr.body) < value(constr.lower)):
-                    # Sometimes if values are close to zero, but not quite
-                    # zero, we run into issues. From a practical perspective,
-                    # let's apply a tolerance.
-                    if (fabs(value(constr.body)) <= self.tolerance and
-                            fabs(value(constr.lower)) <= self.tolerance):
-                        pass
-                    elif ignore_infeasible:
-                        # do nothing, move on to next constraint
-                        continue
-                    else:
-                        raise ValueError(
-                            'Trivial constraint {} violates {} ≤ {}.'
-                            .format(constr.name,
-                                    value(constr.lower),
-                                    value(constr.body)))
-                if (constr.has_ub() and
-                        value(constr.body) > value(constr.upper)):
-                    if (fabs(value(constr.body)) <= self.tolerance and
-                            fabs(value(constr.upper)) <= self.tolerance):
-                        pass
-                    elif ignore_infeasible:
-                        # do nothing, move on to next constraint
-                        continue
-                    else:
-                        raise ValueError(
-                            'Trivial constraint {} violates {} ≤ {}.'
-                            .format(constr.name,
-                                    value(constr.body),
-                                    value(constr.upper)))
-                # Constraint is fine. Deactivate it.
-                if tmp:
-                    instance._tmp_trivial_deactivated_constrs.add(constr)
-                constr.deactivate()
+        # Trivial constraints are those that do not contain any variables, ie.
+        # the polynomial degree is 0
+        trivial_constraints = (
+            c
+            for c in instance.component_data_objects(
+                ctype=Constraint, active=True, descend_into=True)
+            if c.body.polynomial_degree() == 0)
+
+        for constr in trivial_constraints:
+            # We need to check each constraint to sure that it is not violated.
+
+            # Check if the lower bound is violated outside a given tolerance
+            if (constr.has_lb()
+                    and value(constr.body) + tol <= value(constr.lower)):
+                # Trivial constraint is infeasible.
+                if ignore_infeasible:
+                    # do nothing, move on to next constraint
+                    continue
+                else:
+                    raise ValueError(
+                        'Trivial constraint {} violates LB {} ≤ BODY {}.'
+                        .format(constr.name, value(constr.lower),
+                                value(constr.body)))
+
+            # Check if the upper bound is violated outside a given tolerance
+            if (constr.has_ub()
+                    and value(constr.body) >= value(constr.upper) + tol):
+                # Trivial constraint is infeasible.
+                if ignore_infeasible:
+                    # do nothing, move on to next constraint
+                    continue
+                else:
+                    raise ValueError(
+                        'Trivial constraint {} violates BODY {} ≤ UB {}.'
+                        .format(constr.name, value(constr.body),
+                                value(constr.upper)))
+
+            # Constraint is not infeasible. Deactivate it.
+            if tmp:
+                instance._tmp_trivial_deactivated_constrs.add(constr)
+            constr.deactivate()
 
     def revert(self, instance):
         """Revert constraints deactivated by the transformation."""
