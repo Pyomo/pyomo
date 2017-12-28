4b45dedaae7cfe2b8c2e909f1d0ef18706d77b2b
William Hart <whart222@gmail.com>
2017-12-28 08:36:36 -0700

    Adding logic to convert ASCII to byte strings
    The default in sqlite3 and json in Python 2.7 is to return
    unicode strings.  This logic optionally will return byte strings.

--- pyomo/core/plugins/data/json_dict.py
+++ pyomo/core/plugins/data/json_dict.py
@@ -128,7 +128,20 @@ class JSONDictionary(Plugin):
         if not os.path.exists(self.filename):
             raise IOError("Cannot find file '%s'" % self.filename)
         INPUT = open(self.filename, 'r')
-        jdata = json.load(INPUT)
+        if self.options.convert_unicode:
+            def _byteify(data, ignore_dicts=False):
+                if isinstance(data, unicode):
+                    return data.encode('utf-8') 
+                if isinstance(data, list):
+                    return [ _byteify(item, True) for item in data ]
+                if isinstance(data, dict) and not ignore_dicts:
+                    return {
+                    _byteify(key, True): _byteify(value, True)
+                        for key, value in data.iteritems() }
+                return data
+            jdata = json.load(INPUT, object_hook=_byteify)
+        else:
+            jdata = json.load(INPUT)
         INPUT.close()
         if jdata is None or len(jdata) == 0:
             raise IOError("Empty JSON data file")
