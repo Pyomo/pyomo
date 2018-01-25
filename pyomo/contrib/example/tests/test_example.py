e09a906e29f4072424965a0068a938375cf45c67
William Hart <whart222@gmail.com>
2018-01-24 22:54:19 -0700

    Removing use of environment variables
    Tests are now always run for pyomo.contrib packages.

--- pyomo/contrib/example/tests/test_example.py
+++ pyomo/contrib/example/tests/test_example.py
@@ -1,24 +1,17 @@
 #
-# Only run the tests in this package if the TEST_PYOMO_CONTRIB environment
-# variable has been set to '1'.  Otherwise, skip the tests.
+# Only run the tests in this package if the pyomo.contrib.example package
+# has been successfully imported.
 #
 import os
 import sys
-if os.environ.get('TEST_PYOMO_CONTRIB', 0):
-    import pyomo.contrib.example
-    run = True
-else:
-    run = False
-
+import pyomo.contrib.example
 import pyutilib.th as unittest
 
 
-@unittest.skipIf(run == False, "The TEST_PYOMO_CONTRIB environment is not set.")
 class Tests(unittest.TestCase):
 
     def test1(self):
-        env = os.environ.get('TEST_PYOMO_CONTRIB', 0)
-        self.assertNotEqual(env, 0)
+        pass
 
 
 if __name__ == "__main__":
