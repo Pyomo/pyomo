a7bbfe60ff4f051c50d49071073bc8b8b926f831
John Siirola <jsiirola@users.noreply.github.com>
2018-01-26 13:26:33 -0700

    Adding missing copyrights; standardizing module docstrings

--- pyomo/gdp/plugins/cuttingplane.py
+++ pyomo/gdp/plugins/cuttingplane.py
@@ -1,4 +1,19 @@
-# Implements cutting plane reformulation for linear, convex GDPs
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+"""
+Cutting plane-based GDP reformulation.
+
+Implements a general cutting plane-based reformulation for linear and
+convex GDPs.
+"""
 from __future__ import division
 from collections import OrderedDict
 
