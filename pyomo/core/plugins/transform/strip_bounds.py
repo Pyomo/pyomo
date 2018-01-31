e2ee43652ae12f17f755f4e6b41e16db1048e177
Qi Chen <qichen@andrew.cmu.edu>
2018-01-30 20:21:21 -0500

    Rename tmp -> reversible for clarity

--- pyomo/core/plugins/transform/strip_bounds.py
+++ pyomo/core/plugins/transform/strip_bounds.py
@@ -19,37 +19,37 @@ class VariableBoundStripper(NonIsomorphicTransformation):
         """Initialize the transformation."""
         super(VariableBoundStripper, self).__init__()
 
-    def _apply_to(self, instance, strip_domains=True, tmp=False):
+    def _apply_to(self, instance, strip_domains=True, reversible=False):
         """Apply the transformation.
 
         Args:
             instance (Block): the block on which to strip variable bounds
             strip_domains (bool, optional): strip the domain for discrete
                 variables as well
-            tmp (bool, optional): Whether the bound stripping will be
+            reversible (bool, optional): Whether the bound stripping will be
                 temporary. If so, store information for reversion.
 
         Returns:
             None
 
         """
-        if tmp and not hasattr(instance, '_tmp_var_bound_strip_lb'):
+        if reversible and not hasattr(instance, '_tmp_var_bound_strip_lb'):
             instance._tmp_var_bound_strip_lb = ComponentMap()
-        if tmp and not hasattr(instance, '_tmp_var_bound_strip_ub'):
+        if reversible and not hasattr(instance, '_tmp_var_bound_strip_ub'):
             instance._tmp_var_bound_strip_ub = ComponentMap()
-        if tmp and not hasattr(instance, '_tmp_var_bound_strip_domain'):
+        if reversible and not hasattr(instance, '_tmp_var_bound_strip_domain'):
             instance._tmp_var_bound_strip_domain = ComponentMap()
         for var in instance.component_data_objects(ctype=Var):
             if strip_domains and not var.domain == Reals:
-                if tmp:
+                if reversible:
                     instance._tmp_var_bound_strip_domain[var] = var.domain
                 var.domain = Reals
             if var.has_lb():
-                if tmp:
+                if reversible:
                     instance._tmp_var_bound_strip_lb[var] = var.lb
                 var.setlb(None)
             if var.has_ub():
-                if tmp:
+                if reversible:
                     instance._tmp_var_bound_strip_ub[var] = var.ub
                 var.setub(None)
 
