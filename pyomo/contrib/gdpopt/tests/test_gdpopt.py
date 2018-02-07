2309988ae34a20913ab7c55c58bcbd1f2a29e939
Qi Chen <qichen@andrew.cmu.edu>
2018-02-07 16:38:20 -0500

    Remove extraneous use of classes

--- pyomo/contrib/gdpopt/tests/test_gdpopt.py
+++ pyomo/contrib/gdpopt/tests/test_gdpopt.py
@@ -4,7 +4,7 @@ from math import fabs
 import pyutilib.th as unittest
 
 from pyomo.contrib.gdpopt.tests.eight_process_problem import \
-    EightProcessFlowsheet
+    build_eight_process_flowsheet
 from pyomo.environ import SolverFactory, value
 
 required_solvers = ('ipopt', 'gurobi')
@@ -23,7 +23,7 @@ class TestGDPopt(unittest.TestCase):
     def test_LOA(self):
         """Test logic-based outer approximation."""
         with SolverFactory('gdpopt') as opt:
-            model = EightProcessFlowsheet()
+            model = build_eight_process_flowsheet()
             opt.solve(model, strategy='LOA')
 
             self.assertTrue(fabs(value(model.profit.expr) - 68) <= 1E-2)
