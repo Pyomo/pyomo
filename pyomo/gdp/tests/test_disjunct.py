ba10e32a4504ccc5aa93746e8f9a920180811e9b
John Siirola <jsiirola@users.noreply.github.com>
2018-02-14 23:05:52 -0700

    Adding additional tests.

--- pyomo/gdp/tests/test_disjunct.py
+++ pyomo/gdp/tests/test_disjunct.py
@@ -10,8 +10,8 @@
 
 import pyutilib.th as unittest
 
-from pyomo.environ import ConcreteModel
-from pyomo.gdp import Disjunct, Disjunction
+from pyomo.core import ConcreteModel, Var, Constraint
+from pyomo.gdp import Disjunction, Disjunct
 
 class TestDisjunction(unittest.TestCase):
     def test_empty_disjunction(self):
@@ -32,3 +32,156 @@ class TestDisjunction(unittest.TestCase):
         m.x2[2] = [m.d, m.e]
         self.assertEqual(len(m.x2), 1)
         self.assertEqual(m.x2[2].disjuncts, [m.d, m.e])
+
+    def test_construct_implicit_disjuncts(self):
+        m = ConcreteModel()
+        m.x = Var()
+        m.y = Var()
+        m.d = Disjunction(expr=[m.x<=0, m.y>=1])
+        self.assertEqual(len(m.component_map(Disjunction)), 1)
+        self.assertEqual(len(m.component_map(Disjunct)), 1)
+        self.assertEqual(m.component_map(Disjunct).keys()[0][:2], "d_")
+        disjuncts = m.d.disjuncts
+        self.assertEqual(len(disjuncts), 2)
+        self.assertIs(disjuncts[0].parent_block(), m)
+        self.assertIs(disjuncts[0].constraint.body, m.x)
+        self.assertIs(disjuncts[1].parent_block(), m)
+        self.assertIs(disjuncts[1].constraint.body, m.y)
+
+        # Test that the implicit disjuncts get a unique name
+        for i in range(10):
+            m.add_component('e_%s' % (i,), Var())
+        m.e = Disjunction(expr=[m.y<=0, m.x>=1])
+        self.assertEqual(len(m.component_map(Disjunction)), 2)
+        self.assertEqual(len(m.component_map(Disjunct)), 2)
+        self.assertEqual(m.component_map(Disjunct).keys()[1][:2], "e_")
+        disjuncts = m.e.disjuncts
+        self.assertEqual(len(disjuncts), 2)
+        self.assertIs(disjuncts[0].parent_block(), m)
+        self.assertIs(disjuncts[0].constraint.body, m.y)
+        self.assertIs(disjuncts[1].parent_block(), m)
+        self.assertIs(disjuncts[1].constraint.body, m.x)
+        self.assertEqual(len(disjuncts[0].parent_component().name), 4)
+        self.assertEqual(disjuncts[0].name[:2], "e_")
+
+
+class TestDisjunct(unittest.TestCase):
+    def test_deactivate(self):
+        m = ConcreteModel()
+        m.x = Var()
+        m.d1 = Disjunct()
+        m.d1.constraint = Constraint(expr=m.x<=0)
+        m.d = Disjunction(expr=[m.d1, m.x>=1, m.x>=5])
+        d2 = m.d.disjuncts[1].parent_component()
+        self.assertEqual(len(m.component_map(Disjunction)), 1)
+        self.assertEqual(len(m.component_map(Disjunct)), 2)
+        self.assertIsNot(m.d1, d2)
+
+        self.assertTrue(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertTrue(m.d.disjuncts[0].active)
+        self.assertTrue(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertFalse(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        m.d.disjuncts[0].deactivate()
+        self.assertFalse(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertFalse(m.d.disjuncts[0].active)
+        self.assertTrue(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertTrue(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        m.d.disjuncts[1].deactivate()
+        self.assertFalse(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertFalse(m.d.disjuncts[0].active)
+        self.assertFalse(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertTrue(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertTrue(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        d2.deactivate()
+        self.assertFalse(m.d1.active)
+        self.assertFalse(d2.active)
+        self.assertFalse(m.d.disjuncts[0].active)
+        self.assertFalse(m.d.disjuncts[1].active)
+        self.assertFalse(m.d.disjuncts[2].active)
+        self.assertTrue(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertTrue(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertTrue(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        m.d.disjuncts[2].activate()
+        self.assertFalse(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertFalse(m.d.disjuncts[0].active)
+        self.assertFalse(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertTrue(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertTrue(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        d2.activate()
+        self.assertFalse(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertFalse(m.d.disjuncts[0].active)
+        self.assertTrue(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertTrue(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        m.d1.activate()
+        self.assertTrue(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertTrue(m.d.disjuncts[0].active)
+        self.assertTrue(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertFalse(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+    def test_deactivate_without_fixing_indicator(self):
+        m = ConcreteModel()
+        m.x = Var()
+        m.d1 = Disjunct()
+        m.d1.constraint = Constraint(expr=m.x<=0)
+        m.d = Disjunction(expr=[m.d1, m.x>=1, m.x>=5])
+        d2 = m.d.disjuncts[1].parent_component()
+        self.assertEqual(len(m.component_map(Disjunction)), 1)
+        self.assertEqual(len(m.component_map(Disjunct)), 2)
+        self.assertIsNot(m.d1, d2)
+
+        self.assertTrue(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertTrue(m.d.disjuncts[0].active)
+        self.assertTrue(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertFalse(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        m.d.disjuncts[0]._deactivate_without_fixing_indicator()
+        self.assertFalse(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertFalse(m.d.disjuncts[0].active)
+        self.assertTrue(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertFalse(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+        m.d.disjuncts[1]._deactivate_without_fixing_indicator()
+        self.assertFalse(m.d1.active)
+        self.assertTrue(d2.active)
+        self.assertFalse(m.d.disjuncts[0].active)
+        self.assertFalse(m.d.disjuncts[1].active)
+        self.assertTrue(m.d.disjuncts[2].active)
+        self.assertFalse(m.d.disjuncts[0].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
+        self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
