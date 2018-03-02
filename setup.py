42a8702bf75d23550ad21fba53bb948141178138
William Hart <whart222@gmail.com>
2018-03-02 06:21:41 -0700

    Reseting the development version

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(name='Pyomo',
       #     pyomo/pyomo/version/__init__.py
       #     pyomo/RELEASE.txt
       #
-      version='5.4.2',
+      version='5.5.0',
       maintainer='William E. Hart',
       maintainer_email='wehart@sandia.gov',
       url='http://pyomo.org',
