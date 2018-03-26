7e504c03a5e21f76ceef975a122a28c93a1497ca
Bethany Nicholson <blnicho@sandia.gov>
2018-03-26 14:04:53 -0600

    Removing a test committed by mistake

--- pyomo/dae/tests/test_misc.py
+++ pyomo/dae/tests/test_misc.py
@@ -967,35 +967,6 @@ class TestDaeMisc(unittest.TestCase):
         self.assertTrue(len(m.b.con), 6)
         self.assertTrue(len(m.b.y), 6)
 
-    def test_hierarchical_blocks2(self):
-        m = ConcreteModel()
-
-        m.b = Block()
-        m.b.t = ContinuousSet(bounds=(0, 10))
-
-        m.b.c = Block()
-
-        def _d_rule(d, t):
-            m = d.model()
-            d.x = Var()
-            return d
-
-        m.b.c.d = Block(m.b.t, rule=_d_rule)
-
-        m.b.y = Var(m.b.t)
-
-        def _con_rule(b, t):
-            return b.y[t] <= b.c.d[t].x
-
-        m.b.con = Constraint(m.b.t, rule=_con_rule)
-
-        generate_finite_elements(m.b.t, 5)
-        expand_components(m)
-
-        self.assertTrue(len(m.b.c.d), 6)
-        self.assertTrue(len(m.b.con), 6)
-        self.assertTrue(len(m.b.y), 6)
-
-
+    
 if __name__ == "__main__":
     unittest.main()
