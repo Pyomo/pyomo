4d421952f16decb966d59525600173f76fc1d89c
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2018-04-04 17:36:01 -0400

    use inequality function

--- pyomo/pysp/tests/convert/piecewise_model.py
+++ pyomo/pysp/tests/convert/piecewise_model.py
@@ -45,7 +45,7 @@ def create_instance(scenario_name):
                                     force_pw=True)
 
     model.c_second_stage = Constraint(expr= model.x + model.r * cnt >= -100)
-    model.r_second_stage = Constraint(expr= -cnt <= model.r <= 0)
+    model.r_second_stage = Constraint(expr= inequality(-cnt, model.r, 0))
     # exercise more of the code by making this an indexed
     # block
     model.p_second_stage = Piecewise([1], model.fz, model.z,
