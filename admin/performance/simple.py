a3fb5017c40ea2a984116b590c86919e7ce9a00f
William Hart <whart222@gmail.com>
2018-04-15 15:17:05 -0600

    Removing tag for Python 3.4
    We are no longer testing 3.4, so we should not advertise
    support for 3.4

--- admin/performance/simple.py
+++ admin/performance/simple.py
@@ -1,15 +1,18 @@
 from pyomo.environ import *
-import pyomo.core.kernel.expr as EXPR
 import timeit
 import signal
 
 coopr3 = True
-pyomo4 = True
+pyomo4 = False
 
 if coopr3 or pyomo4:
     from pyomo.repn import generate_ampl_repn
+    import pyomo.core.kernel.expr as EXPR
+    def Sum(*args, linear=None):
+        return sum(*args)
 else:
     from pyomo.repn import generate_standard_repn
+    import pyomo.core.expr.current as EXPR
 
 class TimeoutError(Exception):
     pass
@@ -36,9 +39,21 @@ model.x = Var(model.A, initialize=2)
 
 
 def linear(flag):
-    if flag == 1:
+    if flag == 0:
+        expr=sum(model.x[i] for i in model.A)
+    elif flag == 10:
+        with EXPR.linear_expression as expr:
+            expr=sum((model.x[i] for i in model.A), expr)
+    elif flag == 20:
+        expr=Sum(model.x[i] for i in model.A)
+
+    elif flag == 1:
         expr = summation(model.p, model.x)
     elif flag == 6:
+        expr=Sum((model.p[i]*model.x[i] for i in model.A), linear=False)
+    elif flag == 16:
+        expr=Sum((model.p[i]*model.x[i] for i in model.A), linear=True)
+    elif flag == 26:
         expr=Sum(model.p[i]*model.x[i] for i in model.A)
 
     elif flag == 2:
@@ -67,6 +82,9 @@ def linear(flag):
     elif flag == 12:
         with EXPR.linear_expression as expr:
             expr=sum((model.p[i]*model.x[i] for i in model.A), expr)
+    elif flag == 22:
+        with EXPR.nonlinear_expression as expr:
+            expr=sum((model.p[i]*model.x[i] for i in model.A), expr)
     elif flag == 13:
         with EXPR.linear_expression as expr:
             for i in model.A:
@@ -84,11 +102,31 @@ def linear(flag):
         expr=0
         for i in model.A:
             expr += model.p[i] * (1 + model.x[i])
-
     elif flag == 17:
         with EXPR.linear_expression as expr:
             for i in model.A:
                 expr += model.p[i] * (1 + model.x[i])
+    elif flag == 27:
+        expr = Sum(model.p[i]*(1 + model.x[i]) for i in model.A)
+
+    elif flag == 8:
+        expr=0
+        for i in model.A:
+            expr += (model.x[i]+model.x[i])
+    elif flag == 18:
+        # This will assume a nonlinear sum
+        expr = Sum((model.x[i] + model.x[i]) for i in model.A)
+
+    elif flag == 9:
+        expr=0
+        for i in model.A:
+            expr += model.p[i]*(model.x[i]+model.x[i])
+    elif flag == 19:
+        # This will assume a nonlinear sum
+        expr = Sum(model.p[i]*(model.x[i] + model.x[i]) for i in model.A)
+
+    elif flag == -9:
+        expr = Sum(sin(model.x[i]) for i in model.A)
 
     if coopr3 or pyomo4:
         generate_ampl_repn(expr)
@@ -98,19 +136,22 @@ def linear(flag):
 if coopr3:
     import pyomo.core.kernel.expr_coopr3 as COOPR3
     print("REFCOUNT: "+str(COOPR3._getrefcount_available))
-    for i in (2,3,7):
+    for i in (0,2,3,6,7,8,9):
         print((i,timeit.timeit('linear(%d)' % i, "from __main__ import linear", number=1)))
 
 if pyomo4:
     import pyomo.core.kernel.expr_pyomo4 as PYOMO4
     EXPR.set_expression_tree_format(EXPR.common.Mode.pyomo4_trees)
     print("REFCOUNT: "+str(PYOMO4._getrefcount_available))
-    for i in (2,3,7):
+    for i in (0,2,3,6,7,8,9):
         print((i,timeit.timeit('linear(%d)' % i, "from __main__ import linear", number=1)))
 
 if not (coopr3 or pyomo4):
-    import pyomo.core.kernel.expr_pyomo5 as PYOMO5
-    print("REFCOUNT: "+str(PYOMO5._getrefcount_available))
-    for i in (2,12,3,13,4,14,5,15,7,17):
+    import pyomo.core.expr.expr_pyomo5 as PYOMO5
+    #print("REFCOUNT: "+str(PYOMO5._getrefcount_available))
+    #import cProfile
+    #cProfile.run("linear(7)", "stats.7")
+    for i in (0,10,20,2,12,22,3,13,4,14,5,15,6,16,26,7,17,27,8,18,9,19,-9):
+    #for i in (6,16,26):
         print((i,timeit.timeit('linear(%d)' % i, "from __main__ import linear", number=1)))
 
