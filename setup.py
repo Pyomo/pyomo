404e3163dd102ef62cccce0b1511c65311501a65
William Hart <whart222@gmail.com>
2018-04-15 14:53:17 -0600

    Added python_requires to setup.py
    This allows Python 2.7.x and versions greater than 3.5.x

--- setup.py
+++ setup.py
@@ -94,6 +94,7 @@ setup(name='Pyomo',
       packages=packages,
       keywords=['optimization'],
       install_requires=requires,
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*'
       entry_points="""
         [console_scripts]
         runbenders=pyomo.pysp.benders:Benders_main
