0b12a7734504b3f3067774a4dbc94ae13e7aedda
Qi Chen <qichen@andrew.cmu.edu>
2018-04-25 14:16:23 -0400

    add documentation and restrict to positive integers

--- pyomo/repn/plugins/gams_writer.py
+++ pyomo/repn/plugins/gams_writer.py
@@ -66,8 +66,10 @@ class ToGamsVisitor(EXPR.ExpressionValueVisitor):
                 tmp.append(val)
 
         if node.__class__ is EXPR.PowExpression:
+            # If the exponent is a positive integer, use the power() function.
+            # Otherwise, use the ** operator.
             if (tmp[1].__class__ in native_numeric_types and
-                    tmp[1] == int(tmp[1])):
+                    tmp[1] == int(tmp[1]) and tmp[1] > 0):
                 return "power({0}, {1})".format(tmp[0], tmp[1])
             else:
                 return "{0} ** {1}".format(tmp[0], tmp[1])
