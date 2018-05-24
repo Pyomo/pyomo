3c5ba9b82539b6e81b4a2d895b05a10481498764
Grant Seastream <gseastr@sandia.gov>
2018-05-24 14:00:37 -0600

    Add ShortNameLabeler, use to limit gams symbol names

--- pyomo/solvers/tests/checks/test_GAMS.py
+++ pyomo/solvers/tests/checks/test_GAMS.py
@@ -209,6 +209,44 @@ class GAMSTests(unittest.TestCase):
             self.assertEqual(results.solver.termination_condition,
                              TerminationCondition.optimal)
 
+    @unittest.skipIf(not gamspy_available,
+                     "The 'gams' python bindings are not available")
+    def test_long_var_py(self):
+        with SolverFactory("gams", solver_io="python") as opt:
+
+            m = ConcreteModel()
+            x = m.a23456789012345678901234567890123456789012345678901234567890123 = Var()
+            y = m.b234567890123456789012345678901234567890123456789012345678901234 = Var()
+            z = m.c23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 = Var()
+            w = m.d01234567890 = Var()
+            m.c1 = Constraint(expr= x + y + z + w == 0)
+            m.c2 = Constraint(expr= x >= 10)
+            m.o = Objective(expr= x)
+
+            results = opt.solve(m)
+
+            self.assertEqual(results.solver.termination_condition,
+                             TerminationCondition.optimal)
+
+    @unittest.skipIf(not gamsgms_available,
+                     "The 'gams' executable is not available")
+    def test_long_var_gms(self):
+        with SolverFactory("gams", solver_io="gms") as opt:
+
+            m = ConcreteModel()
+            x = m.a23456789012345678901234567890123456789012345678901234567890123 = Var()
+            y = m.b234567890123456789012345678901234567890123456789012345678901234 = Var()
+            z = m.c23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 = Var()
+            w = m.d01234567890 = Var()
+            m.c1 = Constraint(expr= x + y + z + w == 0)
+            m.c2 = Constraint(expr= x >= 10)
+            m.o = Objective(expr= x)
+
+            results = opt.solve(m)
+
+            self.assertEqual(results.solver.termination_condition,
+                             TerminationCondition.optimal)
+
 class GAMSLogfileTestBase(unittest.TestCase):
     def setUp(self):
         """Set up model and temporary directory."""
