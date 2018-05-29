bd00dd62724e04d3345edb36cf71ceaa666e79af
William Hart <whart222@gmail.com>
2018-05-29 16:31:34 -0600

    Adding metasolver flag
    This is used when generating help information:
    
      pyomo help -s

--- pyomo/contrib/gdpopt/GDPopt.py
+++ pyomo/contrib/gdpopt/GDPopt.py
@@ -156,6 +156,9 @@ class GDPoptSolver(pyomo.common.plugin.Plugin):
         "Rounding is done before solving NLP subproblem"
     ))
 
+    def __init__(self):
+        self._metasolver = False
+
     def available(self, exception_flag=True):
         """Check if solver is available.
 
