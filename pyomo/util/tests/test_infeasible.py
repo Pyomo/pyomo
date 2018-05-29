af3a0e467475296e6aaed6d348be0cd1e76b18e8
Qi Chen <qchen@sandia.gov>
2018-05-29 08:18:21 -0600

    Split into multiple tests

--- pyomo/util/tests/test_infeasible.py
+++ pyomo/util/tests/test_infeasible.py
@@ -15,14 +15,19 @@ from pyomo.util.infeasible import (log_active_constraints, log_close_to_bounds,
 class TestInfeasible(unittest.TestCase):
     """Tests infeasible model debugging utilities."""
 
-    def test_log_infeasible_constraints(self):
-        """Test for logging of infeasible constraints."""
+    def build_model(self):
         m = ConcreteModel()
         m.x = Var(initialize=1)
         m.c = Constraint(expr=m.x >= 2)
         m.c2 = Constraint(expr=m.x == 4)
         m.c3 = Constraint(expr=m.x <= 0)
+        m.y = Var(bounds=(0, 2), initialize=1.9999999)
+        m.c4 = Constraint(expr=m.y >= m.y.value)
+        return m
 
+    def test_log_infeasible_constraints(self):
+        """Test for logging of infeasible constraints."""
+        m = self.build_model()
         output = StringIO()
         with LoggingIntercept(output, 'pyomo.util.infeasible', logging.INFO):
             log_infeasible_constraints(m)
@@ -32,6 +37,9 @@ class TestInfeasible(unittest.TestCase):
             "CONSTR c3: 1 > 0.0"]
         self.assertEqual(output.getvalue().splitlines(), expected_output)
 
+    def test_log_infeasible_bounds(self):
+        """Test for logging of infeasible variable bounds."""
+        m = self.build_model()
         m.x.setlb(2)
         m.x.setub(0)
         output = StringIO()
@@ -41,16 +49,21 @@ class TestInfeasible(unittest.TestCase):
                            "VAR x: 1 > UB 0"]
         self.assertEqual(output.getvalue().splitlines(), expected_output)
 
+    def test_log_active_constraints(self):
+        """Test for logging of active constraints."""
+        m = self.build_model()
         output = StringIO()
         with LoggingIntercept(output, 'pyomo.util.infeasible', logging.INFO):
             log_active_constraints(m)
         expected_output = ["c active",
                            "c2 active",
-                           "c3 active"]
+                           "c3 active",
+                           "c4 active"]
         self.assertEqual(output.getvalue().splitlines(), expected_output)
 
-        m.y = Var(bounds=(0, 2), initialize=1.9999999)
-        m.c4 = Constraint(expr=m.y >= m.y.value)
+    def test_log_close_to_bounds(self):
+        """Test logging of variables and constraints near bounds."""
+        m = self.build_model()
         output = StringIO()
         with LoggingIntercept(output, 'pyomo.util.infeasible', logging.INFO):
             log_close_to_bounds(m)
