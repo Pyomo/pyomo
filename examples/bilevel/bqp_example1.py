17944d432f4ba9a3c39f100232820c7c4dfc6174
William Hart <whart222@gmail.com>
2018-05-30 05:53:38 -0600

    Updates to these examples
    The unbounded y2 variable causes an infeasible problem.  Which is
    unbounded in the subproblem.

--- examples/bilevel/bqp_example1.py
+++ examples/bilevel/bqp_example1.py
@@ -16,17 +16,17 @@ def pyomo_create_model(options, model_options):
     M.x1 = Var(bounds=(0,None))
     M.x2 = Var(within=Binary)
     M.y1 = Var(bounds=(1,None))
-    M.y2 = Var(bounds=(None,2))
+    M.y2 = Var(bounds=(-100,2))
     M.y3 = Var(bounds=(None,None))
     M.y4 = Var(bounds=(3,4))
     M.o = Objective(expr=M.x1 - 4*M.y1)
     
     M.sub = SubModel(fixed=(M.x1, M.x2))
-    M.sub.o  = Objective( expr=              11*M.x2 +          0*12*M.x2*M.y1 +         M.y2 +       9*M.y3                      )
-    M.sub.c1 = Constraint(expr=                 M.x1 + 0*13*M.x2*M.y1 + 5*M.y1                -      10*M.y3                 <= 19)
-    M.sub.c2 = Constraint(expr=20 <=          2*M.x1 +                6*M.y1 + 0*14*M.x2*M.y2                                     )
-    M.sub.c3 = Constraint(expr=32 ==          4*M.x1 +                8*M.y1                               + 0*15*M.x2*M.y4       )
-    M.sub.c4 = Constraint(expr=inequality(22, 3*M.x1 +                7*M.y1                + 0*16*M.x2*M.y3                ,  28))
+    M.sub.o  = Objective( expr=              11*M.x2 +          12*M.x2*M.y1 +         M.y2 +       9*M.y3                      )
+    M.sub.c1 = Constraint(expr=                 M.x1 + 13*M.x2*M.y1 + 5*M.y1                                               <= 19)
+    M.sub.c2 = Constraint(expr=20 <=          2*M.x1 +                6*M.y1 + 14*M.x2*M.y2 +      10*M.y3                      )
+    M.sub.c3 = Constraint(expr=32 ==          4*M.x1 +                8*M.y1                               + 15*M.x2*M.y4       )
+    M.sub.c4 = Constraint(expr=inequality(22, 3*M.x1 +                7*M.y1                + 16*M.x2*M.y3                ,  28))
 
     return M
 
