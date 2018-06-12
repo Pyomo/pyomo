9d19966c41d30eb5394181d9c5ebb65ad33e0925
Qi Chen <qchen@sandia.gov>
2018-06-12 08:54:01 -0600

    Passed wrong argument

--- pyomo/contrib/gdpopt/GDPopt.py
+++ pyomo/contrib/gdpopt/GDPopt.py
@@ -233,7 +233,7 @@ class GDPoptSolver(pyomo.common.plugin.Plugin):
             # Reformulate integer variables to binary
             reformulate_integer_variables(solve_data.working_model, config)
             # Fix unused variables
-            fix_unused_working_variables(solve_data.working_model, config)
+            fix_unused_working_variables(solve_data, config)
 
             # Save ordered lists of main modeling components, so that data can
             # be easily transferred between future model clones.
