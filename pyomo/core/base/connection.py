9f0da06b6b753a7456eb083934f4a3ef653f01e2
Grant Seastream <gseastr@sandia.gov>
2018-06-21 08:54:55 -0600

    Make attributes properties

--- pyomo/core/base/connection.py
+++ pyomo/core/base/connection.py
@@ -16,9 +16,11 @@ from pyomo.core.base.connector import (SimpleConnector, IndexedConnector,
 from pyomo.core.base.plugin import (register_component,
     IPyomoScriptModifyInstance, TransformationFactory)
 from pyomo.common.plugin import Plugin, implements
+from six import iteritems
 
 
 class _ConnectionData(_BlockData):
+
     def __init__(self, component, **kwds):
 
         super(_ConnectionData, self).__init__(component)
@@ -26,32 +28,64 @@ class _ConnectionData(_BlockData):
         # We have to be ok with not setting these attributes when initialized,
         # because IndexedComponent._setitem_when_not_present creates this with
         # no arguments except component. Thus we must rely on always using
-        # _set_args to set these attributes so that _validate_conns is called,
-        # especially if it is after instantiating the class.
+        # set_connection to set these attributes so that _validate_conns is
+        # called, especially if it is after instantiating the class.
         if len(kwds):
+            self.set_connection(**kwds)
+
+    @property
+    def source(self):
+        return self._source
+
+    @property
+    def destination(self):
+        return self._destination
+
+    @property
+    def connectors(self):
+        return self._connectors
+
+    @property
+    def directed(self):
+        return self._directed
+
+    def set_connection(self, **kwds):
+        if not hasattr(self, "_source"):
+            # if not yet defined, use None for default
             source = kwds.pop("source", None)
             destination = kwds.pop("destination", None)
             connectors = kwds.pop("connectors", None)
-            self._set_args(source, destination, connectors)
+        else:
+            # if this is resetting them, use previous setting as default so you
+            # can change only source and not have to pass destination again
+            source = kwds.pop("source", self._source)
+            destination = kwds.pop("destination", self._destination)
+            connectors = kwds.pop("connectors", self._connectors)
 
-    def _set_args(self, source=None, destination=None, connectors=None):
-        _setattr = object.__setattr__ # bypass add_component
-        _setattr(self, "source", source)
-        _setattr(self, "destination", destination)
-        _setattr(self, "connectors", connectors)
-        self._validate_conns()
+        if len(kwds):
+            raise ValueError(
+                "set_args passed unrecognized keyword arguments:\n\t" +
+                "\n\t".join("%s = %s" % (k,v) for k,v in iteritems(kwds)))
 
-    def _validate_conns(self):
+        self._validate_conns(source, destination, connectors)
+
+        # bypass add_component
+        object.__setattr__(self, "_source", source)
+        object.__setattr__(self, "_destination", destination)
+        self._connectors = connectors # lists do not go through add_component
+        self._directed = self.source is not None
+
+    def _validate_conns(self, source, destination, connectors):
         connector_types = set([SimpleConnector, _ConnectorData])
-        if self.connectors is not None:
-            if self.source is not None or self.destination is not None:
+        if connectors is not None:
+            if source is not None or destination is not None:
                 raise ValueError("Cannot specify 'source' or 'destination' "
                                  "when using 'connectors' argument.")
-            if (type(self.connectors) not in (list, tuple) or
-                len(self.connectors) != 2):
+            if (type(connectors) not in (list, tuple) or
+                len(connectors) != 2):
                 raise ValueError("Argument 'connectors' must be list or tuple "
                                  "containing exactly 2 Connectors.")
-            for c in self.connectors:
+            for c in connectors:
                 if type(c) not in connector_types:
                     if type(c) is IndexedConnector:
                         raise ValueError(
@@ -60,24 +94,23 @@ class _ConnectionData(_BlockData):
                     raise ValueError("Found object '%s' in 'connectors' not "
                                      "of type Connector." % str(c))
         else:
-            if self.source is None or self.destination is None:
+            if source is None or destination is None:
                 raise ValueError("Must specify both 'source' and "
                                  "'destination' for directed Connection.")
-            if type(self.source) not in connector_types:
-                if type(self.source) is IndexedConnector:
+            if type(source) not in connector_types:
+                if type(source) is IndexedConnector:
                     raise ValueError(
                         "Found IndexedConnector '%s' as source, must use "
-                        "single Connectors for Connection." % self.source)
+                        "single Connectors for Connection." % source)
                 raise ValueError("Source object '%s' not of type "
-                                 "Connector." % str(self.source))
-            if type(self.destination) not in connector_types:
-                if type(self.destination) is IndexedConnector:
+                                 "Connector." % str(source))
+            if type(destination) not in connector_types:
+                if type(destination) is IndexedConnector:
                     raise ValueError(
                         "Found IndexedConnector '%s' as destination, must use "
-                        "single Connectors for Connection." % self.destination)
+                        "single Connectors for Connection." % destination)
                 raise ValueError("Destination object '%s' not of type "
-                                 "Connector." % str(self.destination))
-        self.directed = self.source is not None
+                                 "Connector." % str(destination))
 
 
 class Connection(Block):
@@ -115,48 +148,73 @@ class SimpleConnection(_ConnectionData, Connection):
 
 
 class IndexedConnection(Connection):
+
     def __init__(self, *args, **kwds):
-        object.__setattr__(self, "isource", kwds.pop("source", None))
-        object.__setattr__(self, "idestination", kwds.pop("destination", None))
-        object.__setattr__(self, "iconnectors", kwds.pop("connectors", None))
-        self._validate_iconns()
+        source = kwds.pop("source", None)
+        destination = kwds.pop("destination", None)
+        connectors = kwds.pop("connectors", None)
+
+        self._validate_iconns(source, destination, connectors)
+
+        # these attributes need different names than _ConnectionData
+        # for the Block rule generating routine to copy them over correctly
+        object.__setattr__(self, "_isource", source)
+        object.__setattr__(self, "_idestination", destination)
+        self._iconnectors = connectors
+        self._idirected = self.source is not None
+
         kwds.setdefault("rule", self._indexed_connection_rule())
 
         super(IndexedConnection, self).__init__(*args, **kwds)
 
-    def _validate_iconns(self):
-        if self.iconnectors is not None:
-            if self.isource is not None or self.idestination is not None:
+    @property
+    def source(self):
+        return self._isource
+
+    @property
+    def destination(self):
+        return self._idestination
+
+    @property
+    def connectors(self):
+        return self._iconnectors
+
+    @property
+    def directed(self):
+        return self._idirected
+
+    def _validate_iconns(self, source, destination, connectors):
+        if connectors is not None:
+            if source is not None or destination is not None:
                 raise ValueError("Cannot specify 'source' or 'destination' "
                                  "when using 'connectors' argument.")
-            if (type(self.iconnectors) not in (list, tuple) or
-                len(self.iconnectors) != 2):
+            if (type(connectors) not in (list, tuple) or
+                len(connectors) != 2):
                 raise ValueError("Argument 'connectors' must be list or tuple "
                                  "containing exactly 2 IndexedConnectors.")
-            for c in self.iconnectors:
+            for c in connectors:
                 if type(c) is not IndexedConnector:
                     raise ValueError("Found object '%s' in 'connectors' not "
                                      "of type IndexedConnector." % str(c))
         else:
-            if self.isource is None or self.idestination is None:
+            if source is None or destination is None:
                 raise ValueError("Must specify both 'source' and "
                                  "'destination' for directed Connection.")
-            if type(self.isource) is not IndexedConnector:
+            if type(source) is not IndexedConnector:
                 raise ValueError("Source object '%s' not of type "
-                                 "IndexedConnector." % str(self.isource))
-            if type(self.idestination) is not IndexedConnector:
+                                 "IndexedConnector." % str(source))
+            if type(destination) is not IndexedConnector:
                 raise ValueError("Destination object '%s' not of type "
-                                 "IndexedConnector." % str(self.destination))
-        self.idirected = self.isource is not None
+                                 "IndexedConnector." % str(destination))
 
     def _indexed_connection_rule(self):
-        if self.idirected:
+        if self.directed:
             def generate_connections(m, *args):
-                return _ConnectionData(self, source=self.isource[args],
-                                       destination=self.idestination[args])
+                return _ConnectionData(self, source=self.source[args],
+                                       destination=self.destination[args])
         else:
             def generate_connections(m, *args):
-                connectors = [conn[args] for conn in self.iconnectors]
+                connectors = [conn[args] for conn in self.connectors]
                 return _ConnectionData(self, connectors=connectors)
         return generate_connections
 
