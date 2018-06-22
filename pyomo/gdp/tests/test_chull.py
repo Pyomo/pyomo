0e1c8c167e3244aa4bc0fd882d064cb41ced685d
John Siirola <jsiirola@users.noreply.github.com>
2018-06-22 14:20:38 -0600

    Updating baseline: 0 constants are now floats instead of an int.

--- pyomo/gdp/tests/test_chull.py
+++ pyomo/gdp/tests/test_chull.py
@@ -162,7 +162,8 @@ class TwoTermDisj(unittest.TestCase):
             "(1/(%s*d[0].indicator_var + %s)) + "
             "(_pyomo_gdp_chull_relaxation.relaxedDisjuncts[0].y*"
             "(1/(%s*d[0].indicator_var + %s)))**2) - "
-            "%s*(0 + 0**2)*(1 - d[0].indicator_var) - 14.0*d[0].indicator_var"
+            "%s*(0.0 + 0.0**2)*(1 - d[0].indicator_var) "
+            "- 14.0*d[0].indicator_var"
             % (EPS_1, EPS, EPS_1, EPS, EPS_1, EPS, EPS))
 
     def test_transformed_constraints_linear(self):
