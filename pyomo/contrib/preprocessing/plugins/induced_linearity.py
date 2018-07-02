ca91e59522a8a5d3f12d6a7e9d88cb6ceae67934
Qi Chen <qchen@sandia.gov>
2018-07-02 11:24:49 -0600

    Progress on induced linearity

--- pyomo/contrib/preprocessing/plugins/induced_linearity.py
+++ pyomo/contrib/preprocessing/plugins/induced_linearity.py
@@ -9,6 +9,7 @@ reformulations for some nonlinear discrete design optimization problems.
 from __future__ import division
 
 import textwrap
+from math import fabs
 
 from pyomo.core.base import Block, Constraint, VarList, Objective
 from pyomo.core.expr.current import ExpressionReplacementVisitor
@@ -36,10 +37,45 @@ class InducedLinearity(IsomorphicTransformation):
 
     def _apply_to(self, model):
         """Apply the transformation to the given model."""
+        constraint_bound_tolerance = 1E-6
+        effectively_discrete_vars = ComponentSet(
+            _effectively_discrete_vars(model, constraint_bound_tolerance))
+
+        # Collect find bilinear expressions that can be reformulated using
+        # knowledge of effectively discrete variables
         pass
-        # detect variables that are effectively discrete because they are the
-        # sum of discrete variables
-        pass
-        # Find if these variables participate in a bilinear term
+        # Categorize the constraint as Case 1 or Case 2
         pass
         # Reformulate the bilinear terms
+        pass
+
+
+def _bilinear_expressions(model):
+    pass
+
+
+def _effectively_discrete_vars(model, constraint_bound_tolerance):
+    """Yield variables that are effectively discrete because they are the
+    sum of discrete variables.
+    """
+    for constr in model.component_data_objects(
+            Constraint, descend_into=True):
+        if constr.lower is None or constr.upper is None:
+            continue  # skip inequality constraints
+        if fabs(value(constr.lower) - value(constr.upper)
+                ) > constraint_bound_tolerance:
+            continue  # not equality constriant. Skip.
+        if constr.body.polynomial_degree() not in (1, 0):
+            continue  # skip nonlinear expressions
+        repn = generate_standard_repn(constr.body)
+        if len(repn.linear_vars) < 2:
+            # TODO we should make sure that trivial equality relations are
+            # preprocessed before this, or we will end up reformulating
+            # expressions that we do not need to here.
+            continue
+        non_discrete_vars = list(v for v in repn.linear_vars
+                                 if v.is_continuous())
+        if len(non_discrete_vars) == 1:
+            # We know that this is an effectively discrete continuous
+            # variable. Add it to our identified variable list.
+            yield non_discrete_vars[0]
