622057d2bc127ba6554c098241d5db90972780f9
William Hart <wehart@sandia.gov>
2018-07-04 14:06:23 -0600

    Adding small17 example
    Has an indexed constaint that is sparsely populated.

---
+++ pyomo/repn/tests/ampl/small17_testCase.py
@@ -0,0 +1,39 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and 
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain 
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+#
+# Author:  Gabe Hackebeil
+# Purpose: For regression testing to ensure that the Pyomo
+#          NL writer properly reports the values corresponding
+#          to the nl file header line with the label
+#          '# nonlinear vars in constraints, objectives, both'
+#
+
+from pyomo.environ import *
+
+model = ConcreteModel()
+
+model.A = Set(initialize=[2, '1', 0], within=Any)
+def x_bounds(model, i):
+    if i == 2:
+        return (2, None)
+    if i == 0:
+        return (0, None)
+    return (1, None)
+model.x = Var(model.A, initialize=1.0, bounds=x_bounds)
+
+model.OBJ = Objective(expr=model.x[2]+model.x['1'])
+
+def c_rule(model, i):
+    if i == 2:
+        return model.x[2] + model.x[0] == 1
+    if i == '1':
+        return model.x['1'] + model.x[0] == 1
+    return Constraint.Skip
+model.c = Constraint(model.A, rule=c_rule)
