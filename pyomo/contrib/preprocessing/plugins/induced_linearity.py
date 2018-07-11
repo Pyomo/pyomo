aa09299a54f2bcfc13ebeaf9a5c2aa8bc712fe2d
Qi Chen <qchen@sandia.gov>
2018-07-11 12:51:48 -0600

    Code to detect effectively discrete variables

--- pyomo/contrib/preprocessing/plugins/induced_linearity.py
+++ pyomo/contrib/preprocessing/plugins/induced_linearity.py
@@ -41,20 +41,58 @@ class InducedLinearity(IsomorphicTransformation):
         constraint_bound_tolerance = 1E-6
         effectively_discrete_vars = ComponentSet(
             _effectively_discrete_vars(model, constraint_bound_tolerance))
-        # TODO will need to go through this for each disjunct
+        # TODO will need to go through this for each disjunct, since it does
+        # not (should not) descend into Disjuncts.
 
         # Collect find bilinear expressions that can be reformulated using
         # knowledge of effectively discrete variables
         bilinear_map = _bilinear_expressions(model)
-        for v, pairs in bilinear_map.items():
-            for v2, constrs in pairs.items():
-                print('%s --> %s --> %s' % (
-                    v.name, v2.name, [c.name for c in constrs]))
-        pass
-        # Categorize the constraint as Case 1 or Case 2
-        pass
+
+        # Relevant constraints are those with bilinear terms that involve
+        # effectively_discrete_vars
+        processed_pairs = ComponentSet()
+        for v1, discrete_constr in effectively_discrete_vars:
+            v1_pairs = bilinear_map.get(v1, ())
+            for v2, bilinear_constrs in v1_pairs.items():
+                if (v1, v2) in processed_pairs:
+                    continue
+                _process_bilinear_constraints(
+                    v1, v2, discrete_constr, bilinear_constrs)
+                processed_pairs.add((v2, v1))
+                processed_pairs.add((v1, v2))  # TODO is this necessary?
+
         # Reformulate the bilinear terms
         pass
+
+
+def _process_bilinear_constraints(v1, v2, discrete_constr, bilinear_constrs):
+    # Categorize as case 1 or case 2
+    for bilinear_constr in bilinear_constrs:
+        # repn = generate_standard_repn(bilinear_constr.body)
+
+        # Case 1: no other variables besides bilinear term in constraint. v1
+        # (effectively discrete variable) is positive.
+        # if (len(repn.quadratic_vars) == 1 and len(repn.linear_vars) == 0
+        #         and repn.nonlinear_expr is None):
+        #     _reformulate_case_1(v1, v2, discrete_constr, bilinear_constr)
+
+        # NOTE: Case 1 is left unimplemented for now, because it involves some
+        # messier logic with respect to how the transformation needs to happen.
+
+        # Case 2: this is everything else, but do we want to have a special
+        # case if there are nonlinear expressions involved with the constraint?
+        pass
+        if True:
+            _reformulate_case_2(v1, v2, discrete_constr, bilinear_constr)
+    pass
+
+
+def _reformulate_case_1(v1, v2, discrete_constr, bilinear_constr):
+    raise NotImplementedError()
+
+
+def _reformulate_case_2(v1, v2, discrete_constr, bilinear_constr):
+    pass
 
 
 def _bilinear_expressions(model):
@@ -89,9 +127,57 @@ def _bilinear_expressions(model):
     return bilinear_map
 
 
+def _detect_effectively_discrete_vars(block, equality_tolerance):
+    """Detect effectively discrete variables and their set of valid values.
+
+    These continuous variables are the sum of discrete variables.
+
+    """
+    # Map of effectively_discrete var --> set(possible values)
+    effectively_discrete = ComponentMap()
+
+    # Find SOS sets
+    for constr in block.component_data_objects(Constraint, active=True):
+        pass
+
+    for constr in block.component_data_objects(Constraint, active=True):
+        if constr.lower is None or constr.upper is None:
+            continue  # skip inequality constraints
+        if fabs(value(constr.lower) - value(constr.upper)
+                ) > equality_tolerance:
+            continue  # not equality constriant. Skip.
+        if constr.body.polynomial_degree() not in (1, 0):
+            continue  # skip nonlinear expressions
+        repn = generate_standard_repn(constr.body)
+        constant_term = NoneToZero(repn.constant) - NoneToZero(constr.lower)
+        if len(repn.linear_vars) < 2:
+            # TODO should this be < 2 or < 1?
+            # TODO we should make sure that trivial equality relations are
+            # preprocessed before this, or we will end up reformulating
+            # expressions that we do not need to here.
+            continue
+        non_discrete_vars = list(v for v in repn.linear_vars
+                                 if v.is_continuous())
+        discrete_vars = list(v for v in repn.linear_vars
+                             if v.is_binary() or v.is_integer())
+        if len(non_discrete_vars) == 1:
+            # We know that this is an effectively discrete continuous
+            # variable. Add it to our identified variable list.
+            yield non_discrete_vars[0], constr
+        # TODO we should eventually also look at cases where all other
+        # non_discrete_vars are effectively_discrete_vars
+
+
+def NoneToZero(val):
+    return 0 if val is None else val
+
+
 def _effectively_discrete_vars(block, constraint_bound_tolerance):
-    """Yield variables that are effectively discrete because they are the
-    sum of discrete variables.
+    """Yield variables that are effectively discrete, with relevant constraint.
+
+    These continuous variables are the sum of discrete variables. The
+    constraint is the one which links the variables together.
+
     """
     for constr in block.component_data_objects(
             Constraint, active=True, descend_into=True):
@@ -113,6 +199,6 @@ def _effectively_discrete_vars(block, constraint_bound_tolerance):
         if len(non_discrete_vars) == 1:
             # We know that this is an effectively discrete continuous
             # variable. Add it to our identified variable list.
-            yield non_discrete_vars[0]
+            yield non_discrete_vars[0], constr
         # TODO we should eventually also look at cases where all other
         # non_discrete_vars are effectively_discrete_vars
