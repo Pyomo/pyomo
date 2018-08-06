828900e10249829d051a973b4ab4f736fe57f16d
John Siirola <(none)>
2018-08-06 14:51:34 -0600

    Adding a trap for mal-formed proxy URLs

--- pyomo/neos/kestrel.py
+++ pyomo/neos/kestrel.py
@@ -45,6 +45,9 @@ if sys.version_info[0] < 3:
     class ProxiedTransport(xmlrpclib.Transport):
         def set_proxy(self, proxy):
             self.proxy = urlparse(proxy)
+            if not self.proxy.hostname:
+                # User omitted scheme from the proxy; assume http
+                self.proxy = urlparse('http://'+proxy)
 
         def make_connection(self, host):
             target = urlparse(host)
@@ -79,6 +82,9 @@ else: # Python 3.x
     class ProxiedTransport(xmlrpclib.Transport):
         def set_proxy(self, host):
             self.proxy = urlparse(host)
+            if not self.proxy.hostname:
+                # User omitted scheme from the proxy; assume http
+                self.proxy = urlparse('http://'+proxy)
 
         def make_connection(self, host):
             scheme = urlparse(host).scheme
