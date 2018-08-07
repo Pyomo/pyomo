82d6174fd04d9fccaf5a9dec74571287f72185f1
Grant Seastream <gseastr@sandia.gov>
2018-08-07 10:45:01 -0600

    Move pass_single_value next to pass_value

--- pyomo/network/decomposition.py
+++ pyomo/network/decomposition.py
@@ -381,6 +381,39 @@ class SequentialDecomposition(object):
             fixed_inputs[dest_unit].add(var)
             var.fix(val)
 
+    def pass_single_value(self, port, name, member, val, fixed):
+        """
+        Fix the value of the port member and add it to the fixed set.
+        If the member is an expression, appropriately fix the value of
+        its free variable. Error if the member is already fixed but
+        different from val, or if the member has more than one free
+        variable."
+        """
+        eq_tol = self.options["almost_equal_tol"]
+        if member.is_fixed():
+            if abs(value(member) - val) > eq_tol:
+                raise RuntimeError(
+                    "Member '%s' of port '%s' is already fixed but has a "
+                    "different value (by > %s) than what is being passed to it"
+                    % (name, port.name, eq_tol))
+        elif member.is_expression_type():
+            repn = generate_standard_repn(member - val)
+            if repn.is_linear() and len(repn.linear_vars) == 1:
+                # fix the value of the single variable
+                fval = (0 - repn.constant) / repn.linear_coefs[0]
+                var = repn.linear_vars[0]
+                fixed.add(var)
+                var.fix(fval)
+            else:
+                raise RuntimeError(
+                    "Member '%s' of port '%s' had more than "
+                    "one free variable when trying to pass a value "
+                    "to it. Please fix more variables before passing "
+                    "to this port." % (name, port.name))
+        else:
+            fixed.add(member)
+            member.fix(val)
+
     def load_guesses(self, guesses, port, fixed):
         srcs = port.sources()
         for name, mem in iteritems(port.vars):
@@ -506,39 +539,6 @@ class SequentialDecomposition(object):
         assert all(evar.is_fixed() for evar in evars)
         total = sum(value(evar) for evar in evars)
         self.pass_single_value(port, name, obj, total, fixed)
-
-    def pass_single_value(self, port, name, member, val, fixed):
-        """
-        Fix the value of the port member and add it to the fixed set.
-        If the member is an expression, appropriately fix the value of
-        its free variable. Error if the member is already fixed but
-        different from val, or if the member has more than one free
-        variable."
-        """
-        eq_tol = self.options["almost_equal_tol"]
-        if member.is_fixed():
-            if abs(value(member) - val) > eq_tol:
-                raise RuntimeError(
-                    "Member '%s' of port '%s' is already fixed but has a "
-                    "different value (by > %s) than what is being passed to it"
-                    % (name, port.name, eq_tol))
-        elif member.is_expression_type():
-            repn = generate_standard_repn(member - val)
-            if repn.is_linear() and len(repn.linear_vars) == 1:
-                # fix the value of the single variable
-                fval = (0 - repn.constant) / repn.linear_coefs[0]
-                var = repn.linear_vars[0]
-                fixed.add(var)
-                var.fix(fval)
-            else:
-                raise RuntimeError(
-                    "Member '%s' of port '%s' had more than "
-                    "one free variable when trying to pass a value "
-                    "to it. Please fix more variables before passing "
-                    "to this port." % (name, port.name))
-        else:
-            fixed.add(member)
-            member.fix(val)
 
     def source_dest_peer(self, arc, name, index=None):
         """
