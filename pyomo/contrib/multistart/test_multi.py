7511e494f71a7c82d8a966e4bcd1033ec779a526
Qi Chen <qchen@sandia.gov>
2018-08-08 14:24:32 -0600

    Further cleanup, especially PEP8-compliance

--- pyomo/contrib/multistart/test_multi.py
+++ pyomo/contrib/multistart/test_multi.py
@@ -1,96 +1,93 @@
 import pyutilib.th as unittest
 from pyomo.environ import *
+from six.moves import range
 
 
-__author__ = "Sunjeev Kale <https://github.com/sjkale>"
-
-
-#due to stochastic nature of the random restarts, these tests just demonstrate,
-#that for a small sample, the test will not do worse than the standard solver.
-#this is non-exhaustive due to the randomness
+# due to stochastic nature of the random restarts, these tests just demonstrate,
+# that for a small sample, the test will not do worse than the standard solver.
+# this is non-exhaustive due to the randomness
 class MultistartTests(unittest.TestCase):
 
-    #test standard random restarts
+    # test standard random restarts
     def test_as_good_with_iteration_rand(self):
-        #initialize model with data
+        # initialize model with data
         m = build_model()
 
-        #create ipopt solver
+        # create ipopt solver
         optsolver = SolverFactory('ipopt')
-        optsolver.solve(m);
-        for i in xrange(20):
-
+        optsolver.solve(m)
+        for i in range(20):
 
             m2 = build_model()
             optsolver = SolverFactory('multistart')
-            optsolver.solve(m2,iterations = 10)
+            optsolver.solve(m2, iterations=10)
 
             self.assertTrue(value(m2.obj) >= value(m.obj))
             del m2
 
-    #test that other strategies do no worse
+    # test that other strategies do no worse
     def test_as_good_with_iteration_other_strategies(self):
-        #initialize model with data
+        # initialize model with data
         m = build_model()
 
-        #create ipopt solver
+        # create ipopt solver
         optsolver = SolverFactory('ipopt')
-        optsolver.solve(m);
-        for i in xrange(10):
-
-
-            m2 = build_model()
-            optsolver = SolverFactory('multistart')
-            optsolver.solve(m2,strategy = 'rand_distributed')
-
-            self.assertTrue(value(m2.obj) >= value(m.obj))
-            del m2
-        for i in xrange(10):
-
+        optsolver.solve(m)
+        for i in range(10):
 
             m2 = build_model()
             optsolver = SolverFactory('multistart')
-            optsolver.solve(m2,strategy = 'midpoint_guess_and_bound')
+            optsolver.solve(m2, strategy='rand_distributed')
 
             self.assertTrue(value(m2.obj) >= value(m.obj))
             del m2
-        for i in xrange(10):
-
+        for i in range(10):
 
             m2 = build_model()
             optsolver = SolverFactory('multistart')
-            optsolver.solve(m2,strategy = 'rand_guess_and_bound')
+            optsolver.solve(m2, strategy='midpoint_guess_and_bound')
 
             self.assertTrue(value(m2.obj) >= value(m.obj))
             del m2
-    #test that the high confidence stopping rule with very lenient parameters
-    #does no worse.
+        for i in range(10):
+
+            m2 = build_model()
+            optsolver = SolverFactory('multistart')
+            optsolver.solve(m2, strategy='rand_guess_and_bound')
+
+            self.assertTrue(value(m2.obj) >= value(m.obj))
+            del m2
+    # test that the high confidence stopping rule with very lenient parameters
+    # does no worse.
+
     def test_as_good_with_HCS_rule(self):
-        #initialize model with data
+        # initialize model with data
         m = build_model()
 
-        #create ipopt solver
+        # create ipopt solver
         optsolver = SolverFactory('ipopt')
-        optsolver.solve(m);
-        for i in xrange(5):
-
+        optsolver.solve(m)
+        for i in range(5):
 
             m2 = build_model()
             optsolver = SolverFactory('multistart')
-            optsolver.solve(m2,iterations = -1,HCS_param=(.99,.99))
+            optsolver.solve(m2, iterations=-1, HCS_param=(.99, .99))
 
             self.assertTrue(value(m2.obj) >= value(m.obj))
             del m2
 
-#simple non-convex model with many multiple minima
+# simple non-convex model with many multiple minima
+
+
 def build_model():
     model = ConcreteModel()
-    model.x1 = Var(initialize = 1,bounds=(0,100))
-
+    model.x1 = Var(initialize=1, bounds=(0, 100))
 
     def obj_rule(amodel):
         return model.x1 * sin(model.x1)
-    model.obj = Objective(rule=obj_rule,sense=maximize)
+    model.obj = Objective(rule=obj_rule, sense=maximize)
     return model
+
+
 if __name__ == '__main__':
     unittest.main()
