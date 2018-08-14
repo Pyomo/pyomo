39d225c98d5b5b0baae9d1e4f9099498e970e9f1
John Siirola <jsiirola@users.noreply.github.com>
2018-08-14 00:57:30 -0600

    Updating tests

--- pyomo/core/tests/unit/test_indexed_slice.py
+++ pyomo/core/tests/unit/test_indexed_slice.py
@@ -346,6 +346,7 @@ class TestComponentReferences(unittest.TestCase):
         @m.Block([1,2], [4,5])
         def b(b,i,j):
             b.x = Var([7,8],[10,11], initialize=0)
+            b.y = Var()
 
     def test_simple_lookup(self):
         m = self.m
@@ -380,7 +381,22 @@ class TestComponentReferences(unittest.TestCase):
         self.assertEqual( m.b[1,4].x[8,10].value, 20 )
         self.assertEqual( sum(x.value for x in itervalues(rd)), 20 )
 
-    def test_simple_attribute_assignment(self):
+    def test_single_attribute_assignment(self):
+        m = self.m
+
+        rd = _ReferenceDict(m.b[1,5].x[:,:])
+        self.assertEqual( sum(x.value for x in itervalues(rd)), 0 )
+        rd[7,10].value = 10
+        self.assertEqual( m.b[1,5].x[7,10].value, 10 )
+        self.assertEqual( sum(x.value for x in itervalues(rd)), 10 )
+
+        rd = _ReferenceDict(m.b[1,4].x[8,:])
+        self.assertEqual( sum(x.value for x in itervalues(rd)), 0 )
+        rd[10].value = 20
+        self.assertEqual( m.b[1,4].x[8,10].value, 20 )
+        self.assertEqual( sum(x.value for x in itervalues(rd)), 20 )
+
+    def test_nested_attribute_assignment(self):
         m = self.m
 
         rd = _ReferenceDict(m.b[:,:].x[:,:])
@@ -403,7 +419,7 @@ class TestComponentReferences(unittest.TestCase):
         self.assertTrue((7,10) in rd)
         del rd[7,10]
         self.assertFalse((7,10) in rd)
-        self.assertEqual(len(list(x.value for x in itervalues(rd))), 2*2*2*2-1)
+        self.assertEqual(len(list(x.value for x in itervalues(rd))), 3)
 
         rd = _ReferenceDict(m.b[1,4].x[8,:])
         self.assertEqual(len(list(x.value for x in itervalues(rd))), 2)
@@ -411,6 +427,18 @@ class TestComponentReferences(unittest.TestCase):
         del rd[10]
         self.assertFalse(10 in rd)
         self.assertEqual(len(list(x.value for x in itervalues(rd))), 2-1)
+
+        with self.assertRaisesRegexp(
+                KeyError,
+                r"'\(8, 10\)' is not valid for indexed component 'b\[1,4\].x'"):
+            del rd[10]
+
+        rd = _ReferenceDict(m.b[1,:].x[8,0])
+        with self.assertRaisesRegexp(
+                KeyError,
+                r"'\(8, 0\)' is not valid for indexed component 'b\[1,4\].x'"):
+            del rd[4]
+
 
     def test_nested_deletion(self):
         m = self.m
@@ -428,6 +456,34 @@ class TestComponentReferences(unittest.TestCase):
         del rd[1,10]
         self.assertFalse((1,10) in rd)
         self.assertEqual(len(list(x.value for x in itervalues(rd))), 2*2-1)
+
+    def test_attribute_deletion(self):
+        m = self.m
+
+        rd = _ReferenceDict(m.b[:,:].y)
+        rd._slice.attribute_errors_generate_exceptions = False
+        self.assertEqual(len(list(x.value for x in itervalues(rd))), 2*2)
+        self.assertTrue((1,5) in rd)
+        self.assertTrue( hasattr(m.b[1,5], 'y') )
+        self.assertTrue( hasattr(m.b[2,5], 'y') )
+        del rd[1,5]
+        self.assertFalse((1,5) in rd)
+        self.assertFalse( hasattr(m.b[1,5], 'y') )
+        self.assertTrue( hasattr(m.b[2,5], 'y') )
+        self.assertEqual(len(list(x.value for x in itervalues(rd))), 3)
+
+        rd = _ReferenceDict(m.b[2,:].y)
+        rd._slice.attribute_errors_generate_exceptions = False
+        self.assertEqual(len(list(x.value for x in itervalues(rd))), 2)
+        self.assertTrue(5 in rd)
+        self.assertTrue( hasattr(m.b[2,4], 'y') )
+        self.assertTrue( hasattr(m.b[2,5], 'y') )
+        del rd[5]
+        self.assertFalse(5 in rd)
+        self.assertTrue( hasattr(m.b[2,4], 'y') )
+        self.assertFalse( hasattr(m.b[2,5], 'y') )
+        self.assertEqual(len(list(x.value for x in itervalues(rd))), 2-1)
+
 
 
 if __name__ == "__main__":
