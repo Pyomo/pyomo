f8ad5d8aec1964edbfc06d495deaac5311af5a10
John Siirola <jsiirola@users.noreply.github.com>
2018-08-15 15:10:53 -0600

    Improving _ReferenceDict test coverage

--- pyomo/core/tests/unit/test_reference.py
+++ pyomo/core/tests/unit/test_reference.py
@@ -28,16 +28,62 @@ class TestReferenceDict(unittest.TestCase):
         @m.Block([1,2], [4,5])
         def b(b,i,j):
             b.x = Var([7,8],[10,11], initialize=0)
-            b.y = Var()
+            b.y = Var([7,8], initialize=0)
+            b.z = Var()
+
+        @m.Block([1,2])
+        def c(b,i):
+            b.x = Var([7,8],[10,11], initialize=0)
+            b.y = Var([7,8], initialize=0)
+            b.z = Var()
+
+    def _lookupTester(self, _slice, key, ans):
+        rd = _ReferenceDict(_slice)
+        self.assertIn(key, rd)
+        self.assertIs(rd[key], ans)
+
+        if len(key) == 1:
+            self.assertIn(key[0], rd)
+            self.assertIs(rd[key[0]], ans)
+
+        self.assertNotIn(None, rd)
+        with self.assertRaises(KeyError):
+            rd[None]
+
+        for i in range(len(key)):
+            _ = tuple([0]*i)
+            self.assertNotIn(_, rd)
+            with self.assertRaises(KeyError):
+                rd[_]
 
     def test_simple_lookup(self):
         m = self.m
 
-        rd = _ReferenceDict(m.b[:,:].x[:,:])
-        self.assertIs(rd[1,5,7,10], m.b[1,5].x[7,10])
+        self._lookupTester(m.b[:,:].x[:,:],  (1,5,7,10), m.b[1,5].x[7,10])
+        self._lookupTester(m.b[:,4].x[8,:],  (1,10),     m.b[1,4].x[8,10])
+        self._lookupTester(m.b[:,4].x[8,10], (1,),       m.b[1,4].x[8,10])
+        self._lookupTester(m.b[1,4].x[8,:],  (10,),      m.b[1,4].x[8,10])
 
-        rd = _ReferenceDict(m.b[:,4].x[8,:])
-        self.assertIs(rd[1,10], m.b[1,4].x[8,10])
+        self._lookupTester(m.b[:,:].y[:], (1,5,7), m.b[1,5].y[7])
+        self._lookupTester(m.b[:,4].y[:], (1,7),   m.b[1,4].y[7])
+        self._lookupTester(m.b[:,4].y[8], (1,),    m.b[1,4].y[8])
+
+        self._lookupTester(m.b[:,:].z, (1,5), m.b[1,5].z)
+        self._lookupTester(m.b[:,4].z, (1,),  m.b[1,4].z)
+
+
+        self._lookupTester(m.c[:].x[:,:],  (1,7,10), m.c[1].x[7,10])
+        self._lookupTester(m.c[:].x[8,:],  (1,10),   m.c[1].x[8,10])
+        self._lookupTester(m.c[:].x[8,10], (1,),     m.c[1].x[8,10])
+        self._lookupTester(m.c[1].x[:,:],  (8,10),   m.c[1].x[8,10])
+        self._lookupTester(m.c[1].x[8,:],  (10,),    m.c[1].x[8,10])
+
+        self._lookupTester(m.c[:].y[:], (1,7), m.c[1].y[7])
+        self._lookupTester(m.c[:].y[8], (1,),  m.c[1].y[8])
+        self._lookupTester(m.c[1].y[:], (8,),  m.c[1].y[8])
+
+        self._lookupTester(m.c[:].z, (1,), m.c[1].z)
+
 
     def test_len(self):
         m = self.m
@@ -48,7 +94,7 @@ class TestReferenceDict(unittest.TestCase):
         rd = _ReferenceDict(m.b[:,4].x[8,:])
         self.assertEqual(len(rd), 2*2)
 
-    def test_simple_assignment(self):
+    def test_nested_assignment(self):
         m = self.m
 
         rd = _ReferenceDict(m.b[:,:].x[:,:])
@@ -142,28 +188,28 @@ class TestReferenceDict(unittest.TestCase):
     def test_attribute_deletion(self):
         m = self.m
 
-        rd = _ReferenceDict(m.b[:,:].y)
+        rd = _ReferenceDict(m.b[:,:].z)
         rd._slice.attribute_errors_generate_exceptions = False
         self.assertEqual(len(list(x.value for x in itervalues(rd))), 2*2)
         self.assertTrue((1,5) in rd)
-        self.assertTrue( hasattr(m.b[1,5], 'y') )
-        self.assertTrue( hasattr(m.b[2,5], 'y') )
+        self.assertTrue( hasattr(m.b[1,5], 'z') )
+        self.assertTrue( hasattr(m.b[2,5], 'z') )
         del rd[1,5]
         self.assertFalse((1,5) in rd)
-        self.assertFalse( hasattr(m.b[1,5], 'y') )
-        self.assertTrue( hasattr(m.b[2,5], 'y') )
+        self.assertFalse( hasattr(m.b[1,5], 'z') )
+        self.assertTrue( hasattr(m.b[2,5], 'z') )
         self.assertEqual(len(list(x.value for x in itervalues(rd))), 3)
 
-        rd = _ReferenceDict(m.b[2,:].y)
+        rd = _ReferenceDict(m.b[2,:].z)
         rd._slice.attribute_errors_generate_exceptions = False
         self.assertEqual(len(list(x.value for x in itervalues(rd))), 2)
         self.assertTrue(5 in rd)
-        self.assertTrue( hasattr(m.b[2,4], 'y') )
-        self.assertTrue( hasattr(m.b[2,5], 'y') )
+        self.assertTrue( hasattr(m.b[2,4], 'z') )
+        self.assertTrue( hasattr(m.b[2,5], 'z') )
         del rd[5]
         self.assertFalse(5 in rd)
-        self.assertTrue( hasattr(m.b[2,4], 'y') )
-        self.assertFalse( hasattr(m.b[2,5], 'y') )
+        self.assertTrue( hasattr(m.b[2,4], 'z') )
+        self.assertFalse( hasattr(m.b[2,5], 'z') )
         self.assertEqual(len(list(x.value for x in itervalues(rd))), 2-1)
 
 
