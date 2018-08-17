e8a979fc99f00250b91ec7333ec8ba4b2fd8f804
Dena Vigil <dmvigi@sandia.gov>
2018-08-17 16:56:05 -0600

    Cleaned up examples
    Renamed Example2 files:
    * Example2TRF.py tests fixed variables using trust region code
    * Example2Pyomo.py performs same calculation using Pyomo

--- pyomo/contrib/trustregion/examples/Example2Pyomo.py
+++ pyomo/contrib/trustregion/examples/Example2Pyomo.py
@@ -1,6 +1,7 @@
+# This example should run the same calculation as Example2TRF.py without calling the 
+# black box function and should calculate the same result.
      
 from pyomo.environ import *
-from pyomo.opt import SolverFactory, SolverStatus, TerminationCondition
 
 m = ConcreteModel()
 m.z = Var(range(3), domain=Reals, initialize=2.)
@@ -15,20 +16,10 @@ m.fix_val2 = Var(domain=PositiveReals,
                   initialize=.0033)
 m.fix_val2.fix()
 
-def blackbox(a,b,c):
-   return sin(a-b) + c
-bb = ExternalFunction(blackbox)
-
 m.obj = Objective(
    expr=(m.z[0]-1.0)**2 + (m.z[0]-m.z[1])**2 + (m.z[2]-1.0)**2 \
        + (m.x[0]-1.0)**4 + (m.x[1]-1.0)**6 # + m.bb(m.x[0],m.x[1])
 )
-m.c1 = Constraint(expr=m.x[0] * m.z[0]**2 + 2*bb(m.x[0],m.x[1],m.fix_val1) - bb(m.x[0],m.x[1],m.fix_val2) == 2*sqrt(2.0))
+m.c1 = Constraint(expr=m.x[0] * m.z[0]**2 + 2*(sin(m.x[0] - m.x[1]) + m.fix_val1) - (sin(m.x[0] - m.x[1]) + m.fix_val2) == 2*sqrt(2.0))
 m.c2 = Constraint(expr=m.z[2]**4 * m.z[1]**2 + m.z[1] + m.fix_val2 == 8+sqrt(2.0))
 
-m.pprint()
-
-optTRF = SolverFactory('trustregion')
-optTRF.solve(m, [bb])
-
-m.display()
--- pyomo/contrib/trustregion/examples/Example2TRF.py
+++ pyomo/contrib/trustregion/examples/Example2TRF.py
@@ -1,6 +1,9 @@
+# This example tests the return value for using regular and fixed variables
+# as input to the external function. The return vaule should match the 
+# value returned by Example2Pyomo.py.
      
 from pyomo.environ import *
-#from pyomo.opt import SolverFactory, SolverStatus, TerminationCondition
+from pyomo.opt import SolverFactory, SolverStatus, TerminationCondition
 
 m = ConcreteModel()
 m.z = Var(range(3), domain=Reals, initialize=2.)
@@ -15,16 +18,20 @@ m.fix_val2 = Var(domain=PositiveReals,
                   initialize=.0033)
 m.fix_val2.fix()
 
+def blackbox(a,b,c):
+   return sin(a-b) + c
+bb = ExternalFunction(blackbox)
+
 m.obj = Objective(
    expr=(m.z[0]-1.0)**2 + (m.z[0]-m.z[1])**2 + (m.z[2]-1.0)**2 \
        + (m.x[0]-1.0)**4 + (m.x[1]-1.0)**6 # + m.bb(m.x[0],m.x[1])
 )
-m.c1 = Constraint(expr=m.x[0] * m.z[0]**2 + 2*(sin(m.x[0] - m.x[1]) + m.fix_val1) - (sin(m.x[0] - m.x[1]) + m.fix_val2) == 2*sqrt(2.0))
+m.c1 = Constraint(expr=m.x[0] * m.z[0]**2 + 2*bb(m.x[0],m.x[1],m.fix_val1) - bb(m.x[0],m.x[1],m.fix_val2) == 2*sqrt(2.0))
 m.c2 = Constraint(expr=m.z[2]**4 * m.z[1]**2 + m.z[1] + m.fix_val2 == 8+sqrt(2.0))
 
-#m.pprint()
+m.pprint()
 
-#optTRF = SolverFactory('trustregion')
-#optTRF.solve(m, [bb])
+optTRF = SolverFactory('trustregion')
+optTRF.solve(m, [bb])
 
-#m.display()
+m.display()
