d0d5f149976b868f0352d71df1193b372ddf080e
Bethany Nicholson <blnicho@sandia.gov>
2018-08-27 13:20:38 -0600

    Adding pyodbc import check to fragile dataportal test

--- pyomo/core/tests/examples/test_tutorials.py
+++ pyomo/core/tests/examples/test_tutorials.py
@@ -37,6 +37,11 @@ try:
     _openpyxl=True
 except:
     _openpyxl=False
+try:
+    import pyodbc
+    _pyodbc=True
+except:
+    _pyodbc=False
 
 class PyomoTutorials(unittest.TestCase):
 
@@ -50,7 +55,7 @@ class PyomoTutorials(unittest.TestCase):
         pyutilib.misc.run_file(tutorial_dir+"data.py", logfile=currdir+"data.log", execdir=tutorial_dir)
         self.assertFileEqualsBaseline(currdir+"data.log", tutorial_dir+"data.out")
 
-    @unittest.skipIf(not (_win32com or _xlrd or _openpyxl), "Cannot real excel file.")
+    @unittest.skipIf(not (_win32com or _xlrd or _openpyxl or _pyodbc), "Cannot real excel file.")
     def test_excel(self):
         pyutilib.misc.run_file(tutorial_dir+"excel.py", logfile=currdir+"excel.log", execdir=tutorial_dir)
         self.assertFileEqualsBaseline(currdir+"excel.log", tutorial_dir+"excel.out")
