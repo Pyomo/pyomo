01ead99dd90dac964e949234565a58745209ea58
William Hart <whart222@gmail.com>
2018-08-29 22:01:19 -0600

    Adding new factory for executables
    Currently, Pyomo uses the pyutilib.services logic for
    executables.  This searches immediately when executables are registered.
    While this may be desired in some contexts, within Pyomo this is
    undesirable since executables are searched for when Pyomo is imported
    even if those executables are never used.
    
    This new factory delays the search for an executable until its
    path is requested.

---
+++ pyomo/common/executable.py
@@ -0,0 +1,62 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and 
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain 
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+
+__all__ = ['register_executable', 'registered_executable']
+
+import pyutilib.misc
+from pyomo.common.factory import CachedFactory
+
+ExecutableFactory = CachedFactory('executables')
+
+
+def register_executable(name, validate=None):
+    #
+    # Do not reregister an executable.
+    #
+    if name in ExecutableFactory:
+        return
+
+    @ExecutableFactory.register(name)
+    class TMP(object):
+        def __init__(self):
+            self.executable = None
+            self.exec_default = pyutilib.misc.search_file(
+                name,
+                implicitExt=pyutilib.misc.executable_extension,
+                executable=True,
+                validate=validate)
+
+        def get_path(self):
+            if self.executable:
+                return self.executable
+            return self.exec_default
+
+
+def registered_executable(name=None):
+    """
+    Test if an exectuable is registered.
+
+    If 'name' is None, then return a list of the names of all registered
+    executables that are enabled.
+
+    If either this executable is not registered, then
+    None is returned.  Otherwise, return the path to the executable.
+
+    NOTE: Since ExecutableFactory is cached, the search for the 
+    executable only happens the first time the user calls registered_executable.
+    """
+
+    if name is None:
+        return sorted(list(ExecutableFactory))
+    if name in ExecutableFactory:
+        return ExecutableFactory(name).get_path()
+    return None
+
