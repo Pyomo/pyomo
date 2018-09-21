d71b6809923a994c53aa353085d5aeb2f597ea29
Qi Chen <qichen@andrew.cmu.edu>
2018-09-20 20:00:41 -0400

    Add missing import

--- pyomo/gdp/plugins/gdp_var_mover.py
+++ pyomo/gdp/plugins/gdp_var_mover.py
@@ -18,7 +18,7 @@ import logging
 import textwrap
 from pyomo.common.plugin import alias
 from pyomo.core.base import Transformation, Block, Constraint
-from pyomo.gdp import Disjunct, GDP_Error
+from pyomo.gdp import Disjunct, GDP_Error, Disjunction
 from pyomo.core import TraversalStrategy, TransformationFactory
 from pyomo.common.deprecation import deprecated
 
