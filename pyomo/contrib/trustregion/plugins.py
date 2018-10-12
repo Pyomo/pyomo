309828f67e807266140bd88ecdf9e4b4b61b69f9
Dena Vigil <dmvigi@sandia.gov>
2018-10-12 16:45:56 -0600

    Set up parameters in Pyomo ConfigBlock

--- pyomo/contrib/trustregion/plugins.py
+++ pyomo/contrib/trustregion/plugins.py
@@ -6,9 +6,12 @@ from pyutilib.services import register_executable
 from pyomo.opt.base import SolverFactory, OptSolver
 from pyomo.solvers.plugins.solvers.ASL import ASL
 
+from pyutilib.misc.config import ConfigBlock, ConfigValue
+from pyomo.util.config import ( 
+    PositiveInt, PositiveFloat, NonNegativeFloat, In)
+
 from pyomo.contrib.trustregion.TRF import TRF
 from pyomo.contrib.trustregion.readgjh import readgjh
-import pyomo.contrib.trustregion.param as param
 
 logger = logging.getLogger('pyomo.contrib.trustregion')
 fh = logging.FileHandler('debug_vars.log')
@@ -35,12 +38,211 @@ class TrustRegionSolver(OptSolver):
     """
     #    + param.CONFIG.generte_yaml_template()
 
+    CONFIG = ConfigBlock('Trust Region')
+
+    CONFIG.declare('type', ConfigValue(
+        default = 'trustregion',
+        domain = str,
+        description = '',
+        doc = ''))
+
+    # Initialize trust radius
+    CONFIG.declare('trust radius', ConfigValue(
+        default = 1.0,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Initialize sample region
+    CONFIG.declare('sample region', ConfigValue(
+        default = True,
+        domain = bool,
+        description = '',
+        doc = ''))
+
+    # Initialize sample radius
+    # TODO do we need to keep the if statement?
+    if CONFIG.sample_region:
+        default_sample_radius = 0.1
+    else:
+        default_sample_radius = CONFIG.trust_radius / 2.0
+
+    CONFIG.declare('sample radius', ConfigValue(
+        default = default_sample_radius,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Initialize radius max
+    CONFIG.declare('radius max', ConfigValue(
+        default = 1000.0 * CONFIG.trust_radius,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Termination tolerances
+    CONFIG.declare('ep i', ConfigValue(
+        default = 1e-5,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('ep delta', ConfigValue(
+        default = 1e-5,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('ep chi', ConfigValue(
+        default = 1e-3,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('delta min', ConfigValue(
+        default = 1e-6,
+        domain = PositiveFloat,
+        description = 'delta min <= ep delta',
+        doc = ''))
+
+    CONFIG.declare('max it', ConfigValue(
+        default = 20,
+        domain = PositiveInt,
+        description = '',
+        doc = ''))
+
+    # Compatibility Check Parameters
+    CONFIG.declare('kappa delta', ConfigValue(
+        default = 0.8,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('kappa mu', ConfigValue(
+        default = 1.0,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('mu', ConfigValue(
+        default = 0.5,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('ep compatibility', ConfigValue(
+        default = CONFIG.ep_i,
+        domain = PositiveFloat,
+        description = 'Suggested value: ep compatibility == ep i',
+        doc = ''))
+
+    CONFIG.declare('compatibility penalty', ConfigValue(
+        default = 0.0,
+        domain = NonNegativeFloat,
+        description = '',
+        doc = ''))
+
+    # Criticality Check Parameters
+    CONFIG.declare('criticality check', ConfigValue(
+        default = 0.1,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Trust region update parameters
+    CONFIG.declare('gamma c', ConfigValue(
+        default = 0.5,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('gamma e', ConfigValue(
+        default = 2.5,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Switching Condition
+    CONFIG.declare('gamma s', ConfigValue(
+        default = 2.0,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('kappa theta', ConfigValue(
+        default = 0.1,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('theta min', ConfigValue(
+        default = 1e-4,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Filter
+    CONFIG.declare('gamma f', ConfigValue(
+        default = 0.01,
+        domain = PositiveFloat,
+        description = 'gamma_f and gamma_theta in (0,1) are fixed parameters',
+        doc = ''))
+
+    CONFIG.declare('gamma theta', ConfigValue(
+        default = 0.01,
+        domain = PositiveFloat,
+        description = 'gamma_f and gamma_theta in (0,1) are fixed parameters',
+        doc = ''))
+
+    CONFIG.declare('theta max', ConfigValue(
+        default = 50,
+        domain = PositiveInt,
+        description = '',
+        doc = ''))
+
+    # Ratio test parameters (for theta steps)
+    CONFIG.declare('eta1', ConfigValue(
+        default = 0.05,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    CONFIG.declare('eta2', ConfigValue(
+        default = 0.2,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Output level (replace with real printlevels!!!)
+    CONFIG.declare('print variables', ConfigValue(
+        default = False,
+        domain = bool,
+        description = 'TODO: remove??',
+        doc = ''))
+
+    # Sample Radius reset parameter
+    CONFIG.declare('sample radius adjust', ConfigValue(
+        default = 0.5,
+        domain = PositiveFloat,
+        description = '',
+        doc = ''))
+
+    # Default romtype
+    CONFIG.declare('reduced model type', ConfigValue(
+        default = 1,
+        domain = In([0,1]),
+        description = '0 = Linear, 1 = Quadratic',
+        doc = ''))
+
     def __init__(self, **kwds):
+        # set persistent config options
+        self.config = self.CONFIG(kwds)
+
         #
         # Call base class constructor
         #
-        kwds['type'] = 'trustregion'
-        OptSolver.__init__(self, **kwds)
+        OptSolver.__init__(self, self.config)
+
 
     def available(self, exception_flag=True):
         """Check if solver is available.
@@ -56,9 +258,22 @@ class TrustRegionSolver(OptSolver):
         return __version__
 
     def solve(self, model, eflist, **kwds):
-        assert not kwds
-        #config = param.CONFIG(kwds)
-        return TRF(model, eflist)#, config)
+        # set customized config parameters
+        config = self.config(kwds)
+
+        # first store all data we will need to change in original model as a tuple
+        # [0]=Var component, [1]=external function list, [2]=config block
+        model._tmp_trf_data = (list(model.component_data_objects(Var)), eflist, config)
+        # now clone the model
+        inst = model.clone()
+
+        # call TRF on cloned model
+        TRF(inst, inst._tmp_trf_data[1], inst._tmp_trf_data[2])
+        print('done with TRF')
+
+        # copy potentially changed variable values back to original model and return
+        for inst_var, orig_var in zip(inst._tmp_trf_data[0], model._tmp_trf_data[0]):
+            orig_var.set_value(value(inst_var))
 
 
 @SolverFactory.register('contrib.gjh', doc='Interface to the AMPL GJH "solver"')
