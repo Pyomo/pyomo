272d17fcd00ec0e964a9ccce0b9b91168920f222
Erin Acquesta <eacques@sisu.sandia.gov>
2018-11-28 18:27:30 -0700

    Updated sens.py with unit tests added

--- pyomo/contrib/sensitivity_toolbox/sens.py
+++ pyomo/contrib/sensitivity_toolbox/sens.py
@@ -69,27 +69,37 @@ def sipopt(instance,paramSubList,perturbList,cloneModel=True,
 
     #Verify User Inputs    
     if len(paramSubList)!=len(perturbList):
-        raise Exception("Length of paramSubList argument does not equal "
+        raise ValueError("Length of paramSubList argument does not equal "
                         "length of perturbList")
 
     for pp in paramSubList:
         if pp.type() is not Param:
-            raise Exception("paramSubList argument is expecting a list of Params")
+            raise ValueError("paramSubList argument is expecting a list of Params")
+
+    for pp in paramSubList:
+        if not pp._mutable:
+            raise ValueError("parameters within paramSubList must be mutable")  
+
         
     for pp in perturbList:
         if pp.type() is not Param:
-            raise Exception("perturbList argument is expecting a list of Params")
+            raise ValueError("perturbList argument is expecting a list of Params")
+    #Add model block to compartmentalize all sipopt data
+    b=Block()
+    block_name = unique_component_name(instance, '_sipopt_data')
+    instance.add_component(block_name, b)
 
     #Based on user input clone model or use orignal model for anlaysis
     if cloneModel:
+        b.tmp_lists = (paramSubList, perturbList)
         m = instance.clone()
+        instance.del_component(block_name)
+        b = getattr(m, block_name)
+        paramSubList, perturbList = b.tmp_lists
+        del b.tmp_lists
     else:
         m = instance
     
-    #Add model block to compartmentalize all sipopt data
-    b=Block()
-    m.add_component(unique_component_name(m,'_sipopt_data'),b)
-
     #Generate component maps for associating Variables to perturbations
     varSubList = []
     for parameter in paramSubList:
@@ -98,18 +108,21 @@ def sipopt(instance,paramSubList,perturbList,cloneModel=True,
         myVar = b.component(tempName)
         varSubList.append(myVar)
  
+    #Note: substitutions are not currently compatible with 
+    #      ComponentMap [ECSA 2018/11/23], this relates to Issue #755
     paramCompMap = ComponentMap(zip(paramSubList, varSubList))
-    variableSubMap = ComponentMap()
-   
+    variableSubMap = {}
+    #variableSubMap = ComponentMap()
     paramPerturbMap = ComponentMap(zip(paramSubList,perturbList))
-    perturbSubMap = ComponentMap()
+    perturbSubMap = {}
+    #perturbSubMap = ComponentMap()
    
     paramDataList = [] 
     for parameter in paramSubList:
         #Loop over each ParamData in the Param Component
-        for kk in parameter:
-            variableSubMap[parameter[kk]]=paramCompMap[parameter][kk]
-            perturbSubMap[parameter[kk]]=paramPerturbMap[parameter][kk]
+       for kk in parameter:
+            variableSubMap[id(parameter[kk])]=paramCompMap[parameter][kk]
+            perturbSubMap[id(parameter[kk])]=paramPerturbMap[parameter][kk]
             paramDataList.append(parameter[kk])
 
     #clone Objective, add to Block, and update any Expressions
@@ -133,41 +146,63 @@ def sipopt(instance,paramSubList,perturbList,cloneModel=True,
                     substitute=variableSubMap,
                     remove_named_expressions=True).dfs_postorder_stack(cc.expr))
         else:
-            if cc.lower==None or cc.upper==None:
+            try:
                 b.constList.add(expr=ExpresssionReplacementVisitor(
                     substitute=variableSubMap,
                     remove_named_expressions=True).dfs_postorder_stack(cc.expr))
-            else:
-                b.constList.add(expr=ExpressionReplacementVisitor(
-                      substitute=variableSubMap,
-                      remove_named_expressions=True).dfs_postorder_stack(
-                      cc.expr)
-                    <=ExpressionReplacementVisitor(
-                      substitute=variableSubMap,
-                      remove_named_expressions=True).dfs_postorder_stack(
-                      cc.expr)
-                    <=ExpressionReplacementVisitor(
-                      substitute=variableSubMap,
-                      remove_named_expressions=True).dfs_postorder_stack(
-                      cc.expr))
+            except:
+                # Params in either the upper or lower bounds of a ranged 
+                # inequaltiy will result in an invalid expression (you cannot 
+                # have variables in the bounds of a constraint).  If we hit that
+                # problem, we will break up the ranged inequality into separate
+                # constraints
+
+                # Note that the test for lower / upper == None is probably not
+                # necessary, as the only way we should get here (especially if
+                # we are more careful about the exception that we catch) is if
+                # this is a ranged inequality and we are attempting to insert a
+                # variable into either the lower or upper bound.
+                if cc.lower is not None:
+                    b.constList.add(expr=ExpressionReplacementVisitor(
+                        substitute=variableSubMap,
+                        remove_named_expressions=True).dfs_postorder_stack(
+                            cc.lower) <= ExpressionReplacementVisitor(
+                                substitute=variableSubMap,
+                                remove_named_expressions=
+                                  True).dfs_postorder_stack(cc.body)
+                                )
+                #if cc.lower is not None:
+                #    b.constList.add(expr=0<=ExpressionReplacementVisitor(
+                #        substitute=variableSubMap,
+                #        remove_named_expressions=True).dfs_postorder_stack(
+                #            cc.lower) - ExpressionReplacementVisitor(
+                #                substitute=variableSubMap,
+                #                remove_named_expressions=
+                #                  True).dfs_postorder_stack(cc.body)
+                #                )
+                if cc.upper is not None:
+                    b.constList.add(expr=ExpressionReplacementVisitor(
+                        substitute=variableSubMap,
+                        remove_named_expressions=True).dfs_postorder_stack(
+                            cc.upper) >= ExpressionReplacementVisitor(
+                                substitute=variableSubMap,
+                                remove_named_expressions=
+                                  True).dfs_postorder_stack(cc.body)
+                                )
         cc.deactivate()
 
     #paramData to varData constraint list
     b.paramConst = ConstraintList()
     for ii in paramDataList:
-        jj=variableSubMap[ii]
+        jj=variableSubMap[id(ii)]
         b.paramConst.add(ii==jj)
+
     
     #Create the ipopt_sens (aka sIPOPT) solver plugin using the ASL interface
-    solver = 'ipopt_sens'
-    solver_io = 'nl'
-    opt = SolverFactory(solver, solver_io=solver_io)
+    opt = SolverFactory('ipopt_sens', solver_io='nl')
 
-    if opt is None:
-        print("")
-        print("ERROR: Unable to create solver plugin for 'ipopt_sens' ")
-        print("")
-        exit(1)
+    if not opt.available(False):
+        raise ImportError('ipopt_sens is not available')
     
     #Declare Suffixes
     m.sens_state_0 = Suffix(direction=Suffix.EXPORT)
@@ -182,14 +217,18 @@ def sipopt(instance,paramSubList,perturbList,cloneModel=True,
     #set sIPOPT data
     opt.options['run_sens'] = 'yes'
     
+    # for reasons that are not entirely clear, 
+    #     ipopt_sens requires the indices to start at 1
     kk=1
     for ii in paramDataList:
-        m.sens_state_0[variableSubMap[ii]] = kk
-        m.sens_state_1[variableSubMap[ii]] = kk
-        m.sens_state_value_1[variableSubMap[ii]] = value(perturbSubMap[ii])
+        m.sens_state_0[variableSubMap[id(ii)]] = kk
+        m.sens_state_1[variableSubMap[id(ii)]] = kk
+        m.sens_state_value_1[variableSubMap[id(ii)]] = \
+                                                   value(perturbSubMap[id(ii)])
         m.sens_init_constr[b.paramConst[kk]] = kk
         kk += 1    
-
+    
+    
     #Send the model to the ipopt_sens and collect the solution
     results = opt.solve(m, keepfiles=keepfiles, tee=streamSoln)
 
