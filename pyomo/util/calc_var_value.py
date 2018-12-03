c88ea854220b77a5341d79de98d1085ed88fde16
Carl Laird <carldlaird@users.noreply.github.com>
2018-12-03 16:59:08 -0600

    added linesearch to calc_var_value

--- pyomo/util/calc_var_value.py
+++ pyomo/util/calc_var_value.py
@@ -12,7 +12,7 @@ from pyomo.core.expr.numvalue import native_numeric_types, value
 from pyomo.core.base.symbolic import differentiate
 
 def calculate_variable_from_constraint(variable, constraint,
-                                       eps=1e-8, iterlim=1000):
+                                       eps=1e-8, iterlim=1000, linesearch=True):
     """Calculate the variable value given a specified equality constraint
 
     This function calculates the value of the specified variable
@@ -35,6 +35,9 @@ def calculate_variable_from_constraint(variable, constraint,
         The maximum number of iterations if this method has to fall back
         on using Newton's method.  Raises RuntimeError on iteration
         limit [default=1000]
+    linesearch: `bool`
+        Decides whether or not to use the linesearch (recommended).
+        [default=True]
 
     Returns:
     --------
@@ -51,16 +54,27 @@ def calculate_variable_from_constraint(variable, constraint,
     if variable.value is None:
         if variable.lb is None:
             if variable.ub is None:
+                # no variable values, and no lower or upper bound - set initial value to 0.0
                 variable.set_value(0)
             else:
+                # no variable value or lower bound - set to 0 or upper bound whichever is lower
                 variable.set_value(min(0, variable.ub))
         elif variable.ub is None:
+            # no variable value or upper bound - set to 0 or lower bound, whichever is higher
             variable.set_value(max(0, variable.lb))
         else:
+            # we have upper and lower bounds
             if variable.lb <= 0 and variable.ub >= 0:
+                # set the initial value to 0 if bounds bracket 0
                 variable.set_value(0)
             else:
+                # set the initial value to the midpoint of the bounds
                 variable.set_value((variable.lb+variable.ub)/2.0)
+
+    # store the initial value to use later if necessary
+    orig_initial_value = variable.value
+
+    # solve the common case where variable is linear with coefficient of 1.0
     x1 = value(variable)
     residual_1 = value(constraint.body)
 
@@ -82,10 +96,16 @@ def calculate_variable_from_constraint(variable, constraint,
             return
 
     # Variable appears nonlinearly; solve using Newton's method
+    variable.set_value(orig_initial_value) # restore initial value
     expr = constraint.body - constraint.upper
     expr_deriv = differentiate(expr, wrt=variable)
+
     if type(expr_deriv) in native_numeric_types and expr_deriv == 0:
         raise ValueError("variable derivative == 0, cannot solve for variable")
+
+    if abs(value(expr_deriv)) < 1e-12:
+        raise RuntimeError('Initial value for variable results in derivative value that is '
+                         'very close to zero. Please provide a different initial guess.')
 
     iter_left = iterlim
     while abs(value(expr)) > eps and iter_left:
@@ -94,5 +114,33 @@ def calculate_variable_from_constraint(variable, constraint,
             raise RuntimeError(
                 "Iteration limit (%s) reached; remaining residual=%s"
                 % (iterlim, value(expr)) )
-        variable.set_value(value(variable) - value(expr)/value(expr_deriv))
-        
+
+        # compute step
+        xk = value(variable)
+        fk = value(expr)
+        fpk = value(expr_deriv)
+        if abs(fpk) < 1e-12:
+            raise RuntimeError("Newton's method encountered a derivative that was too close to zero."
+                             " Please provide a different initial guess or enable the linesearch if you have not.")
+        pk = -fk/fpk
+        alpha = 1.0
+        xkp1 = xk + alpha * pk
+        variable.set_value(xkp1)
+
+        # perform line search
+        if linesearch:
+            c1 = 0.999 # ensure sufficient progress
+            while alpha > 1e-8:
+                # check if the value at xkp1 has sufficient reduction in the residual
+                fkp1 = value(expr)
+                if fkp1**2 < c1*fk**2:
+                    # found an alpha value with sufficient reduction
+                    # continue to the next step
+                    break
+                alpha /= 2.0
+                xkp1 = xk + alpha * pk
+                variable.set_value(xkp1)
+
+            if alpha <= 1e-8:
+                raise RuntimeError("Linesearch iteration limit reached. calc_var_value failed to converge.")
+
