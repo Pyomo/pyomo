6d5b6b45e19abba62ed28eb5d818050411a5672e
John Siirola <jsiirola@users.noreply.github.com>
2018-12-05 12:06:02 -0700

    PEP8 reformatting + improving the rigor of some exception tests

--- pyomo/util/calc_var_value.py
+++ pyomo/util/calc_var_value.py
@@ -54,13 +54,16 @@ def calculate_variable_from_constraint(variable, constraint,
     if variable.value is None:
         if variable.lb is None:
             if variable.ub is None:
-                # no variable values, and no lower or upper bound - set initial value to 0.0
+                # no variable values, and no lower or upper bound - set
+                # initial value to 0.0
                 variable.set_value(0)
             else:
-                # no variable value or lower bound - set to 0 or upper bound whichever is lower
+                # no variable value or lower bound - set to 0 or upper
+                # bound whichever is lower
                 variable.set_value(min(0, variable.ub))
         elif variable.ub is None:
-            # no variable value or upper bound - set to 0 or lower bound, whichever is higher
+            # no variable value or upper bound - set to 0 or lower
+            # bound, whichever is higher
             variable.set_value(max(0, variable.lb))
         else:
             # we have upper and lower bounds
@@ -104,15 +107,16 @@ def calculate_variable_from_constraint(variable, constraint,
         raise ValueError("Variable derivative == 0, cannot solve for variable")
 
     if abs(value(expr_deriv)) < 1e-12:
-        raise RuntimeError('Initial value for variable results in derivative value that is '
-                         'very close to zero. Please provide a different initial guess.')
+        raise RuntimeError(
+            'Initial value for variable results in derivative value that is '
+            'very close to zero. Please provide a different initial guess.')
 
     iter_left = iterlim
     while abs(value(expr)) > eps and iter_left:
         iter_left -= 1
         if not iter_left:
             raise RuntimeError(
-                "Iteration limit (%s) reached; remaining residual=%s"
+                "Iteration limit (%s) reached; remaining residual = %s"
                 % (iterlim, value(expr)) )
 
         # compute step
@@ -120,8 +124,10 @@ def calculate_variable_from_constraint(variable, constraint,
         fk = value(expr)
         fpk = value(expr_deriv)
         if abs(fpk) < 1e-12:
-            raise RuntimeError("Newton's method encountered a derivative that was too close to zero."
-                             " Please provide a different initial guess or enable the linesearch if you have not.")
+            raise RuntimeError(
+                "Newton's method encountered a derivative that was too "
+                "close to zero.\nPlease provide a different initial guess "
+                "or enable the linesearch if you have not.")
         pk = -fk/fpk
         alpha = 1.0
         xkp1 = xk + alpha * pk
@@ -131,7 +137,8 @@ def calculate_variable_from_constraint(variable, constraint,
         if linesearch:
             c1 = 0.999 # ensure sufficient progress
             while alpha > 1e-8:
-                # check if the value at xkp1 has sufficient reduction in the residual
+                # check if the value at xkp1 has sufficient reduction in
+                # the residual
                 fkp1 = value(expr)
                 if fkp1**2 < c1*fk**2:
                     # found an alpha value with sufficient reduction
@@ -142,5 +149,7 @@ def calculate_variable_from_constraint(variable, constraint,
                 variable.set_value(xkp1)
 
             if alpha <= 1e-8:
-                raise RuntimeError("Linesearch iteration limit reached. calc_var_value failed to converge.")
+                raise RuntimeError(
+                    "Linesearch iteration limit reached; remaining "
+                    "residual = %s." % (value(expr),))
 
