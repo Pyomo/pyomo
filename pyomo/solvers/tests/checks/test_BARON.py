1fd98d4515071b82a2d55376836c0bd4ed53b409
John Siirola <jsiirola@users.noreply.github.com>
2018-12-05 14:47:00 -0700

    Adding additional BARON reformulations
    The BARON manual calls out specific translations for log10(), abs(), and
    exponentiation where both the base and the exponent are variable.

--- pyomo/solvers/tests/checks/test_BARON.py
+++ pyomo/solvers/tests/checks/test_BARON.py
@@ -15,7 +15,7 @@ class BaronTest(unittest.TestCase):
     @unittest.skipIf(not baron_available,
                      "The 'BARON' solver is not available")
     def test_log10(self):
-        """Tests the log10 functionality."""
+        # Tests the special transformation for log10
         with SolverFactory("baron") as opt:
 
             m = ConcreteModel()
@@ -23,7 +23,36 @@ class BaronTest(unittest.TestCase):
             m.c = Constraint(expr=log10(m.x) >= 2)
             m.obj = Objective(expr=m.x, sense=minimize)
 
-            results = opt.solve(m)
+            results = opt.solve(m, keepfiles=True)
+
+            self.assertEqual(results.solver.termination_condition,
+                             TerminationCondition.optimal)
+
+    def test_abs(self):
+        # Tests the special transformation for abs
+        with SolverFactory("baron") as opt:
+
+            m = ConcreteModel()
+            m.x = Var(bounds=(-100,1))
+            m.c = Constraint(expr=abs(m.x) >= 2)
+            m.obj = Objective(expr=m.x, sense=minimize)
+
+            results = opt.solve(m, keepfiles=True)
+
+            self.assertEqual(results.solver.termination_condition,
+                             TerminationCondition.optimal)
+
+    def test_pow(self):
+        # Tests the special transformation for x ^ y (both variables)
+        with SolverFactory("baron") as opt:
+
+            m = ConcreteModel()
+            m.x = Var(bounds=(10,100))
+            m.y = Var(bounds=(1,10))
+            m.c = Constraint(expr=m.x ** m.y >= 20)
+            m.obj = Objective(expr=m.x, sense=minimize)
+
+            results = opt.solve(m, keepfiles=True)
 
             self.assertEqual(results.solver.termination_condition,
                              TerminationCondition.optimal)
