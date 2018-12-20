b807210b505ebfaca524d04719ed636edf811a13
kaklise <kaklise@sandia.gov>
2018-12-20 12:29:21 -0700

    Added semibatch parallel example, updated data files to include experiment number

--- pyomo/contrib/parmest/examples/semibatch/semibatch_parmest_parallel.py
+++ pyomo/contrib/parmest/examples/semibatch/semibatch_parmest_parallel.py
@@ -1,37 +1,38 @@
 import numpy as np
-import json
 import pyomo.contrib.parmest.parmest as parmest
 import pyomo.contrib.parmest.graphics as grph
+import pyomo.contrib.parmest.mpi_utils as mpiu
 from semibatch import generate_model
+
+mpii = mpiu.MPIInterface() # works with, or without, mpi
 
 ### Parameter estimation
 
 # Vars to estimate
 thetavars = ['k1', 'k2', 'E1', 'E2']
 
-# Data, list of dictionaries
+# Data, list of json file names
 data = [] 
 for exp_num in range(10):
-    fname = 'exp'+str(exp_num+1)+'.out'
-    with open(fname,'r') as infile:
-        d = json.load(infile)
-        data.append(d)
+    data.append('exp'+str(exp_num+1)+'.out')
 
 # Note, the model already includes a 'SecondStageCost' expression 
 # for sum of squared error that will be used in parameter estimation
         
 pest = parmest.Estimator(generate_model, data, thetavars)
 obj, theta = pest.theta_est()
-print(obj)
-print(theta)
+if mpii.rank in [0, None]:
+    print(obj)
+    print(theta)
 
 
 ### Parameter estimation with bootstrap resampling
 
 np.random.seed(38256)
 bootstrap_theta = pest.bootstrap(50)
-print(bootstrap_theta.head())
-grph.pairwise_bootstrap_plot(bootstrap_theta, theta, 0.8)
+if mpii.rank in [0, None]:
+    print(bootstrap_theta.head())
+    grph.pairwise_bootstrap_plot(bootstrap_theta, theta, 0.8)
 
 
 ### Parameter estimation with likelihood ratio
@@ -42,5 +43,6 @@ search_ranges["E2"] = np.arange(38000, 42000, 1000)
 search_ranges["k1"] = np.arange(4, 24, 6) 
 search_ranges["k2"] = np.arange(40, 160, 80)
 LR = pest.likelihood_ratio(search_ranges=search_ranges)
-print(LR.head())
-grph.pairwise_likelihood_ratio_plot(LR, obj, 0.8, len(data))
+if mpii.rank in [0, None]:
+    print(LR.head())
+    grph.pairwise_likelihood_ratio_plot(LR, obj, 0.8, len(data))
