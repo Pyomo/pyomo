4b8e7d9ca159352118ba50a631b644abe7e65ed0
kaklise <kaklise@sandia.gov>
2019-01-08 13:35:47 -0700

    Added multisensor example

--- pyomo/contrib/parmest/examples/reactor_design/reactor_design_parmest_multisensor.py
+++ pyomo/contrib/parmest/examples/reactor_design/reactor_design_parmest_multisensor.py
@@ -8,15 +8,18 @@ from reactor_design import reactor_design_model
 # Vars to estimate
 theta_names = ['k1', 'k2', 'k3']
 
-# Data
-data = pd.read_excel('reactor_data.xlsx') 
+# Data, includes multiple sensors for ca and cc
+data = pd.read_excel('reactor_data.xlsx')  
 
 # Sum of squared error function
 def SSE(model, data): 
-    expr = (float(data['ca']) - model.ca)**2 + \
-           (float(data['cb']) - model.cb)**2 + \
-           (float(data['cc']) - model.cc)**2 + \
-           (float(data['cd']) - model.cd)**2
+    expr = ((float(data['ca1']) - model.ca)**2)*(1/3) + \
+           ((float(data['ca2']) - model.ca)**2)*(1/3) + \
+           ((float(data['ca3']) - model.ca)**2)*(1/3) + \
+            (float(data['cb'])  - model.cb)**2 + \
+           ((float(data['cc1']) - model.cc)**2)*(1/2) + \
+           ((float(data['cc2']) - model.cc)**2)*(1/2) + \
+            (float(data['cd'])  - model.cd)**2
     return expr
 
 pest = parmest.Estimator(reactor_design_model, data, theta_names, SSE)
@@ -26,7 +29,7 @@ print(theta)
 
 ### Parameter estimation with bootstrap resampling
 
-np.random.seed(12524)
+np.random.seed(38256)
 bootstrap_theta = pest.theta_est_bootstrap(50)
 print(bootstrap_theta.head())
 
@@ -37,11 +40,10 @@ kde_dist = parmest.pairwise_plot(bootstrap_theta, theta, 'gaussian_kde', 0.8)
 
 ### Likelihood ratio test
 
-search_ranges = {}
-search_ranges['k1'] = np.arange(0.78, 0.92, 0.02) 
-search_ranges['k2'] = np.arange(1.48, 1.79, 0.05) 
-search_ranges['k3'] = np.arange(0.000155, 0.000185, 0.000005) 
-obj_at_theta = pest.objective_at_theta(search_ranges=search_ranges)
+theta_vals = pd.DataFrame(columns=theta_names)
+for i, k2 in enumerate(np.arange(1.3, 2, 0.1)):
+    theta_vals.loc[i,:] = [0.83, k2, 0.00016]
+obj_at_theta = pest.objective_at_theta(theta_vals=theta_vals)
 print(obj_at_theta)
 LR = pest.likelihood_ratio_test(obj_at_theta, obj, [0.8, 0.85, 0.9, 0.95])
 print(LR.head())
