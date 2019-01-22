c16be274ea2ca2a264e4330e7aa4cf6b41f75158
eslickj <john.eslick@netl.doe.gov>
2019-01-22 09:21:31 -0500

    Fix up example 1 visualization script.

--- pyomo/contrib/petsc/examples/dae_pyomo/example01_vis.py
+++ pyomo/contrib/petsc/examples/dae_pyomo/example01_vis.py
@@ -8,19 +8,30 @@ options:
 That should put results from each time step in the directory Visualization-data
 
 To run this you need $PETSC_DIR/lib/petsc/bin/ in PYTHONPATH
+> export PYTHONPATH=$PYTHONPATH:$PETSC_DIR/lib/petsc/bin/
 """
 
-#commented out because you need some files out of PETSc to run this.
-"""
+
 from __future__ import division  # No integer division
 from __future__ import print_function  # Python 3 style print
-import PetscBinaryIOTrajectory as pbt
+
+try:
+    import PetscBinaryIOTrajectory as pbt
+except:
+    pbt = None
+    print("Could not find PetscBinaryIOTrajectory.py.\n"
+          "Make sure the PETSc python files are in your PYTHONPATH\n"
+          "Try > export PYTHONPATH=$PYTHONPATH:$PETSC_DIR/lib/petsc/bin/")
 
 if __name__ == '__main__':
+    if pbt is None:
+        exit()
     (t,v,names) = pbt.ReadTrajectory("Visualization-data")
     names = [
         "y1", "y2", "y3", "y4", "y5", "y6",
         "r1", "r2", "r3", "r4", "r5",
         "Fin"]
-    pbt.PlotTrajectories(t,v,names,["y1", "y2", "y3", "y4", "y5", "y6"])
-"""
+    pbt.PlotTrajectories(t,v,names,["y1", "y3", "y6"])
+    pbt.PlotTrajectories(t,v,names,["y5"])
+    pbt.PlotTrajectories(t,v,names,["y2"])
+    pbt.PlotTrajectories(t,v,names,["y4"])
