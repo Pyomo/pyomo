afc2212cb22fde6aaf489f7a913c3ece0e8235f2
Romeo Valentin <romeo.valentin@rwth-aachen.de>
2019-01-22 21:17:24 -0500

    Refactored the disjunctions and added more comments
    Before, the disjunctions were individually written down for each heat
    exchanger. Now, we use the @m.Disjunction decorator combined with index
    sets to write it in a more concise way

--- examples/gdp/small_lit/nonconvex_HEN.py
+++ examples/gdp/small_lit/nonconvex_HEN.py
@@ -1,6 +1,8 @@
-""" Example from 'Systematic Modeling of Discrete-Continuous Optimization
-      Models through Generalized Disjunctive Programming
+""" Example from 'Systematic Modeling of Discrete-Continuous Optimization \
+      Models through Generalized Disjunctive Programming'
     Ignacio E. Grossmann and Francisco Trespalacios, 2013
+
+    Optimal solution ca. 114,385
 
     Pyomo model implementation by @RomeoV
 """
@@ -11,8 +13,9 @@ from pyomo.environ import (ConcreteModel, Constraint, NonNegativeReals,
 from pyomo.gdp import Disjunction
 from pyomo.opt import SolverFactory
 
-def build_model():
+def build_gdp_model():
 
+    # PARAMETERS
     T1_lo, T1_up = 350., 400.
     T2_lo, T2_up = 450., 500.
 
@@ -22,71 +25,68 @@ def build_model():
     T_out = {'hot':340., 'cold':560., 'cooling':320., 'steam':600.}
     Cost = {'cooling':20., 'steam':80.}
 
+    # VARIABLES
     m = ConcreteModel()
     m.T1 = Var(domain=NonNegativeReals, bounds=(T1_lo, T1_up))
     m.T2 = Var(domain=NonNegativeReals, bounds=(T2_lo, T2_up))
-    m.A1 = Var(domain=NonNegativeReals, bounds=(0,50), initialize=1)
-    m.A2 = Var(domain=NonNegativeReals, bounds=(0,50), initialize=1)
-    m.A3 = Var(domain=NonNegativeReals, bounds=(0,50), initialize=1)
+
     m.exchangers = RangeSet(1,3)
+    m.A  = Var(m.exchangers, domain=NonNegativeReals, bounds=(1e-4,50))
     m.CP = Var(m.exchangers, domain=NonNegativeReals, bounds=(0,600*pow(50,0.6)+2*46500))
 
+
+    # OBJECTIVE
     m.objective = Objective(
         expr=(sum(m.CP[i] for i in m.exchangers)
               + FCP['hot']*(m.T1-T_out['hot'])*Cost['cooling']
               + FCP['cold']*(T_out['cold']-m.T2)*Cost['steam'])
     )
 
+    # GLOBAL CONSTRAINTS
     m.constr1 = Constraint(
-        expr=FCP['hot']*(T_in['hot']-m.T1)==m.A1*U['1']*((T_in['hot']-m.T2)+(m.T1-T_in['cold']))/2.
+        expr=FCP['hot']*(T_in['hot']-m.T1)==m.A[1]*U['1']*((T_in['hot']-m.T2)+(m.T1-T_in['cold']))/2.
     )
-    m.constr2 = Constraint(
-        expr=FCP['hot']*(m.T1-T_out['hot'])==m.A2*U['2']*((T_out['hot']-T_in['cooling'])+(m.T1-T_out['cooling']))/2.
+    m.constr2 = Constraint( # Note the error in the paper in constraint 2
+        expr=FCP['hot']*(m.T1-T_out['hot'])==m.A[2]*U['2']*((T_out['hot']-T_in['cooling'])+(m.T1-T_out['cooling']))/2.
     )
     m.constr3 = Constraint(
-        expr=FCP['cold']*(T_out['cold']-m.T2)==m.A3*U['3']*((T_out['steam']-m.T2)+(T_in['steam']-T_out['cold']))/2.
+        expr=FCP['cold']*(T_out['cold']-m.T2)==m.A[3]*U['3']*((T_out['steam']-m.T2)+(T_in['steam']-T_out['cold']))/2.
     )
     m.constr4 = Constraint(
         expr=FCP['hot']*(T_in['hot']-m.T1)==FCP['cold']*(m.T2-T_in['cold'])
     )
 
-    # TODO: Can we combine this with a for-loop?
+    # DISJUNCTIONS
+    @m.Disjunction(m.exchangers)
+    def exchanger_disjunction(m, disjctn):
+        return [
+            [m.CP[disjctn] == 2750*pow(m.A[disjctn],0.6)+3000,
+             0. <= m.A[disjctn], m.A[disjctn] <= 10.],
+            [m.CP[disjctn] == 1500*pow(m.A[disjctn],0.6)+15000,
+             10. <= m.A[disjctn], m.A[disjctn] <= 25.],
+            [m.CP[disjctn] == 600*pow(m.A[disjctn],0.6)+46500,
+             25. <= m.A[disjctn], m.A[disjctn] <= 50.]
+        ]
 
-    m.disjunc1 = Disjunction(expr=[
-        [m.CP[1] == 2750*pow(m.A1,0.6)+3000,
-         0. <= m.A1, m.A1 <= 10.],
-        [m.CP[1] == 1500*pow(m.A1,0.6)+15000,
-         10. <= m.A1, m.A1 <= 25.],
-        [m.CP[1] == 600*pow(m.A1,0.6)+46500,
-         25. <= m.A1, m.A1 <= 50.]
-    ])
-
-    m.disjunc2 = Disjunction(expr=[
-        [m.CP[2] == 2750*pow(m.A2,0.6)+3000,
-         0. <= m.A2, m.A2 <= 10.],
-        [m.CP[2] == 1500*pow(m.A2,0.6)+15000,
-         10. <= m.A2, m.A2 <= 25.],
-        [m.CP[2] == 600*pow(m.A2,0.6)+46500,
-         25. <= m.A2, m.A2 <= 50.]
-    ])
-
-    m.disjunc3 = Disjunction(expr=[
-        [m.CP[3] == 2750*pow(m.A3,0.6)+3000,
-         0. <= m.A3, m.A3 <= 10.],
-        [m.CP[3] == 1500*pow(m.A3,0.6)+15000,
-         10. <= m.A3, m.A3 <= 25.],
-        [m.CP[3] == 600*pow(m.A3,0.6)+46500,
-         25. <= m.A3, m.A3 <= 50.]
-    ])
-
-    TransformationFactory('gdp.bigm').apply_to(m,bigM=600*pow(50,0.6)+2*46500)
-    #TransformationFactory('gdp.chull').apply_to(m)
     return m
 
 if __name__ == "__main__":
-    model = build_model()
+    # Decide whether to reformulate as MINLP and what method to use
+    reformulation = True
+    reformulation_method = 'chull'
+
+    model = build_gdp_model()
     model.pprint()
-    res = SolverFactory('gams').solve(model, tee=True, solver='baron', add_options=['option optcr = 0;'], keepfiles=True)
-    #res = SolverFactory('gdpopt').solve(model, tee=True, strategy='LOA')
-    #model.display()
+
+    if reformulation:
+        if reformulation_method == 'bigm':
+            TransformationFactory('gdp.bigm').apply_to(model,bigM=600*pow(50,0.6)+2*46500)
+        elif reformulation_method == 'chull':
+            TransformationFactory('gdp.chull').apply_to(model)
+        res = SolverFactory('gams').solve(model, tee=True, solver='baron', add_options=['option optcr = 0;'], keepfiles=True)
+    else:
+        # Note: MC++ needs to be properly installed to use strategy GLOA
+        res = SolverFactory('gdpopt').solve(model, tee=True, strategy='GLOA')
+
+    # model.display()
     print(res)
