556b2e9aec92abc9a66a6b845529622bf54a265c
Qi Chen <qichen@andrew.cmu.edu>
2019-02-02 00:17:53 -0500

    intermediate commit so that I can continue working on my laptop

--- pyomo/contrib/gdpbb/GDPbb.py
+++ pyomo/contrib/gdpbb/GDPbb.py
@@ -2,12 +2,17 @@ import heapq
 
 from pyomo.common.config import (ConfigBlock, ConfigValue)
 from pyomo.common.modeling import unique_component_name
+from pyomo.contrib.gdpopt.util import create_utility_block
 from pyomo.core.base import (
     Objective, TransformationFactory,
     minimize, value)
 from pyomo.gdp import Disjunct, Disjunction
 from pyomo.opt import SolverFactory, SolverStatus
 from pyomo.opt import TerminationCondition as tc
+
+
+class GDPbbSolveData(object):
+    pass
 
 
 @SolverFactory.register('gdpbb',
@@ -46,99 +51,108 @@ class GDPbbSolver(object):
         self.validate_model(model)
         # Set solver as an MINLP
         solver = SolverFactory(config.solver)
+        solve_data = GDPbbSolveData()
+        
+        with create_utility_block(model, 'GDPbb_utils', solve_data):
+            # Initialize ist containing indicator vars for reupdating model after solving
+            indicator_list_name = unique_component_name(model, "_indicator_list")
+            indicator_vars = []
+            for disjunction in model.component_data_objects(
+                    ctype=Disjunction, active=True):
+                for disjunct in disjunction.disjuncts:
+                    indicator_vars.append(disjunct.indicator_var)
+            setattr(model, indicator_list_name, indicator_vars)
 
-        # Initialize ist containing indicator vars for reupdating model after solving
-        indicator_list_name = unique_component_name(model, "_indicator_list")
-        indicator_vars = []
-        for disjunction in model.component_data_objects(
-                ctype=Disjunction, active=True):
-            for disjunct in disjunction.disjuncts:
-                indicator_vars.append(disjunct.indicator_var)
-        setattr(model, indicator_list_name, indicator_vars)
+            # get objective sense
+            objectives = model.component_data_objects(Objective, active=True)
+            obj = next(objectives, None)
+            obj_sign = 1 if obj.sense == minimize else -1
+            print(obj_sign)
 
-        # get objective sense
-        objectives = model.component_data_objects(Objective, active=True)
-        obj = next(objectives, None)
-        obj_sign = 1 if obj.sense == minimize else -1
-        print(obj_sign)
+            # clone original model for root node of branch and bound
+            root = model.clone()
 
-        # clone original model for root node of branch and bound
-        root = model.clone()
+            # set up lists to keep track of which disjunctions have been covered.
 
-        # set up lists to keep track of which disjunctions have been covered.
+            # this list keeps track of the original disjunctions that were active and are soon to be inactive
+            init_active_disjunctions_name = unique_component_name(root, "_init_active_disjunctions")
+            init_active_disjunctions = list(root.component_data_objects(
+                ctype=Disjunction, active=True))
+            setattr(root, init_active_disjunctions_name, init_active_disjunctions)
 
-        # this list keeps track of the original disjunctions that were active and are soon to be inactive
-        init_active_disjunctions_name = unique_component_name(root, "_init_active_disjunctions")
-        init_active_disjunctions = list(root.component_data_objects(
-            ctype=Disjunction, active=True))
-        setattr(root, init_active_disjunctions_name, init_active_disjunctions)
+            # this list keeps track of the disjunctions that have been activated by the branch and bound
+            curr_active_disjunctions_name = unique_component_name(root, "_curr_active_disjunctions")
+            curr_active_disjunctions = []
+            setattr(root, curr_active_disjunctions_name, curr_active_disjunctions)
 
-        # this list keeps track of the disjunctions that have been activated by the branch and bound
-        curr_active_disjunctions_name = unique_component_name(root, "_curr_active_disjunctions")
-        curr_active_disjunctions = []
-        setattr(root, curr_active_disjunctions_name, curr_active_disjunctions)
+            # deactivate all disjunctions in the model
+            # self.indicate(root)
+            for djn in getattr(root, init_active_disjunctions_name):
+                djn.deactivate()
+            # Deactivate all disjuncts in model. To be reactivated with disjunction
+            # is reactivated.
+            for disj in root.component_data_objects(Disjunct, active=True):
+                disj._deactivate_without_fixing_indicator()
 
-        # deactivate all disjunctions in the model
-        # self.indicate(root)
-        for djn in getattr(root, init_active_disjunctions_name):
-            djn.deactivate()
-        # Deactivate all disjuncts in model. To be reactivated with disjunction
-        # is reactivated.
-        for disj in root.component_data_objects(Disjunct, active=True):
-            disj.deactivate()
-            disj.indicator_var
-        for djn in getattr(root, init_active_disjunctions_name):
-            djn.disjuncts[0].indicator_var = 1
+            # TODO [Qi]: what is this for?
+            for djn in getattr(root, init_active_disjunctions_name):
+                djn.disjuncts[0].indicator_var = 1
 
-        self.indicate(root)
-        # Satisfiability check would go here
+            self.indicate(root)
+            # Satisfiability check would go here
 
-        # solve the root node
-        obj_value = self.minlp_solve(root, solver, config)
+            # solve the root node
+            obj_value = self.minlp_solve(root, solver, config)
 
-        # initialize minheap for Branch and Bound algorithm
-        heap = []
-        heapq.heappush(heap, (obj_sign * obj_value, root))
-        print([i[0] for i in heap])
-        # loop to branch through the tree
-        n = 0
-        while len(heap) > 0:
-            n = n + 1
-            # pop best model off of heap
-            mdlpack = heapq.heappop(heap)
-            # print [i[0] for i in heap]
-            mdl = mdlpack[1]
+            # initialize minheap for Branch and Bound algorithm
+            # Heap structure: (ordering tuple, model)
+            # Ordering tuple: (objective value, disjunctions_left, -counter)
+            #  - select solutions with lower objective value,
+            #    then fewer disjunctions left to explore (depth first),
+            #    then more recently encountered (tiebreaker)
+            heap = []
+            counter = 0
+            disjunctions_left = len(getattr(root, init_active_disjunctions_name))
+            heapq.heappush(heap, ((obj_sign * obj_value, disjunctions_left, counter), root))
+            print([i[0] for i in heap])
+            # loop to branch through the tree
+            while len(heap) > 0:
+                counter += 1
+                # pop best model off of heap
+                mdlpack = heapq.heappop(heap)
+                # print [i[0] for i in heap]
+                mdl = mdlpack[1]
 
-            print(mdlpack[0])
-            # if all the originally active disjunctions are active, solve and
-            # return solution
-            if (len(getattr(mdl, init_active_disjunctions_name)) == 0):
-                orig_var_list = getattr(model, indicator_list_name)
-                best_soln_var_list = getattr(mdl, indicator_list_name)
-                for orig_var, new_var in zip(orig_var_list, best_soln_var_list):
-                    if not orig_var.is_fixed():
-                        orig_var.value = new_var.value
-                TransformationFactory('gdp.fix_disjuncts').apply_to(model)
+                print(mdlpack[0])
+                # if all the originally active disjunctions are active, solve and
+                # return solution
+                if (len(getattr(mdl, init_active_disjunctions_name)) == 0):
+                    orig_disjuncts = model.GDPbb_utils.disjunct_list
+                    best_soln_disjuncts = mdl.GDPbb_utils.disjunct_list
+                    for orig_disjunct, soln_disjunct in zip(orig_disjuncts, best_soln_disjuncts):
+                        if not orig_disjunct.indicator_var.is_fixed():
+                            orig_disjunct.indicator_var.value = soln_disjunct.indicator_var.value
+                    TransformationFactory('gdp.fix_disjuncts').apply_to(model)
 
-                return solver.solve(model, **config.solver_args)
+                    return solver.solve(model, **config.solver_args)
 
-            disjunction = getattr(mdl, init_active_disjunctions_name).pop(0)
-            for disj in list(disjunction.disjuncts):
-                disj.activate()
-            disjunction.activate()
-            getattr(mdl, curr_active_disjunctions_name).append(disjunction)
-            for disj in list(disjunction.disjuncts):
-                disj.indicator_var = 0
-            for disj in list(disjunction.disjuncts):
-                disj.indicator_var = 1
-                mnew = mdl.clone()
-                disj.indicator_var = 0
-                obj_value = self.minlp_solve(mnew, solver, config)
-                print([value(d.indicator_var) for d in mnew.component_data_objects(Disjunct, active=True)])
-                # self.indicate(mnew)
-                djn_left = len(getattr(mdl, init_active_disjunctions_name))
-                ordering_tuple = (obj_sign * obj_value, djn_left, -n)
-                heapq.heappush(heap, (ordering_tuple, mnew))
+                disjunction = getattr(mdl, init_active_disjunctions_name).pop(0)
+                for disj in list(disjunction.disjuncts):
+                    disj.activate()
+                disjunction.activate()
+                getattr(mdl, curr_active_disjunctions_name).append(disjunction)
+                for disj in list(disjunction.disjuncts):
+                    disj.indicator_var = 0
+                for disj in list(disjunction.disjuncts):
+                    disj.indicator_var = 1
+                    mnew = mdl.clone()
+                    disj.indicator_var = 0
+                    obj_value = self.minlp_solve(mnew, solver, config)
+                    print([value(d.indicator_var) for d in mnew.component_data_objects(Disjunct, active=True)])
+                    # self.indicate(mnew)
+                    djn_left = len(getattr(mdl, init_active_disjunctions_name))
+                    ordering_tuple = (obj_sign * obj_value, djn_left, -counter)
+                    heapq.heappush(heap, (ordering_tuple, mnew))
 
     def validate_model(self, model):
         # Validates that model has only exclusive disjunctions
@@ -185,4 +199,4 @@ class GDPbbSolver(object):
         for disjunction in model.component_data_objects(
                 ctype=Disjunction):
             for disjunct in disjunction.disjuncts:
-                print(disjunction.name, disjunct.name, value(disjunct.indicator_var))
+                print(disjunction.name, disjunct.name, disjunct.indicator_var.value)
