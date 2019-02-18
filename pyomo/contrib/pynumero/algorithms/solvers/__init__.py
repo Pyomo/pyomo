c066eb4ff9386f489b45583ea642f254557a6bc9
Santiago <santiagoropb@gmail.com>
2019-02-18 08:40:53 -0600

    workaround not having dependencies installed for cyipopt and mumps

--- pyomo/contrib/pynumero/algorithms/solvers/__init__.py
+++ pyomo/contrib/pynumero/algorithms/solvers/__init__.py
@@ -1,2 +1,12 @@
-from .ip_solver import InteriorPointSolver
-from .cyipopt_solver import CyIpoptSolver
+
+try:
+    from .ip_solver import InteriorPointSolver
+except ImportError:
+    print("Need MA27 or MUMPS to run pynumero interior-point. "
+          "conda install -c conda-forge pymumps")
+
+try:
+    from .cyipopt_solver import CyIpoptSolver
+except ImportError:
+    print("Need CyIpopt to run CyipoptSolver"
+          "conda install -c conda-forge cyipopt")
