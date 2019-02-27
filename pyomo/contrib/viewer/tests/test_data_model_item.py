1c47f8e4b5aa019f490897e194ad5855ecea6126
eslickj <john.eslick@netl.doe.gov>
2019-02-27 14:29:52 -0500

    adding some tests

--- pyomo/contrib/viewer/tests/test_data_model_item.py
+++ pyomo/contrib/viewer/tests/test_data_model_item.py
@@ -29,10 +29,40 @@ class TestDataModelItem(unittest.TestCase):
             )
         m.c1 = Constraint(expr=m.x[0] * m.z[0]**2 + self.bb(m.x[0],m.x[1]) == 2*sqrt(2.0))
         m.c2 = Constraint(expr=m.z[2]**4 * m.z[1]**2 + m.z[1] == 8+sqrt(2.0))
-
+        m.c3 = Constraint(expr=m.x[1] == 3)
         self.m = m.clone()
 
     def test_expr_calc(self):
         cdi = ComponentDataItem(parent=None, ui_setup=None, o=self.m.b1.e1)
         cdi.calculate()
         assert(abs(cdi.get("value")-3) < 0.0001)
+
+    def test_cons_calc(self):
+        cdi = ComponentDataItem(parent=None, ui_setup=None, o=self.m.c3)
+        cdi.calculate()
+        assert(abs(cdi.get("residual") - 2) < 0.0001)
+
+    def test_var_get_value(self):
+        cdi = ComponentDataItem(parent=None, ui_setup=None, o=self.m.x[1])
+        assert(abs(cdi.get("value") - 1) < 0.0001)
+
+    def test_var_get_bounds(self):
+        cdi = ComponentDataItem(parent=None, ui_setup=None, o=self.m.x[1])
+        self.m.x[1].setlb(0)
+        self.m.x[1].setub(10)
+        assert(abs(cdi.get("lb") - 0) < 0.0001)
+        assert(abs(cdi.get("ub") - 10) < 0.0001)
+
+    def test_var_set_bounds(self):
+        cdi = ComponentDataItem(parent=None, ui_setup=None, o=self.m.x[1])
+        cdi.set("lb", 2)
+        cdi.set("ub", 8)
+        assert(abs(cdi.get("lb") - 2) < 0.0001)
+        assert(abs(cdi.get("ub") - 8) < 0.0001)
+
+    def test_var_fixed_bounds(self):
+        cdi = ComponentDataItem(parent=None, ui_setup=None, o=self.m.x[1])
+        cdi.set("fixed", True)
+        assert(cdi.get("fixed"))
+        cdi.set("fixed", False)
+        assert(not cdi.get("fixed"))
