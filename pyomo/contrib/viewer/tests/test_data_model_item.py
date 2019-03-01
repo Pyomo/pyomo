6a4d33c5b4f00ad26af853fb7a8be4c47659edb7
John Eslick <john.eslick@netl.doe.gov>
2019-03-01 17:38:50 -0500

    Updating the copywrite statments.

--- pyomo/contrib/viewer/tests/test_data_model_item.py
+++ pyomo/contrib/viewer/tests/test_data_model_item.py
@@ -1,3 +1,13 @@
+##############################################################################
+# Institute for the Design of Advanced Energy Systems Process Systems
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
+# software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
+# University Research Corporation, et al. All rights reserved.
+#
+# This software is distributed under the 3-clause BSD License.
+##############################################################################
 """
 Test data model items for QTreeView. These tests should work even without PyQt.
 """
@@ -9,8 +19,6 @@ from pyomo.contrib.viewer.model_browser import ComponentDataItem
 class TestDataModelItem(unittest.TestCase):
     def setUp(self):
         # Borrowed this test model from the trust region tests
-
-
         m = ConcreteModel()
         m.z = Var(range(3), domain=Reals, initialize=2.)
         m.x = Var(range(2), initialize=2.)
--- pyomo/contrib/viewer/ui.py
+++ pyomo/contrib/viewer/ui.py
@@ -1,6 +1,6 @@
 ##############################################################################
 # Institute for the Design of Advanced Energy Systems Process Systems
-# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018, by the
+# Engineering Framework (IDAES PSE Framework) Copyright (c) 2018-2019, by the
 # software owners: The Regents of the University of California, through
 # Lawrence Berkeley National Laboratory,  National Technology & Engineering
 # Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia
