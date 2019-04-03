f96b7994b69b6ef2463fda52582d99642e970c70
Laird <carldlaird@users.noreply.github.com>
2019-04-03 07:40:11 -0500

    adding offset to this_file to ensure this_file_dir is using the correct stack entry

--- pyomo/common/fileutils.py
+++ pyomo/common/fileutils.py
@@ -18,18 +18,18 @@ import six
 from .deprecation import deprecated
 from . import config
 
-def this_file():
+def this_file(stack_offset=1):
     """Returns the file name for the module that calls this function.
 
     This function is more reliable than __file__ on platforms like
-    WIndows and in situations where the program has called
+    Windows and in situations where the program has called
     os.chdir().
 
     """
     # __file__ fails if script is called in different ways on Windows
     # __file__ fails if someone does os.chdir() before
     # sys.argv[0] also fails because it does not always contains the path
-    callerFrame = inspect.stack()[1]
+    callerFrame = inspect.stack()[stack_offset]
     frameName = callerFrame[1]
     if frameName and frameName[0] == '<' and frameName[-1] == '>':
         return frameName
@@ -39,7 +39,7 @@ def this_file():
 def this_file_dir():
     """Returns the directory containing the module that calls this function.
     """
-    return os.path.dirname(this_file())
+    return os.path.dirname(this_file(stack_offset=2))
 
 
 PYOMO_ROOT_DIR = os.path.dirname(os.path.dirname(this_file_dir()))
