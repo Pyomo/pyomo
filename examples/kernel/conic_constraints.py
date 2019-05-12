3cd50e0c8b56c2ae6e6b024ae4adc96148b6f9b8
Gabriel Hackebeil <gabe.hackebeil@gmail.com>
2019-05-12 16:29:45 -0400

    reordering arguments for conic constraints so that it better matches commonly used notation for conic domains

--- examples/kernel/conic_constraints.py
+++ examples/kernel/conic_constraints.py
@@ -5,35 +5,35 @@ import pyomo.kernel as pmo
 #
 
 c = pmo.conic.quadratic(
-    x=[pmo.variable(), pmo.variable()],
-    r=pmo.variable(lb=0))
+    r=pmo.variable(lb=0),
+    x=[pmo.variable(), pmo.variable()])
 assert not c.has_lb()
 assert c.has_ub() and (c.ub == 0)
 assert c.check_convexity_conditions()
 print(c.body)
 
 c = pmo.conic.rotated_quadratic(
-    x=[pmo.variable(), pmo.variable()],
     r1=pmo.variable(lb=0),
-    r2=pmo.variable(lb=0))
+    r2=pmo.variable(lb=0),
+    x=[pmo.variable(), pmo.variable()])
 assert not c.has_lb()
 assert c.has_ub() and (c.ub == 0)
 assert c.check_convexity_conditions()
 print(c.body)
 
 c = pmo.conic.primal_exponential(
+    r=pmo.variable(lb=0),
     x1=pmo.variable(lb=0),
-    x2=pmo.variable(),
-    r=pmo.variable(lb=0))
+    x2=pmo.variable())
 assert not c.has_lb()
 assert c.has_ub() and (c.ub == 0)
 assert c.check_convexity_conditions()
 print(c.body)
 
 c = pmo.conic.primal_power(
-    x=[pmo.variable(), pmo.variable()],
     r1=pmo.variable(lb=0),
     r2=pmo.variable(lb=0),
+    x=[pmo.variable(), pmo.variable()],
     alpha=0.5)
 assert not c.has_lb()
 assert c.has_ub() and (c.ub == 0)
@@ -41,18 +41,18 @@ assert c.check_convexity_conditions()
 print(c.body)
 
 c = pmo.conic.dual_exponential(
+    r=pmo.variable(lb=0),
     x1=pmo.variable(),
-    x2=pmo.variable(ub=0),
-    r=pmo.variable(lb=0))
+    x2=pmo.variable(ub=0))
 assert not c.has_lb()
 assert c.has_ub() and (c.ub == 0)
 assert c.check_convexity_conditions()
 print(c.body)
 
 c = pmo.conic.dual_power(
-    x=[pmo.variable(), pmo.variable()],
     r1=pmo.variable(lb=0),
     r2=pmo.variable(lb=0),
+    x=[pmo.variable(), pmo.variable()],
     alpha=0.5)
 assert not c.has_lb()
 assert c.has_ub() and (c.ub == 0)
