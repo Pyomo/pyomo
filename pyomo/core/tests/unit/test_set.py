ad7f66ad96a9e6e979398edfc4997af54ef2e514
John Siirola <jsiirola@users.noreply.github.com>
2019-06-24 10:39:12 -0600

    Adding test verifying this branch resolves #134

--- pyomo/core/tests/unit/test_set.py
+++ pyomo/core/tests/unit/test_set.py
@@ -4848,3 +4848,16 @@ class TestIssues(unittest.TestCase):
         self.assertEqual(list(model.s), [1,2,3])
         model.s = [3,9]
         self.assertEqual(list(model.s), [3,9])
+
+    def test_issue_134(self):
+        m = ConcreteModel()
+        m.I = Set(initialize=[1,2])
+        m.J = Set(initialize=[4,5])
+        m.IJ = m.I * m.J
+        self.assertEqual(len(m.IJ), 4)
+        self.assertEqual(m.IJ.dimen, 2)
+        m.IJ2 = m.IJ * m.IJ
+        self.assertEqual(len(m.IJ2), 16)
+        self.assertEqual(m.IJ2.dimen, 4)
+        self.assertEqual(len(m.IJ), 4)
+        self.assertEqual(m.IJ.dimen, 2)
