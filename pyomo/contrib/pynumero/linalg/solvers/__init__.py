6885be414ea98afaf3efa72b6019e98ffdec18cd
Santiago <santiagoropb@gmail.com>
2019-07-24 19:42:14 -0500

    Fixes linear solver imports

--- pyomo/contrib/pynumero/linalg/solvers/__init__.py
+++ pyomo/contrib/pynumero/linalg/solvers/__init__.py
@@ -1,6 +1,5 @@
-from pyomo.contrib.pynumero import mumps_available
-from pyomo.contrib.pynumero.extensions.hsl import _MA27_LinearSolver
-if _MA27_LinearSolver.available():
+from pyomo.contrib.pynumero import mumps_available, ma27_available
+if ma27_available:
     from pyomo.contrib.pynumero.linalg.solvers.ma27_solver import MA27LinearSolver
 if mumps_available:
     from pyomo.contrib.pynumero.linalg.solvers.mumps_solver import MUMPSSymLinearSolver
