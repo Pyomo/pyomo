998e3f5016489ce659d01cc8658dd0e1c7125eaf
Emma Johnson <emma58@users.noreply.github.com>
2019-08-20 18:36:39 -0400

    Adding test for Disjunct set_value (that are not actually useful yet)

--- pyomo/gdp/tests/test_disjunct.py
+++ pyomo/gdp/tests/test_disjunct.py
@@ -10,10 +10,13 @@
 
 import pyutilib.th as unittest
 
-from pyomo.core import ConcreteModel, Var, Constraint
+from pyomo.core import ConcreteModel, Var, Constraint, Block
 from pyomo.gdp import Disjunction, Disjunct
 
 from six import iterkeys
+
+# TODO DEBUG
+from nose.tools import set_trace
 
 class TestDisjunction(unittest.TestCase):
     def test_empty_disjunction(self):
@@ -226,6 +229,66 @@ class TestDisjunct(unittest.TestCase):
         self.assertFalse(m.d.disjuncts[0].indicator_var.is_fixed())
         self.assertFalse(m.d.disjuncts[1].indicator_var.is_fixed())
         self.assertFalse(m.d.disjuncts[2].indicator_var.is_fixed())
+
+    def test_set_value_assign_disjunct(self):
+        m = ConcreteModel()
+        m.y = Var()
+        m.d = Disjunct()
+        m.d.v = Var()
+        m.d.c = Constraint(expr=m.d.v >= 8)
+        
+        new_d = Disjunct()
+        new_d.v = Var()
+        new_d.c = Constraint(expr=m.y <= 89)
+        new_d.b = Block()
+        @new_d.b.Constraint([0,1])
+        def c(b, i):
+            m = b.model()
+            if i == 0:
+                return m.y >= 18
+            else:
+                return b.parent_block().v >= 20
+        m.d = new_d
+
+        self.assertIsInstance(m.d, Disjunct)
+        self.assertIsInstance(m.d.c, Constraint)
+        self.assertIsInstance(m.d.b, Block)
+        self.assertIsInstance(m.d.b.c, Constraint)
+        self.assertEqual(len(m.d.b.c), 2)
+        self.assertIsInstance(m.d.v, Var)
+        self.assertIsInstance(m.d.indicator_var, Var)
+
+    def test_set_value_assign_block(self):
+        print("TODO: I don't actually know how to test this at the moment...")
+        m = ConcreteModel()
+        m.y = Var()
+        m.d = Disjunct()
+        m.d.v = Var()
+        m.d.c = Constraint(expr=m.d.v >= 8)
+        
+        # [ESJ 08/16/2019]: Is this related to the Block set_value bugs? I don't
+        # know whether what I am doing below should be legal or not?
+        new_d = m.new_d = Block()
+        new_d.v = Var()
+        new_d.c = Constraint(expr=m.y <= 89)
+        new_d.b = Block()
+        new_d.b.v = Var()
+        @new_d.b.Constraint([0,1])
+        def c(b, i):
+            if i == 0:
+                return b.v >= 18
+            else:
+                return b.parent_block().v >= 20
+        m.del_component(m.new_d)
+        m.d.set_value(new_d)
+
+        self.assertIsInstance(m.d, Disjunct)
+        self.assertIsInstance(m.d.c, Constraint)
+        self.assertIsInstance(m.d.b, Block)
+        self.assertIsInstance(m.d.b.c, Constraint)
+        self.assertEqual(len(m.d.b.c), 2)
+        self.assertIsInstance(m.d.v, Var)
+        self.assertIsInstance(m.d.indicator_var, Var)
 
 
 if __name__ == '__main__':
