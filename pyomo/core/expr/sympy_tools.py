3074bce9b385c73085febde071c222ab7ee6fe6d
John Siirola <jsiirola@users.noreply.github.com>
2019-10-18 11:18:18 -0600

    Force sympy to treat all Vars as real variables.
    This tells sympy to treat all Pyomo variables as real values,
    simplifying some results returned by differentiate().  Fixes #1139.

--- pyomo/core/expr/sympy_tools.py
+++ pyomo/core/expr/sympy_tools.py
@@ -108,7 +108,10 @@ class PyomoSympyBimap(object):
     def getSympySymbol(self, pyomo_object):
         if pyomo_object in self.pyomo2sympy:
             return self.pyomo2sympy[pyomo_object]
-        sympy_obj = sympy.Symbol("x%d" % self.i)
+        # Pyomo currently ONLY supports Real variables (not complex
+        # variables).  If that ever changes, then we will need to
+        # revisit hard-coding the symbol type here
+        sympy_obj = sympy.Symbol("x%d" % self.i, real=True)
         self.i += 1
         self.pyomo2sympy[pyomo_object] = sympy_obj
         self.sympy2pyomo[sympy_obj] = pyomo_object
