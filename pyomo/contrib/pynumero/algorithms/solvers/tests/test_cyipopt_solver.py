83883a1b1257f83c48a80c1c9ad60ab65b9659a3
Carl Laird <carldlaird@users.noreply.github.com>
2020-01-16 11:08:31 -0600

    removing commented line and changing aml to pyo

--- pyomo/contrib/pynumero/algorithms/solvers/tests/test_cyipopt_solver.py
+++ pyomo/contrib/pynumero/algorithms/solvers/tests/test_cyipopt_solver.py
@@ -9,7 +9,7 @@
 #  ___________________________________________________________________________
 
 import pyutilib.th as unittest
-import pyomo.environ as aml
+import pyomo.environ as pyo
 
 from pyomo.contrib.pynumero import numpy_available, scipy_available
 if not (numpy_available and scipy_available):
@@ -34,22 +34,21 @@ from pyomo.contrib.pynumero.algorithms.solvers.cyipopt_solver import CyIpoptSolv
 
 
 def create_model1():
-    m = aml.ConcreteModel()
-    m.x = aml.Var([1, 2, 3], initialize=4.0)
-    m.c = aml.Constraint(expr=m.x[3] ** 2 + m.x[1] == 25)
-    m.d = aml.Constraint(expr=m.x[2] ** 2 + m.x[1] <= 18.0)
-    # m.d = aml.Constraint(expr=aml.inequality(-18, m.x[2] ** 2 + m.x[1],  28))
-    m.o = aml.Objective(expr=m.x[1] ** 4 - 3 * m.x[1] * m.x[2] ** 3 + m.x[3] ** 2 - 8.0)
+    m = pyo.ConcreteModel()
+    m.x = pyo.Var([1, 2, 3], initialize=4.0)
+    m.c = pyo.Constraint(expr=m.x[3] ** 2 + m.x[1] == 25)
+    m.d = pyo.Constraint(expr=m.x[2] ** 2 + m.x[1] <= 18.0)
+    m.o = pyo.Objective(expr=m.x[1] ** 4 - 3 * m.x[1] * m.x[2] ** 3 + m.x[3] ** 2 - 8.0)
     m.x[1].setlb(0.0)
     m.x[2].setlb(0.0)
 
     return m
 
 def create_model2():
-    m = aml.ConcreteModel()
-    m.x = aml.Var([1, 2], initialize=4.0)
-    m.d = aml.Constraint(expr=m.x[1] + m.x[2] <= 5)
-    m.o = aml.Objective(expr=m.x[1] ** 2 + 4 * m.x[2] ** 2 - 8 * m.x[1] - 16 * m.x[2])
+    m = pyo.ConcreteModel()
+    m.x = pyo.Var([1, 2], initialize=4.0)
+    m.d = pyo.Constraint(expr=m.x[1] + m.x[2] <= 5)
+    m.o = pyo.Objective(expr=m.x[1] ** 2 + 4 * m.x[2] ** 2 - 8 * m.x[1] - 16 * m.x[2])
     m.x[1].setub(3.0)
     m.x[1].setlb(0.0)
     m.x[2].setlb(0.0)
@@ -61,19 +60,19 @@ def create_model3(G, A, b, c):
     nx = G.shape[0]
     nl = A.shape[0]
 
-    model = aml.ConcreteModel()
+    model = pyo.ConcreteModel()
     model.var_ids = range(nx)
     model.con_ids = range(nl)
 
-    model.x = aml.Var(model.var_ids, initialize=0.0)
-    model.hessian_f = aml.Param(model.var_ids, model.var_ids, mutable=True, rule=lambda m, i, j: G[i, j])
-    model.jacobian_c = aml.Param(model.con_ids, model.var_ids, mutable=True, rule=lambda m, i, j: A[i, j])
-    model.rhs = aml.Param(model.con_ids, mutable=True, rule=lambda m, i: b[i])
-    model.grad_f = aml.Param(model.var_ids, mutable=True, rule=lambda m, i: c[i])
+    model.x = pyo.Var(model.var_ids, initialize=0.0)
+    model.hessian_f = pyo.Param(model.var_ids, model.var_ids, mutable=True, rule=lambda m, i, j: G[i, j])
+    model.jacobian_c = pyo.Param(model.con_ids, model.var_ids, mutable=True, rule=lambda m, i, j: A[i, j])
+    model.rhs = pyo.Param(model.con_ids, mutable=True, rule=lambda m, i: b[i])
+    model.grad_f = pyo.Param(model.var_ids, mutable=True, rule=lambda m, i: c[i])
 
     def equality_constraint_rule(m, i):
         return sum(m.jacobian_c[i, j] * m.x[j] for j in m.var_ids) == m.rhs[i]
-    model.equalities = aml.Constraint(model.con_ids, rule=equality_constraint_rule)
+    model.equalities = pyo.Constraint(model.con_ids, rule=equality_constraint_rule)
 
     def objective_rule(m):
         accum = 0.0
@@ -83,40 +82,40 @@ def create_model3(G, A, b, c):
         accum += sum(m.x[j] * m.grad_f[j] for j in m.var_ids)
         return accum
 
-    model.obj = aml.Objective(rule=objective_rule, sense=aml.minimize)
+    model.obj = pyo.Objective(rule=objective_rule, sense=pyo.minimize)
 
     return model
 
 def create_model4():
-    m = aml.ConcreteModel()
-    m.x = aml.Var([1, 2], initialize=1.0)
-    m.c1 = aml.Constraint(expr=m.x[1] + m.x[2] - 1 == 0)
-    m.obj = aml.Objective(expr=2 * m.x[1] ** 2 + m.x[2] ** 2)
+    m = pyo.ConcreteModel()
+    m.x = pyo.Var([1, 2], initialize=1.0)
+    m.c1 = pyo.Constraint(expr=m.x[1] + m.x[2] - 1 == 0)
+    m.obj = pyo.Objective(expr=2 * m.x[1] ** 2 + m.x[2] ** 2)
     return m
 
 
 def create_model6():
-    model = aml.ConcreteModel()
+    model = pyo.ConcreteModel()
 
     model.S = [1, 2]
-    model.x = aml.Var(model.S, initialize=1.0)
+    model.x = pyo.Var(model.S, initialize=1.0)
 
     def f(model):
-        return model.x[1] ** 4 + (model.x[1] + model.x[2]) ** 2 + (-1.0 + aml.exp(model.x[2])) ** 2
+        return model.x[1] ** 4 + (model.x[1] + model.x[2]) ** 2 + (-1.0 + pyo.exp(model.x[2])) ** 2
 
-    model.f = aml.Objective(rule=f)
+    model.f = pyo.Objective(rule=f)
     return model
 
 
 def create_model9():
     # clplatea OXR2-MN-V-0
-    model = aml.ConcreteModel()
+    model = pyo.ConcreteModel()
 
     p = 71
     wght = -0.1
     hp2 = 0.5 * p ** 2
 
-    model.x = aml.Var(aml.RangeSet(1, p), aml.RangeSet(1, p), initialize=0.0)
+    model.x = pyo.Var(pyo.RangeSet(1, p), pyo.RangeSet(1, p), initialize=0.0)
 
     def f(model):
         return sum(0.5 * (model.x[i, j] - model.x[i, j - 1]) ** 2 + \
@@ -125,7 +124,7 @@ def create_model9():
                    hp2 * (model.x[i, j] - model.x[i - 1, j]) ** 4 \
                    for i in range(2, p + 1) for j in range(2, p + 1)) + (wght * model.x[p, p])
 
-    model.f = aml.Objective(rule=f)
+    model.f = pyo.Objective(rule=f)
 
     for j in range(1, p + 1):
         model.x[1, j] = 0.0
