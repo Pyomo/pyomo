ae4a7530b596147937d373837c24bc1a01c048dc
Qi Chen <qichen@andrew.cmu.edu>
2020-01-27 14:12:02 -0500

    Add demonstration of AtLeastExpression + two tests that do not yet produce correct results

--- pyomo/core/plugins/transform/test_logical_to_linear.py
+++ pyomo/core/plugins/transform/test_logical_to_linear.py
@@ -1,4 +1,6 @@
 import pyutilib.th as unittest
+
+from pyomo.core.expr.logical_expr import AtLeast
 from pyomo.environ import ConcreteModel, AbstractModel, BooleanVar, LogicalStatement, TransformationFactory, RangeSet
 
 
@@ -7,23 +9,41 @@ class TestLogicalToLinearTransformation(unittest.TestCase):
         m = ConcreteModel()
         m.x = BooleanVar()
         m.y = BooleanVar()
-
         m.p = LogicalStatement(expr=m.x.implies(m.y))
-
         TransformationFactory('core.logical_to_linear').apply_to(m)
-
         m.pprint()
 
     def test_longer_statement(self):
         m = ConcreteModel()
         m.s = RangeSet(3)
         m.Y = BooleanVar(m.s)
-
         m.p = LogicalStatement(expr=m.Y[1] >> (m.Y[2] | m.Y[3]))
-
         TransformationFactory('core.logical_to_linear').apply_to(m)
-
         m.pprint()
+
+    def test_xfrm_atleast_statement(self):
+        m = ConcreteModel()
+        m.s = RangeSet(3)
+        m.Y = BooleanVar(m.s)
+        m.p = LogicalStatement(expr=AtLeast(2, m.Y[1], m.Y[2], m.Y[3]))
+        TransformationFactory('core.logical_to_linear').apply_to(m)
+        m.pprint()
+
+        # TODO two below do not work yet
+
+        # m = ConcreteModel()
+        # m.s = RangeSet(3)
+        # m.Y = BooleanVar(m.s)
+        # m.p = LogicalStatement(expr=m.Y[1] >> AtLeast(2, m.Y[1], m.Y[2], m.Y[3]))
+        # TransformationFactory('core.logical_to_linear').apply_to(m)
+        # m.pprint()
+
+        # m = ConcreteModel()
+        # m.s = RangeSet(3)
+        # m.Y = BooleanVar(m.s)
+        # m.p = LogicalStatement(expr=m.Y[1] >> AtLeast(2, m.Y))
+        # TransformationFactory('core.logical_to_linear').apply_to(m)
+        # m.pprint()
 
 
 if __name__ == "__main__":
