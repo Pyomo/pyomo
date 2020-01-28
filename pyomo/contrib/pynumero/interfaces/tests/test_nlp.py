6a25e0b9754ad706903ce799d4dbd024e4273a78
Bethany Nicholson <blnicho@sandia.gov>
2020-01-28 10:48:26 -0700

    Enable PyNumero tests on Windows

--- pyomo/contrib/pynumero/interfaces/tests/test_nlp.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_nlp.py
@@ -363,7 +363,6 @@ class TestAslNLP(unittest.TestCase):
         anlp = AslNLP(self.filename)
         execute_extended_nlp_interface(self, anlp)
         
-@unittest.skipIf(os.name in ['nt', 'dos'], "Do not test on windows")
 class TestAmplNLP(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -441,7 +440,6 @@ class TestAmplNLP(unittest.TestCase):
         self.assertEqual(sum(ineq_constraint_idxs), 3)
 
 
-@unittest.skipIf(os.name in ['nt', 'dos'], "Do not test on windows")
 class TestPyomoNLP(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -550,7 +548,6 @@ class TestPyomoNLP(unittest.TestCase):
         with self.assertRaises(NotImplementedError):
             nlp = PyomoNLP(m)
 
-@unittest.skipIf(os.name in ['nt', 'dos'], "Do not test on windows")
 class TestUtils(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
