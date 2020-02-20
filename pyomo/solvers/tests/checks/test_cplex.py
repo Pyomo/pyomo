81f0a6e6b2c23a188e069f772d051eaec83e8aa3
Ruaridh Williamson <ruaridh.williamson@flexciton.com>
2020-02-20 14:18:09 +0000

    :rotating_light: Add tests of solving with priorities
    - End-to-end tests of `opt.solve()` using branching priorities with CPLEXSHELL

--- pyomo/solvers/tests/checks/test_cplex.py
+++ pyomo/solvers/tests/checks/test_cplex.py
@@ -9,12 +9,13 @@
 #  ___________________________________________________________________________
 
 import os
+import sys
 
 import pyutilib
 import pyutilib.th as unittest
 
-from pyomo.core import Binary, ConcreteModel, Constraint, Objective, Var
-from pyomo.opt import ProblemFormat, convert_problem
+from pyomo.core import Binary, ConcreteModel, Constraint, Objective, Var, Integers, RangeSet, minimize, quicksum
+from pyomo.opt import ProblemFormat, convert_problem, SolverFactory
 from pyomo.solvers.plugins.solvers.CPLEX import (CPLEXSHELL, MockCPLEX,
                                                  _validate_file_name)
 
@@ -61,7 +62,7 @@ class CPLEX_utils(unittest.TestCase):
             _validate_file_name(_128, fname, 'xxx')
 
 
-class CPLEXShellPrioritiesFile(unittest.TestCase):
+class CPLEXShellWritePrioritiesFile(unittest.TestCase):
     def setUp(self):
         from pyomo.solvers.plugins.converter.model import PyomoMIPConverter  # register the `ProblemConverterFactory`
         from pyomo.repn.plugins.cpxlp import ProblemWriter_cpxlp  # register the `WriterFactory`
@@ -151,6 +152,99 @@ class CPLEXShellPrioritiesFile(unittest.TestCase):
         self.assertEqual(
             priorities_file,
             "* ENCODING=ISO-8859-1\nNAME             Priority Order\n  x1 10\nENDATA\n",
+        )
+
+
+class CPLEXShellSolvePrioritiesFile(unittest.TestCase):
+    def setUp(self):
+        from pyomo.solvers.plugins.converter.model import PyomoMIPConverter  # register the `ProblemConverterFactory`
+        from pyomo.repn.plugins.cpxlp import ProblemWriter_cpxlp  # register the `WriterFactory`
+
+    def get_mock_model_with_priorities(self):
+        m = ConcreteModel()
+        m.x = Var(domain=Integers)
+        m.s = RangeSet(10)
+        m.y = Var(m.s, domain=Integers)
+        m.o = Objective(expr=m.x + sum(m.y), sense=minimize)
+        m.c = Constraint(expr=m.x >= 1)
+        m.c2 = Constraint(expr=quicksum(m.y[i] for i in m.s) >= 10)
+
+        m.x.branch_priority = 1
+
+        for var in m.y.values():
+            var.branch_priority = 2
+            var.branch_direction = -1
+
+        m.y[10].branch_direction = 1
+        return m
+
+    def test_use_variable_priorities(self):
+        model = self.get_mock_model_with_priorities()
+        with SolverFactory('cplex', solver_io='lp') as opt:
+            opt.solve(model, priorities=True, keepfiles=True)
+
+            with open(opt._priorities_file_name, 'r') as ord_file:
+                priorities_file = ord_file.read()
+
+        assert priorities_file == (
+            '* ENCODING=ISO-8859-1\n'
+            'NAME             Priority Order\n'
+            '  x1 1\n'
+            ' DN x2 2\n'
+            ' DN x3 2\n'
+            ' DN x4 2\n'
+            ' DN x5 2\n'
+            ' DN x6 2\n'
+            ' DN x7 2\n'
+            ' DN x8 2\n'
+            ' DN x9 2\n'
+            ' DN x10 2\n'
+            ' UP x11 2\n'
+            'ENDATA\n'
+        )
+
+        assert "read %s\n" % (opt._priorities_file_name,) in opt._command.script
+
+    def test_ignore_variable_priorities(self):
+        model = self.get_mock_model_with_priorities()
+        with SolverFactory('cplex', solver_io='lp') as opt:
+            opt.solve(model, priorities=False, keepfiles=True)
+
+            assert opt._priorities_file_name is None
+            assert ".ord" not in opt._command.script
+
+    def test_can_use_manual_priorities_file_with_lp_solve(self):
+        """ Test that we can pass an LP file (not a pyomo model) along with a priorities file to `.solve()` """
+        model = self.get_mock_model_with_priorities()
+
+        with SolverFactory('cplex', solver_io='lp') as pre_opt:
+            pre_opt._presolve(model, priorities=True, keepfiles=True)
+            lp_file = pre_opt._problem_files[0]
+            priorities_file_name = pre_opt._priorities_file_name
+
+        with SolverFactory('cplex', solver_io='lp') as opt:
+            opt.solve(lp_file, priorities=True, priorities_file=priorities_file_name, keepfiles=True)
+
+            assert ".ord" in opt._command.script
+
+            with open(opt._priorities_file_name, 'r') as ord_file:
+                priorities_file = ord_file.read()
+
+        assert priorities_file == (
+            '* ENCODING=ISO-8859-1\n'
+            'NAME             Priority Order\n'
+            '  x1 1\n'
+            ' DN x2 2\n'
+            ' DN x3 2\n'
+            ' DN x4 2\n'
+            ' DN x5 2\n'
+            ' DN x6 2\n'
+            ' DN x7 2\n'
+            ' DN x8 2\n'
+            ' DN x9 2\n'
+            ' DN x10 2\n'
+            ' UP x11 2\n'
+            'ENDATA\n'
         )
 
 
