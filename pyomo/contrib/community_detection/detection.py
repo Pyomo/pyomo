557e50fa0527dac9540d637c5a7c237b86ff0ce1
Rahul Joglekar <rjogleka@andrew.cmu.edu>
2020-02-28 16:22:30 -0500

    Setting up framework to add community detection

---
+++ pyomo/contrib/community_detection/detection.py
@@ -0,0 +1,16 @@
+"""Community detection code"""
+
+
+def detect_constraint_communities(model):
+    """
+
+    :return:
+    """
+    return dict()
+
+def detect_variable_communities(model):
+    """
+
+    :return:
+    """
+    return dict()
