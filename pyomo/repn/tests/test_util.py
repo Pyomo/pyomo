57262e0f83a4c667ea280856c4b61103cff8edb2
John Siirola <jsiirola@users.noreply.github.com>
2020-03-03 14:56:43 -0700

    Updating the ftoa tests to trigger loss of precision warning

--- pyomo/repn/tests/test_util.py
+++ pyomo/repn/tests/test_util.py
@@ -6,20 +6,31 @@ from six import StringIO
 from pyomo.common.log import LoggingIntercept
 from pyomo.repn.util import ftoa
 
+try:
+    import numpy as np
+    numpy_available = True
+except:
+    numpy_available = False
 
 class TestRepnUtils(unittest.TestCase):
     def test_ftoa(self):
-        warning_output = StringIO()
-        with LoggingIntercept(warning_output, 'pyomo.core', logging.WARNING):
-            x1 = 1.123456789012345678
-            x1str = ftoa(x1)
-            self.assertEqual(x1str, '1.1234567890123457')
-            # self.assertIn("to string resulted in loss of precision", warning_output.getvalue())
-            # Not sure how to construct a case that hits that part of the code, but it should be done.
-        x2 = 1.0 + 1E-15
-        x2str = ftoa(x2)
-        self.assertEqual(x2str, str(x2))
+        # Test that trailing zeros are removed
+        f = 1.0
+        a = ftoa(f)
+        self.assertEqual(a, '1')
 
+    @unittest.skipIf(not numpy_available, "NumPy is not available")
+    def test_ftoa_precision(self):
+        log = StringIO()
+        with LoggingIntercept(log, 'pyomo.core', logging.WARNING):
+            f = np.longdouble('1.1234567890123456789')
+            a = ftoa(f)
+        self.assertNotEqual(f, float(a))
+        self.assertEqual(a, '1.1234567890123457')
+        self.assertRegexpMatches(
+            log.getvalue(),
+            '.*Converting 1.1234567890123456789 to string '
+            'resulted in loss of precision')
 
 if __name__ == "__main__":
     unittest.main()
