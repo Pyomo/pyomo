534335cb828a721e8b85e5b0e90dcf3b1462b61a
David L. Woodruff <dlwoodruff@ucdavis.edu>
2020-03-15 18:28:00 -0700

    bootstrap works, but I still need to write a test

--- pyomo/contrib/parmest/ScenarioCreator.py
+++ pyomo/contrib/parmest/ScenarioCreator.py
@@ -11,6 +11,9 @@ class ScenarioSet(object):
     Class to hold scenario sets
     Args:
     name (str): name of the set (might be "")
+    NOTE: Delete this note by May 2020
+         As of March 2020, this uses a list as the underlying data structure.
+         The list could be changed to a dataframe with not outside impact.
     """
 
     def __init__(self, name):
@@ -36,18 +39,44 @@ class ScenarioSet(object):
         newlist = self.scens + set1.scens
         retval = ScenarioSet(newname)
         retval.scens = newlist
-        
+        return retval
+
+
+    def append_bootstrap(self, bootstrap_theta):
+        """ Append a boostrap theta df to the scenario set; equally likely
+        Args:
+            boostrap_theta (dataframe): created by the bootstrap
+        Note: this can be cleaned up a lot with the list becomes a df,
+              which is why I put it in the ScenarioSet class.
+        """
+        assert(len(bootstrap_theta > 0))
+        prob = 1. / len(bootstrap_theta)
+
+        # dict of ThetaVal dicts
+        dfdict = bootstrap_theta.to_dict(orient='index')
+
+        for index, ThetaVals in dfdict.items():
+            name = "Boostrap"+str(index)
+            self.addone(_ParmestScen(name, ThetaVals, prob))
+
 
     def write_csv(self, filename):
         """ write a csv file with the scenarios in the set
         Args:
             filename (str): full path and full name of file
         """
+        if len(self.scens) == 0:
+            print ("Empty scenario set, not writing file={}".format(filename))
+            return
         with open(filename, "w") as f:
+            f.write("Name,Probability")
+            for n in self.scens[0].ThetaVals.keys():
+                f.write(",{}".format(n))
+            f.write('\n')
             for s in self.scens:
-                f.write("{}, {}".format(s.name, s.probability))
-                for n,v in s.ThetaVals.items():
-                    f.write(", {}, {}".format(n,v))
+                f.write("{},{}".format(s.name, s.probability))
+                for v in s.ThetaVals.values():
+                    f.write(",{}".format(v))
                 f.write('\n')
 
 
@@ -61,7 +90,9 @@ class _ParmestScen(object):
         self.ThetaVals = ThetaVals
         self.probability = probability
 
-        
+############################################################
+
+
 class ScenarioCreator(object):
     """ Create scenarios from parmest.
 
@@ -89,7 +120,8 @@ class ScenarioCreator(object):
         prob = 1. / len(self.pest._numbers_list)
         for exp_num in self.pest._numbers_list:
             print("Experiment number=", exp_num)
-            model = self.pest._instance_creation_callback(exp_num, data)
+            model = self.pest._instance_creation_callback(exp_num,
+                                                        self.pest.callback_data)
             opt = pyo.SolverFactory(self.solvername)
             results = opt.solve(model)  # solves and updates model
             ## pyo.check_termination_optimal(results)
@@ -101,7 +133,19 @@ class ScenarioCreator(object):
                 ThetaVals[theta] = tval
             addtoSet.addone(_ParmestScen("ExpScen"+str(exp_num), ThetaVals, prob))
             
+    def ScenariosFromBoostrap(self, addtoSet, numtomake):
+        """Creates new self.Scenarios list using the experiments only.
+        Args:
+            addtoSet (ScenarioSet): the scenarios will be added to this set
+            numtomake (int) : number of scenarios to create
+        """
 
+        assert(isinstance(addtoSet, ScenarioSet))
+
+        bootstrap_thetas = self.pest.theta_est_bootstrap(numtomake)
+        addtoSet.append_bootstrap(bootstrap_thetas)
+
+        
 if __name__ == "__main__":
     # quick test using semibatch
     import pyomo.contrib.parmest.examples.semibatch.semibatch as sb
@@ -124,6 +168,12 @@ if __name__ == "__main__":
     
     scenmaker = ScenarioCreator(pest, "ipopt")
 
-    experimentscens = ScenarioSet("Experiments")
-    scenmaker.ScenariosFromExperiments(experimentscens)
-    experimentscens.write_csv("delme_exp_csv.csv")
+    ####experimentscens = ScenarioSet("Experiments")
+    ####scenmaker.ScenariosFromExperiments(experimentscens)
+    ####experimentscens.write_csv("delme_exp_csv.csv")
+
+    bootscens = ScenarioSet("Bootstrap")
+    numtomake = 3
+    scenmaker.ScenariosFromBoostrap(bootscens, numtomake)
+    
+    bootscens.write_csv("delme_boot_csv.csv")
