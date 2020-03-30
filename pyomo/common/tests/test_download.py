7198a24879ae146e9c884f7a30bbf6dc5895698b
John Siirola <jsiirola@users.noreply.github.com>
2020-03-30 15:38:10 -0600

    Add FileDownloader.get_os_version()
    This adds a utility method that makes a best effort to determine the os
    platform, for use when determining appropriate binary images to download.

--- pyomo/common/tests/test_download.py
+++ pyomo/common/tests/test_download.py
@@ -11,17 +11,19 @@
 import io
 import os
 import platform
+import re
 import six
 import shutil
 import tempfile
 
 import pyutilib.th as unittest
 from pyutilib.misc import capture_output
+from pyutilib.subprocess import run
 
 from pyomo.common import DeveloperError
 from pyomo.common.config import PYOMO_CONFIG_DIR
 from pyomo.common.fileutils import this_file
-from pyomo.common.download import FileDownloader
+from pyomo.common.download import FileDownloader, distro_available
 
 class Test_FileDownloader(unittest.TestCase):
     def setUp(self):
@@ -138,6 +140,69 @@ class Test_FileDownloader(unittest.TestCase):
         self.assertTrue(platform.system().lower().startswith(ans[0]))
         self.assertFalse(any(c in ans[0] for c in '.-_'))
         self.assertIn(ans[1], (32,64))
+
+    def test_get_os_version(self):
+        f = FileDownloader()
+        _os, _ver = f.get_os_version(normalize=False)
+        _norm = f.get_os_version(normalize=True)
+        _sys = f.get_sysinfo()[0]
+        if _sys == 'linux':
+            dist, dist_ver = re.match('^([^0-9]+)(.*)', _norm).groups()
+            self.assertNotIn('.', dist_ver)
+            self.assertGreater(int(dist_ver), 0)
+            if dist == 'ubuntu':
+                self.assertEqual(dist_ver, ''.join(_ver.split('.')[:2]))
+            else:
+                self.assertEqual(dist_ver, _ver.split('.')[0])
+
+            if distro_available:
+                d, v = f._get_distver_from_distro()
+                self.assertEqual(_os, d)
+                self.assertEqual(_ver, v)
+                self.assertTrue(v.startswith(dist_ver))
+
+            if os.path.exists('/etc/redhat-release'):
+                d, v = f._get_distver_from_redhat_release()
+                self.assertEqual(_os, d)
+                self.assertEqual(_ver, v)
+                self.assertTrue(v.startswith(dist_ver))
+
+            if run(['lsb_release'])[0] == 0:
+                d, v = f._get_distver_from_lsb_release()
+                self.assertEqual(_os, d)
+                self.assertEqual(_ver, v)
+                self.assertTrue(v.startswith(dist_ver))
+
+            if os.path.exists('/etc/os-release'):
+                d, v = f._get_distver_from_os_release()
+                self.assertEqual(_os, d)
+                # Note that (at least on centos), os_release is an
+                # imprecise version string
+                self.assertTrue(_ver.startswith(v))
+                self.assertTrue(v.startswith(dist_ver))
+
+        elif _sys == 'darwin':
+            dist, ver = re.match('^([^0-9]+)(.*)', _norm).groups()
+            self.assertEqual(_os, 'macos')
+            self.assertEqual(dist, 'macos')
+            self.assertIn('.', ver)
+            self.assertGreater(int(ver.split('.')[0]), 0)
+        elif _sys == 'windows':
+            self.assertEqual(_os, 'win')
+            self.assertEqual(_norm, _os+_ver)
+        else:
+            self.assertEqual(ans, '')
+
+        self.assertEqual((_os, _ver), FileDownloader._os_version)
+        # Exercise the fetch from CACHE
+        try:
+            FileDownloader._os_version, tmp \
+                = ("test", '2'), FileDownloader._os_version
+            self.assertEqual(f.get_os_version(False), ("test","2"))
+            self.assertEqual(f.get_os_version(), "test2")
+        finally:
+            FileDownloader._os_version = tmp
+
 
     def test_get_platform_url(self):
         f = FileDownloader()
