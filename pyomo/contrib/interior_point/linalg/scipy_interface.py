b27b728259160c09fc5dce93c85c8c7817a70ee0
Michael Bynum <michaelbynum@users.noreply.github.com>
2020-04-03 09:45:27 -0600

    updates to scipy interface for interior point

--- pyomo/contrib/interior_point/linalg/scipy_interface.py
+++ pyomo/contrib/interior_point/linalg/scipy_interface.py
@@ -1,27 +1,29 @@
 from .base_linear_solver_interface import LinearSolverInterface
 from scipy.sparse.linalg import splu
-from scipy.linalg import eigvalsh
+from scipy.linalg import eigvals
 from scipy.sparse import isspmatrix_csc
 from pyomo.contrib.pynumero.sparse.block_vector import BlockVector
+import numpy as np
 
 
 class ScipyInterface(LinearSolverInterface):
-    def __init__(self):
+    def __init__(self, compute_inertia=False):
         self._lu = None
         self._inertia = None
+        self.compute_inertia = compute_inertia
 
     def do_symbolic_factorization(self, matrix):
         pass
 
-    def do_numeric_factorization(self, matrix, compute_inertia=False):
+    def do_numeric_factorization(self, matrix):
         if not isspmatrix_csc(matrix):
             matrix = matrix.tocsc()
         self._lu = splu(matrix)
-        if compute_inertia:
-            eig = eigvalsh(matrix.toarray())
-            pos_eig = (eig > 0).nonzero()[0]
-            neg_eigh = (eig < 0).nonzero()[0]
-            zero_eig = (eig == 0).nonzero()[0]
+        if self.compute_inertia:
+            eig = eigvals(matrix.toarray())
+            pos_eig = np.count_nonzero((eig > 0))
+            neg_eigh = np.count_nonzero((eig < 0))
+            zero_eig = np.count_nonzero(eig == 0)
             self._inertia = (pos_eig, neg_eigh, zero_eig)
 
     def do_back_solve(self, rhs):
@@ -41,5 +43,5 @@ class ScipyInterface(LinearSolverInterface):
 
     def get_inertia(self):
         if self._inertia is None:
-            raise RuntimeError('The intertia was not computed during do_numeric_factorization.')
+            raise RuntimeError('The intertia was not computed during do_numeric_factorization. Set compute_inertia to True.')
         return self._inertia
