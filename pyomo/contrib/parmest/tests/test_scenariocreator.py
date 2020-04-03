f9a2bccba765971fa449e8e68534b3a9d9e4ef40
David L. Woodruff <DLWoodruff@UCDavis.edu>
2020-04-02 19:04:16 -0700

    ../examples/semibatch/scencreate.py

--- pyomo/contrib/parmest/tests/test_scenariocreator.py
+++ pyomo/contrib/parmest/tests/test_scenariocreator.py
@@ -25,6 +25,7 @@ import pyomo.contrib.parmest.scenariocreator as sc
 import pyomo.contrib.parmest.graphics as graphics
 import pyomo.contrib.parmest as parmestbase
 import pyomo.environ as pyo
+import pyomo.contrib.parmest.examples.semibatch.scencreate as sbc
 
 from pyomo.opt import SolverFactory
 ipopt_available = SolverFactory('ipopt').available()
@@ -109,12 +110,12 @@ class  pamest_Scenario_creator_semibatch(unittest.TestCase):
         # Vars to estimate in parmest
         theta_names = ['k1', 'k2', 'E1', 'E2']
 
-        fbase = os.path.join(testdir,"..","examples","semibatch")
+        self.fbase = os.path.join(testdir,"..","examples","semibatch")
         # Data, list of dictionaries
         data = [] 
         for exp_num in range(10):
             fname = "exp"+str(exp_num+1)+".out"
-            fullname = os.path.join(fbase, fname)
+            fullname = os.path.join(self.fbase, fname)
             with open(fullname,'r') as infile:
                 d = json.load(infile)
                 data.append(d)
@@ -124,14 +125,20 @@ class  pamest_Scenario_creator_semibatch(unittest.TestCase):
 
         self.pest = parmest.Estimator(sb.generate_model, data, theta_names)
 
+    @unittest.skipIf(True, "Bootstrap is tested by semibatch_example")
     def test_semibatch_bootstrap(self):
 
         scenmaker = sc.ScenarioCreator(self.pest, "ipopt")
         bootscens = sc.ScenarioSet("Bootstrap")
-        numtomake = 3
+        numtomake = 2
         scenmaker.ScenariosFromBoostrap(bootscens, numtomake, seed=1134)
         tval = bootscens.ScenarioNumber(0).ThetaVals["k1"]
         self.assertAlmostEqual(tval, 20.64, places=1)
+
+    def test_semibatch_example(self):
+        # this is referenced in the documentation so at least look for smoke
+        sbc.main(self.fbase)
+        
         
 if __name__ == '__main__':
     unittest.main()
