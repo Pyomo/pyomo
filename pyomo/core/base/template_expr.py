6e7557f74613e6db466c64d1705ebab4c8e43296
John Siirola <jsiirola@users.noreply.github.com>
2020-04-22 01:07:44 -0600

    Adding pyomo.core.base.template_expr deprecation module

---
+++ pyomo/core/base/template_expr.py
@@ -0,0 +1,19 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+from pyomo.core.expr.template_expr import (
+    IndexTemplate, _GetItemIndexer, TemplateExpressionError
+)
+
+from pyomo.common.deprecation import deprecation_warning
+deprecation_warning(
+    'The pyomo.core.base.template_expr module is deprecated.  '
+    'Import expression template objects from pyomo.core.expr.template_expr.',
+    version='TBD')
