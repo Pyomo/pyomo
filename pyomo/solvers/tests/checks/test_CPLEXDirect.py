8e4be65140863a12d38e26de497c613a3dca3e37
Ruaridh Williamson <ruaridh.williamson@flexciton.com>
2020-05-06 13:10:30 +0100

    :rotating_light: Test `_add_constraint()`

--- pyomo/solvers/tests/checks/test_CPLEXDirect.py
+++ pyomo/solvers/tests/checks/test_CPLEXDirect.py
@@ -998,5 +998,112 @@ class TestAddVar(unittest.TestCase):
         self.assertEqual(opt._solver_model.variables.get_num(), 3)
 
 
+@unittest.skipIf(not unittest.mock_available, "'mock' is not available")
+@unittest.skipIf(not cplexpy_available, "The 'cplex' python bindings are not available")
+class TestAddCon(unittest.TestCase):
+    def test_add_single_constraint(self):
+        model = ConcreteModel()
+        model.X = Var(within=Binary)
+
+        opt = SolverFactory("cplex", solver_io="python")
+        opt._set_instance(model)
+
+        self.assertEqual(opt._solver_model.linear_constraints.get_num(), 0)
+
+        model.C = Constraint(expr=model.X == 1)
+
+        con_interface = opt._solver_model.linear_constraints
+        with unittest.mock.patch.object(
+            con_interface, "add", wraps=con_interface.add
+        ) as wrapped_add_call:
+            opt._add_constraint(model.C)
+
+            self.assertEqual(wrapped_add_call.call_count, 1)
+            self.assertEqual(
+                wrapped_add_call.call_args,
+                (
+                    {
+                        "lin_expr": [[[0], (1,)]],
+                        "names": ["x2"],
+                        "range_values": [0.0],
+                        "rhs": [1.0],
+                        "senses": ["E"],
+                    },
+                ),
+            )
+
+        self.assertEqual(opt._solver_model.linear_constraints.get_num(), 1)
+
+    def test_add_block_containing_single_constraint(self):
+        model = ConcreteModel()
+        model.X = Var(within=Binary)
+
+        opt = SolverFactory("cplex", solver_io="python")
+        opt._set_instance(model)
+
+        self.assertEqual(opt._solver_model.linear_constraints.get_num(), 0)
+
+        model.B = Block()
+        model.B.C = Constraint(expr=model.X == 1)
+
+        con_interface = opt._solver_model.linear_constraints
+        with unittest.mock.patch.object(
+            con_interface, "add", wraps=con_interface.add
+        ) as wrapped_add_call:
+            opt._add_block(model.B)
+
+            self.assertEqual(wrapped_add_call.call_count, 1)
+            self.assertEqual(
+                wrapped_add_call.call_args,
+                (
+                    {
+                        "lin_expr": [[[0], (1,)]],
+                        "names": ["x2"],
+                        "range_values": [0.0],
+                        "rhs": [1.0],
+                        "senses": ["E"],
+                    },
+                ),
+            )
+
+        self.assertEqual(opt._solver_model.linear_constraints.get_num(), 1)
+
+    def test_add_block_containing_multiple_constraints(self):
+        model = ConcreteModel()
+        model.X = Var(within=Binary)
+
+        opt = SolverFactory("cplex", solver_io="python")
+        opt._set_instance(model)
+
+        self.assertEqual(opt._solver_model.linear_constraints.get_num(), 0)
+
+        model.B = Block()
+        model.B.C1 = Constraint(expr=model.X == 1)
+        model.B.C2 = Constraint(expr=model.X <= 1)
+        model.B.C3 = Constraint(expr=model.X >= 1)
+
+        con_interface = opt._solver_model.linear_constraints
+        with unittest.mock.patch.object(
+            con_interface, "add", wraps=con_interface.add
+        ) as wrapped_add_call:
+            opt._add_block(model.B)
+
+            self.assertEqual(wrapped_add_call.call_count, 1)
+            self.assertEqual(
+                wrapped_add_call.call_args,
+                (
+                    {
+                        "lin_expr": [[[0], (1,)], [[0], (1,)], [[0], (1,)]],
+                        "names": ["x2", "x3", "x4"],
+                        "range_values": [0.0, 0.0, 0.0],
+                        "rhs": [1.0, 1.0, 1.0],
+                        "senses": ["E", "L", "G"],
+                    },
+                ),
+            )
+
+        self.assertEqual(opt._solver_model.linear_constraints.get_num(), 3)
+
+
 if __name__ == "__main__":
     unittest.main()
