365c8002980c4d15edae8be4fdaaf1db440fed71
Qi Chen <qichen@andrew.cmu.edu>
2020-05-12 12:56:15 -0400

    adding tests

--- pyomo/core/plugins/transform/test_logical_to_linear.py
+++ pyomo/core/plugins/transform/test_logical_to_linear.py
@@ -4,8 +4,10 @@ from pyomo.core.expr.logical_expr import AtLeast, AtMost, Exactly
 from pyomo.core.expr.sympy_tools import sympy_available
 from pyomo.core.plugins.transform.logical_to_linear import update_boolean_vars_from_binary
 from pyomo.environ import ConcreteModel, BooleanVar, LogicalStatement, TransformationFactory, RangeSet, \
-    Var, Constraint
+    Var, Constraint, ComponentMap, value
 from pyomo.gdp import Disjunct, Disjunction
+from pyomo.gdp.tests.test_bigm import check_linear_coef
+from pyomo.repn import generate_standard_repn
 
 
 def _generate_boolean_model(nvars):
@@ -15,34 +17,80 @@ def _generate_boolean_model(nvars):
     return m
 
 
+def _constrs_contained_within(test_case, test_constr_tuples, constraint_list):
+    """Checks to see if constraints defined by test_constr_tuples are in the constraint list.
+
+    Parameters
+    ----------
+    constraint_list : Constraint
+    test_constr_tuples : list of tuple
+    test_case : unittest.TestCase
+    """
+    # Move const term from body
+    def _move_const_from_body(lower, repn, upper):
+        if repn.constant is not None and not repn.constant == 0:
+            if lower is not None:
+                lower -= repn.constant
+            if upper is not None:
+                upper -= repn.constant
+        return value(lower), repn, value(upper)
+
+    def _repns_match(repn, test_repn):
+        if not len(repn.linear_vars) == len(test_repn.linear_vars):
+            return False
+        coef_map = ComponentMap((var, coef) for var, coef in zip(repn.linear_vars, repn.linear_coefs))
+        for var, coef in zip(test_repn.linear_vars, test_repn.linear_coefs):
+            if not coef_map.get(var, 0) == coef:
+                return False
+        return True
+
+    constr_list_tuples = [
+        _move_const_from_body(constr.lower, generate_standard_repn(constr.body), constr.upper)
+        for constr in constraint_list.values()]
+    for test_lower, test_body, test_upper in test_constr_tuples:
+        test_repn = generate_standard_repn(test_body)
+        test_lower, test_repn, test_upper = _move_const_from_body(test_lower, test_repn, test_upper)
+        found_match = False
+        # Make sure one of the list tuples matches
+        for lower, repn, upper in constr_list_tuples:
+            if lower == test_lower and upper == test_upper and _repns_match(repn, test_repn):
+                found_match = True
+                break
+        test_case.assertTrue(found_match, "{} <= {} <= {} was not found in constraint list.".format(
+            test_lower, test_body, test_upper))
+
+
 @unittest.skipUnless(sympy_available, "Sympy not available")
 class TestAtomicTransformations(unittest.TestCase):
+
     def test_implies(self):
         m = ConcreteModel()
         m.x = BooleanVar()
         m.y = BooleanVar()
         m.p = LogicalStatement(expr=m.x.implies(m.y))
         TransformationFactory('core.logical_to_linear').apply_to(m)
-        m.pprint()
+        _constrs_contained_within(
+            self, [(1, (1 - m.x.as_binary()) + m.y.as_binary(), None)], m.logic_to_linear)
 
     def test_literal(self):
         m = ConcreteModel()
         m.Y = BooleanVar()
         m.p = LogicalStatement(expr=m.Y)
         TransformationFactory('core.logical_to_linear').apply_to(m)
-        m.pprint()
+        _constrs_contained_within(
+            self, [(1, m.Y.as_binary(), 1)], m.logic_to_linear)
 
     def test_constant_True(self):
         m = ConcreteModel()
         m.p = LogicalStatement(expr=True)
         TransformationFactory('core.logical_to_linear').apply_to(m)
-        m.pprint()
+        self.assertIsNone(m.component('logic_to_linear'))
 
     def test_nothing_to_do(self):
         m = ConcreteModel()
         m.p = LogicalStatement()
         TransformationFactory('core.logical_to_linear').apply_to(m)
-        m.pprint()
+        self.assertIsNone(m.component('logic_to_linear'))
 
 
 @unittest.skipUnless(sympy_available, "Sympy not available")
@@ -53,7 +101,10 @@ class TestLogicalToLinearTransformation(unittest.TestCase):
         m.Y = BooleanVar(m.s)
         m.p = LogicalStatement(expr=m.Y[1] >> (m.Y[2] | m.Y[3]))
         TransformationFactory('core.logical_to_linear').apply_to(m)
-        m.pprint()
+        _constrs_contained_within(
+            self, [
+                (1, m.Y[2].as_binary() + m.Y[3].as_binary() + (1 - m.Y[1].as_binary()), None)
+            ], m.logic_to_linear)
 
     def test_xfrm_atleast_statement(self):
         m = ConcreteModel()
@@ -61,7 +112,10 @@ class TestLogicalToLinearTransformation(unittest.TestCase):
         m.Y = BooleanVar(m.s)
         m.p = LogicalStatement(expr=AtLeast(2, m.Y[1], m.Y[2], m.Y[3]))
         TransformationFactory('core.logical_to_linear').apply_to(m)
-        m.pprint()
+        _constrs_contained_within(
+            self, [
+                (2, m.Y[1].as_binary() + m.Y[2].as_binary() + m.Y[3].as_binary(), None)
+            ], m.logic_to_linear)
 
         # TODO add in other statements
         # TODO add in asserts to make sure things are generated correctly
