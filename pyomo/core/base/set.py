efa7ba62b59875bebd3ac783ad0815b8b1287b8c
Qi Chen <qichen@andrew.cmu.edu>
2020-05-12 13:15:53 -0400

    small correction

--- pyomo/core/base/set.py
+++ pyomo/core/base/set.py
@@ -2527,7 +2527,7 @@ class RangeSet(Component):
 
     Parameters
     ----------
-    *args: tuple | int | None
+    *args: int | float | None
         The range defined by ([start=1], end, [step=1]).  If only a
         single positional parameter, `end` is supplied, then the
         RangeSet will be the integers starting at 1 up through and
