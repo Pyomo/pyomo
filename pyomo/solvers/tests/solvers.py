8037b298be9ed9a5f461048caabeb22a3ebc264b
Bernard Knueven <Bernard.Knueven@nrel.gov>
2020-05-20 11:32:48 -0600

    setting barrier tolerance for xpress

--- pyomo/solvers/tests/solvers.py
+++ pyomo/solvers/tests/solvers.py
@@ -299,25 +299,29 @@ def test_solver_cases(*args):
             name='xpress',
             io='lp',
             capabilities=_xpress_capabilities,
-            import_suffixes=['dual','rc','slack'])
+            import_suffixes=['dual','rc','slack'],
+            options={'bargapstop':1e-9,})
 
         _test_solver_cases['xpress', 'mps'] = initialize(
             name='xpress',
             io='mps',
             capabilities=_xpress_capabilities,
-            import_suffixes=['dual','rc','slack'])
+            import_suffixes=['dual','rc','slack'],
+            options={'bargapstop':1e-9,})
 
         _test_solver_cases['xpress', 'nl'] = initialize(
             name='xpress',
             io='nl',
             capabilities=_xpress_capabilities,
-            import_suffixes=['dual'])
+            import_suffixes=['dual'],
+            options={'bargapstop':1e-9,})
 
         _test_solver_cases['xpress', 'python'] = initialize(
             name='xpress',
             io='mps',
             capabilities=_xpress_capabilities,
-            import_suffixes=['dual','rc','slack'])
+            import_suffixes=['dual','rc','slack'],
+            options={'bargapstop':1e-9,})
 
         #
         # XPRESS PERSISTENT 
@@ -327,7 +331,8 @@ def test_solver_cases(*args):
             name='xpress_persistent',
             io='python',
             capabilities=_xpress_capabilities,
-            import_suffixes=['slack', 'dual', 'rc'])
+            import_suffixes=['slack', 'dual', 'rc'],
+            options={'bargapstop':1e-9,})
 
         #
         # IPOPT
