6c4fe78a0c0fa1fc07603b7cba41815f0d602be6
Alex Dowling <alex@dowlinglab.org>
2020-05-29 23:12:11 -0400

    Update parmest.py
    Removed extra space.

--- pyomo/contrib/parmest/parmest.py
+++ pyomo/contrib/parmest/parmest.py
@@ -426,7 +426,6 @@ class Estimator(object):
         construct the tree just once and reuse it, then remember to
         remove thetavals from it when none is desired.
         """
-        
         assert(solver != "k_aug" or ThetaVals == None)
         # Create a tree with dummy scenarios (callback will supply when needed).
         # Which names to use (i.e., numbers) depends on if it is for bootstrap.
