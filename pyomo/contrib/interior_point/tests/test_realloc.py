77f4fc0f307186eff731f1bcaba9c42993f8b6f2
Robert Parker <robbybparker@gmail.com>
2020-06-16 14:00:24 -0400

    Skip test if not using Mumps 5.3.1

--- pyomo/contrib/interior_point/tests/test_realloc.py
+++ pyomo/contrib/interior_point/tests/test_realloc.py
@@ -7,6 +7,11 @@ np, numpy_available = attempt_import('numpy', 'Interior point requires numpy',
         minimum_version='1.13.0')
 scipy, scipy_available = attempt_import('scipy', 'Interior point requires scipy')
 mumps, mumps_available = attempt_import('mumps', 'Interior point requires mumps')
+if mumps_available:
+    mumps_version = mumps._dmumps.__version__
+else:
+    mumps_version = None
+
 if not (numpy_available and scipy_available):
     raise unittest.SkipTest('Interior point tests require numpy and scipy')
 
@@ -48,6 +53,8 @@ class TestReallocation(unittest.TestCase):
 
         return ip_solver
 
+    @unittest.skipIf(mumps_version != '5.3.1', 
+            'This test is calibrated for Mumps version 5.3.1')
     @unittest.skipIf(not mumps_available, 'Mumps is not available')
     def test_mumps(self):
         n = 20000
