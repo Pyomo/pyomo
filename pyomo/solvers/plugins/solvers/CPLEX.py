677eefef49dac05ca2b308b072f6d618a67a024a
ioanniskonstantelos <ioakonstant@gmail.com>
2020-06-22 10:46:14 +0100

    Use `rstrip()` to remove trailing characters.

--- pyomo/solvers/plugins/solvers/CPLEX.py
+++ pyomo/solvers/plugins/solvers/CPLEX.py
@@ -564,9 +564,9 @@ class CPLEXSHELL(ILMLicensedSystemCallSolver):
                 if results.problem.number_of_variables is None: # CPLEX 11.2 and subsequent versions have two Variables sections in the log file output.
                     results.problem.number_of_variables = int(tokens[2])
                 if len(tokens) >= 5 and "Nneg" in tokens[3]:
-                    results.problem.number_of_continuous_variables = int(tokens[4][:-1])  # remove trailing comma
+                    results.problem.number_of_continuous_variables = int(tokens[4].rstrip(','))
                 if len(tokens) >= 7 and "Binary" in tokens[5]:
-                    results.problem.number_of_binary_variables = int(tokens[6][:-1])  # remove trailing bracket
+                    results.problem.number_of_binary_variables = int(tokens[6].rstrip(']'))
             # In CPLEX 11 (and presumably before), there was only a single line output to
             # indicate the constriant count, e.g., "Linear constraints : 16 [Less: 7, Greater: 6, Equal: 3]".
             # In CPLEX 11.2 (or somewhere in between 11 and 11.2 - I haven't bothered to track it down
