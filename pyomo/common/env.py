52a1abccdb55ba5f99890cb1b7791ca6acbbff4c
John Siirola <jsiirola@users.noreply.github.com>
2020-07-29 17:18:50 -0600

    Fixing typos in comments/documentation

--- pyomo/common/env.py
+++ pyomo/common/env.py
@@ -34,6 +34,7 @@ def _as_unicode(val):
 
 
 def _attempt_ctypes_cdll(name):
+    """Load a CDLL library, returning bool indicating success"""
     try:
         dll = ctypes.CDLL(name)
         return True
@@ -344,7 +345,7 @@ class _Win32DLL(object):
         # points to a block of memory that contains the environment
         # strings.  Each environment string is NULL terminated, and an
         # empty string (effectively two consecutive NULLs) indicates the
-        # end of the block.  The following jult parses that data
+        # end of the block.  The following just parses that data
         # character by character to reconstitute the original
         # environment strings.
         _null = {u'\0', b'\0'}
@@ -370,6 +371,55 @@ class _Win32DLL(object):
 
 
 class CtypesEnviron(object):
+    """A context manager for managing environment variables
+
+    This class provides a simplified interface for consistently setting
+    and restoring environment variables, with special handling to ensure
+    consistency with the C Runtime Library environment on Windows
+    platforms.
+
+    `os.environ` reflects the current python environment variables, and
+    will be passed to subprocesses.  However, it does not reflect the C
+    Runtime Library (MSVCRT) environment on Windows platforms.  This can
+    be problemmatic as DLLs loaded through the CTYPES interface will see
+    the MSVCRT environment and not os.environ.  This class provides a
+    way to manage environment variables and pass changes to both
+    os.environ and the MSVCRT runtime.
+
+    This class implements a context manager API, so that clients can
+    temporarily change - and then subsequently restore - the
+    environment.
+
+    .. testcode::
+       :hide:
+
+       import os
+       from pyomo.common.env import TemporaryEnv
+       orig_env_val = os.environ.get('TEMP_ENV_VAR', None)
+
+    .. doctest::
+
+       >>> os.environ['TEMP_ENV_VAR'] = 'original value'
+       >>> print(os.environ['TEMP_ENV_VAR'])
+       original value
+
+       >>> with CtypesEnviron(TEMP_ENV_VAR='temporary value'):
+       ...    print(os.envion['TEMP_ENV_VAR'])
+       temporary value
+
+       >>> print(os.environ['TEMP_ENV_VAR'])
+       original value
+
+    .. testcode::
+       :hide:
+
+       if orig_env_val is None:
+           del os.environ['TEMP_ENV_VAR']
+       else:
+           os.environ['TEMP_ENV_VAR'] = orig_env_val
+
+    """
+
     # Windows has a number of C Runtime Libraries, each of which can
     # hold its own independent copy of the system environment
     #
@@ -391,55 +441,6 @@ class CtypesEnviron(object):
     ]
 
     def __init__(self, **kwds):
-        """A context manager for managing environment variables
-
-        This class provides a simplified interface for consistently
-        setting and restoring environment variables, with special
-        handling to ensure consistency with the C Runtime Library
-        environment on Windows platforms.
-
-        `os.environ` reflects the current python environment variables,
-        and will be passed to subprocesses.  However, it does not
-        reflect the C Runtime Library (MSVCRT) environment on Windows
-        platforms.  This can be problemmatic as DLLs loaded through the
-        CTYPES interface will see the MSVCRT environment and not
-        os.environ.  This class provides a way to manage environment
-        variables and pass changes to both os.environ and the MSVCRT
-        runtime.
-
-        This class implements a context manager API, so that clients can
-        temporarily change - and then subsequently restore - the
-        environment.
-
-        .. testcode::
-           :hide:
-
-           import os
-           from pyomo.common.env import TemporaryEnv
-           orig_env_val = os.environ.get('TEMP_ENV_VAR', None)
-
-        .. doctest::
-
-           >>> os.environ['TEMP_ENV_VAR'] = 'original value'
-           >>> print(os.environ['TEMP_ENV_VAR'])
-           original value
-
-           >>> with CtypesEnviron(TEMP_ENV_VAR='temporary value'):
-           ...    print(os.envion['TEMP_ENV_VAR'])
-           temporary value
-
-           >>> print(os.environ['TEMP_ENV_VAR'])
-           original value
-
-        .. testcode::
-           :hide:
-
-           if orig_env_val is None:
-               del os.environ['TEMP_ENV_VAR']
-           else:
-               os.environ['TEMP_ENV_VAR'] = orig_env_val
-
-        """
         self.interfaces = [
             _RestorableEnvironInterface(_OSEnviron()),
         ]
@@ -482,17 +483,19 @@ class CtypesEnviron(object):
             lib.restore()
 
     def __getitem__(self, key):
-        """Retun the current environment variable value from os.environ"""
+        """Return the current environment variable value from os.environ"""
         return os.environ[key]
 
     def __contains__(self, key):
-        """Retun true if the key is in os.environ"""
+        """Return True if the key is in os.environ"""
         return key in os.environ
 
     def __setitem__(self, key, val):
+        """Set an environment variable in all known runtime environments"""
         for lib in self.interfaces:
             lib[key] = val
 
     def __delitem__(self, key):
+        """Remove an environment variable from all known runtime environments"""
         for lib in self.interfaces:
             del lib[key]
