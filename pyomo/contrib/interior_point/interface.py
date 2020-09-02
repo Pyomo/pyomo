da5703da2157eb9363788b26de00fcf694e2cf04
Robert Parker <robbybparker@gmail.com>
2020-09-02 17:10:49 -0400

    Add equality constraint access functions to interior point interface

--- pyomo/contrib/interior_point/interface.py
+++ pyomo/contrib/interior_point/interface.py
@@ -244,7 +244,7 @@ class InteriorPointInterface(BaseInteriorPointInterface):
             self._nlp = pyomo_nlp.PyomoNLP(pyomo_model)
         self._slacks = self.init_slacks()
 
-        # set the init_duals_primals_lb/ub from ipopt_zL_out, ipopt_zU_out if available
+        # set the init_duals_primals_lb/ub from ipopt_zL_in, ipopt_zU_in if available
         # need to compress them as well and initialize the duals_primals_lb/ub 
         self._init_duals_primals_lb, self._init_duals_primals_ub =\
             self._get_full_duals_primals_bounds()
@@ -571,15 +571,15 @@ class InteriorPointInterface(BaseInteriorPointInterface):
             hasattr(self._nlp, 'get_pyomo_variables')):
             pyomo_model = self._nlp.pyomo_model()
             pyomo_variables = self._nlp.get_pyomo_variables()
-            if hasattr(pyomo_model,'ipopt_zL_out'):
-                zL_suffix = pyomo_model.ipopt_zL_out 
+            if hasattr(pyomo_model,'ipopt_zL_in'):
+                zL_suffix = pyomo_model.ipopt_zL_in
                 full_duals_primals_lb = np.empty(self._nlp.n_primals())
                 for i,v in enumerate(pyomo_variables):
                     if v in zL_suffix:
                         full_duals_primals_lb[i] = zL_suffix[v]
 
-            if hasattr(pyomo_model,'ipopt_zU_out'):
-                zU_suffix = pyomo_model.ipopt_zU_out 
+            if hasattr(pyomo_model,'ipopt_zU_in'):
+                zU_suffix = pyomo_model.ipopt_zU_in
                 full_duals_primals_ub = np.empty(self._nlp.n_primals())
                 for i,v in enumerate(pyomo_variables):
                     if v in zU_suffix:
@@ -611,11 +611,17 @@ class InteriorPointInterface(BaseInteriorPointInterface):
     def get_pyomo_constraints(self):
         return self._nlp.get_pyomo_constraints()
 
+    def get_pyomo_equality_constraints(self):
+        return self._nlp.get_pyomo_equality_constraints()
+
     def variable_names(self):
         return self._nlp.variable_names()
 
     def constraint_names(self):
         return self._nlp.constraint_names()
+
+    def equality_constraint_names(self):
+        return self._nlp.eq_constraint_names()
 
     def get_primal_indices(self, pyomo_variables):
         return self._nlp.get_primal_indices(pyomo_variables)
