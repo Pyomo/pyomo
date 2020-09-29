fixed to jacobian and further testing when additional pyomo constraints exist

pyomo/contrib/pynumero/interfaces/pyomo_nlp.py
@@ -360,7 +360,7 @@ class PyomoGreyBoxNLP(NLP):
         # Update the primal index map with any variables in the
         # greybox models that do not otherwise appear in the NL
         # and capture some other book keeping items
-        n_primals = self.n_primals()
+        n_primals = self._pyomo_nlp.n_primals()
         greybox_primals = []
         self._vardata_to_idx = ComponentMap(self._pyomo_nlp._vardata_to_idx)
         for data in greybox_data:
@@ -720,15 +720,23 @@ class PyomoGreyBoxNLP(NLP):
             return out
         else:
             base = self._pyomo_nlp.evaluate_jacobian()
+            base = coo_matrix((base.data, (base.row, base.col)), shape=(base.shape[0], self.n_primals()))
+            
+            jac = BlockMatrix(2,1)
+            jac.set_block(0, 0, base)
+            jac.set_block(1, 0, self._cached_greybox_jac)
+            return jac.tocoo()
+
             # TODO: Doesn't this need a "shape" specification?
-            return coo_matrix((
-                np.concatenate((base.data, self._cached_greybox_jac.data)),
-                ( np.concatenate((base.row, self._cached_greybox_jac.row)),
-                  np.concatenate((base.col, self._cached_greybox_jac.col)) )
-            ))
+            #return coo_matrix((
+            #    np.concatenate((base.data, self._cached_greybox_jac.data)),
+            #    ( np.concatenate((base.row, self._cached_greybox_jac.row)),
+            #      np.concatenate((base.col, self._cached_greybox_jac.col)) )
+            #))
 
     # overloaded from ExtendedNLP
     def evaluate_jacobian_eq(self, out=None):
+        raise NotImplementedError()
         self._evaluate_greybox_jacobians_and_cache_if_necessary()
 
         if out is not None:

pyomo/contrib/pynumero/interfaces/tests/test_external_grey_box_model.py
@@ -947,8 +947,6 @@ class Test_ExternalGreyBoxModelHelper(unittest.TestCase):
         x_order = pyomo_nlp.variable_names()
         comparison_c_order = ['egb.pdrop1', 'egb.pdrop3', 'egb.P2_con', 'egb.Pout_con']
         c_order = pyomo_nlp.constraint_names()
-        print(x_order)
-        print(c_order)
 
         xlb = pyomo_nlp.primals_lb()
         comparison_xlb = np.asarray([50, 1, 1, 10, 20, 15, 30], dtype=np.float64)
@@ -1019,6 +1017,125 @@ class Test_ExternalGreyBoxModelHelper(unittest.TestCase):
 
         with self.assertRaises(NotImplementedError):
             h = pyomo_nlp.evaluate_hessian_lag()
+
+    def test_external_additional_constraints_vars(self):
+        m = pyo.ConcreteModel()
+        m.hin = pyo.Var(bounds=(0,None), initialize=10)
+        m.hout = pyo.Var(bounds=(0,None))
+        m.egb = ExternalGreyBoxBlock()
+        m.egb.set_external_model(PressureDropTwoEqualitiesTwoOutputs())
+        m.incon = pyo.Constraint(expr= 0 <= m.egb.inputs['Pin'] - 10*m.hin)
+        m.outcon = pyo.Constraint(expr= 0 == m.egb.outputs['Pout'] - 10*m.hout)
+        m.egb.inputs['Pin'].value = 100
+        m.egb.inputs['Pin'].setlb(50)
+        m.egb.inputs['Pin'].setub(150)
+        m.egb.inputs['c'].value = 2
+        m.egb.inputs['c'].setlb(1)
+        m.egb.inputs['c'].setub(5)
+        m.egb.inputs['F'].value = 3
+        m.egb.inputs['F'].setlb(1)
+        m.egb.inputs['F'].setub(5)
+        m.egb.inputs['P1'].value = 80
+        m.egb.inputs['P1'].setlb(10)
+        m.egb.inputs['P1'].setub(90)
+        m.egb.inputs['P3'].value = 70
+        m.egb.inputs['P3'].setlb(20)
+        m.egb.inputs['P3'].setub(80)
+        m.egb.outputs['P2'].value = 75
+        m.egb.outputs['P2'].setlb(15)
+        m.egb.outputs['P2'].setub(85)
+        m.egb.outputs['Pout'].value = 50
+        m.egb.outputs['Pout'].setlb(30)
+        m.egb.outputs['Pout'].setub(70)
+        m.obj = pyo.Objective(expr=(m.egb.outputs['Pout']-20)**2)
+        pyomo_nlp = PyomoGreyBoxNLP(m)
+
+        self.assertEqual(9, pyomo_nlp.n_primals())
+        self.assertEqual(6, pyomo_nlp.n_constraints())
+        self.assertEqual(20, pyomo_nlp.nnz_jacobian())
+        with self.assertRaises(NotImplementedError):
+            tmp = pyomo_nlp.nnz_hessian_lag()
+
+        comparison_x_order = ['egb.inputs[Pin]', 'egb.inputs[c]', 'egb.inputs[F]',
+                              'egb.inputs[P1]', 'egb.inputs[P3]',
+                              'egb.outputs[P2]', 'egb.outputs[Pout]',
+                              'hin', 'hout']
+        x_order = pyomo_nlp.variable_names()
+        comparison_c_order = ['egb.pdrop1', 'egb.pdrop3', 'egb.P2_con', 'egb.Pout_con', 'incon', 'outcon']
+        c_order = pyomo_nlp.constraint_names()
+
+        xlb = pyomo_nlp.primals_lb()
+        comparison_xlb = np.asarray([50, 1, 1, 10, 20, 15, 30, 0, 0], dtype=np.float64)
+        check_vectors_specific_order(self, xlb, x_order, comparison_xlb, comparison_x_order)
+        xub = pyomo_nlp.primals_ub()
+        comparison_xub = np.asarray([150, 5, 5, 90, 80, 85, 70, np.inf, np.inf], dtype=np.float64)
+        check_vectors_specific_order(self, xub, x_order, comparison_xub, comparison_x_order)
+        clb = pyomo_nlp.constraints_lb()
+        comparison_clb = np.asarray([0, 0, 0, 0, 0, 0], dtype=np.float64)
+        check_vectors_specific_order(self, clb, c_order, comparison_clb, comparison_c_order)
+        cub = pyomo_nlp.constraints_ub()
+        comparison_cub = np.asarray([0, 0, 0, 0, np.inf, 0], dtype=np.float64)
+        check_vectors_specific_order(self, cub, c_order, comparison_cub, comparison_c_order)
+
+        xinit = pyomo_nlp.init_primals()
+        comparison_xinit = np.asarray([100, 2, 3, 80, 70, 75, 50, 10, 0], dtype=np.float64)
+        check_vectors_specific_order(self, xinit, x_order, comparison_xinit, comparison_x_order)
+        duals_init = pyomo_nlp.init_duals()
+        comparison_duals_init = np.asarray([1, 1, 1, 1, 0, 0], dtype=np.float64)
+        check_vectors_specific_order(self, duals_init, c_order, comparison_duals_init, comparison_c_order)
+
+        self.assertEqual(9, len(pyomo_nlp.create_new_vector('primals')))
+        self.assertEqual(6, len(pyomo_nlp.create_new_vector('constraints')))
+        self.assertEqual(6, len(pyomo_nlp.create_new_vector('duals')))
+        self.assertEqual(5, len(pyomo_nlp.create_new_vector('eq_constraints')))
+        self.assertEqual(1, len(pyomo_nlp.create_new_vector('ineq_constraints')))
+        self.assertEqual(5, len(pyomo_nlp.create_new_vector('duals_eq')))
+        self.assertEqual(1, len(pyomo_nlp.create_new_vector('duals_ineq')))
+
+        pyomo_nlp.set_primals(np.asarray([1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=np.float64))
+        x = pyomo_nlp.get_primals()
+        self.assertTrue(np.array_equal(x, np.asarray([1,2,3,4,5,6,7,8,9], dtype=np.float64)))
+        pyomo_nlp.set_primals(pyomo_nlp.init_primals())
+
+        pyomo_nlp.set_duals(np.asarray([42, 10, 11, 12, 13, 14], dtype=np.float64))
+        y = pyomo_nlp.get_duals()
+        self.assertTrue(np.array_equal(y, np.asarray([42, 10, 11, 12, 13, 14], dtype=np.float64)))
+        pyomo_nlp.set_duals(np.asarray([1, 1, 1, 1, 0, 0], dtype=np.float64))
+        y = pyomo_nlp.get_duals()
+        self.assertTrue(np.array_equal(y, np.asarray([1, 1, 1, 1, 0, 0], dtype=np.float64)))
+
+        fac = pyomo_nlp.get_obj_factor()
+        self.assertEqual(fac, 1)
+        pyomo_nlp.set_obj_factor(42)
+        self.assertEqual(pyomo_nlp.get_obj_factor(), 42)
+        pyomo_nlp.set_obj_factor(1)
+
+        f = pyomo_nlp.evaluate_objective()
+        self.assertEqual(f, 900)
+
+        gradf = pyomo_nlp.evaluate_grad_objective()
+        comparison_gradf = np.asarray([0, 0, 0, 0, 0, 0, 60, 0, 0], dtype=np.float64)
+        check_vectors_specific_order(self, gradf, x_order, comparison_gradf, comparison_x_order)
+        c = pyomo_nlp.evaluate_constraints()
+        comparison_c = np.asarray([-2, 26, -13, -22, 0, 50], dtype=np.float64)
+        check_vectors_specific_order(self, c, c_order, comparison_c, comparison_c_order)
+        c = np.zeros(6)
+        pyomo_nlp.evaluate_constraints(out=c)
+        check_vectors_specific_order(self, c, c_order, comparison_c, comparison_c_order)
+
+        j = pyomo_nlp.evaluate_jacobian()
+        comparison_j = np.asarray([[-1,   9,  12,  1, 0,  0,  0,   0,   0],
+                                   [ 0,  18,  24, -1, 1,  0,  0,   0,   0],
+                                   [ 0,  -9, -12,  1, 0, -1,  0,   0,   0],
+                                   [ 1, -36, -48,  0, 0,  0, -1,   0,   0],
+                                   [ 1,   0,   0,  0, 0,  0,  0, -10,   0],
+                                   [ 0,   0,   0,  0, 0,  0,  1,   0, -10]])
+        
+        check_sparse_matrix_specific_order(self, j, c_order, x_order, comparison_j, comparison_c_order, comparison_x_order)
+
+        with self.assertRaises(NotImplementedError):
+            h = pyomo_nlp.evaluate_hessian_lag()
+
 
 def check_vectors_specific_order(tst, v1, v1order, v2, v2order):
     tst.assertEqual(len(v1), len(v1order))