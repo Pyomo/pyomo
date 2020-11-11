fde86b70ca37cd3e1fda87bd41f79d7ad62c41da
Miranda Mundt <mmundt@sandia.gov>
2020-11-11 07:35:56 -0700

    Adding copyright

--- pyomo/common/tests/test_config.py
+++ pyomo/common/tests/test_config.py
@@ -6,12 +6,18 @@
 #  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain 
 #  rights in this software.
 #  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________ 
+#
+#  This module was originally developed as part of the PyUtilib project
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
 #  ___________________________________________________________________________
 
 import os
 import sys
 import os.path
-import pickle
 import re
 import pyutilib.th as unittest
 
