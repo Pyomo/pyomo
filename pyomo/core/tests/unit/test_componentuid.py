c4762ae1896406dbca0aaf393e3b349f6190bdf9
John Siirola <jsiirola@users.noreply.github.com>
2020-11-16 11:41:12 -0700

    Improve ComponentUID comparison testing

--- pyomo/core/tests/unit/test_componentuid.py
+++ pyomo/core/tests/unit/test_componentuid.py
@@ -354,6 +354,47 @@ class TestComponentUID(unittest.TestCase):
         self.assertTrue ( ComponentUID('baz') == b )
         self.assertFalse( ComponentUID('baz') != b )
 
+    def test_comparisons_lt(self):
+        a = ComponentUID('foo.x[*]')
+        a1 = ComponentUID('foo.x[1]')
+        a2 = ComponentUID('foo.x[2]')
+        aa = ComponentUID("foo.x['a']")
+        a11 = ComponentUID('foo.x[1,1]')
+        ae = ComponentUID('foo.x[**]')
+        self.assertTrue( a < ae )
+        self.assertTrue( a1 < ae )
+        self.assertTrue( a1 < a )
+        self.assertTrue( a1 < a2 )
+        self.assertTrue( a1 < aa )
+        self.assertTrue( a1 < a11 )
+        self.assertTrue( a11 < a2 )
+        self.assertFalse( ae < a )
+        self.assertFalse( ae < a1 )
+        self.assertFalse( a < a1 )
+        self.assertFalse( a2 < a1 )
+        self.assertFalse( aa < a1 )
+        self.assertFalse( a11 < a1 )
+        self.assertFalse( a2 < a11 )
+
+        x = ComponentUID('foo.x')
+        xy = ComponentUID('foo.x.y')
+        self.assertTrue( x < xy )
+        self.assertFalse( xy < x )
+
+        with self.assertRaisesRegex(
+                TypeError, "'<' not supported between instances of "
+                "'ComponentUID' and 'int'"):
+            a < 5
+
+    def test_comparisons_eq(self):
+        a = ComponentUID('foo.x[*]')
+        a1 = ComponentUID('foo.x[1]')
+        b = ComponentUID('foo.x[*]')
+        self.assertEqual(a, b)
+        self.assertNotEqual(a, a1)
+        self.assertNotEqual(a, 5)
+
+
     def test_generate_cuid_string_map(self):
         model = Block(concrete=True)
         model.x = Var()
