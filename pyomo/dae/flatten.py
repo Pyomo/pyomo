4ce0aaffe0913fd516ceaedd3a07f53672c20eb0
Robert Parker <robbybparker@gmail.com>
2020-11-19 23:05:34 -0500

    iterate over component to get temporary time index

--- pyomo/dae/flatten.py
+++ pyomo/dae/flatten.py
@@ -47,7 +47,18 @@ def generate_time_only_slices(obj, time):
     # We now form a temporary slice that slices over all the regular
     # indices for a fixed value of the time index.
     tmp_sliced = {i: slice(None) for i in regular_idx}
-    tmp_fixed = {time_idx: time.first()}
+
+    # Need to choose some arbitrary time index to fix, but indices
+    # may have been skipped, so choosing e.g. time.first() is not
+    # reliable. Approach here is to take the first index we encounter
+    # when iterating over obj.
+    for idx in obj:
+        if idx.__class__ is not tuple:
+            idx = (idx,)
+        t = idx[time_idx]
+        break
+    tmp_fixed = {time_idx: t}
+
     tmp_ellipsis = ellipsis_idx
     _slice = IndexedComponent_slice(
         obj, tmp_fixed, tmp_sliced, tmp_ellipsis
