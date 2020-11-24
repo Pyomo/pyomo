Pyomo/pyomo

commit a662e33b1f674c6d5a3bb10a97b4d451bbdad526

removing lines

pyomo/pysp/phsolverserver.py
@@ -62,21 +62,13 @@ class PHPyroWorker(TaskWorker):
         self._bulk_task_collection = True
         self._modules_imported = {}
 
-    def del_server(self, name):
-        phsolver = self._phsolverserver_map[name]
-        # Avoid memory leaks
-        # Solvers no longer support/require calls to deactivate()
-        #for object_name, object_solver in iteritems(phsolver._solver_map):
-        #    object_solver.deactivate()
-        del self._phsolverserver_map[name]
-
     def process(self, data):
 
         data = pyutilib.misc.Bunch(**data)
         result = None
         if data.action == "release":
 
-            self.del_server(data.object_name)
+            del self._phsolverserver_map[name]
             result = True
 
         elif data.action == "initialize":

pyomo/pysp/drive_lagrangian_cc.py
@@ -334,7 +334,6 @@ def run(args=None):
 #####################################################################################
 
    finally:
-
       # delete temporary unarchived directories
       scenario_instance_factory.close()
 

pyomo/pysp/lagrangeMorePR.py
@@ -463,7 +463,6 @@ def run(args=None):
       Result = LagrangeMorePR()
 #####################################################################################
    finally:
-
       # delete temporary unarchived directories
       scenario_instance_factory.close()
 

commit 33dad45191f6f226fd9fa60a45925549d58ff610

Fixing bug introduced in a662e33

pyomo/pysp/phsolverserver.py
@@ -66,7 +66,7 @@ class PHPyroWorker(TaskWorker):
         result = None
         if data.action == "release":
 
-            del self._phsolverserver_map[name]
+            del self._phsolverserver_map[data.object_name]
             result = True
 
         elif data.action == "initialize":