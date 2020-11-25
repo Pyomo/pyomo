7782d0dd0c7097496ca804020dbcb1588e37bbd3
Bethany Nicholson <blnicho@sandia.gov>
2020-11-25 16:47:50 -0700

    Improving performance of pyomo.dae by replacing uses of sorted and <list>.index

--- pyomo/dae/contset.py
+++ pyomo/dae/contset.py
@@ -244,7 +244,7 @@ class ContinuousSet(SortedSimpleSet):
             raise ValueError("ContinuousSet '%s' must have at least two values"
                              " indicating the range over which a differential "
                              "equation is to be discretized" % self.name)
-        self._fe = sorted(self)
+        self._fe = list(self)
         timer.report()
 
     def find_nearest_index(self, target, tolerance=None):
