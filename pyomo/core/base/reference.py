aa770b383b55b67da64839ab8ef502c417313823
Robert Parker <robbybparker@gmail.com>
2020-12-08 20:02:20 -0500

    referent attribute

--- pyomo/core/base/reference.py
+++ pyomo/core/base/reference.py
@@ -669,4 +669,5 @@ def Reference(reference, ctype=_NotSpecified):
     obj = ctype(index, ctype=ctype)
     obj._constructed = True
     obj._data = _data
+    obj.referent = reference
     return obj
