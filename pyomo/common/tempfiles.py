e5448590a02bb0dbb37230e96285456d5f01d50e
John Siirola <jsiirola@users.noreply.github.com>
2020-12-15 15:17:05 -0700

    Update module logger

--- pyomo/common/tempfiles.py
+++ pyomo/common/tempfiles.py
@@ -2,8 +2,8 @@
 #
 #  Pyomo: Python Optimization Modeling Objects
 #  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
-#  Under the terms of Contract DE-NA0003525 with National Technology and 
-#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain 
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
 #  rights in this software.
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
@@ -152,8 +152,7 @@ class TempfileManagerClass:
                                 else:
                                     # Failure to delete a tempfile
                                     # should NOT be fatal
-                                    logger = logging.getLogger(
-                                        'pyutilib.component.config')
+                                    logger = logging.getLogger(__name__)
                                     logger.warning("Unable to delete temporary "
                                                    "file %s" % (filename,))
 
