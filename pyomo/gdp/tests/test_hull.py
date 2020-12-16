e8ff79aa827acf530658195fb22c37b4e8948500
Emma Johnson <emma58@users.noreply.github.com>
2020-12-16 11:24:46 -0500

    Adding (currently failing) tiny kmeans test related to local var bug

--- pyomo/gdp/tests/test_hull.py
+++ pyomo/gdp/tests/test_hull.py
@@ -12,7 +12,10 @@ import pyutilib.th as unittest
 from pyomo.common.log import LoggingIntercept
 import logging
 
-from pyomo.environ import TransformationFactory, Block, Set, Constraint, Var, RealSet, ComponentMap, value, log, ConcreteModel, Any, Suffix, SolverFactory
+from pyomo.environ import (TransformationFactory, Block, Set, Constraint, Var,
+                           RealSet, ComponentMap, value, log, ConcreteModel,
+                           Any, Suffix, SolverFactory, RangeSet, Param,
+                           Objective, TerminationCondition)
 from pyomo.repn import generate_standard_repn
 
 from pyomo.gdp import Disjunct, Disjunction, GDP_Error
@@ -2039,3 +2042,72 @@ class NameDeprecationTest(unittest.TestCase):
         m1.pprint(ostream=out1)
         m2.pprint(ostream=out2)
         self.assertMultiLineEqual(out1.getvalue(), out2.getvalue())
+
+class KmeansTest(unittest.TestCase):
+    @unittest.skipIf('gurobi' not in linear_solvers, 
+                     "Gurobi solver not available")
+    def test_optimal_soln_feasible(self):
+        m = ConcreteModel()
+        m.Points = RangeSet(3)
+        m.Centroids = RangeSet(2)
+
+        m.X = Param(m.Points, initialize={1:0.3672, 2:0.8043, 3:0.3059})
+
+        m.cluster_center = Var(m.Centroids, bounds=(0,2))
+        m.distance = Var(m.Points, bounds=(0,2))
+        m.t = Var(m.Points, m.Centroids, bounds=(0,2))
+
+        @m.Disjunct(m.Points, m.Centroids)
+        def AssignPoint(d, i, k):
+            m = d.model()
+            d.LocalVars = Suffix(direction=Suffix.LOCAL)
+            d.LocalVars[d] = [m.t[i,k]]
+            def distance1(d):
+                return m.t[i,k] >= m.X[i] - m.cluster_center[k]
+            def distance2(d):
+                return m.t[i,k] >= - (m.X[i] - m.cluster_center[k])
+            d.dist1 = Constraint(rule=distance1)
+            d.dist2 = Constraint(rule=distance2)
+            d.define_distance = Constraint(expr=m.distance[i] == m.t[i,k])
+
+        @m.Disjunction(m.Points)
+        def OneCentroidPerPt(m, i):
+            return [m.AssignPoint[i, k] for k in m.Centroids]
+
+        m.obj = Objective(expr=sum(m.distance[i] for i in m.Points))
+
+        TransformationFactory('gdp.hull').apply_to(m)
+
+        # fix an optimal solution
+        m.AssignPoint[1,1].indicator_var.fix(1)
+        m.AssignPoint[1,2].indicator_var.fix(0)
+        m.AssignPoint[2,1].indicator_var.fix(0)
+        m.AssignPoint[2,2].indicator_var.fix(1)
+        m.AssignPoint[3,1].indicator_var.fix(1)
+        m.AssignPoint[3,2].indicator_var.fix(0)
+
+        m.cluster_center[1].fix(0.3059)
+        m.cluster_center[2].fix(0.8043)
+
+        m.distance[1].fix(0.0613)
+        m.distance[2].fix(0)
+        m.distance[3].fix(0)
+
+        m.t[1,1].fix(0.0613)
+        m.t[1,2].fix(0)
+        m.t[2,1].fix(0)
+        m.t[2,2].fix(0)
+        m.t[3,1].fix(0)
+        m.t[3,2].fix(0)
+
+        results = SolverFactory('gurobi').solve(m)
+        
+        self.assertEqual(results.solver.termination_condition,
+                         TerminationCondition.optimal)
+        
+        TOL = 1e-8
+        for c in m.component_data_objects(Constraint, active=True): 
+            if c.lower is not None:
+                self.assertGreaterEqual(value(c.body) + TOL, value(c.lower))
+            if c.upper is not None:
+                self.assertLessEqual(value(c.body) - TOL, value(c.upper))
