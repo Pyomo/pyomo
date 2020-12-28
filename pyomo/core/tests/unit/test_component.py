84a3943cae08302624f8578ced0c92449a18980d
Robert <robbybparker@gmail.com>
2020-12-28 13:23:42 -0500

    test to cover base class owns_data

--- pyomo/core/tests/unit/test_component.py
+++ pyomo/core/tests/unit/test_component.py
@@ -95,6 +95,12 @@ class TestComponent(unittest.TestCase):
                     '2 :  None :  None :  None : False :  True :  Reals\n'
         self.assertEqual(correct_s, stream.getvalue())
 
+    def test_owns_data(self):
+        m = ConcreteModel()
+        class _NotSpecified(object):
+            pass
+        m.comp = Component(ctype=_NotSpecified)
+        self.assertTrue(m.comp.owns_data())
 
 class TestEnviron(unittest.TestCase):
 
