5268dd3cc651e98887a431b21a5e911210b3a956
Carl Laird <carldlaird@users.noreply.github.com>
2021-02-18 17:41:05 -0600

    fixing dynamic model test to use new external grey box code

--- pyomo/contrib/pynumero/interfaces/tests/test_dynamic_model.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_dynamic_model.py
@@ -30,6 +30,7 @@ from pyomo.contrib.pynumero.algorithms.solvers.cyipopt_solver import (
 
 from pyomo.contrib.pynumero.interfaces.external_grey_box import ExternalGreyBoxModel, ExternalGreyBoxBlock
 from pyomo.contrib.pynumero.interfaces.pyomo_nlp import PyomoGreyBoxNLP, PyomoNLP
+from pyomo.contrib.pynumero.interfaces.pyomo_grey_box_nlp import PyomoNLPWithGreyBoxBlocks
 
 from pyomo.contrib.pynumero.interfaces.tests.compare_utils import check_vectors_specific_order, check_sparse_matrix_specific_order
 
@@ -385,13 +386,14 @@ class TestGreyBoxModel(unittest.TestCase):
         m_nlp = PyomoNLP(m)
 
         mex = create_pyomo_external_grey_box_model(A1, A2, c1, c2, N, dt)
-        mex_nlp = PyomoGreyBoxNLP(mex)
+        # mex_nlp = PyomoGreyBoxNLP(mex)
+        mex_nlp = PyomoNLPWithGreyBoxBlocks(mex)
 
         # get the variable and constraint order and create the maps
         # reliable order independent comparisons
-        m_x_order = m_nlp.variable_names()
+        m_x_order = m_nlp.primals_names()
         m_c_order = m_nlp.constraint_names()
-        mex_x_order = mex_nlp.variable_names()
+        mex_x_order = mex_nlp.primals_names()
         mex_c_order = mex_nlp.constraint_names()
         
         x1list = ['h1[0]', 'h1[1]', 'h1[2]', 'h1[3]', 'h1[4]', 'h1[5]', 'h2[0]', 'h2[1]', 'h2[2]', 'h2[3]', 'h2[4]', 'h2[5]', 'F1[1]', 'F1[2]', 'F1[3]', 'F1[4]', 'F1[5]', 'F2[1]', 'F2[2]', 'F2[3]', 'F2[4]', 'F2[5]', 'F12[0]', 'F12[1]', 'F12[2]', 'F12[3]', 'F12[4]', 'F12[5]', 'Fo[0]', 'Fo[1]', 'Fo[2]', 'Fo[3]', 'Fo[4]', 'Fo[5]']
@@ -400,7 +402,7 @@ class TestGreyBoxModel(unittest.TestCase):
         x1idx_x2idx_map = {i: mex_x_order.index(x1_x2_map[m_x_order[i]]) for i in range(len(m_x_order))}
 
         c1list = ['h1bal[1]', 'h1bal[2]', 'h1bal[3]', 'h1bal[4]', 'h1bal[5]', 'h2bal[1]', 'h2bal[2]', 'h2bal[3]', 'h2bal[4]', 'h2bal[5]', 'F12con[0]', 'F12con[1]', 'F12con[2]', 'F12con[3]', 'F12con[4]', 'F12con[5]', 'Focon[0]', 'Focon[1]', 'Focon[2]', 'Focon[3]', 'Focon[4]', 'Focon[5]', 'min_inflow[1]', 'min_inflow[2]', 'min_inflow[3]', 'min_inflow[4]', 'min_inflow[5]', 'max_outflow[0]', 'max_outflow[1]', 'max_outflow[2]', 'max_outflow[3]', 'max_outflow[4]', 'max_outflow[5]', 'h10', 'h20']
-        c2list = ['egb.h1bal_1', 'egb.h1bal_2', 'egb.h1bal_3', 'egb.h1bal_4', 'egb.h1bal_5', 'egb.h2bal_1', 'egb.h2bal_2', 'egb.h2bal_3', 'egb.h2bal_4', 'egb.h2bal_5', 'egb.F12_0_con', 'egb.F12_1_con', 'egb.F12_2_con', 'egb.F12_3_con', 'egb.F12_4_con', 'egb.F12_5_con', 'egb.Fo_0_con', 'egb.Fo_1_con', 'egb.Fo_2_con', 'egb.Fo_3_con', 'egb.Fo_4_con', 'egb.Fo_5_con', 'min_inflow[1]', 'min_inflow[2]', 'min_inflow[3]', 'min_inflow[4]', 'min_inflow[5]', 'max_outflow[0]', 'max_outflow[1]', 'max_outflow[2]', 'max_outflow[3]', 'max_outflow[4]', 'max_outflow[5]', 'h10', 'h20']
+        c2list = ['egb.h1bal_1', 'egb.h1bal_2', 'egb.h1bal_3', 'egb.h1bal_4', 'egb.h1bal_5', 'egb.h2bal_1', 'egb.h2bal_2', 'egb.h2bal_3', 'egb.h2bal_4', 'egb.h2bal_5', 'egb.output_constraints[F12_0]', 'egb.output_constraints[F12_1]', 'egb.output_constraints[F12_2]', 'egb.output_constraints[F12_3]', 'egb.output_constraints[F12_4]', 'egb.output_constraints[F12_5]', 'egb.output_constraints[Fo_0]', 'egb.output_constraints[Fo_1]', 'egb.output_constraints[Fo_2]', 'egb.output_constraints[Fo_3]', 'egb.output_constraints[Fo_4]', 'egb.output_constraints[Fo_5]', 'min_inflow[1]', 'min_inflow[2]', 'min_inflow[3]', 'min_inflow[4]', 'min_inflow[5]', 'max_outflow[0]', 'max_outflow[1]', 'max_outflow[2]', 'max_outflow[3]', 'max_outflow[4]', 'max_outflow[5]', 'h10', 'h20']
         c1_c2_map = dict(zip(c1list, c2list))
         c1idx_c2idx_map = {i: mex_c_order.index(c1_c2_map[m_c_order[i]]) for i in range(len(m_c_order))}
 
