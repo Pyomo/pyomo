1cb2ebd9cd13d72ba075ef8bf0175d78fb4b79e7
Michael Bynum <mlbynum@sandia.gov>
2021-02-24 06:28:44 -0700

    appsi: working on tests

--- pyomo/contrib/appsi/examples/tests/test_examples.py
+++ pyomo/contrib/appsi/examples/tests/test_examples.py
@@ -5,6 +5,10 @@ import pyomo.environ as pe
 
 class TestExamples(unittest.TestCase):
     def test_getting_started(self):
+        try:
+            import numpy as np
+        except:
+            raise unittest.SkipTest('numpy is not available')
         opt = pe.SolverFactory('appsi_cplex')
         if not opt.available():
             raise unittest.SkipTest('cplex is not available')
