0014152d26ac87f1f9167e8cec65ee4599c5f05b
Zedong <peng_zedong@126.com>
2021-03-08 22:35:09 +0800

    add todo for tabu list

--- pyomo/contrib/mindtpy/tabu_list.py
+++ pyomo/contrib/mindtpy/tabu_list.py
@@ -9,6 +9,8 @@ class IncumbentCallback_cplex(IncumbentCallback):
         This is an inherent function in LazyConstraintCallback in cplex. 
         This callback will be used after each new potential incumbent is found.
         https://www.ibm.com/support/knowledgecenter/SSSA5P_12.10.0/ilog.odms.cplex.help/refpythoncplex/html/cplex.callbacks.IncumbentCallback-class.html
+        IncumbentCallback will be activated after Lazyconstraint callback, when the potential incumbent solution is satisfies the lazyconstraints.
+        TODO: need to handle GOA same integer combination check in lazyconstraint callback in single_tree.py
         """
         solve_data = self.solve_data
         opt = self.opt
