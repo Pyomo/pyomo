97d558b5326c0e1e0ad24b748a10d72a5e7ea2e6
John Eslick <john.eslick@netl.doe.gov>
2021-04-29 21:10:17 -0400

    Add test function and build stuff.

---
+++ pyomo/contrib/ampl_function_demo/src/functions.h
@@ -0,0 +1,13 @@
+#include "funcadd.h"
+
+#ifndef _INCLUDE_FUNCTIONS_H_
+#define _INCLUDE_FUNCTIONS_H_
+
+#include <time.h>
+#include <math.h>
+
+
+real scbrt(arglist *al);
+real testing_only(arglist *al);
+
+#endif
---
+++ pyomo/contrib/ampl_function_demo/tests/test_ampl_function_demo.py
@@ -0,0 +1,25 @@
+import pyomo.environ as pyo
+import pyomo.common.unittest as unittest
+from pyomo.core.base.external import ExternalFunction
+from pyomo.common.fileutils import find_library
+from pyomo.opt import check_available_solvers
+
+flib = find_library("function_ASL")
+
+class TestAMPLExternalFunction(unittest.TestCase):
+    def test_eval_function(self):
+        m = pyo.ConcreteModel()
+        m.tf = pyo.ExternalFunction(library=flib, function="testing_only")
+        assert abs(pyo.value(m.tf("whatevs", 1, 2, 3) - 6))/6 < 1e-5
+        assert abs(pyo.value(m.tf("inv", 1, 2, 3) - 1.8333333))/1.8333 < 1e-5
+
+    def test_solve_function(self):
+        m = pyo.ConcreteModel()
+        m.tf = pyo.ExternalFunction(library=flib, function="testing_only")
+        m.x = pyo.Var(initialize=0.5)
+        m.x.fix()
+        m.y = pyo.Var(initialize=4.0)
+        m.c = pyo.Constraint(expr=m.x == m.tf("inv", 3, m.y))
+        solver = pyo.SolverFactory("ipopt")
+        solver.solve(m, tee=True)
+        assert abs(pyo.value(m.y - 6))/6 < 1e-5
