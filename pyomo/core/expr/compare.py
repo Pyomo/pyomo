26a9d7a809733790be05432e34932222304a464f
Michael Bynum <mlbynum@sandia.gov>
2021-05-18 15:10:50 -0600

    utilities to compare expressions

--- pyomo/core/expr/compare.py
+++ pyomo/core/expr/compare.py
@@ -1,3 +1,12 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright 2017 National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
 from .visitor import StreamBasedExpressionVisitor
 from .numvalue import nonpyomo_leaf_types
 from .numeric_expr import (LinearExpression, MonomialTermExpression, SumExpression, ExpressionBase,
