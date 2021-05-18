d8663a5825069f0cfebfeffe124bbfe49b407714
John Siirola <jsiirola@users.noreply.github.com>
2021-05-18 10:39:58 -0600

    Acknowledge the origin of the plugin system

--- pyomo/common/plugin.py
+++ pyomo/common/plugin.py
@@ -7,6 +7,13 @@
 #  rights in this software.
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
+#
+#  This module was originally developed as part of the PyUtilib project
+#  Copyright (c) 2008 Sandia Corporation.
+#  This software is distributed under the BSD License.
+#  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+#  the U.S. Government retains certain rights in this software.
+#  ___________________________________________________________________________
 
 import collections
 import inspect
