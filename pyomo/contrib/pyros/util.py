79082391d050cba7b7f1462dc6ea0db6180d58d5
natalieisenberg <isenberg.natalie@gmail.com>
2021-06-02 18:46:25 -0400

    Tabs to spaces

--- pyomo/contrib/pyros/util.py
+++ pyomo/contrib/pyros/util.py
@@ -608,19 +608,18 @@ def output_logger(config, **kwargs):
                        "Developed by Natalie M. Isenberg (1), John D. Siirola (2), Chrysanthos E. Gounaris (1)\n" \
                        "(1) Carnegie Mellon University, Department of Chemical Engineering\n" \
                        "(2) Sandia National Laboratory, Discrete Math and Optimization\n" \
-                       "The developers gratefully acknowledge support from " \
-                       "the U.S. Department of Energy's Institute for the Design of Advanced Energy Systems (IDAES)" \
+                       "The developers gratefully acknowledge support from the U.S. Department of Energy's\n" \
+                       "                                Institute for the Design of Advanced Energy Systems (IDAES)" \
                        "Licensing: TODO" \
                        "===========================================================================================" % version
             config.progress_logger.info(preamble)
     # === DISCLAIMER
     if "disclaimer" in kwargs:
         if kwargs["disclaimer"]:
-            config.progress_logger.info("\n ============ DISCLAIMER ============ \n"
-                                        "PyROS is still under development.\n"
-                                        "This version is a beta release.\n"
-                                        "Please report any issues via Pyomo.\n"
-                                        "=====================================\n")
+            config.progress_logger.info("\n ========================= DISCLAIMER ========================= \n"
+                                        "PyROS is still under development. This version is a beta release.\n"
+                                        "Please provide feedback and/or report any issues by opening a Pyomo ticket.\n"
+                                        "=================================================================\n")
     # === ALL LOGGER RETURN MESSAGES
     if "robust_optimal" in kwargs:
         if kwargs["robust_optimal"]:
