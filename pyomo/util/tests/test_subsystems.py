c0bbee86d9a653e531aae299298e073158c018a0
Robert Parker <robbybparker@gmail.com>
2021-06-07 23:47:50 -0400

    tests for generate_subsystem_blocks

--- pyomo/util/tests/test_subsystems.py
+++ pyomo/util/tests/test_subsystems.py
@@ -13,6 +13,7 @@ import pyomo.common.unittest as unittest
 from pyomo.common.collections import ComponentSet, ComponentMap
 from pyomo.util.subsystems import (
         create_subsystem_block,
+        generate_subsystem_blocks,
         TemporarySubsystemManager,
         ParamSweeper,
         )
@@ -137,6 +138,173 @@ class TestSubsystemBlock(unittest.TestCase):
 
         # Rest of model has not changed
         self.assertEqual(m.v5.value, 1.0)
+
+    def test_generate_subsystems_without_fixed_var(self):
+        m = _make_simple_model()
+        subsystems = [
+                ([m.con1], [m.v1, m.v4]),
+                ([m.con2, m.con3], [m.v2, m.v3]),
+                ]
+        other_vars = [
+                [m.v2, m.v3],
+                [m.v1, m.v4],
+                ]
+        for i, block in enumerate(generate_subsystem_blocks(subsystems)):
+            self.assertIs(block.model(), block)
+            var_set = ComponentSet(subsystems[i][1])
+            con_set = ComponentSet(subsystems[i][0])
+            input_set = ComponentSet(other_vars[i])
+            
+            self.assertEqual(len(var_set), len(block.vars))
+            self.assertEqual(len(con_set), len(block.cons))
+            self.assertEqual(len(input_set), len(block.input_vars))
+            self.assertTrue(all(var in var_set for var in block.vars[:]))
+            self.assertTrue(all(con in con_set for con in block.cons[:]))
+            self.assertTrue(
+                    all(var in input_set for var in block.input_vars[:]))
+            self.assertTrue(all(var.fixed for var in block.input_vars[:]))
+            self.assertFalse(any(var.fixed for var in block.vars[:]))
+
+        self.assertFalse(any(var.fixed for var in
+            m.component_data_objects(pyo.Var)))
+
+    def test_generate_subsystems_with_fixed_var(self):
+        m = _make_simple_model()
+        m.v4.fix()
+        subsystems = [
+                ([m.con1], [m.v1]),
+                ([m.con2, m.con3], [m.v2, m.v3]),
+                ]
+        other_vars = [
+                [m.v2, m.v3],
+                [m.v1],
+                ]
+        for i, block in enumerate(generate_subsystem_blocks(subsystems)):
+            self.assertIs(block.model(), block)
+            var_set = ComponentSet(subsystems[i][1])
+            con_set = ComponentSet(subsystems[i][0])
+            input_set = ComponentSet(other_vars[i])
+            
+            self.assertEqual(len(var_set), len(block.vars))
+            self.assertEqual(len(con_set), len(block.cons))
+            self.assertEqual(len(input_set), len(block.input_vars))
+            self.assertTrue(all(var in var_set for var in block.vars[:]))
+            self.assertTrue(all(con in con_set for con in block.cons[:]))
+            self.assertTrue(
+                    all(var in input_set for var in block.input_vars[:]))
+            self.assertTrue(all(var.fixed for var in block.input_vars[:]))
+            self.assertFalse(any(var.fixed for var in block.vars[:]))
+
+        self.assertFalse(m.v1.fixed)
+        self.assertFalse(m.v2.fixed)
+        self.assertFalse(m.v3.fixed)
+        self.assertTrue(m.v4.fixed)
+
+    def test_generate_subsystems_include_fixed_var(self):
+        m = _make_simple_model()
+        m.v4.fix()
+        subsystems = [
+                ([m.con1], [m.v1]),
+                ([m.con2, m.con3], [m.v2, m.v3]),
+                ]
+        other_vars = [
+                [m.v2, m.v3, m.v4],
+                [m.v1, m.v4],
+                ]
+        for i, block in enumerate(generate_subsystem_blocks(
+            subsystems,
+            include_fixed=True,
+            )):
+            self.assertIs(block.model(), block)
+            var_set = ComponentSet(subsystems[i][1])
+            con_set = ComponentSet(subsystems[i][0])
+            input_set = ComponentSet(other_vars[i])
+            
+            self.assertEqual(len(var_set), len(block.vars))
+            self.assertEqual(len(con_set), len(block.cons))
+            self.assertEqual(len(input_set), len(block.input_vars))
+            self.assertTrue(all(var in var_set for var in block.vars[:]))
+            self.assertTrue(all(con in con_set for con in block.cons[:]))
+            self.assertTrue(
+                    all(var in input_set for var in block.input_vars[:]))
+            self.assertTrue(all(var.fixed for var in block.input_vars[:]))
+            self.assertFalse(any(var.fixed for var in block.vars[:]))
+
+        self.assertFalse(m.v1.fixed)
+        self.assertFalse(m.v2.fixed)
+        self.assertFalse(m.v3.fixed)
+        self.assertTrue(m.v4.fixed)
+
+    def test_generate_subsystems_dont_fix_inputs(self):
+        m = _make_simple_model()
+        subsystems = [
+                ([m.con1], [m.v1]),
+                ([m.con2, m.con3], [m.v2, m.v3]),
+                ]
+        other_vars = [
+                [m.v2, m.v3, m.v4],
+                [m.v1, m.v4],
+                ]
+        for i, block in enumerate(generate_subsystem_blocks(
+            subsystems,
+            fix_inputs=False,
+            )):
+            self.assertIs(block.model(), block)
+            var_set = ComponentSet(subsystems[i][1])
+            con_set = ComponentSet(subsystems[i][0])
+            input_set = ComponentSet(other_vars[i])
+            
+            self.assertEqual(len(var_set), len(block.vars))
+            self.assertEqual(len(con_set), len(block.cons))
+            self.assertEqual(len(input_set), len(block.input_vars))
+            self.assertTrue(all(var in var_set for var in block.vars[:]))
+            self.assertTrue(all(con in con_set for con in block.cons[:]))
+            self.assertTrue(
+                    all(var in input_set for var in block.input_vars[:]))
+            self.assertFalse(any(var.fixed for var in block.input_vars[:]))
+            self.assertFalse(any(var.fixed for var in block.vars[:]))
+
+        self.assertFalse(m.v1.fixed)
+        self.assertFalse(m.v2.fixed)
+        self.assertFalse(m.v3.fixed)
+        self.assertFalse(m.v4.fixed)
+
+    def test_generate_dont_fix_inputs_with_fixed_var(self):
+        m = _make_simple_model()
+        m.v4.fix()
+        subsystems = [
+                ([m.con1], [m.v1]),
+                ([m.con2, m.con3], [m.v2, m.v3]),
+                ]
+        other_vars = [
+                [m.v2, m.v3],
+                [m.v1],
+                ]
+        for i, block in enumerate(generate_subsystem_blocks(
+            subsystems,
+            fix_inputs=False,
+            )):
+            self.assertIs(block.model(), block)
+            var_set = ComponentSet(subsystems[i][1])
+            con_set = ComponentSet(subsystems[i][0])
+            input_set = ComponentSet(other_vars[i])
+            
+            self.assertEqual(len(var_set), len(block.vars))
+            self.assertEqual(len(con_set), len(block.cons))
+            self.assertEqual(len(input_set), len(block.input_vars))
+            self.assertTrue(all(var in var_set for var in block.vars[:]))
+            self.assertTrue(all(con in con_set for con in block.cons[:]))
+            self.assertTrue(
+                    all(var in input_set for var in block.input_vars[:]))
+            self.assertFalse(m.v1.fixed)
+            self.assertFalse(m.v2.fixed)
+            self.assertFalse(m.v3.fixed)
+            self.assertTrue(m.v4.fixed)
+
+        self.assertFalse(m.v1.fixed)
+        self.assertFalse(m.v2.fixed)
+        self.assertFalse(m.v3.fixed)
+        self.assertTrue(m.v4.fixed)
 
 
 class TestTemporarySubsystemManager(unittest.TestCase):
