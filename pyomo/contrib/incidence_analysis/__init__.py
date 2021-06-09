0aaa7373a3c04c06785625d17dfdf6e56f10c8fe
Robert Parker <robbybparker@gmail.com>
2021-06-08 22:24:09 -0400

    add generate_strongly_connected_components to __init__

--- pyomo/contrib/incidence_analysis/__init__.py
+++ pyomo/contrib/incidence_analysis/__init__.py
@@ -1,4 +1,7 @@
 from .triangularize import block_triangularize
 from .matching import maximum_matching
 from .interface import IncidenceGraphInterface
-from .util import solve_strongly_connected_components
+from .util import (
+        generate_strongly_connected_components,
+        solve_strongly_connected_components,
+        )
