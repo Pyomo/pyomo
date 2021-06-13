d0c6b7fab5be2503e193e4f2d0ac1c8bf9644a69
Robert Parker <robbybparker@gmail.com>
2021-06-13 17:39:07 -0400

    add test for simpler 2x2 external function

--- pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_model.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_model.py
@@ -94,6 +94,60 @@ class SimpleModel2(object):
         return 2*0.2**(1/3)/(x**3)
 
 
+class SimpleModel2by2_1(object):
+    """
+    The purpose of this model is to test second derivative computation
+    when the external model is nonlinear only in x. This exercises
+    the first term in the second derivative implicit function theorem.
+    """
+
+    def make_model(self):
+        m = pyo.ConcreteModel()
+        m.x = pyo.Var([0, 1], initialize=2.0)
+        m.y = pyo.Var([0, 1], initialize=2.0)
+
+        # Residual equation is not important for now, as we are only
+        # testing the external function and its derivative.
+        m.residual_eqn = pyo.Constraint(expr=
+                m.x[0]**2 + m.x[1]**2 + m.y[0]**2 + m.y[1]**2 == 1.0
+                )
+
+        def external_eqn_rule(m, i):
+            if i == 0:
+                return m.y[0] + m.y[1] + m.x[0]*m.x[1] + m.x[0]**2 == 1.0
+            elif i == 1:
+                return m.y[0] + 2.0*m.x[0]*m.x[1] + m.x[1]**2 == 2.0
+
+        m.external_eqn = pyo.Constraint([0, 1], rule=external_eqn_rule)
+
+        return m
+
+    def evaluate_external_variables(self, x):
+        y0 = 2.0 - 2.0*x[0]*x[1] - x[1]**2
+        y1 = 1.0 - y0 - x[0]*x[1] - x[0]**2
+        return [y0, y1]
+
+    def evaluate_external_jacobian(self, x):
+        dy0dx0 = -2.0*x[1]
+        dy0dx1 = -2.0*x[0] - 2.0*x[1]
+        dy1dx0 = -dy0dx0 - x[1] - 2.0*x[0]
+        dy1dx1 = -dy0dx1 - x[0]
+        return np.array([[dy0dx0, dy0dx1], [dy1dx0, dy1dx1]])
+
+    def evaluate_external_hessian(self, x):
+        dy0dx0dx0 = 0.0
+        dy0dx0dx1 = -2.0
+        dy0dx1dx1 = -2.0
+
+        dy1dx0dx0 = -dy0dx0dx0 - 2.0
+        dy1dx0dx1 = -dy0dx0dx1 - 1.0
+        dy1dx1dx1 = -dy0dx1dx1
+
+        dy0dxdx = np.array([[dy0dx0dx0, dy0dx0dx1], [dy0dx0dx1, dy0dx1dx1]])
+        dy1dxdx = np.array([[dy1dx0dx0, dy1dx0dx1], [dy1dx0dx1, dy1dx1dx1]])
+        return [dy0dxdx, dy1dxdx]
+
+
 class Model2by2(object):
     """
     The purpose of this model is to test d2ydx2 Hessian computation when
@@ -431,8 +485,56 @@ class TestExternalPyomoModel(unittest.TestCase):
                 matrix2 = np.matrix(matrix2)
                 np.testing.assert_allclose(matrix1, matrix2, rtol=1e-8)
 
+    def test_external_jacobian_SimpleModel2x2_1(self):
+        model = SimpleModel2by2_1()
+        m = model.make_model()
+        m.x[0].set_value(1.0)
+        m.x[1].set_value(2.0)
+        m.y[0].set_value(3.0)
+        m.y[1].set_value(4.0)
+        x0_init_list = [-5.0, -3.0, 0.5, 1.0, 2.5]
+        x1_init_list = [-4.5, -2.3, 0.0, 1.0, 4.1]
+        x_init_list = list(itertools.product(x0_init_list, x1_init_list))
+        external_model = ExternalPyomoModel(
+                list(m.x.values()),
+                list(m.y.values()),
+                list(m.residual_eqn.values()),
+                list(m.external_eqn.values()),
+                )
+
+        for x in x_init_list:
+            external_model.set_input_values(x)
+            jac = external_model.evaluate_jacobian_external_variables()
+            expected_jac = model.evaluate_external_jacobian(x)
+            np.testing.assert_allclose(jac, expected_jac, rtol=1e-8)
+
+    def test_external_hessian_SimpleModel2x2_1(self):
+        model = SimpleModel2by2_1()
+        m = model.make_model()
+        m.x[0].set_value(1.0)
+        m.x[1].set_value(2.0)
+        m.y[0].set_value(3.0)
+        m.y[1].set_value(4.0)
+        x0_init_list = [-5.0, -3.0, 0.5, 1.0, 2.5]
+        x1_init_list = [-4.5, -2.3, 0.0, 1.0, 4.1]
+        x_init_list = list(itertools.product(x0_init_list, x1_init_list))
+        external_model = ExternalPyomoModel(
+                list(m.x.values()),
+                list(m.y.values()),
+                list(m.residual_eqn.values()),
+                list(m.external_eqn.values()),
+                )
+
+        for x in x_init_list:
+            external_model.set_input_values(x)
+            hess = external_model.evaluate_hessian_external_variables()
+            expected_hess = model.evaluate_external_hessian(x)
+            for matrix1, matrix2 in zip(hess, expected_hess):
+                np.testing.assert_allclose(matrix1, matrix2, rtol=1e-8)
+
 
 if __name__ == '__main__':
     #unittest.main()
     test = TestExternalPyomoModel()
-    test.test_external_hessian_Model2by2()
+    test.test_external_jacobian_Model2by2()
+    #test.test_external_hessian_Model2by2()
