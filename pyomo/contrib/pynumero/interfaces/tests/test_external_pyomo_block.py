f0749e355e836cd53cc66aec5e103476ae7df4e6
Robert Parker <robbybparker@gmail.com>
2021-06-20 22:11:37 -0400

    test constraint evaluation from grey box nlp

--- pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_block.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_block.py
@@ -39,6 +39,9 @@ from pyomo.contrib.pynumero.interfaces.external_grey_box import (
     ScalarExternalGreyBoxBlock,
     IndexedExternalGreyBoxBlock,
 )
+from pyomo.contrib.pynumero.interfaces.pyomo_grey_box_nlp import (
+    PyomoNLPWithGreyBoxBlocks,
+)
 
 if not pyo.SolverFactory("ipopt").available():
     raise unittest.SkipTest(
@@ -55,8 +58,8 @@ def make_external_model():
     m.y = pyo.Var()
     m.x_out = pyo.Var()
     m.y_out = pyo.Var()
-    m.c_out_1 = pyo.Constraint(expr=m.x_out == m.x)
-    m.c_out_2 = pyo.Constraint(expr=m.y_out == m.y)
+    m.c_out_1 = pyo.Constraint(expr=m.x_out - m.x == 0)
+    m.c_out_2 = pyo.Constraint(expr=m.y_out - m.y == 0)
     m.c_ex_1 = pyo.Constraint(expr=
             m.x**3 - 2*m.y == m.a**2 + m.b**3 - m.r**3 - 2
             )
@@ -147,7 +150,7 @@ class TestExternalGreyBoxBlock(unittest.TestCase):
             elif i == 2:
                 return m.r == m.ex_block.inputs["input_2"]
 
-        m.linking_constraint = pyo.Constraint(range(3),
+        m.linking_constraint = pyo.Constraint(range(n_inputs),
                 rule=linking_constraint_rule)
 
         m.a.fix(1)
@@ -204,7 +207,7 @@ class TestExternalGreyBoxBlock(unittest.TestCase):
 
         # Solve with external model embedded
         solver = pyo.SolverFactory("cyipopt")
-        solver.solve(m, tee=True)
+        solver.solve(m)
 
         m_ex.obj = pyo.Objective(expr=
                 (m_ex.x-2.0)**2 + (m_ex.y-2.0)**2 + (m_ex.a-2.0)**2 +
@@ -218,7 +221,7 @@ class TestExternalGreyBoxBlock(unittest.TestCase):
 
         # Solve external model, now with same objective function
         ipopt = pyo.SolverFactory("ipopt")
-        ipopt.solve(m_ex, tee=True)
+        ipopt.solve(m_ex)
 
         # Make sure full space and reduced space solves give same
         # answers.
@@ -227,6 +230,106 @@ class TestExternalGreyBoxBlock(unittest.TestCase):
         self.assertAlmostEqual(m_ex.r.value, r.value, delta=1e-8)
         self.assertAlmostEqual(m_ex.x.value, x.value, delta=1e-8)
         self.assertAlmostEqual(m_ex.y.value, y.value, delta=1e-8)
+
+class TestPyomoNLPWithGreyBoxBLocks(unittest.TestCase):
+
+    def test_pyomo_nlp(self):
+        m = pyo.ConcreteModel()
+        m.ex_block = ExternalGreyBoxBlock(concrete=True)
+        block = m.ex_block
+
+        m_ex = make_external_model()
+        input_vars = [m_ex.a, m_ex.b, m_ex.r, m_ex.x_out, m_ex.y_out]
+        external_vars = [m_ex.x, m_ex.y]
+        residual_cons = [m_ex.c_out_1, m_ex.c_out_2]
+        external_cons = [m_ex.c_ex_1, m_ex.c_ex_2]
+        ex_model = ExternalPyomoModel(
+                input_vars,
+                external_vars,
+                residual_cons,
+                external_cons,
+                )
+        block.set_external_model(ex_model)
+
+        a = m.ex_block.inputs["input_0"]
+        b = m.ex_block.inputs["input_1"]
+        r = m.ex_block.inputs["input_2"]
+        x = m.ex_block.inputs["input_3"]
+        y = m.ex_block.inputs["input_4"]
+        m.obj = pyo.Objective(expr=
+                (x-2.0)**2 + (y-2.0)**2 + (a-2.0)**2 + (b-2.0)**2 + (r-2.0)**2
+                )
+
+        m.a = pyo.Var()
+        m.b = pyo.Var()
+        m.r = pyo.Var()
+
+        n_inputs = 3
+
+        def linking_constraint_rule(m, i):
+            if i == 0:
+                return m.a - m.ex_block.inputs["input_0"] == 0
+            elif i == 1:
+                return m.b - m.ex_block.inputs["input_1"] == 0
+            elif i == 2:
+                return m.r - m.ex_block.inputs["input_2"] == 0
+
+        m.linking_constraint = pyo.Constraint(range(n_inputs),
+                rule=linking_constraint_rule)
+
+        nlp = PyomoNLPWithGreyBoxBlocks(m)
+
+        # Set primals in model, get primals in nlp
+        # set/get duals
+        # evaluate constraints
+        # evaluate Jacobian
+        # evaluate Hessian
+        self.assertEqual(nlp.n_primals(), 8)
+
+        # PyomoNLPWithGreyBoxBlocks sorts variables by name
+        primals_names = [
+                "a",
+                "b",
+                "ex_block.inputs[input_0]",
+                "ex_block.inputs[input_1]",
+                "ex_block.inputs[input_2]",
+                "ex_block.inputs[input_3]",
+                "ex_block.inputs[input_4]",
+                "r",
+                ]
+        self.assertEqual(nlp.primals_names(), primals_names)
+        np.testing.assert_equal(np.zeros(8), nlp.get_primals())
+
+        primals = np.array([0, 1, 2, 3, 4, 5, 6, 7])
+        nlp.set_primals(primals)
+        np.testing.assert_equal(primals, nlp.get_primals())
+        nlp.load_state_into_pyomo()
+
+        for name, val in zip(primals_names, primals):
+            var = m.find_component(name)
+            self.assertEqual(var.value, val)
+
+        constraint_names = [
+                "linking_constraint[0]",
+                "linking_constraint[1]",
+                "linking_constraint[2]",
+                "ex_block.residual_0",
+                "ex_block.residual_1",
+                ]
+        self.assertEqual(constraint_names, nlp.constraint_names())
+        residuals = np.array([
+                -2.0,
+                -2.0,
+                3.0,
+                # These values were obtained by solving the same system
+                # with Ipopt in another script. It may be better to do
+                # the solve in this test in case the system changes.
+                5.0-(-3.03051522),
+                6.0-3.583839997,
+                ])
+        np.testing.assert_allclose(residuals, nlp.evaluate_constraints(),
+                rtol=1e-8)
+
 
 
 if __name__ == '__main__':
