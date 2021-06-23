14d00b975618306aa7b4eff67451d10c5e75c0d5
John Siirola <jsiirola@users.noreply.github.com>
2021-06-23 12:17:05 -0600

    Only cast ufunc results to NumericNDArray if it is an ndarray

--- pyomo/core/expr/numvalue.py
+++ pyomo/core/expr/numvalue.py
@@ -1047,4 +1047,8 @@ class NumericNDArray(np.ndarray if numpy_available else object):
 
         # Delegate to the base ufunc, but return an instance of this
         # class so that additional operators hit this method.
-        return getattr(ufunc, method)(*args, **kwargs).view(NumericNDArray)
+        ans = getattr(ufunc, method)(*args, **kwargs)
+        if isinstance(ans, np.ndarray):
+            return ans.view(NumericNDArray)
+        else:
+            return ans
