f2ef5ff97dca3d4aca6e82008fd7e766aab7f0c7
Katherine Klise <kaklise@sandia.gov>
2021-06-30 12:52:33 -0700

    Updated RB example to use romodel

--- pyomo/contrib/parmest/examples/reactor_design/reactor_design.py
+++ pyomo/contrib/parmest/examples/reactor_design/reactor_design.py
@@ -11,8 +11,10 @@
 Continuously stirred tank reactor model, based on
 pyomo\examples\doc\pyomobook\nonlinear-ch\react_design\ReactorDesign.py
 """
+import numpy as np
 import pandas as pd
 from pyomo.environ import ConcreteModel, Var, PositiveReals, Objective, Constraint, maximize, SolverFactory
+from romodel import UncSet, UncParam
 
 def reactor_design_model(data):
     
@@ -60,6 +62,60 @@ def reactor_design_model(data):
     
     return model
 
+
+
+def reactor_design_model_RO(data, theta, cov):
+    
+    # Create the concrete model
+    model = ConcreteModel()
+        
+    # Rate constants defined as an uncertain parameter
+    theta_names = theta.keys()
+    model.E = UncSet()
+    model.k = UncParam(theta_names, uncset=model.E, nominal=theta) # {'k1': 5.0/6.0, 'k2': 5.0/3.0, 'k3': 1.0/6000.0}) #
+    # Ellipsoidal set defined using covariance informed by data/parmest
+    mu = list(theta.values())
+    cov = cov.values
+    A = np.linalg.inv(cov).tolist()
+    lhs = 0
+    for i, ki in enumerate(theta_names):
+        for j, kj in enumerate(theta_names):
+            lhs += (model.k[ki] - mu[i])*A[i][j]*(model.k[kj] - mu[j])
+    model.E.cons = Constraint(expr=lhs <= 1)
+        
+    # Inlet concentration of A, gmol/m^3
+    model.caf = Var(initialize = float(data['caf']), within=PositiveReals)
+    model.caf.fixed = True
+        
+    # Space velocity (flowrate/volume)
+    model.sv = Var(initialize = float(data['sv']), within=PositiveReals)
+    model.sv.fixed = True
+        
+    # Outlet concentration of each component
+    model.ca = Var(initialize = float(data['ca']), within=PositiveReals) 
+    model.cb = Var(initialize = float(data['cb']), within=PositiveReals) 
+    model.cc = Var(initialize = float(data['cc']), within=PositiveReals) 
+    model.cd = Var(initialize = float(data['cd']), within=PositiveReals)
+
+    # Objective
+    model.obj = Objective(expr = model.cb, sense=maximize)
+        
+    # Constraints
+    model.ca_bal = Constraint(expr = (0 == model.sv * model.caf \
+                     - model.sv * model.ca - model.k['k1'] * model.ca \
+                     -  2.0 * model.k['k3'] * model.ca ** 2.0))
+        
+    model.cb_bal = Constraint(expr=(0 == -model.sv * model.cb \
+                     + model.k['k1'] * model.ca - model.k['k2'] * model.cb))
+    
+    model.cc_bal = Constraint(expr=(0 == -model.sv * model.cc \
+                     + model.k['k2'] * model.cb))
+        
+    model.cd_bal = Constraint(expr=(0 == -model.sv * model.cd \
+                     + model.k['k3'] * model.ca ** 2.0))
+        
+    return model
+    
 if __name__ == "__main__":
     
     # For a range of sv values, return ca, cb, cc, and cd
