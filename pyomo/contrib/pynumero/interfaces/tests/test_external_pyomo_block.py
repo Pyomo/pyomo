7bcea1aa340c69ea1f942b15d66658cb1988665b
Robert Parker <robbybparker@gmail.com>
2021-07-02 13:54:47 -0400

    test proper construction of EGBB with dynamic model

--- pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_block.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_block.py
@@ -292,6 +292,83 @@ class TestExternalGreyBoxBlock(unittest.TestCase):
         self.assertAlmostEqual(m_ex.x.value, x.value, delta=1e-8)
         self.assertAlmostEqual(m_ex.y.value, y.value, delta=1e-8)
 
+    def test_construct_dynamic(self):
+        m = make_dynamic_model()
+        time = m.time
+        t0 = m.time.first()
+
+        inputs = [m.h, m.dhdt, m.flow_in]
+        ext_vars = [m.flow_out]
+        residuals = [m.h_diff_eqn]
+        ext_cons = [m.flow_out_eqn]
+
+        external_model_dict = {
+                t: ExternalPyomoModel(
+                    [var[t] for var in inputs],
+                    [var[t] for var in ext_vars],
+                    [con[t] for con in residuals],
+                    [con[t] for con in ext_cons],
+                    )
+                for t in time
+                }
+
+        reduced_space = pyo.Block(concrete=True)
+        reduced_space.external_block = ExternalGreyBoxBlock(
+                time,
+                external_model=external_model_dict,
+                )
+        block = reduced_space.external_block
+        block[t0].deactivate()
+        self.assertIs(type(block), IndexedExternalGreyBoxBlock)
+
+        for t in time:
+            b = block[t]
+            self.assertEqual(len(b.inputs), len(inputs))
+            self.assertEqual(len(b.outputs), 0)
+            self.assertEqual(len(b._equality_constraint_names), len(residuals))
+
+        reduced_space.diff_var = pyo.Reference(m.h)
+        reduced_space.deriv_var = pyo.Reference(m.dhdt)
+        reduced_space.input_var = pyo.Reference(m.flow_in)
+        reduced_space.disc_eqn = pyo.Reference(m.dhdt_disc_eqn)
+
+        pyomo_vars = list(reduced_space.component_data_objects(pyo.Var))
+        pyomo_cons = list(reduced_space.component_data_objects(pyo.Constraint))
+        # NOTE: Variables in the EGBB are not found by component_data_objects
+        self.assertEqual(len(pyomo_vars), len(inputs)*len(time))
+        # "Constraints" defined by the EGBB are not found either, although
+        # this is expected.
+        self.assertEqual(len(pyomo_cons), len(time)-1)
+
+        reduced_space._obj = pyo.Objective(expr=0)
+
+        # This is required to avoid a failure in the implicit function
+        # evaluation when "initializing" (?) the PNLPwGBB.
+        # Why exactly is function evaluation necessary for this
+        # initialization again?
+        block[:].inputs[:].set_value(1.0)
+
+        # This is necessary for these variables to appear in the PNLPwGBB.
+        # Otherwise they don't appear in any "real" constraints of the
+        # PyomoNLP. 
+        reduced_space.const_input_eqn = pyo.Constraint(
+            expr=reduced_space.input_var[2] - reduced_space.input_var[1] == 0
+            )
+
+        nlp = PyomoNLPWithGreyBoxBlocks(reduced_space)
+        self.assertEqual(
+                nlp.n_primals(),
+                # EGBB "inputs", dhdt, and flow_in exist for t != t0.
+                # h exists for all time.
+                (2+len(inputs))*(len(time)-1) + len(time),
+                )
+        self.assertEqual(
+                nlp.n_constraints(),
+                # EGBB equality constraints and disc_eqn exist for t != t0.
+                # const_input_eqn is a single constraint
+                (len(residuals)+1)*(len(time)-1) + 1,
+                )
+
     @unittest.skipUnless(cyipopt_available, "cyipopt is not available")
     def test_solve_square_dynamic(self):
         # Create the "external model"
@@ -798,4 +875,6 @@ class TestPyomoNLPWithGreyBoxBLocks(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    #unittest.main()
+    test = TestExternalGreyBoxBlock()
+    test.test_construct_dynamic()
