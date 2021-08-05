8055743b0bbe01581d4262485ec7f8ee8d401914
John Siirola <jsiirola@users.noreply.github.com>
2021-08-05 01:31:03 -0600

    Resolve test failure when numpy is not present

--- pyomo/core/tests/unit/test_kernel_register_numpy_types.py
+++ pyomo/core/tests/unit/test_kernel_register_numpy_types.py
@@ -9,36 +9,41 @@
 #  ___________________________________________________________________________
 
 import pyomo.common.unittest as unittest
+from pyomo.common.dependencies import numpy_available
 from pyomo.common.log import LoggingIntercept
 
 # Boolean
 numpy_bool_names = []
-numpy_bool_names.append('bool_')
+if numpy_available:
+    numpy_bool_names.append('bool_')
 # Integers
 numpy_int_names = []
-numpy_int_names.append('int_')
-numpy_int_names.append('intc')
-numpy_int_names.append('intp')
-numpy_int_names.append('int8')
-numpy_int_names.append('int16')
-numpy_int_names.append('int32')
-numpy_int_names.append('int64')
-numpy_int_names.append('uint8')
-numpy_int_names.append('uint16')
-numpy_int_names.append('uint32')
-numpy_int_names.append('uint64')
+if numpy_available:
+    numpy_int_names.append('int_')
+    numpy_int_names.append('intc')
+    numpy_int_names.append('intp')
+    numpy_int_names.append('int8')
+    numpy_int_names.append('int16')
+    numpy_int_names.append('int32')
+    numpy_int_names.append('int64')
+    numpy_int_names.append('uint8')
+    numpy_int_names.append('uint16')
+    numpy_int_names.append('uint32')
+    numpy_int_names.append('uint64')
 # Reals
 numpy_float_names = []
-numpy_float_names.append('float_')
-numpy_float_names.append('float16')
-numpy_float_names.append('float32')
-numpy_float_names.append('float64')
-numpy_float_names.append('ndarray')
+if numpy_available:
+    numpy_float_names.append('float_')
+    numpy_float_names.append('float16')
+    numpy_float_names.append('float32')
+    numpy_float_names.append('float64')
+    numpy_float_names.append('ndarray')
 # Complex
 numpy_complex_names = []
-numpy_complex_names.append('complex_')
-numpy_complex_names.append('complex64')
-numpy_complex_names.append('complex128')
+if numpy_available:
+    numpy_complex_names.append('complex_')
+    numpy_complex_names.append('complex64')
+    numpy_complex_names.append('complex128')
 
 
 class TestNumpyRegistration(unittest.TestCase):
