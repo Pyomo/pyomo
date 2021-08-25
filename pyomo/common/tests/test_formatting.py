e50961dfe230d5aafa7abb83df1437d61df46b60
John Siirola <jsiirola@users.noreply.github.com>
2021-08-25 00:19:19 -0600

    Improve StreamIndenter testing

--- pyomo/common/tests/test_formatting.py
+++ pyomo/common/tests/test_formatting.py
@@ -173,17 +173,30 @@ Key : i : j
 
 
 class TestStreamIndenter(unittest.TestCase):
-    def test_StreamIndenter_noprefix(self):
+    def test_noprefix(self):
         OUT1 = StringIO()
         OUT2 = StreamIndenter(OUT1)
         OUT2.write('Hello?\nHello, world!')
         self.assertEqual('    Hello?\n    Hello, world!',
                          OUT2.getvalue())
 
-    def test_StreamIndenter_prefix(self):
+    def test_prefix(self):
         prefix = 'foo:'
         OUT1 = StringIO()
         OUT2 = StreamIndenter(OUT1, prefix)
         OUT2.write('Hello?\nHello, world!')
         self.assertEqual('foo:Hello?\nfoo:Hello, world!', OUT2.getvalue())
 
+    def test_blank_lines(self):
+        OUT1 = StringIO()
+        OUT2 = StreamIndenter(OUT1)
+        OUT2.write('Hello?\n\nText\n\nHello, world!')
+        self.assertEqual('    Hello?\n\n    Text\n\n    Hello, world!',
+                         OUT2.getvalue())
+
+    def test_writelines(self):
+        OUT1 = StringIO()
+        OUT2 = StreamIndenter(OUT1)
+        OUT2.writelines(['Hello?\n', '\n', 'Text\n', '\n', 'Hello, world!'])
+        self.assertEqual('    Hello?\n\n    Text\n\n    Hello, world!',
+                         OUT2.getvalue())
