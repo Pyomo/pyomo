838926582e67b74fbc2bd41298464596b2f982f0
John Siirola <jsiirola@users.noreply.github.com>
2021-09-26 22:56:27 -0600

    Switch Expression to use _pop_from_kwargs()

--- pyomo/core/base/expression.py
+++ pyomo/core/base/expression.py
@@ -269,27 +269,19 @@ class Expression(IndexedComponent):
             return IndexedExpression.__new__(IndexedExpression)
 
     def __init__(self, *args, **kwds):
-        _init = tuple(
-            arg for arg in
-            (kwds.pop(_arg, None) for _arg in ('rule', 'expr', 'initialize'))
-            if arg is not None
-        )
-        if len(_init) == 1:
-            _init = _init[0]
-        elif not _init:
-            _init = None
-        else:
-            raise ValueError(
-                "Duplicate initialization: Expression() only "
-                "accepts one of 'rule=', 'expr=', and 'initialize='")
-
-        kwds.setdefault('ctype', Expression)
-        IndexedComponent.__init__(self, *args, **kwds)
-
+        _init = self._pop_from_kwargs(
+            'Expression', kwds, ('rule', 'expr', 'initialize'), None)
         # Historically, Expression objects were dense (but None):
         # setting arg_not_specified causes Initializer to recognize
         # _init==None as a constant initializer returning None
+        #
+        # To initialize a completely empty Expression, pass either
+        # initialize={} (to require explicit setitem before a getitem),
+        # or initialize=NOTSET (to allow getitem before setitem)
         self._rule = Initializer(_init, arg_not_specified=NOTSET)
+
+        kwds.setdefault('ctype', Expression)
+        IndexedComponent.__init__(self, *args, **kwds)
 
     def _pprint(self):
         return (
