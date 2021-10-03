922e3901d6f2d390287c01fdc03626c30b2b8e67
Robert <robbybparker@gmail.com>
2021-10-02 21:30:23 -0400

    one more test

--- pyomo/dae/tests/test_flatten.py
+++ pyomo/dae/tests/test_flatten.py
@@ -1275,6 +1275,29 @@ class TestCUID(unittest.TestCase):
             else:
                 raise RuntimeError()
 
+    def test_cuid_one_set_no_subblocks(self):
+        m = ConcreteModel()
+        m.s1 = Set(initialize=[1, 2, 3])
+        m.v = Var(m.s1)
+
+        pred_cuid_set = {
+            "v[*]",
+        }
+
+        sets = (m.s1,)
+        ctype = Var
+        sets_list, comps_list = flatten_components_along_sets(m, sets, Var)
+        self.assertEqual(len(sets_list), 1)
+        self.assertEqual(len(comps_list), 1)
+        for sets, comps in zip(sets_list, comps_list):
+            if len(sets) == 1 and sets[0] is m.s1:
+                self.assertEqual(len(comps), 1)
+                cuid_set = set(str(ComponentUID(comp.referent))
+                        for comp in comps)
+                self.assertEqual(cuid_set, pred_cuid_set)
+            else:
+                raise RuntimeError()
+
     def test_cuids_no_sets_with_subblocks(self):
         m = ConcreteModel()
         m.s1 = Set(initialize=[1, 2, 3])
