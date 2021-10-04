8525938affaeb71925f4d742569fba56787f6462
John Siirola <jsiirola@users.noreply.github.com>
2021-10-04 16:27:26 -0600

    Do not mark ASL solvers without a version number as unavailable

--- pyomo/solvers/plugins/solvers/ASL.py
+++ pyomo/solvers/plugins/solvers/ASL.py
@@ -98,7 +98,12 @@ class ASL(SystemCallSolver):
                                      stdout=subprocess.PIPE,
                                      stderr=subprocess.STDOUT,
                                      universal_newlines=True)
-            return _extract_version(results.stdout)
+            ver = _extract_version(results.stdout)
+            if ver is None:
+                # Some ASL solvers do not export a version number
+                if results.stdout.strip().split()[-1].startswith('ASL('):
+                    return '0.0.0'
+            return ver
         except OSError:
             pass
         except subprocess.TimeoutExpired:
