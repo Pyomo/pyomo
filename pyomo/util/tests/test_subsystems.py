b19fecf948e1e23738678fa7f134feb8020e1536
Robert <robbybparker@gmail.com>
2021-10-05 14:09:50 -0400

    test for identify_external_functions

--- pyomo/util/tests/test_subsystems.py
+++ pyomo/util/tests/test_subsystems.py
@@ -16,6 +16,7 @@ from pyomo.util.subsystems import (
         generate_subsystem_blocks,
         TemporarySubsystemManager,
         ParamSweeper,
+        identify_external_functions,
         )
 from pyomo.common.getGSL import find_GSL
 
@@ -343,6 +344,33 @@ class TestSubsystemBlock(unittest.TestCase):
             expr=m.bessel(m.v1) - m.bessel(m.v2) + m.v3**2 == 2.0
         )
         return m
+
+    @unittest.skipUnless(find_GSL(), "Could not find the AMPL GSL library")
+    def test_identify_external_functions(self):
+        m = self._make_model_with_external_functions()
+        m._con = pyo.Constraint(expr=2*m.fermi(m.bessel(m.v1**2) + 0.1) == 1.0)
+
+        gsl = find_GSL()
+
+        fcns = list(identify_external_functions(m.con2.expr))
+        self.assertEqual(len(fcns), 1)
+        self.assertEqual(fcns[0]._fcn._library, gsl)
+        self.assertEqual(fcns[0]._fcn._function, "gsl_sf_fermi_dirac_m1")
+
+        fcns = list(identify_external_functions(m.con3.expr))
+        fcn_data = set((fcn._fcn._library, fcn._fcn._function) for fcn in fcns)
+        self.assertEqual(len(fcns), 2)
+        pred_fcn_data = {(gsl, "gsl_sf_bessel_J0")}
+        self.assertEqual(fcn_data, pred_fcn_data)
+
+        fcns = list(identify_external_functions(m._con.expr))
+        fcn_data = set((fcn._fcn._library, fcn._fcn._function) for fcn in fcns)
+        self.assertEqual(len(fcns), 2)
+        pred_fcn_data = {
+            (gsl, "gsl_sf_bessel_J0"),
+            (gsl, "gsl_sf_fermi_dirac_m1"),
+        }
+        self.assertEqual(fcn_data, pred_fcn_data)
 
     def _solve_ef_model_with_ipopt(self):
         m = self._make_model_with_external_functions()
