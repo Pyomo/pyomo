e94289d2fc5303aaf4ca428a0bad394a0f5b8744
Michael Bynum <mlbynum@sandia.gov>
2021-10-11 07:27:57 -0600

    tests for differentiation with nested expressions

--- pyomo/core/tests/unit/test_derivs.py
+++ pyomo/core/tests/unit/test_derivs.py
@@ -267,3 +267,17 @@ class TestDerivs(unittest.TestCase):
         self.assertAlmostEqual(derivs[m.x], approx_deriv(e, m.x), tol)
         self.assertAlmostEqual(derivs[m.y], pyo.value(symbolic[m.y]), tol+3)
         self.assertAlmostEqual(derivs[m.y], approx_deriv(e, m.y), tol)
+
+    def test_nested_named_expressions(self):
+        m = pyo.ConcreteModel()
+        m.x = pyo.Var(initialize=0.23)
+        m.y = pyo.Var(initialize=0.88)
+        m.a = pyo.Expression(expr=(m.x + 1)**2)
+        m.b = pyo.Expression(expr=3*(m.a + m.y))
+        e = 2*m.a + 2*m.b + 2*m.b + 2*m.a
+        derivs = reverse_ad(e)
+        symbolic = reverse_sd(e)
+        self.assertAlmostEqual(derivs[m.x], pyo.value(symbolic[m.x]), tol+3)
+        self.assertAlmostEqual(derivs[m.x], approx_deriv(e, m.x), tol)
+        self.assertAlmostEqual(derivs[m.y], pyo.value(symbolic[m.y]), tol+3)
+        self.assertAlmostEqual(derivs[m.y], approx_deriv(e, m.y), tol)
