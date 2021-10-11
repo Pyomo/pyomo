3d1ddfd0cd391def34736adbd1105ab6736248e3
John Siirola <jsiirola@users.noreply.github.com>
2021-10-11 17:23:00 -0600

    Add test for keys(ordered=True)

--- pyomo/core/tests/unit/test_indexed.py
+++ pyomo/core/tests/unit/test_indexed.py
@@ -172,6 +172,34 @@ class TestIndexedComponent(unittest.TestCase):
             TypeError, '.*',
             m.x.__getitem__, {})
 
+    def test_ordered_keys(self):
+        m = ConcreteModel()
+        # Pick a set whose unordered iteration order should never match
+        # the "ordered" iteration order.
+        init_keys = [2, 1, (1, 2), (1, 'a'), (1, 1)]
+        m.I = Set(ordered=False, dimen=None, initialize=init_keys)
+        ordered_keys = [1, 2, (1, 1), (1, 2), (1, 'a')]
+        m.x = Var(m.I)
+        self.assertNotEqual(list(m.x.keys()), list(m.x.keys(True)))
+        self.assertEqual(set(m.x.keys()), set(m.x.keys(True)))
+        self.assertEqual(ordered_keys, list(m.x.keys(True)))
+
+        m.P = Param(m.I, initialize={k:v for v,k in enumerate(init_keys)})
+        self.assertNotEqual(list(m.P.keys()), list(m.P.keys(True)))
+        self.assertEqual(set(m.P.keys()), set(m.P.keys(True)))
+        self.assertEqual(ordered_keys, list(m.P.keys(True)))
+        self.assertEqual([1, 0, 4, 2, 3], list(m.P.values(True)))
+        self.assertEqual(list(zip(ordered_keys, [1, 0, 4, 2, 3])),
+                         list(m.P.items(True)))
+
+        m.P = Param(m.I, initialize={(1,2): 30, 1:10, 2:20}, default=1)
+        self.assertNotEqual(list(m.P.keys()), list(m.P.keys(True)))
+        self.assertEqual(set(m.P.keys()), set(m.P.keys(True)))
+        self.assertEqual(ordered_keys, list(m.P.keys(True)))
+        self.assertEqual([10, 20, 1, 30, 1], list(m.P.values(True)))
+        self.assertEqual(list(zip(ordered_keys, [10, 20, 1, 30, 1])),
+                         list(m.P.items(True)))
+
 
 if __name__ == "__main__":
     unittest.main()
