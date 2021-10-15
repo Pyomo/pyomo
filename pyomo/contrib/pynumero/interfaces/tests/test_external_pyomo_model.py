17a1c80a2b94c17d53e1da47f86592804f9430ef
Robert Parker <robbybparker@gmail.com>
2021-10-15 13:12:51 -0400

    finish test for full space hessians

--- pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_model.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_model.py
@@ -291,14 +291,25 @@ class Model2by2(object):
         dg0dx0dx0 = 2*y[0]*x[1]**0.5*y[1]
         dg0dx0dx1 = x[0]*y[0]*y[1]/x[1]**0.5
         dg0dx1dx1 = -1/4*x[0]**2*y[0]*y[1]/x[1]**(3/2)
+        dg0dx0dy0 = 2*x[0]*x[1]**0.5*y[1]
+        dg0dx0dy1 = 2*x[0]*y[0]*x[1]**0.5
+        dg0dx1dy0 = 0.5*x[0]**2*y[1]/x[1]**0.5
+        dg0dx1dy1 = 0.5*x[0]**2*y[0]/x[1]**0.5
+        dg0dy0dy1 = x[0]**2*x[1]**0.5
         hg0xx = np.array([[dg0dx0dx0, dg0dx0dx1], [dg0dx0dx1, dg0dx1dx1]])
+        hg0xy = np.array([[dg0dx0dy0, dg0dx0dy1], [dg0dx1dy0, dg0dx1dy1]])
+        hg0yy = np.array([[0, dg0dy0dy1], [dg0dy0dy1, 0]])
 
         dg1dx0dx1 = y[0]
+        dg1dx0dy0 = x[1]
+        dg1dx1dy0 = x[0]
         hg1xx = np.array([[0, dg1dx0dx1], [dg1dx0dx1, 0]])
+        hg1xy = np.array([[dg1dx0dy0, 0], [dg1dx1dy0, 0]])
+        hg1yy = np.zeros((2, 2))
 
         hlxx = lam[0]*hfxx + lam_g[0]*hg0xx + lam_g[1]*hg1xx
-        hlxy = np.zeros((2, 2))
-        hlyy = np.zeros((2, 2))
+        hlxy = lam[0]*hfxy + lam_g[0]*hg0xy + lam_g[1]*hg1xy
+        hlyy = lam[0]*hfyy + lam_g[0]*hg0yy + lam_g[1]*hg1yy
         return hlxx, hlxy, hlyy
 
 
