32bb5b4c9d2378324fed3fe182ad9351a146c2ed
Robert Parker <robbybparker@gmail.com>
2021-10-15 17:42:27 -0400

    test with order of set_primals/set_duals swapped

--- pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_model.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_external_pyomo_model.py
@@ -974,7 +974,50 @@ class TestUpdatedHessianCalculationMethods(unittest.TestCase):
             x = [x0, x1]
             lam = [lam]
             external_model.set_input_values(x)
-            external_model.set_external_constraint_multipliers(lam)
+            external_model.set_equality_constraint_multipliers(lam)
+            hess = external_model.evaluate_hessian_equality_constraints()
+            pred_hess = model.calculate_reduced_lagrangian_hessian(lam, x)
+            # This test asserts that we are doing the block reduction properly.
+            np.testing.assert_allclose(
+                hess.toarray(), np.tril(pred_hess), rtol=1e-8
+            )
+
+            from_individual = external_model.evaluate_hessians_of_residuals()
+            hl_from_individual = sum(l*h for l, h in zip(lam, from_individual))
+            # This test asserts that the block reduction is correct.
+            np.testing.assert_allclose(
+                hess.toarray(), np.tril(hl_from_individual), rtol=1e-8
+            )
+
+    def test_evaluate_hessian_equality_constraints_order(self):
+        model = Model2by2()
+        m = model.make_model()
+        m.x[0].set_value(1.0)
+        m.x[1].set_value(2.0)
+        m.y[0].set_value(3.0)
+        m.y[1].set_value(4.0)
+        x0_init_list = [-5.0, -3.0, 0.5, 1.0, 2.5]
+        x1_init_list = [0.5, 1.0, 1.5, 2.5, 4.1]
+        lam_init_list = [-2.5, -0.5, 0.0, 1.0, 2.0]
+        init_list = list(
+            itertools.product(x0_init_list, x1_init_list, lam_init_list)
+        )
+        external_model = ExternalPyomoModel(
+                list(m.x.values()),
+                list(m.y.values()),
+                list(m.residual_eqn.values()),
+                list(m.external_eqn.values()),
+                )
+
+        for x0, x1, lam in init_list:
+            x = [x0, x1]
+            lam = [lam]
+            external_model.set_equality_constraint_multipliers(lam)
+            external_model.set_input_values(x)
+            # Using evaluate_hessian_equality_constraints, which calculates
+            # external multiplier values, we can calculate the correct Hessian
+            # regardless of the order in which primal and dual variables are
+            # set.
             hess = external_model.evaluate_hessian_equality_constraints()
             pred_hess = model.calculate_reduced_lagrangian_hessian(lam, x)
             # This test asserts that we are doing the block reduction properly.
