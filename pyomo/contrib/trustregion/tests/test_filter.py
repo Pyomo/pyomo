47b1ec189fc83651724425ecc56c552226ad2620
Miranda Mundt <mmundt@sandia.gov>
2021-10-19 11:52:11 -0600

    Adding filter tests

--- pyomo/contrib/trustregion/tests/test_filter.py
+++ pyomo/contrib/trustregion/tests/test_filter.py
@@ -9,11 +9,34 @@
 #  ___________________________________________________________________________
 
 import pyomo.common.unittest as unittest
-from pyomo.contrib.trustregion.filter import Filter
+from pyomo.contrib.trustregion.filter import Filter, FilterElement
 
-class TestUtils(unittest.TestCase):
+class TestFilter(unittest.TestCase):
     def setUp(self):
-        pass
+        self.objective = 1.0
+        self.infeasible = 0.5
+        self.theta_max = 10.0
+        self.tmpFilter = Filter()
 
     def tearDown(self):
         pass
+
+    def test_FilterElement(self):
+        fe = FilterElement(self.objective, self.infeasible)
+        self.assertEqual(fe.objective, self.objective)
+        self.assertEqual(fe.infeasible, self.infeasible)
+
+    def test_addToFilter(self):
+        fe = FilterElement(self.objective, self.infeasible)
+        self.tmpFilter.addToFilter(fe)
+        self.assertIn(fe, self.tmpFilter.TrustRegionFilter)
+
+    def test_isAcceptable(self):
+        fe = FilterElement(0.5, 0.25)
+        # A sufficiently "small enough" element to pass the filter
+        self.assertTrue(self.tmpFilter.isAcceptable(fe,
+                                                    self.theta_max))
+        fe = FilterElement(1.0, 15.0)
+        # A sufficiently "large enough" element to fail the filter
+        self.assertFalse(self.tmpFilter.isAcceptable(fe,
+                                                    self.theta_max))
