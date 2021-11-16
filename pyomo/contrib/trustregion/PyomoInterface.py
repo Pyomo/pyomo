7e4b6782c71633fe72afb56ffe36208d01cc9686
Miranda Mundt <mmundt@sandia.gov>
2021-11-16 10:29:38 -0700

    Fixing more numpy imports in TRF

--- pyomo/contrib/trustregion/PyomoInterface.py
+++ pyomo/contrib/trustregion/PyomoInterface.py
@@ -10,7 +10,10 @@
 
 import logging
 
-import numpy as np
+from pyomo.common.dependencies import numpy_available
+if numpy_available:
+    import numpy as np
+
 from math import inf
 from pyomo.common.collections import ComponentSet
 from pyomo.common.modeling import unique_component_name
