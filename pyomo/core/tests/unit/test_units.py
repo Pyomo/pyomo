191d1a1da426ed7336d1f2792c2698aa82311d47
John Siirola <jsiirola@users.noreply.github.com>
2021-12-03 19:24:25 -0700

    Add as_quantity() function

--- pyomo/core/tests/unit/test_units.py
+++ pyomo/core/tests/unit/test_units.py
@@ -10,6 +10,7 @@
 #  ___________________________________________________________________________
 #
 #
+import math
 import pickle
 
 from pyomo.common.errors import PyomoException
@@ -19,16 +20,18 @@ from pyomo.environ import (
     ExternalFunction, value, sum_product, maximize, units,
     log, log10, exp, sqrt, cos, sin, tan, asin, acos, atan, cosh, sinh,
     tanh, asinh, acosh, atanh, ceil, floor,
+    BooleanVar
 )
 from pyomo.common.log import LoggingIntercept
 from pyomo.util.check_units import (
     assert_units_consistent, check_units_equivalent,
 )
 from pyomo.core.expr import inequality
+from pyomo.core.expr.numvalue import NumericConstant
 import pyomo.core.expr.current as EXPR
 from pyomo.core.base.units_container import (
     pint_available, pint_module, _DeferredUnitsSingleton,
-    InconsistentUnitsError, UnitsError, PyomoUnitsContainer,
+    InconsistentUnitsError, UnitsError, PyomoUnitsContainer, as_quantity
 )
 from io import StringIO
 
@@ -665,6 +668,147 @@ class TestPyomoUnit(unittest.TestCase):
             "system after the PyomoUnitsContainer was constructed",
             LOG.getvalue()
         )
+
+    def test_as_quantity_scalar(self):
+        _pint = units._pint_registry
+        Quantity = _pint.Quantity
+        m = ConcreteModel()
+        m.x = Var(initialize=1)
+        m.y = Var(initialize=2, units=units.g)
+        m.p = Param(initialize=3)
+        m.q = Param(initialize=4, units=1/units.s)
+        m.b = BooleanVar(initialize=True)
+
+        q = as_quantity(0)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 0 * _pint.dimensionless)
+
+        q = as_quantity(None)
+        self.assertIs(q.__class__, None.__class__)
+        self.assertEqual(q, None)
+
+        q = as_quantity(str('aaa'))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 'aaa' * _pint.dimensionless)
+
+        q = as_quantity(True)
+        self.assertIs(q.__class__, bool)
+        self.assertEqual(q, True)
+
+        q = as_quantity(units.kg)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 1 * _pint.kg)
+
+        q = as_quantity(NumericConstant(5))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 5 * _pint.dimensionless)
+
+        q = as_quantity(m.x)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 1 * _pint.dimensionless)
+
+        q = as_quantity(m.y)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 2 * _pint.g)
+
+        q = as_quantity(m.p)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 3 * _pint.dimensionless)
+
+        q = as_quantity(m.q)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 4 / _pint.s)
+
+        q = as_quantity(m.b)
+        self.assertIs(q.__class__, bool)
+        self.assertEqual(q, True)
+
+        class UnknownPyomoType(object):
+            def is_expression_type(self):
+                return False
+
+            def is_numeric_type(self):
+                return False
+
+            def is_logical_type(self):
+                return False
+
+        other = UnknownPyomoType()
+        q = as_quantity(other)
+        self.assertIs(q.__class__, UnknownPyomoType)
+        self.assertIs(q, other)
+
+    def test_as_quantity_expression(self):
+        _pint = units._pint_registry
+        Quantity = _pint.Quantity
+        m = ConcreteModel()
+        m.x = Var(initialize=1)
+        m.y = Var(initialize=2, units=units.g)
+        m.p = Param(initialize=3)
+        m.q = Param(initialize=4, units=1/units.s)
+
+        q = as_quantity(m.x * m.p)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 3 * _pint.dimensionless)
+
+        q = as_quantity(m.x * m.q)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 4 / _pint.s)
+
+        q = as_quantity(m.y * m.p)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 6 * _pint.g)
+
+        q = as_quantity(m.y * m.q)
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 8 * _pint.g / _pint.s)
+
+        q = as_quantity(m.y <= 2*m.y)
+        self.assertIs(q.__class__, bool)
+        self.assertEqual(q, True)
+
+        q = as_quantity(m.y >= 2*m.y)
+        self.assertIs(q.__class__, bool)
+        self.assertEqual(q, False)
+
+        q = as_quantity(EXPR.Expr_if(IF=m.y <= 2*m.y, THEN=m.x, ELSE=m.p))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 1 * _pint.dimensionless)
+
+        q = as_quantity(EXPR.Expr_if(IF=m.y >= 2*m.y, THEN=m.x, ELSE=m.p))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 3 * _pint.dimensionless)
+
+        # NOTE: The following two tests are not unit consistent (but can
+        # be evaluated)
+
+        q = as_quantity(EXPR.Expr_if(IF=m.x <= 2*m.x, THEN=m.y, ELSE=m.q))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 2 * _pint.g)
+
+        q = as_quantity(EXPR.Expr_if(IF=m.x >= 2*m.x, THEN=m.y, ELSE=m.q))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q, 4 / _pint.s)
+
+        # Note: check the units explicitly, as
+        #   Quantity(x, radian) == Quantity(x, dimensionless)
+        q = as_quantity(acos(m.x))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q.units, _pint.radian)
+        self.assertEqual(q, 0 * _pint.radian)
+
+        q = as_quantity(cos(m.x*math.pi))
+        self.assertIs(q.__class__, Quantity)
+        self.assertEqual(q.units, _pint.dimensionless)
+        self.assertAlmostEqual(q, -1 * _pint.dimensionless)
+
+        def MyAdder(x, y):
+            return x + y
+        m.EF = ExternalFunction(MyAdder, units=units.kg)
+        ef = m.EF(m.x, m.y)
+        q = as_quantity(ef)
+        self.assertIs(q.__class__, Quantity)
+        self.assertAlmostEqual(q, 3 * _pint.kg)
 
 
 if __name__ == "__main__":
