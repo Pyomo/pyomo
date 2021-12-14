6a4fff396e2431a68db330f39be4c0d51f109162
Michael Bynum <mlbynum@sandia.gov>
2021-12-14 10:40:11 -0700

    adding support for abs in numeric differentiation

--- pyomo/core/tests/unit/test_derivs.py
+++ pyomo/core/tests/unit/test_derivs.py
@@ -176,7 +176,7 @@ class TestDerivs(unittest.TestCase):
         m = pyo.ConcreteModel()
         m.x = pyo.Var(initialize=2.0)
         e = 2 * abs(m.x)
-        with self.assertRaises(DifferentiationException):
+        with self.assertRaisesRegexp(DifferentiationException, 'Cannot perform symbolic differentiation of abs\(x\)'):
             reverse_sd(e)
         derivs = reverse_ad(e)
         self.assertAlmostEqual(derivs[m.x], approx_deriv(e, m.x), tol)
@@ -184,7 +184,7 @@ class TestDerivs(unittest.TestCase):
         derivs = reverse_ad(e)
         self.assertAlmostEqual(derivs[m.x], approx_deriv(e, m.x), tol)
         m.x.value = 0
-        with self.assertRaises(DifferentiationException):
+        with self.assertRaisesRegexp(DifferentiationException, 'Cannot differentiate abs\(x\) at x=0'):
             reverse_ad(e)
 
     def test_nested(self):
