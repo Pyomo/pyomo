c4868accf340410bc5bb92fcc106529ca47ab170
Katherine Klise <kaklise@sandia.gov>
2022-01-24 17:27:43 -0800

    switch to using subprocess.run instead of call

--- pyomo/contrib/parmest/tests/test_examples.py
+++ pyomo/contrib/parmest/tests/test_examples.py
@@ -1,10 +1,11 @@
 # This is a test to ensure all of the examples run.
+# assert statements should be included in the example files
 import os
 import sys
 import unittest
 from os import listdir
 from os.path import abspath, dirname, isfile, join
-from subprocess import call
+import subprocess
 
 testdir = dirname(abspath(str(__file__)))
 examplesdir = join(testdir, "..", "examples")
@@ -22,7 +23,6 @@ class TestExamples(unittest.TestCase):
     def test_examples(self):
         cwd = os.getcwd()
         os.chdir(examplesdir)
-        flag = 0
         failed_examples = []
         for folder in ['rooney_biegler']:
             subdir = join(examplesdir, folder)
@@ -32,19 +32,18 @@ class TestExamples(unittest.TestCase):
                 for f in listdir(subdir)
                 if isfile(join(subdir, f))
                 and f.endswith(".py")
-                and not f.startswith("test")
             ]
             for f in example_files:
-                tmp_flag = call([sys.executable, join(subdir, f)])
-                print(folder, f, tmp_flag)
-                if tmp_flag == 1:
-                    failed_examples.append(f)
-                    flag = 1
-            os.chdir(examplesdir)
+                abspath = os.path.abspath(join(subdir, f))
+                ret = subprocess.run([sys.executable, abspath])
+                retcode = ret.returncode
+                print(folder, f, retcode)
+                if retcode == 1:
+                    failed_examples.append(abspath)
         os.chdir(cwd)
         if len(failed_examples) > 0:
             print("failed examples: {0}".format(failed_examples))
-        self.assertEqual(flag, 0)
+        self.assertEqual(len(failed_examples), 0)
 
 
 if __name__ == "__main__":
