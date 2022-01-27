ebc07f9ccebf6d0bf098757793f1c462c30f1e2c
Miranda Mundt <mmundt@sandia.gov>
2022-01-27 14:03:48 -0700

    Registering markers in setup.cfg and setting default markers

--- conftest.py
+++ conftest.py
@@ -8,3 +8,12 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
+import pytest
+
+def pytest_runtest_setup(item):
+    marker = item.iter_markers()
+    approved_markers = ['parametrize', 'nightly', 'smoke']
+    item_markers = [mark.name for mark in marker]
+    if (item_markers and
+        not any(mark in approved_markers for mark in item_markers)):
+        pytest.skip('SKIPPED: Default test categories are nightly and smoke.')
