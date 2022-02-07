b7124489724b74eca87f8bf6677770378d265bc9
zedongpeng <peng_zedong@126.com>
2022-02-07 10:27:40 +0800

    fix typo

--- pyomo/contrib/mindtpy/MindtPy.py
+++ pyomo/contrib/mindtpy/MindtPy.py
@@ -115,8 +115,9 @@ class MindtPySolver(object):
 
             MindtPy = solve_data.working_model.MindtPy_utils
             setup_results_object(solve_data, config)
-            # In the process_objective function, as long as the objective function is nonlinear, it will be reformulated and the variable/constraint/objective lists will be updated. For OA/GOA/LP-NLP algorithm, if the objective funtion is linear, the objecive function will not be reformulated.
+            # In the process_objective function, as long as the objective function is nonlinear, it will be reformulated and the variable/constraint/objective lists will be updated. For OA/GOA/LP-NLP algorithm, if the objective funtion is linear, it will not be reformulated as epigraph constraint.
             # If the objective function is linear, it will be reformulated as epigraph constraint only if the Feasibility Pump or ROA/RLP-NLP algorithm is activated. (move_linear_objective = True)
+            # In some cases, the variable/constraint/objective lists will not be updated even if the objective is epigraph-reformulated.  
             # In Feasibility Pump, since the distance calculation only includes discrete variables and the epigraph slack variables are continuous variables, the Feasibility Pump algorithm will not affected even if the variable list are updated.
             # In ROA and RLP/NLP, since the distance calculation does not include these epigraph slack variables, they should not be added to the variable list. (update_var_con_list = False)
             # In the process_objective function, once the objective function has been reformulated as epigraph constraint, the variable/constraint/objective lists will not be updated only if the MINLP has a linear objective function and regularization is activated at the same time. This is because the epigraph constraint is very "flat" for branching rules. The orignal objective function will be used for the main problem and epigraph reformulation will be used for the projection problem.
