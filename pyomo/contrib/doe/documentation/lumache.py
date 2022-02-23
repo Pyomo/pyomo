e73ceddf7da64113334df2fba18fe205b7ce3c72
jialuw96 <jwang44@nd.edu>
2022-02-23 15:09:38 -0500

    add documentation

---
+++ pyomo/contrib/doe/documentation/lumache.py
@@ -0,0 +1,23 @@
+"""
+Lumache - Python library for cooks and food lovers.
+"""
+
+__version__ = "0.1.0"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
+
+
+def get_random_ingredients(kind=None):
+    """
+    Return a list of random ingredients as strings.
+
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
+    return ["shells", "gorgonzola", "parsley"]
