2dcb9ad2d7b19097a617a4278b9a8579437bd9da
Emma Johnson <emma58@users.noreply.github.com>
2022-03-01 10:00:19 -0700

    remove_zero_terms transformation skips trivial constraints on the assumption that they should already be deactivated.

--- pyomo/contrib/preprocessing/tests/test_deactivate_trivial_constraints.py
+++ pyomo/contrib/preprocessing/tests/test_deactivate_trivial_constraints.py
@@ -110,6 +110,17 @@ class TestTrivialConstraintDeactivator(unittest.TestCase):
 
         self.assertFalse(m.c.active)
 
+    def test_higher_degree_trivial_constraint(self):
+        m = ConcreteModel()
+        m.x = Var()
+        m.y = Var()
+        m.z = Var()
+        m.c = Constraint(expr=(m.x**2 + m.y)*m.z >= -8)
+        m.z.fix(0)
+        TransformationFactory(
+            'contrib.deactivate_trivial_constraints').apply_to(m)
+        self.assertFalse(m.c.active)
+
     def test_trivial_linear_constraint_due_to_cancellation(self):
         m = ConcreteModel()
         m.x = Var()
--- pyomo/contrib/preprocessing/tests/test_zero_term_removal.py
+++ pyomo/contrib/preprocessing/tests/test_zero_term_removal.py
@@ -3,6 +3,7 @@ import pyomo.common.unittest as unittest
 from pyomo.environ import (ConcreteModel, Constraint, TransformationFactory,
                            Var)
 from pyomo.core.expr import current as EXPR
+from pyomo.repn import generate_standard_repn
 
 
 class TestRemoveZeroTerms(unittest.TestCase):
@@ -33,6 +34,27 @@ class TestRemoveZeroTerms(unittest.TestCase):
         self.assertFalse(any(id(m.v1) == id(v)
                              for v in EXPR.identify_variables(m.c4.body)))
 
+    def test_trivial_constraints_skipped(self):
+        m = ConcreteModel()
+        m.x = Var()
+        m.y = Var()
+        m.z = Var()
+        m.c = Constraint(expr=(m.x + m.y)*m.z >= 8)
+        m.z.fix(0)
+        TransformationFactory('contrib.remove_zero_terms').apply_to(m)
+        m.z.unfix()
+        # check constraint is unchanged
+        self.assertEqual(m.c.lower, 8)
+        self.assertIsNone(m.c.upper)
+        repn = generate_standard_repn(m.c.body)
+        self.assertTrue(repn.is_quadratic())
+        self.assertEqual(repn.quadratic_coefs[0], 1)
+        self.assertEqual(repn.quadratic_coefs[1], 1)
+        self.assertIs(repn.quadratic_vars[0][0], m.x)
+        self.assertIs(repn.quadratic_vars[0][1], m.z)
+        self.assertIs(repn.quadratic_vars[1][0], m.y)
+        self.assertIs(repn.quadratic_vars[1][1], m.z)
+        self.assertEqual(repn.constant, 0)
 
 if __name__ == '__main__':
     unittest.main()
