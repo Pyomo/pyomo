6b8fd0b62b057faf8452e146ee160bcf8b989dd1
John Siirola <jsiirola@users.noreply.github.com>
2022-03-09 09:45:24 -0700

    Improve timing tests

--- pyomo/common/tests/test_timing.py
+++ pyomo/common/tests/test_timing.py
@@ -13,13 +13,17 @@ from pyomo.common.tee import capture_output
 
 import gc
 from io import StringIO
+import logging
 import sys
 import time
 
 from pyomo.common.log import LoggingIntercept
-from pyomo.common.timing import (ConstructionTimer, report_timing,
-                                 TicTocTimer, HierarchicalTimer)
+from pyomo.common.timing import (
+    ConstructionTimer, report_timing, TicTocTimer, HierarchicalTimer,
+    TransformationTimer,
+)
 from pyomo.environ import ConcreteModel, RangeSet, Var, TransformationFactory
+from pyomo.core.base.var import _VarData
 
 class TestTiming(unittest.TestCase):
     def setUp(self):
@@ -33,9 +37,24 @@ class TestTiming(unittest.TestCase):
 
     def test_raw_construction_timer(self):
         a = ConstructionTimer(None)
-        self.assertIn(
-            "ConstructionTimer object for NoneType (unknown); ",
-            str(a))
+        self.assertRegex(
+            str(a),
+            r"ConstructionTimer object for NoneType \(unknown\); "
+            r"[0-9\.]+ elapsed seconds")
+        v = Var()
+        v.construct()
+        a = ConstructionTimer(_VarData(v))
+        self.assertRegex(
+            str(a),
+            r"ConstructionTimer object for Var \(unknown\); "
+            r"[0-9\.]+ elapsed seconds")
+
+    def test_raw_transformation_timer(self):
+        a = TransformationTimer(None)
+        self.assertRegex(
+            str(a),
+            r"TransformationTimer object for NoneType; "
+            r"[0-9\.]+ elapsed seconds")
 
     def test_report_timing(self):
         # Create a set to ensure that the global sets have already been
@@ -198,6 +217,65 @@ class TestTiming(unittest.TestCase):
         abs_time = time.perf_counter() - abs_time
         self.assertGreater(abs_time, SLEEP*3 - RES/10)
         self.assertAlmostEqual(timer.toc(None), abs_time - exclude, delta=RES)
+
+    def test_TicTocTimer_logger(self):
+        # test specifying a logger to the timer constructor disables all
+        # output to stdout
+        timer = TicTocTimer(logger=logging.getLogger(__name__))
+        with capture_output() as OUT:
+            with LoggingIntercept(level=logging.INFO) as LOG:
+                timer.toc("msg1")
+            self.assertRegex(LOG.getvalue(), r"^\[[^\]]+\] msg1$")
+            with LoggingIntercept(level=logging.INFO) as LOG:
+                timer.toc("msg2", level=logging.DEBUG)
+            self.assertEqual(LOG.getvalue(), "")
+        self.assertEqual(OUT.getvalue(), "")
+
+        with capture_output() as OUT:
+            with LoggingIntercept(level=logging.DEBUG) as LOG:
+                timer.toc("msg1")
+                timer.toc("msg2", level=logging.DEBUG)
+        self.assertEqual(OUT.getvalue(), "")
+        self.assertRegex(LOG.getvalue(), r"^\[[^\]]+\] msg1\n\[[^\]]+\] msg2$")
+
+        # test specifying a logger to toc() for a general timer disables
+        # output to stdout
+        timer = TicTocTimer()
+        with capture_output() as OUT:
+            with LoggingIntercept(level=logging.INFO) as LOG:
+                timer.toc("msg1", logger=logging.getLogger(__name__))
+        self.assertRegex(LOG.getvalue(), r"^\[[^\]]+\] msg1$")
+        self.assertEqual(OUT.getvalue(), "")
+
+    def test_TicTocTimer_deprecated(self):
+        timer = TicTocTimer()
+        with LoggingIntercept() as LOG, capture_output() as out:
+            timer.tic("msg", None, None)
+        self.assertEqual(out.getvalue(), "")
+        self.assertRegex(
+            LOG.getvalue().replace('\n', ' ').strip(),
+            "DEPRECATED: 'ostream', and 'logger' should be specified "
+            r"as keyword arguments( +\([^\)]+\)){2}")
+
+        with LoggingIntercept() as LOG, capture_output() as out:
+            timer.toc("msg", True, None, None)
+        self.assertEqual(out.getvalue(), "")
+        self.assertRegex(
+            LOG.getvalue().replace('\n', ' ').strip(),
+            "DEPRECATED: 'delta', 'ostream', and 'logger' should be specified "
+            r"as keyword arguments( +\([^\)]+\)){2}")
+
+        timer = TicTocTimer()
+        with LoggingIntercept() as LOG, capture_output() as out:
+            timer.tic("msg %s, %s", None, None)
+        self.assertIn('msg None, None', out.getvalue())
+        self.assertEqual(LOG.getvalue(), "")
+
+        with LoggingIntercept() as LOG, capture_output() as out:
+            timer.toc("msg %s, %s, %s", True, None, None)
+        self.assertIn('msg True, None, None', out.getvalue())
+        self.assertEqual(LOG.getvalue(), "")
+
 
     def test_HierarchicalTimer(self):
         RES = 0.01 # resolution (seconds)
