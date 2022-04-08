103d8c8b281988a05970c6cea83ce6c0428ff85a
Emma Johnson <emma58@users.noreply.github.com>
2022-04-08 08:46:40 -0600

    Replacing _index_set with the public index_set() method

--- pyomo/core/plugins/transform/util.py
+++ pyomo/core/plugins/transform/util.py
@@ -176,9 +176,9 @@ def _getAbstractIndices(comp):
     """
     Returns the index or index set of this component
     """
-    if type(comp._index_set) != type({}):
+    if type(comp.index_set()) != type({}):
         # Singly indexed component
-        return comp._index_set
+        return comp.index_set()
     else:
         # Unindexed constraint
         return {None: None}
