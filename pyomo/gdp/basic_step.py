344fec6a8186e98b39e12f655c7f7a8ed20cd33f
Emma Johnson <emma58@users.noreply.github.com>
2022-04-13 14:47:46 -0600

    removing use of name_buffer in gdp

--- pyomo/gdp/basic_step.py
+++ pyomo/gdp/basic_step.py
@@ -104,7 +104,6 @@ def apply_basic_step(disjunctions_or_constraints):
     # Link the new disjunct indicator_var's to the original
     # indicator_var's.  Since only one of the new
     #
-    NAME_BUFFER = {}
     ans.indicator_links = ConstraintList()
     for i in ans.DISJUNCTIONS:
         for j in range(len(disjunctions[i].disjuncts)):
@@ -117,8 +116,7 @@ def apply_basic_step(disjunctions_or_constraints):
                      if (idx[i] if isinstance(idx, tuple) else idx) == j ))
             # and throw on a Reference to original on the block
             for v in (orig_var, orig_binary_var):
-                name_base = v.getname(
-                    fully_qualified=True, name_buffer=NAME_BUFFER)
+                name_base = v.getname(fully_qualified=True)
                 ans.add_component(unique_component_name( ans, name_base),
                                   Reference(v))
 
