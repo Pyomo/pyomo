ed89f460e954e3f3e5896490c12c79d043db5b35
Emma Johnson <emma58@users.noreply.github.com>
2022-04-15 21:24:27 -0600

    Adding a test for when constraints have so many variables LOA switches differentiation modes

--- pyomo/contrib/gdpopt/tests/test_gdpopt.py
+++ pyomo/contrib/gdpopt/tests/test_gdpopt.py
@@ -27,7 +27,7 @@ from pyomo.contrib.mcpp.pyomo_mcpp import mcpp_available
 from pyomo.core.expr.sympy_tools import sympy_available
 from pyomo.environ import ( ConcreteModel, Objective, SolverFactory, Var, value,
                             Integers, Block, Constraint, maximize,
-                            LogicalConstraint, sqrt)
+                            LogicalConstraint, sqrt, RangeSet)
 from pyomo.gdp import Disjunct, Disjunction
 from pyomo.gdp.tests import models
 from pyomo.opt import TerminationCondition
@@ -451,6 +451,36 @@ class TestGDPopt(unittest.TestCase):
         # and the solution is optimal
         self.assertAlmostEqual(value(m.x), 0)
         self.assertAlmostEqual(value(m.y), 1)
+
+    def test_reverse_numeric_differentiation_in_LOA(self):
+        # Having more than 1000 variables in the same constraint expression will
+        # trigger reverse numeric differentiation. We make sure that works with
+        # a disjunction between a pair of hyperspheres in 1300-dimensional
+        # space.
+        m = ConcreteModel()
+        m.s = RangeSet(1300)
+        m.x = Var(m.s, bounds=(-10, 10))
+        m.d1 = Disjunct()
+        m.d1.hypersphere = Constraint(expr=sum(m.x[i]**2 for i in m.s) <= 1)
+        m.d2 = Disjunct()
+        m.d2.translated_hyper_sphere = Constraint(expr=sum((m.x[i] - i)**2 for i
+                                                           in m.s) <= 1)
+        m.disjunction = Disjunction(expr=[m.d1, m.d2])
+        # we'll go in the sphere centered at (0,0,...,0)
+        m.obj = Objective(expr=sum(m.x[i] for i in m.s))
+
+        results = SolverFactory('gdpopt', algorithm='LOA').solve(
+            m, mip_solver=mip_solver, nlp_solver=nlp_solver)
+
+        self.assertEqual(results.solver.termination_condition,
+                         TerminationCondition.optimal)
+        self.assertTrue(value(m.d1.indicator_var))
+        self.assertFalse(value(m.d2.indicator_var))
+        x_val = -sqrt(1300)/1300
+        for x in m.x.values():
+            self.assertAlmostEqual(value(x), x_val)
+        self.assertAlmostEqual(results.problem.upper_bound, 1300*x_val,
+                               places=6)
 
     @unittest.skipUnless(sympy_available, "Sympy not available")
     def test_logical_constraints_on_disjuncts(self):
