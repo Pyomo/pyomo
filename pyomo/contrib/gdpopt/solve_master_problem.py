7ed212a8e72e32a3efc77c63c90c700efa08bd58
Emma Johnson <emma58@users.noreply.github.com>
2022-07-15 11:57:22 -0600

    Just emmitting the GAMS error, if there is one, changing to more precise note on infeasible master problems, to account for the fact that the cuts may have been invalid.

--- pyomo/contrib/gdpopt/solve_master_problem.py
+++ pyomo/contrib/gdpopt/solve_master_problem.py
@@ -58,29 +58,17 @@ def solve_MILP_master_problem(util_block, solver, config):
     # Callback immediately before solving MIP master problem
     config.call_before_master_solve(solver, m, util_block)
 
-    try:
-        with SuppressInfeasibleWarning():
-            mip_args = dict(config.mip_solver_args)
-            elapsed = get_main_elapsed_time(timing)
-            remaining = max(config.time_limit - elapsed, 1)
-            if config.mip_solver == 'gams':
-                mip_args['add_options'] = mip_args.get('add_options', [])
-                mip_args['add_options'].append('option reslim=%s;' % remaining)
-            elif config.mip_solver == 'multisolve':
-                mip_args['time_limit'] = min(mip_args.get('time_limit',
-                                                          float('inf')),
-                                             remaining)
-            results = SolverFactory(config.mip_solver).solve(m, **mip_args)
-
-    except RuntimeError as e:
-        # ESJ: I don't know enough about GAMS to know if this is a good
-        # idea. Does this always mean it's infeasible?
-        if 'GAMS encountered an error during solve.' in str(e):
-            config.logger.warning(
-                "GAMS encountered an error in solve. Treating as infeasible.")
-            return tc.infeasible
-        else:
-            raise
+    with SuppressInfeasibleWarning():
+        mip_args = dict(config.mip_solver_args)
+        elapsed = get_main_elapsed_time(timing)
+        remaining = max(config.time_limit - elapsed, 1)
+        if config.mip_solver == 'gams':
+            mip_args['add_options'] = mip_args.get('add_options', [])
+            mip_args['add_options'].append('option reslim=%s;' % remaining)
+        elif config.mip_solver == 'multisolve':
+            mip_args['time_limit'] = min(mip_args.get('time_limit',
+                                                      float('inf')), remaining)
+        results = SolverFactory(config.mip_solver).solve(m, **mip_args)
 
     config.call_after_master_solve(solver, m, util_block)
 
@@ -131,8 +119,8 @@ def solve_MILP_master_problem(util_block, solver, config):
         return tc.feasible
     elif terminate_cond is tc.infeasible:
         config.logger.info(
-            'MILP master problem is now infeasible. '
-            'GDPopt has finished exploring feasible discrete configurations.')
+            'MILP master problem is now infeasible. GDPopt has explored or '
+            'cut off all feasible discrete configurations.')
         return tc.infeasible
     elif terminate_cond is tc.maxTimeLimit:
         if len(results.solution) > 0:
