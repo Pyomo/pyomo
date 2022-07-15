8b7dd79e3bf7e443043009f4e609f103e71288cf
jasherma <jasherma@andrew.cmu.edu>
2022-07-15 17:43:35 -0400

    Refine uncertainty set module imports

--- pyomo/contrib/pyros/uncertainty_sets.py
+++ pyomo/contrib/pyros/uncertainty_sets.py
@@ -46,14 +46,14 @@ Classes
 
 
 import abc
-import functools
 import math
+import functools
 from enum import Enum
+
 from pyomo.common.dependencies import numpy as np, scipy as sp
 from pyomo.core.base import ConcreteModel, Objective, maximize, minimize, Block
 from pyomo.core.base.constraint import ConstraintList
-from pyomo.core.base.var import Var, _VarData, IndexedVar
-from pyomo.core.base.param import Param, _ParamData, IndexedParam
+from pyomo.core.base.var import Var, IndexedVar
 from pyomo.core.expr.numvalue import value, native_numeric_types
 from pyomo.opt.results import check_optimal_termination
 from pyomo.contrib.pyros.util import add_bounds_for_uncertain_parameters
