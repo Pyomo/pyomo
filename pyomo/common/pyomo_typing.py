dc3237b001d135f5504686921d92edf5e14f77b9
John Siirola <jsiirola@users.noreply.github.com>
2022-07-26 12:12:00 -0600

    NFC: fix typo

--- pyomo/common/pyomo_typing.py
+++ pyomo/common/pyomo_typing.py
@@ -20,7 +20,7 @@ def overload(func: typing.Callable):
     """Wrap typing.overload that remembers the overloaded signatures
 
     This provides a custom implementation of typing.overload that
-    remembers the overloaded signitures so that they are available for
+    remembers the overloaded signatures so that they are available for
     runtime inspection.
 
     """
