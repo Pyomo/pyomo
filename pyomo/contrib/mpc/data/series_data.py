ba83345868a397f2290f2af2bbe87b855501a129
Robert Parker <robbybparker@gmail.com>
2022-07-28 20:20:25 -0400

    inherit from _DynamicDataBase

--- pyomo/contrib/mpc/data/series_data.py
+++ pyomo/contrib/mpc/data/series_data.py
@@ -17,16 +17,18 @@ from pyomo.contrib.mpc.data.find_nearest_index import (
 from pyomo.contrib.mpc.data.get_cuid import (
     get_time_indexed_cuid,
 )
+from pyomo.contrib.mpc.data.dynamic_data_base import _DynamicDataBase
 from pyomo.contrib.mpc.data.scalar_data import ScalarData
 
 
 TimeSeriesTuple = namedtuple("TimeSeriesTuple", ["data", "time"])
 
 
-class TimeSeriesData(object):
+class TimeSeriesData(_DynamicDataBase):
     """
     An object to store time series data associated with time-indexed
     variables.
+
     """
 
     def __init__(self, data, time, time_set=None, context=None):
@@ -37,55 +39,33 @@ class TimeSeriesData(object):
             Maps variables, names, or CUIDs to lists of values
         time: list
             Contains the time points corresponding to variable data points.
+
         """
-        # This is used if we ever need to process a VarData to get
-        # a time-indexed CUID. We need to know what set to slice.
-        self._orig_time_set = time_set
         self._time = list(time)
+
         # When looking up a value at a particular time point, we will use
-        # this map to try and the index of the time point. If this lookup
-        # fails, we will use binary search-within-tolerance to attempt to
-        # find a point that is close enough.
+        # this map to try and find the index of the time point. If this lookup
+        # fails, we will use binary search-within-tolerance to attempt to find
+        # a point that is close enough.
         self._time_idx_map = {t: idx for idx, t in enumerate(time)}
 
-        if time is not None:
-            # First make sure provided lists of variable data have the
-            # same lengths as the provided time list.
-            for key, data_list in data.items():
-                # What if time is a number? Do I ever want to support that
-                # here?
-                if len(data_list) != len(time):
-                    raise ValueError(
-                        "Data lists must have same length as time. "
-                        "Length of time is %s while length of data for "
-                        "key %s is %s."
-                        % (len(time), key, len(data_list))
-                    )
-
-        # Process keys of the provided data object to get CUIDs
-        self._data = {
-            get_time_indexed_cuid(key, (self._orig_time_set,)): values
-            for key, values in data.items()
-        }
+        # First make sure provided lists of variable data have the
+        # same lengths as the provided time list.
+        for key, data_list in data.items():
+            if len(data_list) != len(time):
+                raise ValueError(
+                    "Data lists must have same length as time. "
+                    "Length of time is %s while length of data for "
+                    "key %s is %s."
+                    % (len(time), key, len(data_list))
+                )
+        super().__init__(data, time_set=time_set, context=context)
 
     def get_time_points(self):
         """
         Get time points of the time series data
         """
         return self._time
-
-    def get_data(self):
-        """
-        Return a dictionary mapping CUIDs to values
-        """
-        return self._data
-
-    def get_data_from_key(self, key):
-        """
-        Returns the list of values corresponding to the provided key.
-        """
-        cuid = get_time_indexed_cuid(key, (self._orig_time_set,))
-        return self._data[cuid]
 
     def get_data_at_time_indices(self, indices):
         """
@@ -107,9 +87,6 @@ class TimeSeriesData(object):
             return ScalarData({
                 cuid: values[indices] for cuid, values in self._data.items()
             })
-
-    # TODO: Should there be a get_scalar_data_at_time method that replaces
-    # the functionality of get_data_at_time with a scalar time point?
 
     def get_data_at_time(self, time=None, tolerance=None):
         """
@@ -149,8 +126,11 @@ class TimeSeriesData(object):
         except TypeError:
             # TODO: Probably shouldn't rely on TypeError here.
             # time is a scalar
-            indices = self._time_idx_map[time] if time in self._time_idx_map \
+            indices = (
+                self._time_idx_map[time]
+                if time in self._time_idx_map
                 else find_nearest_index(self._time, time, tolerance=tolerance)
+            )
         return self.get_data_at_time_indices(indices)
 
     def to_serializable(self):
@@ -186,34 +166,18 @@ class TimeSeriesData(object):
         # shift_values_by_time in the helper class.
         self._time = [t + offset for t in self._time]
 
-    #
-    # Unused
-    #
-    #def get_projection_onto_variables(self, variables):
-    #    new = self.copy()
-    #    new.project_onto_variables(variables)
-    #    return new
-
-    def extract_variables(self, variables, context=None):
-        """
-        Only keep variables specified by the user.
-        """
-        data = {}
-        for var in variables:        
-            cuid = get_time_indexed_cuid(
-                var, (self._orig_time_set,), context=context
-            )
-            data[cuid] = self._data[cuid]
-        return TimeSeriesData(
-            data,
-            self._time,
-            time_set=self._orig_time_set,
-        )
-
-    #
-    # Unused
-    #
-    #def copy(self):
-    #    data = {key: list(values) for key, values in self._data.items()}
-    #    time = list(self._time)
-    #    return TimeSeriesData(data, time)
+    #def extract_variables(self, variables, context=None):
+    #    """
+    #    Only keep variables specified by the user.
+    #    """
+    #    data = {}
+    #    for var in variables:        
+    #        cuid = get_time_indexed_cuid(
+    #            var, (self._orig_time_set,), context=context
+    #        )
+    #        data[cuid] = self._data[cuid]
+    #    return TimeSeriesData(
+    #        data,
+    #        self._time,
+    #        time_set=self._orig_time_set,
+    #    )
