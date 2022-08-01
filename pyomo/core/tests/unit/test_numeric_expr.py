6c191eedb4042eb6763c19fa4e39e40f0b0033a2
Shouyuan <shishouyuan@outlook.com>
2022-08-01 15:50:11 +0800

    improve testing for `quicksum`

--- pyomo/core/tests/unit/test_numeric_expr.py
+++ pyomo/core/tests/unit/test_numeric_expr.py
@@ -3679,43 +3679,30 @@ class TestSumExpression(unittest.TestCase):
         self.assertIs(type(e), SumExpression)
         
     def test_quicksum_reject_noniterable(self):
-        msg = 'Failed to recognize non-iterable args.'
-        try:
-            e = quicksum(1)
-            self.fail(msg)
-        except Exception as ex:
-            self.assertIsInstance(ex, TypeError, msg)
+        with LoggingIntercept() as LOG:
+            with self.assertRaisesRegex(TypeError, "'int' object is not iterable"):
+                quicksum(1)
+        self.assertEqual(LOG.getvalue(), 'The argument `args` to quicksum() is not iterable!\n')
 
     def test_quicksum_exception_exposure(self):
-        msg = 'Exception raised in the iterator was hidden.'
         ex0 = Exception()
         def f(): raise ex0
-        
-        try:
-            e = quicksum((f() for i in [1, 2, 3]), linear=None)
-            self.fail(msg)
-        except Exception as ex:
-            self.assertIs(ex, ex0, msg)
 
-        try:
-            e = quicksum((f() for i in [1, 2, 3]), linear=True)
-            self.fail(msg)
-        except Exception as ex:
-            self.assertIs(ex, ex0, msg)
-        
-        try:
-            e = quicksum((f() for i in [1, 2, 3]), linear=False)
-            self.fail(msg)
-        except Exception as ex:
-            self.assertIs(ex, ex0, msg)
+        with self.assertRaises(Exception) as cm:
+            quicksum((f() for i in [1, 2, 3]), linear=None)
+        self.assertIs(cm.exception, ex0)
 
-        try:
-            e = quicksum((f() for i in [1, 2, 3]), start=self.m.a[1])
-            self.fail(msg)
-        except Exception as ex:
-            self.assertIs(ex, ex0, msg)
+        with self.assertRaises(Exception) as cm:
+            quicksum((f() for i in [1, 2, 3]), linear=True)
+        self.assertIs(cm.exception, ex0)
 
+        with self.assertRaises(Exception) as cm:
+            quicksum((f() for i in [1, 2, 3]), linear=False)
+        self.assertIs(cm.exception, ex0)
 
+        with self.assertRaises(Exception) as cm:
+            quicksum((f() for i in [1, 2, 3]), start=self.m.a[1])
+        self.assertIs(cm.exception, ex0)
 
 
 class TestCloneExpression(unittest.TestCase):
