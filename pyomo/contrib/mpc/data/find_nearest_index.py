b4af2535e0c9379df1fdc688bf8ca768868e848b
Robert Parker <robbybparker@gmail.com>
2022-08-03 19:33:34 -0400

    add tolerance to find_nearest_interval_index

--- pyomo/contrib/mpc/data/find_nearest_index.py
+++ pyomo/contrib/mpc/data/find_nearest_index.py
@@ -43,40 +43,90 @@ def find_nearest_index(array, target, tolerance=None):
     return nearest_index
 
 
-def _distance_from_interval(point, interval):
+def _distance_from_interval(point, interval, tolerance=None):
     lo, hi = interval
-    if point < lo:
+    if tolerance is None:
+        tolerance = 0.0
+    if point < lo - tolerance:
         return lo - point
-    elif lo <= point and point <= hi:
+    elif lo - tolerance <= point and point <= hi + tolerance:
         return 0.0
-    elif point > hi:
+    elif point > hi + tolerance:
         return point - hi
 
 
-def find_nearest_interval_index(interval_array, target, tolerance=None):
+def find_nearest_interval_index(
+    interval_array, target, tolerance=None, prefer_left=True,
+):
+    # NOTE: This function quickly begins to behave badly if tolerance
+    # gets too large, e.g. greater than the width of the smallest
+    # interval. For this reason, intervals that represent a single
+    # point, e.g. (1.0, 1.0) should not be supported.
     array_lo = 0
     array_hi = len(interval_array)
     target_tuple = (target,)
     i = bisect.bisect_right(
         interval_array, target_tuple, lo=array_lo, hi=array_hi
     )
-    # Possible cases:
-    # - target is less than everything
-    # - target is within some interval (will appear to left of that interval)
-    # - target is between two intervals
-    # - target is greater than everything
+    distance_tol = 0.0 if tolerance is None else tolerance
     if i == array_lo:
+        # We are at or to the left of the left endpoint of the
+        # first interval.
         nearest_index = i
-        delta = _distance_from_interval(target, interval_array[i])
-    elif i == array_hi:
-        nearest_index = i
-        delta = _distance_from_interval(target, interval_array[i-1])
-    else:
-        delta, nearest_index = min(
-            (_distance_from_interval(target, interval_array[j]), j)
-            for j in [i-1, i]
-            #(abs(target - array[j]), j) for j in [i-1, i]
+        delta = _distance_from_interval(
+            target, interval_array[i], tolerance=distance_tol
         )
+    elif i == array_hi:
+        # We are within or to the right of the last interval.
+        nearest_index = i - 1
+        delta = _distance_from_interval(
+            target, interval_array[i-1], tolerance=distance_tol
+        )
+    else:
+        # Find closest interval
+        if prefer_left:
+            # In the case of a tie, we return the left interval
+            # by default.
+            delta, nearest_index = min(
+                (
+                    _distance_from_interval(
+                        target, interval_array[j], tolerance=distance_tol
+                    ),
+                    j,
+                )
+                for j in [i-1, i]
+            )
+        else:
+            # If prefer_left=False, we return the right interval.
+            delta, neg_nearest_index = min(
+                (
+                    _distance_from_interval(
+                        target, interval_array[j], tolerance=distance_tol
+                    ),
+                    -j,
+                )
+                for j in [i-1, i]
+            )
+            nearest_index = -neg_nearest_index
+
+    # If we have two adjacent intervals, e.g. [(0, 1), (1, 2)], and are just
+    # to the right of the boundary, we will not check the left interval as
+    # bisect places our tuple, e.g. (1.0+1e-10,), to the right of the right
+    # interval.
+    if prefer_left and nearest_index >= array_lo + 1:
+        delta_left = _distance_from_interval(
+            target, interval_array[nearest_index-1], tolerance=distance_tol
+        )
+        if delta_left <= delta:
+            nearest_index = nearest_index - 1
+            delta = delta_left
+    elif not prefer_left and nearest_index < array_hi - 1:
+        delta_right = _distance_from_interval(
+            target, interval_array[nearest_index+1], tolerance=distance_tol
+        )
+        if delta_right <= delta:
+            nearest_index = nearest_index + 1
+            delta = delta_right
 
     if tolerance is not None:
         if delta > tolerance:
--- pyomo/contrib/mpc/data/tests/test_find_nearest_index.py
+++ pyomo/contrib/mpc/data/tests/test_find_nearest_index.py
@@ -91,14 +91,165 @@ class TestFindNearestIntervalIndex(unittest.TestCase):
         idx = find_nearest_interval_index(intervals, target)
         self.assertEqual(idx, 0)
 
+        target = 0.099
+        idx = find_nearest_interval_index(intervals, target)
+        self.assertEqual(idx, 0)
+
         target = 0.1
         idx = find_nearest_interval_index(intervals, target)
-        # TODO: Need some logic to break ties in this case.
         self.assertEqual(idx, 0)
+
+        target = 0.1
+        idx = find_nearest_interval_index(intervals, target, prefer_left=False)
+        self.assertEqual(idx, 1)
 
         target = 0.55
         idx = find_nearest_interval_index(intervals, target)
         self.assertEqual(idx, 1)
+
+        target = 0.60
+        idx = find_nearest_interval_index(intervals, target)
+        self.assertEqual(idx, 1)
+
+        target = 0.6999
+        idx = find_nearest_interval_index(intervals, target)
+        self.assertEqual(idx, 2)
+
+        target = 1.0
+        idx = find_nearest_interval_index(intervals, target)
+        self.assertEqual(idx, 2)
+
+        target = -0.1
+        idx = find_nearest_interval_index(intervals, target)
+        self.assertEqual(idx, 0)
+
+        target = 1.1
+        idx = find_nearest_interval_index(intervals, target)
+        self.assertEqual(idx, 2)
+
+    def test_find_interval_tolerance(self):
+        intervals = [(0.0, 0.1), (0.1, 0.5), (0.7, 1.0)]
+
+        target = 0.501
+        idx = find_nearest_interval_index(intervals, target, tolerance=None)
+        self.assertEqual(idx, 1)
+
+        idx = find_nearest_interval_index(intervals, target, tolerance=1e-5)
+        self.assertEqual(idx, None)
+
+        idx = find_nearest_interval_index(intervals, target, tolerance=1e-2)
+        self.assertEqual(idx, 1)
+
+        target = 1.001
+        idx = find_nearest_interval_index(intervals, target, tolerance=1e-2)
+        self.assertEqual(idx, 2)
+
+        #
+        # Behavior when distance between target and nearest interval "equals"
+        # the tolerance is not well-defined. Here the computed distance may
+        # not be exactly 1e-3 due to roundoff error.
+        #
+        #idx = find_nearest_interval_index(intervals, target, tolerance=1e-3)
+        #self.assertEqual(idx, None)
+
+        idx = find_nearest_interval_index(intervals, target, tolerance=1e-4)
+        self.assertEqual(idx, None)
+
+    def test_find_interval_with_tolerance_on_boundary(self):
+        # Our target is on the boundary between two intervals.
+        intervals = [(0.0, 0.1), (0.1, 0.5), (0.5, 1.0)]
+        target = 0.1001
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=None, prefer_left=True
+        )
+        self.assertEqual(idx, 1)
+
+        # target != 0.1 (the interval boundary) within tolerance. We are
+        # within interval 1.
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-5, prefer_left=True
+        )
+        self.assertEqual(idx, 1)
+        # This is true even if we prefer the right interval
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-5, prefer_left=False
+        )
+        self.assertEqual(idx, 1)
+
+        # target == 0.1 within tolerance. We are on the boundary, and
+        # should return the "preferred" interval.
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=True
+        )
+        self.assertEqual(idx, 0)
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=False
+        )
+        self.assertEqual(idx, 1)
+
+        target = 0.4999
+        # We are not equal to boundary (0.5) within tolerance
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-5, prefer_left=True
+        )
+        self.assertEqual(idx, 1)
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-5, prefer_left=False
+        )
+        self.assertEqual(idx, 1)
+
+        # We are equal to boundary within tolerance
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=True
+        )
+        self.assertEqual(idx, 1)
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=False
+        )
+        self.assertEqual(idx, 2)
+
+    def test_find_interval_with_tolerance_singleton(self):
+        intervals = [(0.0, 0.1), (0.1, 0.1), (0.5, 0.5), (0.5, 1.0)]
+
+        target = 0.1001
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=True
+        )
+        self.assertEqual(idx, 0)
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=False
+        )
+        self.assertEqual(idx, 1)
+
+        target = 0.0999
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=True
+        )
+        self.assertEqual(idx, 0)
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=False
+        )
+        self.assertEqual(idx, 1)
+
+        target = 0.4999
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=True
+        )
+        self.assertEqual(idx, 2)
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=False
+        )
+        self.assertEqual(idx, 3)
+
+        target = 0.5001
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=True
+        )
+        self.assertEqual(idx, 2)
+        idx = find_nearest_interval_index(
+            intervals, target, tolerance=1e-3, prefer_left=False
+        )
+        self.assertEqual(idx, 3)
 
 
 if __name__ == "__main__":
