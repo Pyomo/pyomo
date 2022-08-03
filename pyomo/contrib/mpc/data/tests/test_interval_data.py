d92d692eb4d66517991a50a45d16410dd4224673
Robert Parker <robbybparker@gmail.com>
2022-08-03 19:34:12 -0400

    first test for IntervalData.get_data_at_time

--- pyomo/contrib/mpc/data/tests/test_interval_data.py
+++ pyomo/contrib/mpc/data/tests/test_interval_data.py
@@ -27,6 +27,7 @@ import pytest
 
 import pyomo.environ as pyo
 import pyomo.dae as dae
+from pyomo.contrib.mpc.data.scalar_data import ScalarData
 from pyomo.contrib.mpc.data.interval_data import (
     assert_disjoint_intervals,
     load_inputs_into_model,
@@ -110,8 +111,40 @@ class TestIntervalData(unittest.TestCase):
         interval_data = IntervalData(data, intervals)
         data = interval_data.get_data_at_interval_indices(1)
         pred_data = ScalarData({m.var[:, "A"]: 2.0, m.var[:, "B"]: 5.0})
+        self.assertEqual(data, pred_data)
 
-    def test_get_data_at_time(self):
+    def test_get_data_at_time_scalar(self):
+        m = self._make_model()
+        intervals = [(0.0, 0.2), (0.2, 0.5), (0.5, 1.0)]
+        data = {
+            m.var[:, "A"]: [1.0, 2.0, 3.0],
+            m.var[:, "B"]: [4.0, 5.0, 6.0],
+        }
+        interval_data = IntervalData(data, intervals)
+
+        data = interval_data.get_data_at_time(0.1)
+        pred_data = ScalarData({m.var[:, "A"]: 1.0, m.var[:, "B"]: 4.0})
+        self.assertEqual(data, pred_data)
+
+        # Default is to allow time points outside of intervals
+        # (finds the nearest interval)
+        data = interval_data.get_data_at_time(1.1)
+        pred_data = ScalarData({m.var[:, "A"]: 3.0, m.var[:, "B"]: 6.0})
+        self.assertEqual(data, pred_data)
+
+        msg = "Time point.*not found"
+        with self.assertRaisesRegex(RuntimeError, msg):
+            data = interval_data.get_data_at_time(1.1, tolerance=1e-3)
+
+        # If a point on an interval boundary is supplied, default is to
+        # use value on left.
+        data = interval_data.get_data_at_time(0.5)
+        pred_data = ScalarData({m.var[:, "A"]: 2.0, m.var[:, "B"]: 5.0})
+        self.assertEqual(data, pred_data)
+
+        data = interval_data.get_data_at_time(0.5, prefer_left=False)
+        pred_data = ScalarData({m.var[:, "A"]: 3.0, m.var[:, "B"]: 6.0})
+        self.assertEqual(data, pred_data)
 
 
 class TestAssertDisjoint(unittest.TestCase):
