b4068753518d9f39652ec7a83597137eef09e653
Emma Johnson <emma58@users.noreply.github.com>
2022-08-18 16:11:32 -0600

    Adding IntervalVar component and some tests

---
+++ pyomo/core/base/interval_var.py
@@ -0,0 +1,168 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2022
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+import logging
+
+from pyomo.common.collections import Sequence
+from pyomo.common.log import is_debug_set
+from pyomo.common.modeling import NOTSET
+from pyomo.common.timing import ConstructionTimer
+
+from pyomo.core.base.block import _BlockData, Block
+from pyomo.core.base.component import ModelComponentFactory
+from pyomo.core.base.global_set import UnindexedComponent_index
+from pyomo.core.base.initializer import BoundInitializer
+from pyomo.core.base import Var, BooleanVar
+from pyomo.core import Integers
+
+from pyomo.core.base.indexed_component import (
+    IndexedComponent, UnindexedComponent_set)
+
+import sys
+
+logger = logging.getLogger('pyomo.core')
+
+class _IntervalVarData(_BlockData):
+    """This class defines the abstract interface for a single interval variable.
+    """
+    def __init__(self, component=None):
+        _BlockData.__init__(self, component)
+
+        self.is_present = BooleanVar()
+        self.start_time = Var(domain=Integers)
+        self.end_time = Var(domain=Integers)
+        self.length = Var(domain=Integers)
+
+    @property
+    def optional(self):
+        # We only store this information in one place, but it's kind of annoying
+        # to have to check if the BooleanVar is fixed, so this way you can ask
+        # the IntervalVar directly.
+        return not self.is_present.fixed
+
+@ModelComponentFactory.register("Interval variables for scheduling.")
+class IntervalVar(Block):
+    """And interval variable, which may be defined over an index.
+
+    Args:
+        start (tuple of two integers): Feasible range for the 
+            interval variable's start time
+        end (tuple of two integers, optional): Feasible range for the
+            interval variables end time
+        length (integer or tuple of two integers, optional): Feasible
+            range for the length of the interval variable
+        optional (boolean, optional) : If False, the interval variable must
+            be scheduled. Otherwise the interval variable is optionally 
+            present. Default behavior is 'False'
+        name (str, optional): Name for this component.
+        doc (str, optional): Text describing this component.
+    """
+
+    _ComponentDataClass = _IntervalVarData
+
+    def __new__(cls, *args, **kwds):
+        if cls != IntervalVar:
+            return super(IntervalVar, cls).__new__(cls)
+        if args == ():
+            return ScalarIntervalVar.__new__(ScalarIntervalVar)
+        else:
+            return IndexedIntervalVar.__new__(IndexedIntervalVar)
+
+    def __init__(self, *args, **kwargs):
+        _start_arg = kwargs.pop('start', None)
+        _end_arg = kwargs.pop('end', None)
+        _length_arg = kwargs.pop('length', None)
+        self._optional = kwargs.pop('optional', False)
+
+        kwargs.setdefault('ctype', IntervalVar)
+        Block.__init__(self, *args, **kwargs)
+
+        self._start_bounds = BoundInitializer(self, _start_arg)
+        self._end_bounds = BoundInitializer(self, _end_arg)
+        self._length_bounds = BoundInitializer(self, _length_arg)
+
+    def _getitem_when_not_present(self, index):
+        if index is None and not self.is_indexed():
+            obj = self._data[index] = self
+        else:
+            obj = self._data[index] = self._ComponentDataClass(component=self)
+        parent = obj.parent_block()
+        obj._index = index
+        if not self._optional:
+            obj.is_present.fix(True)
+
+        if self._start_bounds is not None:
+            start_bounds = self._start_bounds(parent, index)
+            if not isinstance(start_bounds, Sequence):
+                start_bounds = (start_bounds, start_bounds)
+            obj.start_time.lower, obj.start_time.upper = start_bounds
+        if self._end_bounds is not None:
+            end_bounds = self._end_bounds(parent, index)
+            if not isinstance(end_bounds, Sequence):
+                end_bounds = (end_bounds, end_bounds)
+            obj.end_time.lower, obj.end_time.upper = end_bounds
+        if self._length_bounds is not None:
+            length_bounds = self._length_bounds(parent, index)
+            if not isinstance(length_bounds, Sequence):
+                length_bounds = (length_bounds, length_bounds)
+            obj.length.lower, obj.length.upper = length_bounds
+
+        return obj
+
+    # def construct(self, data=None):
+    #     """
+    #     Construct the _IntervalVarDatas for this IntervalVar
+    #     """
+    #     if self._constructed:
+    #         return
+    #     self._constructed=True
+
+    #     timer = ConstructionTimer(self)
+    #     if is_debug_set(logger):
+    #         logger.debug("Constructing IntervalVar %s" % (self.name))
+
+    #     start_rule = self._start_bounds
+    #     end_rule = self._end_bounds
+    #     length_rule = self._length_bounds
+    #     try:
+    #         index = None
+    #         assert data is None
+
+    #         if not self.is_indexed():
+    #             self._getitem_when_not_present(index)
+    #         else:
+    #             for idx in self.index_set():
+    #                 if idx not in self._data:
+    #                     self._getitem_when_not_present(idx)
+    #     except Exception:
+    #         err = sys.exc_info()[1]
+    #         logger.error(
+    #             "Rule failed when initializing IntervalVar "
+    #             "%s with index %s:\n%s: %s"
+    #             % (self.name,
+    #                str(index),
+    #                type(err).__name__,
+    #                err))
+    #         raise
+    #     finally:
+    #         timer.report()
+
+class ScalarIntervalVar(_IntervalVarData, IntervalVar):
+    def __init__(self, *args, **kwds):
+        self._suppress_ctypes = set()
+
+        _IntervalVarData.__init__(self, self)
+        IntervalVar.__init__(self, *args, **kwds)
+        self._data[None] = self
+        self._index = UnindexedComponent_index
+
+class IndexedIntervalVar(IntervalVar):
+    pass
---
+++ pyomo/core/tests/unit/test_interval_var.py
@@ -0,0 +1,131 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2022
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+
+import pyomo.common.unittest as unittest
+from pyomo.core.base.interval_var import IntervalVar
+from pyomo.environ import (
+    ConcreteModel, BooleanVar, Integers, Var, value)
+
+class TestScalarIntervalVar(unittest.TestCase):
+    def test_initialize_with_no_data(self):
+        m = ConcreteModel()
+        m.i = IntervalVar()
+
+        self.assertIsInstance(m.i.start_time, Var)
+        self.assertEqual(m.i.start_time.domain, Integers)
+        self.assertIsNone(m.i.start_time.lower)
+        self.assertIsNone(m.i.start_time.upper)
+
+        self.assertIsInstance(m.i.end_time, Var)
+        self.assertEqual(m.i.end_time.domain, Integers)
+        self.assertIsNone(m.i.end_time.lower)
+        self.assertIsNone(m.i.end_time.upper)
+
+        self.assertIsInstance(m.i.length, Var)
+        self.assertEqual(m.i.length.domain, Integers)
+        self.assertIsNone(m.i.length.lower)
+        self.assertIsNone(m.i.length.upper)
+
+        self.assertIsInstance(m.i.is_present, BooleanVar)
+
+    def test_start_and_end_bounds(self):
+        m = ConcreteModel()
+        m.i = IntervalVar(start=(0,5))
+
+        self.assertEqual(m.i.start_time.lower, 0)
+        self.assertEqual(m.i.start_time.upper, 5)
+
+        m.i.end_time.bounds = (12, 14)
+
+        self.assertEqual(m.i.end_time.lower, 12)
+        self.assertEqual(m.i.end_time.upper, 14)
+
+    def test_constant_length_and_start(self):
+        m = ConcreteModel()
+        m.i = IntervalVar(length=7, start=3)
+
+        self.assertEqual(m.i.length.lower, 7)
+        self.assertEqual(m.i.length.upper, 7)
+
+        self.assertEqual(m.i.start_time.lower, 3)
+        self.assertEqual(m.i.start_time.upper, 3)
+
+    def test_non_optional(self):
+        m = ConcreteModel()
+        m.i = IntervalVar(length=2, end=(4,9), optional=False)
+
+        self.assertEqual(value(m.i.is_present), True)
+        self.assertTrue(m.i.is_present.fixed)
+        self.assertFalse(m.i.optional)
+
+        # Should also be true by default
+        m.i2 = IntervalVar()
+        
+        self.assertEqual(value(m.i2.is_present), True)
+        self.assertTrue(m.i.is_present.fixed)
+        self.assertFalse(m.i2.optional)
+
+    def test_optional(self):
+        m = ConcreteModel()
+        m.i = IntervalVar(optional=True)
+
+        self.assertFalse(m.i.is_present.fixed)
+        self.assertTrue(m.i.optional)
+
+class TestIndexedIntervalVar(unittest.TestCase):
+    def test_initialize_with_no_data(self):
+        m = ConcreteModel()
+
+        m.i = IntervalVar([1, 2])
+
+        for j in [1, 2]:
+            self.assertIsInstance(m.i[j].start_time, Var)
+            self.assertEqual(m.i[j].start_time.domain, Integers)
+            self.assertIsNone(m.i[j].start_time.lower)
+            self.assertIsNone(m.i[j].start_time.upper)
+
+            self.assertIsInstance(m.i[j].end_time, Var)
+            self.assertEqual(m.i[j].end_time.domain, Integers)
+            self.assertIsNone(m.i[j].end_time.lower)
+            self.assertIsNone(m.i[j].end_time.upper)
+
+            self.assertIsInstance(m.i[j].length, Var)
+            self.assertEqual(m.i[j].length.domain, Integers)
+            self.assertIsNone(m.i[j].length.lower)
+            self.assertIsNone(m.i[j].length.upper)
+
+            self.assertIsInstance(m.i[j].is_present, BooleanVar)
+        
+    def test_constant_length(self):
+        m = ConcreteModel()
+        m.i = IntervalVar(['a', 'b'], length=45)
+        
+        for j in ['a', 'b']:
+            self.assertEqual(m.i[j].length.lower, 45)
+            self.assertEqual(m.i[j].length.upper, 45)
+
+    def test_rule_based_start(self):
+        m = ConcreteModel()
+        def start_rule(m, i):
+            return (1 - i, 13 + i)
+        m.act = IntervalVar([1, 2, 3], start=start_rule, length=4)
+
+        for i in [1, 2, 3]:
+            self.assertEqual(m.act[i].start_time.lower, 1 - i)
+            self.assertEqual(m.act[i].start_time.upper, 13 + i)
+
+            self.assertEqual(m.act[i].length.lower, 4)
+            self.assertEqual(m.act[i].length.upper, 4)
+
+            self.assertFalse(m.act[i].optional)
+            self.assertTrue(m.act[i].is_present.fixed)
+            self.assertEqual(value(m.act[i].is_present), True)
