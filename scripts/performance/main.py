310fb5f27ff392c9677d7c80669f2f78160c59f1
John Siirola <jsiirola@users.noreply.github.com>
2022-08-22 09:38:57 -0600

    Update the performance driver to support specifying "main project"

--- scripts/performance/main.py
+++ scripts/performance/main.py
@@ -11,6 +11,7 @@
 #  ___________________________________________________________________________
 
 import argparse
+import importlib
 import gc
 import logging
 import os
@@ -25,8 +26,6 @@ except ImportError:
 from collections import OrderedDict
 
 import pyomo.common.unittest as unittest
-import pyomo
-from pyomo.version import version_info as pyomo_version
 from pyomo.common.timing import TicTocTimer
 
 
@@ -98,24 +97,31 @@ class DataRecorder(object):
         self._timingHandler.clearTest()
 
 
-def getPyomoInfo():
+def getProjectInfo(project):
     cwd = os.getcwd()
     sha = None
     diffs = []
     branch = None
+    version = None
     try:
-        os.chdir(os.path.dirname(pyomo.__file__))
+        _module = importlib.import_module(project)
+        os.chdir(os.path.dirname(_module.__file__))
         sha = os.popen('git rev-parse HEAD').read().strip()
         diffs = os.popen('git diff-index --name-only HEAD').read()
         diffs = diffs.strip().split()
         branch = os.popen('git symbolic-ref -q --short HEAD').read().strip()
+        version = _module.__version__
     finally:
         os.chdir(cwd)
-    return { 'branch':branch, 'sha':sha, 'diffs':diffs,
-             'pyomo_version': pyomo_version }
+    return {
+        'branch': branch,
+        'sha': sha,
+        'diffs': diffs,
+        'version': version,
+    }
 
 
-def getRunInfo(cython):
+def getRunInfo(options):
     info = {
         'time': time.time(),
         'python_implementation': platform.python_implementation(),
@@ -126,17 +132,18 @@ def getRunInfo(cython):
     }
     if info['python_implementation'].lower() == 'pypy':
         info['pypy_version'] = tuple(sys.pypy_version_info)
-    if cython:
+    if options.cython:
         import Cython
         info['cython'] = tuple(int(x) for x in Cython.__version__.split('.'))
-    info.update(getPyomoInfo())
+    for project in options.projects:
+        info[project] = getProjectInfo(project)
     return info
 
 
-def run_tests(cython, argv):
+def run_tests(options, argv):
     gc.collect()
     gc.collect()
-    results = ( getRunInfo(cython), OrderedDict() )
+    results = ( getRunInfo(options), OrderedDict() )
     recorder = DataRecorder(results[1])
     unittest.pytest.main(argv, plugins=[recorder])
     gc.collect()
@@ -162,7 +169,15 @@ def main(argv):
         default=None,
         help='Store the test results in the specified directory.  If -o '
         'is not specified, then a file name is automatically generated '
-        'based on the git branch and hash.'
+        'based on the first "main project" git branch and hash.'
+    )
+    parser.add_argument(
+        '-p', '--project',
+        action='append',
+        dest='projects',
+        default=[],
+        help='Main project (used for generating and recording SHA and '
+        'DIFF information)'
     )
     parser.add_argument(
         '-n', '--replicates',
@@ -180,17 +195,22 @@ def main(argv):
     )
 
     options, argv = parser.parse_known_args(argv)
+    if not options.projects:
+        options.projects.append('pyomo')
+    # Pytest really, really wants the initial script to belong to the
+    # "main" project being tested.
+    argv[0] = options.projects[0]
     argv.append('-W ignore::Warning')
-    cython = options.cython
-    results = tuple(run_tests(cython, argv) for i in range(options.replicates))
+
+    results = tuple(run_tests(options, argv) for i in range(options.replicates))
     results = (results[0][0],) + tuple(r[1] for r in results)
 
     if options.output_dir:
         if not options.output:
             options.output = 'perf-%s-%s-%s-%s.json' % (
-                results[0]['branch'],
-                results[0]['sha'][:7] + (
-                    '_mod' if results[0]['diffs'] else ''),
+                results[0][options.projects[0]]['branch'],
+                results[0][options.projects[0]]['sha'][:7] + (
+                    '_mod' if results[0][options.projects[0]]['diffs'] else ''),
                 results[0]['python_implementation'].lower() + (
                     '.'.join(str(i) for i in results[0]['python_version'][:3])),
                 time.strftime('%y%m%d_%H%M', time.localtime())
