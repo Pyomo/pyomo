d00fdbe9caaf67bb440c4eef1a0abfec28725ee4
John Siirola <jsiirola@users.noreply.github.com>
2022-09-19 13:05:56 -0600

    Add testing

--- pyomo/repn/tests/ampl/test_nlv2.py
+++ pyomo/repn/tests/ampl/test_nlv2.py
@@ -18,6 +18,7 @@ import pyomo.repn.plugins.nl_writer as nl_writer
 
 from pyomo.common.log import LoggingIntercept
 from pyomo.core.expr.current import Expr_if, inequality
+from pyomo.core.base.expression import ScalarExpression
 from pyomo.environ import ConcreteModel, Param, Var, log
 
 
@@ -621,3 +622,33 @@ class Test_AMPLRepnVisitor(unittest.TestCase):
         self.assertEqual(repn.const, 0)
         self.assertEqual(repn.linear, {id(m.y):1})
         self.assertEqual(repn.nonlinear, None)
+
+    def test_custom_named_expression(self):
+        class CustomExpression(ScalarExpression):
+            pass
+
+        m = ConcreteModel()
+        m.x = Var()
+        m.e = CustomExpression()
+        m.e.expr = m.x + 3
+
+        expr = m.e + m.e
+        info = INFO()
+        with LoggingIntercept() as LOG:
+            repn = info.visitor.walk_expression((expr, None, None))
+        self.assertEqual(LOG.getvalue(), "")
+        self.assertEqual(repn.nl, None)
+        self.assertEqual(repn.mult, 1)
+        self.assertEqual(repn.const, 6)
+        self.assertEqual(repn.linear, {id(m.x): 2})
+        self.assertEqual(repn.nonlinear, None)
+
+        self.assertEqual(len(info.subexpression_cache), 1)
+        obj, repn, info = info.subexpression_cache[id(m.e)]
+        self.assertIs(obj, m.e)
+        self.assertEqual(repn.nl, ('v%s\n', (id(m.e),)))
+        self.assertEqual(repn.mult, 1)
+        self.assertEqual(repn.const, 3)
+        self.assertEqual(repn.linear, [(id(m.x), 1)])
+        self.assertEqual(repn.nonlinear, None)
+        self.assertEqual(info, [None, None, False])
