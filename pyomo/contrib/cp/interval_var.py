d4475d55a1db15c753fe55312f700e87d19f307f
Emma Johnson <emma58@users.noreply.github.com>
2022-09-19 11:34:08 -0600

    Starting to support indexing interval vars by variables

--- pyomo/contrib/cp/interval_var.py
+++ pyomo/contrib/cp/interval_var.py
@@ -19,10 +19,10 @@ from pyomo.core.base import Any, ScalarVar, ScalarBooleanVar
 from pyomo.core.base.block import _BlockData, Block
 from pyomo.core.base.component import ModelComponentFactory
 from pyomo.core.base.global_set import UnindexedComponent_index
-from pyomo.core.base.initializer import BoundInitializer, Initializer
-
 from pyomo.core.base.indexed_component import (
     IndexedComponent, UnindexedComponent_set)
+from pyomo.core.base.initializer import BoundInitializer, Initializer
+from pyomo.core.expr.current import GetItemExpression
 
 class IntervalVarTimePoint(ScalarVar):
     """This class defines the abstract interface for a single variable
@@ -171,5 +171,12 @@ class ScalarIntervalVar(IntervalVarData, IntervalVar):
 
 
 class IndexedIntervalVar(IntervalVar):
-    pass
-
+    # We allow indexing IntervalVars by expressions (including Vars).
+    def __getitem__(self, args):
+        tmp = args if args.__class__ is tuple else (args,)
+        if any(hasattr(arg, 'is_potentially_variable')
+               and arg.is_potentially_variable()
+               for arg in tmp
+        ):
+            return GetItemExpression((self,) + tmp)
+        return super().__getitem__(args)
--- pyomo/contrib/cp/tests/test_interval_var.py
+++ pyomo/contrib/cp/tests/test_interval_var.py
@@ -12,7 +12,9 @@
 import pyomo.common.unittest as unittest
 from pyomo.contrib.cp.interval_var import (
     IntervalVar, IntervalVarTimePoint, IntervalVarLength, IntervalVarPresence)
-from pyomo.environ import ConcreteModel, Integers, Set, value
+from pyomo.core.expr.current import GetItemExpression
+from pyomo.core.expr.template_expr import GetAttrExpression
+from pyomo.environ import ConcreteModel, Integers, Set, value, Var
 
 class TestScalarIntervalVar(unittest.TestCase):
     def test_initialize_with_no_data(self):
@@ -172,3 +174,25 @@ class TestIndexedIntervalVar(unittest.TestCase):
         m.act = IntervalVar(m.idx, optional=optional_rule)
         self.assertTrue(m.act[4,2].optional)
         self.assertFalse(m.act[5,2].optional)
+
+    def test_index_by_expr(self):
+        m = ConcreteModel()
+        m.act = IntervalVar([(1, 2), (2, 1), (2, 2)])
+        m.i = Var(domain=Integers)
+        m.i2 = Var([1, 2], domain=Integers)
+
+        thing1 = m.act[m.i, 2]
+        self.assertIsInstance(thing1, GetItemExpression)
+        self.assertEqual(len(thing1.args), 3)
+        self.assertIs(thing1.args[0], m.act)
+        self.assertIs(thing1.args[1], m.i)
+        self.assertEqual(thing1.args[2], 2)
+
+        thing2 = thing1.start_time
+        self.assertIsInstance(thing2, GetAttrExpression)
+        self.assertEqual(len(thing2.args), 2)
+        self.assertIs(thing2.args[0], thing1)
+        self.assertEqual(thing2.args[1], 'start_time')
+
+        # TODO: But this is where it dies.
+        expr1 = m.act[m.i, 2].start_time.before(m.act[m.i**2, 1].end_time)
