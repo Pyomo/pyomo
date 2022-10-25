42e26d3ed539846496b82d789f99082df61013c4
John Siirola <jsiirola@users.noreply.github.com>
2022-10-25 12:07:15 -0600

    Add test for relative path to GAMS logfile

--- pyomo/solvers/tests/checks/test_GAMS.py
+++ pyomo/solvers/tests/checks/test_GAMS.py
@@ -17,6 +17,7 @@ from pyomo.solvers.plugins.solvers.GAMS import (
     GAMSShell, GAMSDirect, gdxcc_available
 )
 import pyomo.common.unittest as unittest
+from pyomo.common.tempfiles import TempfileManager
 from pyomo.common.tee import capture_output
 import os, shutil
 from tempfile import mkdtemp
@@ -395,6 +396,23 @@ class GAMSLogfileGmsTests(GAMSLogfileTestBase):
         self._check_stdout(output.getvalue(), exists=False)
         self._check_logfile(exists=True)
 
+    def test_logfile_relative(self):
+        cwd = os.getcwd()
+        with TempfileManager:
+            tmpdir = TempfileManager.create_tempdir()
+            os.chdir(tmpdir)
+            try:
+                self.logfile = 'test-gams.log'
+                with SolverFactory("gams", solver_io="gms") as opt:
+                    with capture_output() as output:
+                        opt.solve(self.m, logfile=self.logfile)
+                self._check_stdout(output.getvalue(), exists=False)
+                self._check_logfile(exists=True)
+                self.assertTrue(
+                    os.path.exists(os.path.join(tmpdir, self.logfile)))
+            finally:
+                os.chdir(cwd)
+
     def test_tee_and_logfile(self):
         with SolverFactory("gams", solver_io="gms") as opt:
             with capture_output() as output:
@@ -433,6 +451,22 @@ class GAMSLogfilePyTests(GAMSLogfileTestBase):
         self._check_stdout(output.getvalue(), exists=False)
         self._check_logfile(exists=True)
 
+    def test_logfile_relative(self):
+        cwd = os.getcwd()
+        with TempfileManager:
+            tmpdir = TempfileManager.create_tempdir()
+            os.chdir(tmpdir)
+            try:
+                self.logfile = 'test-gams.log'
+                with SolverFactory("gams", solver_io="python") as opt:
+                    with capture_output() as output:
+                        opt.solve(self.m, logfile=self.logfile)
+                self._check_stdout(output.getvalue(), exists=False)
+                self._check_logfile(exists=True)
+                self.assertTrue(
+                    os.path.exists(os.path.join(tmpdir, self.logfile)))
+            finally:
+                os.chdir(cwd)
     def test_tee_and_logfile(self):
         with SolverFactory("gams", solver_io="python") as opt:
             with capture_output() as output:
