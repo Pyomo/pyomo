d9c96c05466972a16c3c45dbbf90a5a6376dacbd
robbybp <robbybparker@gmail.com>
2022-10-28 20:40:26 -0600

    add docstrings and refactor to index expressions by variable list indices

--- pyomo/contrib/mpc/modeling/terminal.py
+++ pyomo/contrib/mpc/modeling/terminal.py
@@ -24,94 +24,154 @@
 
 from pyomo.core.base.componentuid import ComponentUID
 from pyomo.core.base.expression import Expression
+from pyomo.core.base.set import Set
 
 from pyomo.contrib.mpc.data.series_data import get_indexed_cuid
 from pyomo.contrib.mpc.data.scalar_data import ScalarData
 
 
-def get_quadratic_penalty_at_time(var, t, target, weight=None):
+def _get_quadratic_penalty_at_time(var, t, target, weight=None):
     if weight is None:
         weight = 1.0
     return weight * (var[t] - target)**2
 
 
-#
-# TODO: These penalty expressions should be indexed by a set of variable
-# indices
-#
-def get_penalty_expressions_at_time(
+def _get_penalty_expressions_at_time(
     variables,
-    time,
     t,
     target_data,
     weight_data=None,
+    time_set=None,
 ):
     """
     """
-    if not isinstance(target_data, ScalarData):
-        target_data = ScalarData(target_data)
-    target_data = target_data.get_data()
-
-    cuids = [
-        get_indexed_cuid(var, sets=(time,))
-        for var in variables
-    ]
-    # TODO: Weight data (and setpoint data) are user-provided and don't
-    # necessarily have CUIDs as keys. Should I process the keys here
-    # with get_indexed_cuid?
     if weight_data is None:
-        weight_data = {cuid: 1.0 for cuid in cuids}
-    for i, cuid in enumerate(cuids):
-        if cuid not in target_data:
+        weight_data = ScalarData(ComponentMap((var, 1.0) for var in variables))
+    if not isinstance(weight_data, ScalarData):
+        # We pass time_set as an argument in case the user provides a
+        # ComponentMap of VarData -> values. In this case knowing the
+        # time set is necessary to recover the indexed CUID.
+        weight_data = ScalarData(weight_data, time_set=time_set)
+    if not isinstance(target_data, ScalarData):
+        target_data = ScalarData(target_data, time_set=time_set)
+
+    for var in variables:
+        if not target_data.contains_key(var):
             raise KeyError(
-                "Target data dictionary does not contain a key for variable"
-                " %s with ComponentUID %s" % (variables[i].name, cuid)
+                "Target data does not contain a key for variable %s"
+                % var.name
             )
-        if cuid not in weight_data:
+        if not weight_data.contains_key(var):
             raise KeyError(
-                "Terminal penalty weight dictionary does not contain a key for"
-                " variable %s with ComponentUID %s" % (variables[i].name, cuid)
+                "Penalty weight data does not contain a key for variable %s"
+                % var.name
             )
 
     penalties = [
-        get_quadratic_penalty_at_time(
-            var, t, target_data[cuid], weight_data[cuid]
-        ) for var, cuid in zip(variables, cuids)
+        _get_quadratic_penalty_at_time(
+            var,
+            t,
+            target_data.get_data_from_key(var),
+            weight_data.get_data_from_key(var),
+        ) for var in variables
     ]
     return penalties
 
 
 def get_penalty_at_time(
     variables,
-    time,
     t,
     target_data,
     weight_data=None,
+    time_set=None,
+    variable_set=None,
 ):
     """Returns an Expression penalizing the deviation of the specified
-    variables from the specified target
+    variables at the specified point in time from the specified target
+
+    Arguments
+    ---------
+    variables: List
+        List of time-indexed variables that will be penalized
+    t: Float
+        Time point at which to apply the penalty
+    target_data: ScalarData
+        ScalarData object containing the target for (at least) the variables
+        to be penalized
+    weight_data: ScalarData (optional)
+        ScalarData object containing the penalty weights for (at least) the 
+        variables to be penalized
+    time_set: Set (optional)
+        Time set that indexes the provided variables. This is only used if
+        target or weight data are provided as a ComponentMap with VarData
+        as keys. In this case the Set is necessary to recover the CUIDs
+        used internally as keys
+    variable_set: Set (optional)
+        Set indexing the list of variables provided, if such a set already
+        exists
+
+    Returns
+    -------
+    Set, Expression
+        Set indexing the list of variables provided and an Expression,
+        indexed by this set, containing the weighted penalty expressions
 
     """
-    terminal_penalty = Expression(
-        expr=sum(get_penalty_expressions_at_time(
-            variables, time, t, target_data, weight_data
-        ))
+    if variable_set is None:
+        variable_set = Set(initialize=range(len(variables)))
+    penalty_expressions = _get_penalty_expressions_at_time(
+        variables,
+        t,
+        target_data,
+        weight_data=weight_data,
+        time_set=time_set,
     )
-    return terminal_penalty
+    def penalty_rule(m, i):
+        return penalty_expressions[i]
+    penalty = Expression(variable_set, rule=penalty_rule)
+    return variable_set, penalty
 
 
 def get_terminal_penalty(
     variables,
-    time,
+    time_set,
     target_data,
     weight_data=None,
+    variable_set=None,
 ):
-    t = time.last()
-    #terminal_penalty = Expression(
-    #    expr=sum(get_penalty_expressions_at_time(
-    #        variables, time, t, target_data, weight_data
-    #    ))
-    #)
+    """Returns an Expression penalizing the deviation of the specified
+    variables at the final point in time from the specified target
+
+    Arguments
+    ---------
+    variables: List
+        List of time-indexed variables that will be penalized
+    time_set: Set
+        Time set that indexes the provided variables. Penalties are applied
+        at the last point in this set.
+    target_data: ScalarData
+        ScalarData object containing the target for (at least) the variables
+        to be penalized
+    weight_data: ScalarData (optional)
+        ScalarData object containing the penalty weights for (at least) the 
+        variables to be penalized
+    variable_set: Set (optional)
+        Set indexing the list of variables provided, if such a set already
+        exists
+
+    Returns
+    -------
+    Set, Expression
+        Set indexing the list of variables provided and an Expression,
+        indexed by this set, containing the weighted penalty expressions
+
+    """
+    t = time_set.last()
     return get_penalty_at_time(
-        variables, time, t, target_data, weight_data=weight_data
+        variables,
+        t,
+        target_data,
+        weight_data=weight_data,
+        time_set=time_set,
+        variable_set=variable_set,
     )
