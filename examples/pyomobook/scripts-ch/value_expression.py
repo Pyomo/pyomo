aeb199e14c7909073c6bd6e86bafd7b14d360456
John Siirola <jsiirola@users.noreply.github.com>
2022-10-31 15:40:15 -0600

    Update test to reflect the more aggressive creation of LinearExpressions

--- examples/pyomobook/scripts-ch/value_expression.py
+++ examples/pyomobook/scripts-ch/value_expression.py
@@ -6,7 +6,7 @@ model.u = pyo.Var(initialize=2.0)
 # unexpected expression instead of value
 a = model.u - 1
 print(a)       # "u - 1"
-print(type(a)) # <class 'pyomo.core.expr.numeric_expr.SumExpression'>
+print(type(a)) # <class 'pyomo.core.expr.numeric_expr.LinearExpression'>
 
 # correct way to access the value
 b = pyo.value(model.u) - 1
