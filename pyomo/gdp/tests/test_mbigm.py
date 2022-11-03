897552e37a77eee9dfdb790666c8f1dba4009644
Emma Johnson <emma58@users.noreply.github.com>
2022-11-03 15:34:27 -0600

    adding a test to prove that I don't handle redundant bounds constraints correctly

--- pyomo/gdp/tests/test_mbigm.py
+++ pyomo/gdp/tests/test_mbigm.py
@@ -425,6 +425,29 @@ class LinearModelDecisionTreeExample(unittest.TestCase):
         self.check_pretty_bound_constraints(cons[1], m.x2, {m.d1: 3, m.d2: 10,
                                                             m.d3: 1}, lb=False)
 
+    def test_bound_constraints_correct_with_redundant_constraints(self):
+        m = self.make_model()
+
+        m.d1.bogus_x1_bounds = Constraint(expr=(0.25, m.x1, 2.25))
+        mbm = TransformationFactory('gdp.mbigm')
+        mbm.apply_to(m, tighten_bound_constraints=True)
+
+        cons = mbm.get_transformed_constraints(m.d1.x1_bounds)
+        self.assertEqual(len(cons), 2)
+        self.check_pretty_bound_constraints(cons[0], m.x1, {m.d1: 0.5, m.d2:
+                                                            0.65, m.d3: 2},
+                                            lb=True)
+        self.check_pretty_bound_constraints(cons[1], m.x1, {m.d1: 2, m.d2: 3,
+                                                            m.d3: 10}, lb=False)
+
+        cons = mbm.get_transformed_constraints(m.d1.x2_bounds)
+        self.assertEqual(len(cons), 2)
+        self.check_pretty_bound_constraints(cons[0], m.x2, {m.d1: 0.75, m.d2: 3,
+                                                            m.d3: 0.55},
+                                            lb=True)
+        self.check_pretty_bound_constraints(cons[1], m.x2, {m.d1: 3, m.d2: 10,
+                                                            m.d3: 1}, lb=False)
+
     def test_Ms_specified_as_args_honored(self):
         m = self.make_model()
 
