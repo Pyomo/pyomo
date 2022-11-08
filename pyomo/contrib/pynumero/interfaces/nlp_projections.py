0d3715c9303d15d058baacee8ecba00a90707142
robbybp <robbybparker@gmail.com>
2022-11-07 20:31:28 -0700

    add evaluate_jacobian_ineq method to ProjExtNLP

--- pyomo/contrib/pynumero/interfaces/nlp_projections.py
+++ pyomo/contrib/pynumero/interfaces/nlp_projections.py
@@ -324,6 +324,7 @@ class ProjectedExtendedNLP(ProjectedNLP, _ExtendedNLPDelegator):
     def __init__(self, original_nlp, primals_ordering):
         super(ProjectedExtendedNLP, self).__init__(original_nlp, primals_ordering)
         self._jacobian_eq_nz_mask = None
+        self._jacobian_ineq_nz_mask = None
 
     def evaluate_jacobian_eq(self, out=None):
         original_jacobian = self._original_nlp.evaluate_jacobian_eq()
@@ -349,4 +350,29 @@ class ProjectedExtendedNLP(ProjectedNLP, _ExtendedNLPDelegator):
         return sp.coo_matrix(
             (new_data, (new_row, new_col)),
             shape=(self.n_eq_constraints(), self.n_primals()),
+        )
+
+    def evaluate_jacobian_ineq(self, out=None):
+        original_jacobian = self._original_nlp.evaluate_jacobian_ineq()
+        if out is not None:
+            np.copyto(
+                out.data, original_jacobian.data[self._jacobian_ineq_nz_mask]
+            )
+            return out
+
+        row = original_jacobian.row
+        col = original_jacobian.col
+        data = original_jacobian.data
+
+        if self._jacobian_ineq_nz_mask is None:
+            self._jacobian_ineq_nz_mask = np.isin(col, self._original_idxs)
+
+        new_col = col[self._jacobian_ineq_nz_mask]
+        new_col = self._original_to_projected[new_col]
+        new_row = row[self._jacobian_ineq_nz_mask]
+        new_data = data[self._jacobian_ineq_nz_mask]
+
+        return sp.coo_matrix(
+            (new_data, (new_row, new_col)),
+            shape=(self.n_ineq_constraints(), self.n_primals()),
         )
