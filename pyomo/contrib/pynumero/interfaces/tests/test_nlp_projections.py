eac10b062be494531da3d3af5d31645b292ace6d
robbybp <robbybparker@gmail.com>
2022-11-08 11:27:16 -0700

    more tests for ProjectedExtendedNLP

--- pyomo/contrib/pynumero/interfaces/tests/test_nlp_projections.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_nlp_projections.py
@@ -238,6 +238,57 @@ class TestProjectedExtendedNLP(unittest.TestCase):
             reordered_values[i] = val
         return reordered_values
 
+    def _eq_to_nlp(self, m, nlp, values):
+        indices = nlp.get_equality_constraint_indices([m.eq_con_1, m.eq_con_2])
+        reordered_values = [None]*2
+        for i, val in zip(indices, values):
+            reordered_values[i] = val
+        return reordered_values
+
+    def _ineq_to_nlp(self, m, nlp, values):
+        indices = nlp.get_inequality_constraint_indices(
+            [m.ineq_con_1, m.ineq_con_2, m.ineq_con_3]
+        )
+        reordered_values = [None]*3
+        for i, val in zip(indices, values):
+            reordered_values[i] = val
+        return reordered_values
+
+    def _rc_to_nlp(self, m, nlp, rc):
+        var_indices = nlp.get_primal_indices(list(m.x.values()))
+        con_indices = nlp.get_constraint_indices(
+            [m.eq_con_1, m.eq_con_2, m.ineq_con_1, m.ineq_con_2, m.ineq_con_3]
+        )
+        i, j = rc
+        return (con_indices[i], var_indices[j])
+
+    def _rc_to_proj_nlp(self, m, nlp, rc):
+        var_indices = [1, 0]
+        con_indices = nlp.get_constraint_indices(
+            [m.eq_con_1, m.eq_con_2, m.ineq_con_1, m.ineq_con_2, m.ineq_con_3]
+        )
+        i, j = rc
+        return (con_indices[i], var_indices[j])
+
+    def _rc_to_proj_nlp_eq(self, m, nlp, rc):
+        # Expects variable coords in order [x0, x1], constraint coords
+        # in order [eq1, eq2]
+        var_indices = [1, 0]
+        con_indices = nlp.get_equality_constraint_indices(
+            [m.eq_con_1, m.eq_con_2]
+        )
+        i, j = rc
+        return (con_indices[i], var_indices[j])
+
+    def _rc_to_proj_nlp_ineq(self, m, nlp, rc):
+        # Expects variable coords in order [x0, x1], constraint coords
+        # in order [ineq1, ineq2, ineq3]
+        var_indices = [1, 0]
+        con_indices = nlp.get_inequality_constraint_indices(
+            [m.ineq_con_1, m.ineq_con_2, m.ineq_con_3]
+        )
+        i, j = rc
+        return (con_indices[i], var_indices[j])
 
     def test_n_primals_constraints(self):
         m, nlp, proj_nlp = self._get_nlps()
@@ -262,10 +313,13 @@ class TestProjectedExtendedNLP(unittest.TestCase):
             nlp.get_primals(), self._x_to_nlp(m, nlp, [-1.1, -1.0, 1.4, 1.5])
         )
 
-    def test_set_primals_with_list(self):
+    def test_set_primals_with_list_error(self):
         # This doesn't work. Get a TypeError due to treating list as numpy array
+        # when indexing another array.
         m, nlp, proj_nlp = self._get_nlps()
         msg = "only integer scalar arrays can be converted to a scalar index"
+        # This test may be too specific. If NumPy changes this error message,
+        # the test could fail.
         with self.assertRaisesRegex(TypeError, msg):
             proj_nlp.set_primals([1.0, 2.0])
 
@@ -282,19 +336,191 @@ class TestProjectedExtendedNLP(unittest.TestCase):
 
     def test_eval_constraints(self):
         m, nlp, proj_nlp = self._get_nlps()
-        nlp.set_primals(self._x_to_nlp(m, nlp, [1.2, 1.3, 1.4, 1.5]))
+        x0, x1, x2, x3 = [1.2, 1.3, 1.4, 1.5]
+        nlp.set_primals(self._x_to_nlp(m, nlp, [x0, x1, x2, x3]))
 
         con_resids = nlp.evaluate_constraints()
         pred_con_body = [
-            1.2*1.3**1.1*1.4**1.2 - 3.0,
-            1.2**2 + 1.5**2 + 1.3 - 2.0,
-            1.2 + 1.2*1.5,
-            1.3 + 1.4,
-            1.4,
+            x0*x1**1.1*x2**1.2 - 3.0,
+            x0**2 + x3**2 + x1 - 2.0,
+            x0 + x0*x3,
+            x1 + x2,
+            x2,
         ]
         np.testing.assert_array_equal(
             con_resids, self._c_to_nlp(m, nlp, pred_con_body)
         )
+
+        con_resids = proj_nlp.evaluate_constraints()
+        np.testing.assert_array_equal(
+            con_resids, self._c_to_nlp(m, nlp, pred_con_body)
+        )
+
+        eq_resids = proj_nlp.evaluate_eq_constraints()
+        pred_eq_body = [
+            x0*x1**1.1*x2**1.2 - 3.0,
+            x0**2 + x3**2 + x1 - 2.0,
+        ]
+        np.testing.assert_array_equal(
+            eq_resids, self._eq_to_nlp(m, nlp, pred_eq_body)
+        )
+
+        ineq_body = proj_nlp.evaluate_ineq_constraints()
+        pred_ineq_body = [
+            x0 + x0*x3,
+            x1 + x2,
+            x2,
+        ]
+        np.testing.assert_array_equal(
+            ineq_body, self._ineq_to_nlp(m, nlp, pred_ineq_body)
+        )
+
+    def test_eval_jacobian_orig_nlp(self):
+        m, nlp, proj_nlp = self._get_nlps()
+        x0, x1, x2, x3 = [1.2, 1.3, 1.4, 1.5]
+        nlp.set_primals(self._x_to_nlp(m, nlp, [x0, x1, x2, x3]))
+
+        jac = nlp.evaluate_jacobian()
+        # Predicted row/col indices in the "natural ordering" of the model
+        pred_rc = [
+            (0, 0), (0, 1), (0, 2), # eq 1
+            (1, 0), (1, 1), (1, 3), # eq 2
+            (2, 0), (2, 3),         # ineq 1
+            (3, 1), (3, 2),         # ineq 2
+            (4, 2),                 # ineq 3
+        ]
+        pred_data_dict = {
+            # eq 1
+            (0, 0): x1**1.1 * x2**1.2,
+            (0, 1): 1.1 * x0 * (x1**0.1) * x2**1.2,
+            (0, 2): 1.2 * x0 * x1**1.1 * x2**0.2,
+            # eq 2
+            (1, 0): 2*x0,
+            (1, 1): 1.0,
+            (1, 3): 2*x3,
+            # ineq 1
+            (2, 0): 1.0 + x3,
+            (2, 3): x0,
+            # ineq 2
+            (3, 1): 1.0,
+            (3, 2): 1.0,
+            # ineq 3
+            (4, 2): 1.0,
+        }
+        pred_rc_set = set(self._rc_to_nlp(m, nlp, rc) for rc in pred_rc)
+        pred_data_dict = {
+            self._rc_to_nlp(m, nlp, rc): val
+            for rc, val in pred_data_dict.items()
+        }
+        rc_set = set(zip(jac.row, jac.col))
+        self.assertEqual(pred_rc_set, rc_set)
+
+        data_dict = dict(zip(zip(jac.row, jac.col), jac.data))
+        self.assertEqual(pred_data_dict, data_dict)
+
+    def test_eval_jacobian_proj_nlp(self):
+        m, nlp, proj_nlp = self._get_nlps()
+        x0, x1, x2, x3 = [1.2, 1.3, 1.4, 1.5]
+        nlp.set_primals(self._x_to_nlp(m, nlp, [x0, x1, x2, x3]))
+
+        jac = proj_nlp.evaluate_jacobian()
+        self.assertEqual(jac.shape, (5, 2))
+        # Predicted row/col indices. In the "natural ordering" of the model.
+        pred_rc = [
+            (0, 0), (0, 1), # eq 1
+            (1, 0), (1, 1), # eq 2
+            (2, 0),         # ineq 1
+            (3, 1),         # ineq 2
+        ]
+        pred_data_dict = {
+            # eq 1
+            (0, 0): x1**1.1 * x2**1.2,
+            (0, 1): 1.1 * x0 * (x1**0.1) * x2**1.2,
+            # eq 2
+            (1, 0): 2*x0,
+            (1, 1): 1.0,
+            # ineq 1
+            (2, 0): 1.0 + x3,
+            # ineq 2
+            (3, 1): 1.0,
+        }
+        # Projected NLP has primals: [x1, x0]
+        pred_rc_set = set(self._rc_to_proj_nlp(m, nlp, rc) for rc in pred_rc)
+        pred_data_dict = {
+            self._rc_to_proj_nlp(m, nlp, rc): val
+            for rc, val in pred_data_dict.items()
+        }
+        rc_set = set(zip(jac.row, jac.col))
+        self.assertEqual(pred_rc_set, rc_set)
+
+        data_dict = dict(zip(zip(jac.row, jac.col), jac.data))
+        self.assertEqual(pred_data_dict, data_dict)
+
+    def test_eval_eq_jacobian_proj_nlp(self):
+        m, nlp, proj_nlp = self._get_nlps()
+        x0, x1, x2, x3 = [1.2, 1.3, 1.4, 1.5]
+        nlp.set_primals(self._x_to_nlp(m, nlp, [x0, x1, x2, x3]))
+
+        jac = proj_nlp.evaluate_jacobian_eq()
+        self.assertEqual(jac.shape, (2, 2))
+        # Predicted row/col indices. In the "natural ordering" of the equality
+        # constraints (eq1, eq2)
+        pred_rc = [
+            (0, 0), (0, 1), # eq 1
+            (1, 0), (1, 1), # eq 2
+        ]
+        pred_data_dict = {
+            # eq 1
+            (0, 0): x1**1.1 * x2**1.2,
+            (0, 1): 1.1 * x0 * (x1**0.1) * x2**1.2,
+            # eq 2
+            (1, 0): 2*x0,
+            (1, 1): 1.0,
+        }
+        # Projected NLP has primals: [x1, x0]
+        pred_rc_set = set(self._rc_to_proj_nlp_eq(m, nlp, rc) for rc in pred_rc)
+        pred_data_dict = {
+            self._rc_to_proj_nlp_eq(m, nlp, rc): val
+            for rc, val in pred_data_dict.items()
+        }
+        rc_set = set(zip(jac.row, jac.col))
+        self.assertEqual(pred_rc_set, rc_set)
+
+        data_dict = dict(zip(zip(jac.row, jac.col), jac.data))
+        self.assertEqual(pred_data_dict, data_dict)
+
+    def test_eval_ineq_jacobian_proj_nlp(self):
+        m, nlp, proj_nlp = self._get_nlps()
+        x0, x1, x2, x3 = [1.2, 1.3, 1.4, 1.5]
+        nlp.set_primals(self._x_to_nlp(m, nlp, [x0, x1, x2, x3]))
+
+        jac = proj_nlp.evaluate_jacobian_ineq()
+        self.assertEqual(jac.shape, (3, 2))
+        # Predicted row/col indices. In the "natural ordering" of the inequality
+        # constraints (ineq1, ineq2, ineq3)
+        pred_rc = [
+            (0, 0),         # ineq 1
+            (1, 1),         # ineq 2
+        ]
+        pred_data_dict = {
+            # ineq 1
+            (0, 0): 1.0 + x3,
+            # ineq 2
+            (1, 1): 1.0,
+        }
+        # Projected NLP has primals: [x1, x0]
+        pred_rc_set = set(
+            self._rc_to_proj_nlp_ineq(m, nlp, rc) for rc in pred_rc
+        )
+        pred_data_dict = {
+            self._rc_to_proj_nlp_ineq(m, nlp, rc): val
+            for rc, val in pred_data_dict.items()
+        }
+        rc_set = set(zip(jac.row, jac.col))
+        self.assertEqual(pred_rc_set, rc_set)
+
+        data_dict = dict(zip(zip(jac.row, jac.col), jac.data))
+        self.assertEqual(pred_data_dict, data_dict)
 
 
 if __name__ == '__main__':
