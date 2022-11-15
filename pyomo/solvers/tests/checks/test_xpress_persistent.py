64ec6e6af41588d87d95599b2cb9a8559a1e7433
Daniel Junglas <danieljunglas@fico.com>
2022-11-15 11:15:35 +0100

    Fix typo.

--- pyomo/solvers/tests/checks/test_xpress_persistent.py
+++ pyomo/solvers/tests/checks/test_xpress_persistent.py
@@ -289,3 +289,24 @@ class TestXpressPersistent(unittest.TestCase):
         self.assertGreater(m.x1.value, 0.0)
         self.assertGreater(m.x2.value, 0.0)
         self.assertGreater(m.x3.value, 0.0)
+
+    @unittest.skipIf(not xpress_available, "xpress is not available")
+    def test_nonconvexqp_infeasible(self):
+        """Test non-convex QP which xpress_direct should prove infeasible."""
+        m = pe.ConcreteModel()
+        m.x1 = pe.Var()
+        m.x2 = pe.Var()
+        m.x3 = pe.Var()
+
+        m.obj = pe.Objective(rule=lambda m: 2 * m.x1 + m.x2 + m.x3,
+                             sense=pe.minimize)
+        m.equ1a = pe.Constraint(rule=lambda m: m.x1 + m.x2 + m.x3 == 1)
+        m.equ1b = pe.Constraint(rule=lambda m: m.x1 + m.x2 + m.x3 == -1)
+        m.cone = pe.Constraint(rule=lambda m: m.x2 * m.x2 + m.x3 * m.x3 <= m.x1 * m.x1)
+        m.equ2 = pe.Constraint(rule=lambda m: m.x1 >= 0)
+
+        opt = pe.SolverFactory('xpress_direct')
+
+        results = opt.solve(m)
+        self.assertEqual(results.solver.status, SolverStatus.ok)
+        self.assertEqual(results.solver.termination_condition, TerminationCondition.infeasible)
