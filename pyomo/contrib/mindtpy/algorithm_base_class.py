3493757db953ba9455e16b33e2992b2c0230e367
ZedongPeng <peng_zedong@126.com>
2022-12-04 17:43:41 +0800

    remove unused function

--- pyomo/contrib/mindtpy/algorithm_base_class.py
+++ pyomo/contrib/mindtpy/algorithm_base_class.py
@@ -610,62 +610,6 @@ class _MindtPyAlgorithm(object):
                     else:
                         util_blk.nonlinear_constraint_list.append(constr)
 
-    # TODO: check this function
-    def _create_pyomo_results_object_with_problem_info(self, original_model,
-                                                       config):
-        """
-        Initialize a results object with results.problem information
-        """
-        results = self.pyomo_results = SolverResults()
-
-        results.solver.name = 'MindtPy %s - %s' % (self.version(),
-                                                  self.algorithm)
-
-        prob = results.problem
-        prob.name = original_model.name
-        prob.number_of_nonzeros = None  # TODO
-
-        num_of = build_model_size_report(original_model)
-
-        # Get count of constraints and variables
-        prob.number_of_constraints = num_of.activated.constraints
-        prob.number_of_variables = num_of.activated.variables
-        prob.number_of_binary_variables = num_of.activated.binary_variables
-        prob.number_of_continuous_variables = num_of.activated.\
-                                              continuous_variables
-        prob.number_of_integer_variables = num_of.activated.integer_variables
-
-        config.logger.info(
-            "Original model has %s constraints (%s nonlinear) "
-            "with %s variables, of which %s are binary, %s are integer, "
-            "and %s are continuous." %
-            (num_of.activated.constraints,
-             num_of.activated.nonlinear_constraints,
-             num_of.activated.variables,
-             num_of.activated.binary_variables,
-             num_of.activated.integer_variables,
-             num_of.activated.continuous_variables))
-
-        # Handle missing or multiple objectives, and get sense
-        active_objectives = list(original_model.component_data_objects(
-            ctype=Objective, active=True, descend_into=True))
-        number_of_objectives = len(active_objectives)
-        # TODO: Does MindtPy need this?
-        if number_of_objectives == 0:
-            config.logger.warning(
-                'Model has no active objectives. Adding dummy objective.')
-            discrete_obj = Objective(expr=1)
-            original_model.add_component(unique_component_name(original_model,
-                                                               'dummy_obj'),
-                                         discrete_obj)
-        elif number_of_objectives > 1:
-            raise ValueError('Model has multiple active objectives.')
-        else:
-            discrete_obj = active_objectives[0]
-        prob.sense = minimize if discrete_obj.sense == 1 else maximize
-
-        return results
-
 
     def set_up_solve_data(self, model, config):
         """Set up the solve data.
