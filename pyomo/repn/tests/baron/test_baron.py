2607f2b2004a52312fa0cf261e4eba8c5c69ee24
John Siirola <jsiirola@users.noreply.github.com>
2023-01-13 09:10:51 -0700

    Add tests for handling POW in BaronWriter

--- pyomo/repn/tests/baron/test_baron.py
+++ pyomo/repn/tests/baron/test_baron.py
@@ -17,13 +17,16 @@ from filecmp import cmp
 from io import StringIO
 
 import pyomo.common.unittest as unittest
-
+from pyomo.common.collections import OrderedSet
 from pyomo.common.fileutils import this_file_dir
+import pyomo.core.expr.current as EXPR
+from pyomo.core.base import SymbolMap
 
 from pyomo.environ import (
     ConcreteModel, Var, Param,  Constraint, Objective,  Block, sin,
     maximize, Binary, Suffix
 )
+from pyomo.repn.plugins.baron_writer import expression_to_string
 
 thisdir = this_file_dir()
 
@@ -215,6 +218,28 @@ class Test(unittest.TestCase):
                 "the block 'blk.sub' or deactivate it."):
             m.write(StringIO(), format='bar')
 
+
+class TestToBaronVisitor(unittest.TestCase):
+    def test_pow(self):
+        variables = OrderedSet()
+        smap = SymbolMap()
+
+        m = ConcreteModel()
+        m.x = Var(initialize=1)
+        m.y = Var(initialize=2)
+        m.p = Param(mutable=True, initialize=0)
+
+        e = m.x ** m.y
+        test = expression_to_string(e, variables, smap)
+        self.assertEqual(test, "exp((x) * log(y))")
+
+        e = m.x ** (3 + m.p*m.y)
+        test = expression_to_string(e, variables, smap)
+        self.assertEqual(test, "x ^ 3")
+
+        e = (3 + m.p*m.y) ** m.x
+        test = expression_to_string(e, variables, smap)
+        self.assertEqual(test, "3 ^ x")
 
 #class TestBaron_writer(unittest.TestCase):
 class XTestBaron_writer(object):
