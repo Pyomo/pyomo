520097aaaf26f8b153f6f0cd34a26dab7c1f1550
Emma Johnson <emma58@users.noreply.github.com>
2023-01-20 17:25:05 -0700

    Beginning to redesign pw-linear so that the argument of the function is a (Python) function

--- pyomo/contrib/piecewise/piecewise_linear_function.py
+++ pyomo/contrib/piecewise/piecewise_linear_function.py
@@ -9,12 +9,25 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
-from pyomo.core import Any, NonNegativeIntegers
+from pyomo.common.collections import ComponentMap
+from pyomo.common.dependencies import attempt_import
+from pyomo.core import Any, NonNegativeIntegers, value
 from pyomo.core.base.block import _BlockData, Block
 from pyomo.core.base.component import ModelComponentFactory
 from pyomo.core.base.expression import Expression
 from pyomo.core.base.global_set import UnindexedComponent_index
 from pyomo.core.base.indexed_component import UnindexedComponent_set
+from pyomo.core.base.initializer import Initializer, FunctionInitializer
+import pyomo.core.expr.current as EXPR
+
+np, numpy_available = attempt_import('numpy')
+scipy, scipy_available = attempt_import('scipy')
+spatial, scipy_available = attempt_import('scipy.spatial')
+
+# DEBUG
+from pytest import set_trace
+
+
 
 class PiecewiseLinearFunctionData(_BlockData):
     _Block_reserved_words = Any
@@ -24,6 +37,62 @@ class PiecewiseLinearFunctionData(_BlockData):
 
         with self._declare_reserved_components():
             self._expressions = Expression(NonNegativeIntegers)
+            self._simplices = []
+            self._points = []
+            self._linear_functions = []
+
+    def __call__(self, *args):
+        ans = 0
+        if all(arg in EXPR.native_types for arg in args):
+            # We need to actually evaluate
+            return self._evaluate(*args)
+
+    def _evaluate(self, *args):
+        # ESJ: This is a very inefficient implementation in high dimension, but
+        # for now we will just do a linear scan of the simplices.
+        pt = (arg for arg in args)
+        for idx, simplex, func in enumerate(zip(sef._simplices,
+                                                self._linear_functions)):
+            if _pt_in_simplex(pt, simplex):
+                return func(*args)
+
+    def _pt_in_simplex(self, pt, simplex):
+        dim = len(pt)
+        if dim == 1:
+            return self._points[simplex[0]] <= pt and \
+                self._points[simplex[1]] >= pt
+        # Otherwise, we check if pt is a convex combination of the simplex's
+        # extreme points
+        A = np.ones((dim, dim))
+        b = np.ones((dim, 0))
+        for i, extreme_point in enumerate(simplex):
+            b[i] = pt[i]
+            for j, coord in enumerate(self._points[extreme_point]):
+                A[i, j] = coord
+        try:
+            lambdas = np.linalg.solve(A, b)
+        except np.linalg.LinAlgError as e:
+            if 'Singular' in str(e):
+                # It's singular
+                return False
+            else:
+                # This would be a bug (non-square system)
+                raise
+        for l in lambdas:
+            if l < 0:
+                # TODO: Do we need a tolerance?? Eeeeeek
+                return False
+        return True
+
+    @property
+    def simplices(self):
+        # TODO: Is this really what you want? These are by index right now,
+        # scipy style
+        return self._simplices
+
+    @property
+    def linear_functions(self):
+        return self._linear_functions
 
 
 @ModelComponentFactory.register("Multidimensional piecewise linear function")
@@ -63,17 +132,121 @@ class PiecewiseLinearFunction(Block):
                 IndexedPiecewiseLinearFunction)
 
     def __init__(self, *args, **kwargs):
+        self._handlers = {
+            # (f, pts, simplices, linear_funcs) : handler
+            (True, True, False, 
+             False): self._construct_from_function_and_points,
+            (True, False, True, 
+             False): self._construct_from_function_and_simplices,
+            (False, False, True, 
+             True): self._construct_from_linear_functions_and_simplices
+        }
+
         _func_arg = kwargs.pop('function', None)
         _points_arg = kwargs.pop('points', None)
+        _simplices_arg = kwargs.pop('simplices', None)
+        _linear_functions = kwargs.pop('linear_functions', None)
 
         kwargs.setdefault('ctype', PiecewiseLinearFunction)
         Block.__init__(self, *args, **kwargs)
 
-        # args: points, f
-        # Or domains, linear functions
-        # Or simplices, f
+        self._func_rule = FunctionInitializer(_func_arg)
+        self._points_rule = Initializer(_points_arg,
+                                        treat_sequences_as_mappings=False)
+        self._simplices_rule = Initializer(_simplices_arg,
+                                           treat_sequences_as_mappings=False)
+        self._linear_funcs_rule = Initializer(_linear_functions)
+
+    def _construct_from_function_and_points(self, obj, parent, nonlinear):
+        parent = obj.parent_block()
+        idx = obj._index
+
+        nonlinear_function = self._func_rule(parent, idx)
+        points = self._points_rule(parent, idx)
+
+        func_vars = list(EXPR.identify_variables(nonlinear_function))
+        if len(func_vars) == 1:
+            # This is univariate and we'll handle it separately in order to
+            # avoid a dependence on numpy.
+            points.sort()
+            simplices = [(points[i], points[i + 1]) for i in range(len(points) -
+                                                                   1)]
+            return self._construct_from_univariate_function_and_segments(
+                obj, nonlinear_function, simplices, func_vars[0]) 
+
+        try:
+            triangulation = spatial.Delaunay(points)
+        except (spatial.QhullError, ValueError) as error:
+            logger.error("Unable to triangulate the set of input points.")
+            raise
+
+        obj._points = [pt for pt in points]
+        obj._simplices = [simplex for simplex in map(tuple,
+                                                     triangulation.simplices)]
+
+        # TODO: Need to make some modifications to the below for this to
+        # work--we should check if we already have simplices. And we should
+        # always store them in the numpy/scipy style.
+        return self._construct_from_function_and_simplices(obj)
+        
+        # TODO: Get simplices from scipy, then revert to the method below
+
+    def _construct_from_univariate_function_and_segments(self, obj, func,
+                                                         simplices):
+        point_set = set()
+        indices = {}
+        for (x1, x2) in simplices:
+            y_val = {}
+            for xi in (x1, x2):
+                y_val[xi] = func(xi)
+                if xi not in point_set:
+                    point_set.add(xi)
+                    self._points.append(xi)
+                    indices[xi] = len(self._points) - 1
+            obj._simplices.append((indices[x1], indices[x2]))
+            slope = (y_val[x2] - y_val[x1])/(x2 - x1)
+            def f(x, y):
+                return slope*x + y_val[x1] - slope*x1
+            obj._linear_functions.append(f)
+
+        return obj
+
+    def _construct_from_function_and_simplices(self, obj, parent,
+                                               nonlinear_function):
+        simplices = self._simplices_rule(parent, idx)
+        if len(simplices) < 1:
+            raise ValueError("Cannot construct PiecewiseLinearFunction "
+                             "with empty list of simplices")
+
+        dimension = len(simplices[0]) - 1
+        if dimension == 1:
+            # Back to high school with us--this is univariate and we'll handle
+            # it separately in order to avoid a kind of silly dependence on
+            # numpy.
+            return self._construct_from_univariate_function_and_segments(
+                obj, nonlinear_function, simplices) 
+
+        A = np.ones((dimension, dimension + 1))
+        b = np.empty((dimension + 1, 0))
+
+        # evaluate the function at each of the points and form the homogeneous
+        # system of equations
+        for num_piece, simplex in enumerate(simplices):
+            for i, pt in enumerate(simplex):
+                for j, val in enumerate(pt):
+                    A[i, j] = val
+                b[i] = nonlinear_function(*pt)
+            # This system has a solution unless there's a bug--we know there is
+            # a hyperplane that passes through dimension + 1 points.
+            normal = np.linalg.solve(A, b)
+
+            def f(*args):
+                return sum(normal[i]*args[i] for i in dimension) - \
+                    normal[dimension + 1]
+            obj._linear_functions.append(f)
+
+    def _construct_from_linear_functions_and_simplices(self, obj):
         pass
-        # TODO
 
     def _getitem_when_not_present(self, index):
         if index is None and not self.is_indexed():
@@ -82,6 +255,20 @@ class PiecewiseLinearFunction(Block):
             obj = self._data[index] = self._ComponentDataClass(component=self)
 
         obj._index = index
+        handler = self._handlers.get((self._func_rule is not None, 
+                                      self._points_rule is not None, 
+                                      self._simplices_rule is not None,
+                                      self._linear_funcs_rule is not None))
+        if handler is None:
+            raise ValueError("Unsupported set of arguments given for "
+                             "constructing PiecewiseLinearFunction. "
+                             "Expected a nonlinear function and a list"
+                             "of breakpoints, a nonlinear function an a list "
+                             "of simplices, or a list of linear functions and "
+                             "a list of corresponding domains.")
+        parent = obj.parent_block()
+        nonlinear_function = self._func_rule(parent, index)
+        handler(obj, parent, nonlinear_function)
 
         return obj
 
--- pyomo/contrib/piecewise/tests/test_piecewise_linear_function.py
+++ pyomo/contrib/piecewise/tests/test_piecewise_linear_function.py
@@ -11,19 +11,75 @@
 
 import pyomo.common.unittest as unittest
 from pyomo.contrib.piecewise import PiecewiseLinearFunction
+from pyomo.core.expr.compare import assertExpressionsStructurallyEqual
 from pyomo.environ import ConcreteModel, log, Var
 
-class TestPiecewiseLinearFunction(unittest.TestCase):
-    def test_pw_linear_approx_of_ln_x(self):
+class TestPiecewiseLinearFunction2D(unittest.TestCase):
+    def make_ln_x_model(self):
         m = ConcreteModel()
         m.x = Var(bounds=(1, 10))
-        m.f = log(m.x)
+        def f(x):
+            return log(x)
+        m.f = f
 
+        return m
+
+    def check_ln_x_approx(self, m):
+        # indices of extreme points.
+        simplices = [(0, 1), (1, 2), (2, 3)]
+
+        self.assertEqual(len(m.pw.simplices), 3)
+        self.assertEqual(len(m.pw.linear_functions), 3)
+        for idx, simplex in enumerate(simplices):
+            self.assertEqual(m.pw.simplices[idx], simplices[idx])
+        # assertExpressionsStructurallyEqual(self, m.pw.linear_functions[0],
+        #                                    (log(3)/2)*m.x - log(3)/2)
+        # assertExpressionsStructurallyEqual(self, m.pw.linear_functions[1],
+        #                                    (log(2)/3)*m.x + log(3/2))
+        # assertExpressionsStructurallyEqual(self, m.pw.linear_functions[2],
+        #                                    (log(5/3)/4)*m.x +
+        #                                    log(6/((5/3)**(3/2))))
+
+    def test_pw_linear_approx_of_ln_x_simplices(self):
+        m = self.make_ln_x_model()
+        simplices = [(1, 3), (3, 6), (6, 10)]
+        m.pw = PiecewiseLinearFunction(simplices=simplices, function=m.f)
+        self.check_ln_x_approx(m)
+
+    def test_pw_linear_approx_of_ln_x_points(self):
+        m = self.make_ln_x_model()
         m.pw = PiecewiseLinearFunction(points=[1, 3, 6, 10], function=m.f)
+        self.check_ln_x_approx(m)
 
-    def test_pw_linear_approx_of_paraboloid(self):
+class TestPiecewiseLinearFunction3D(unittest.TestCase):
+    def test_pw_linear_approx_of_paraboloid_points(self):
         m = ConcreteModel()
-        m.x1 = Var(bounds=(0, 9))
+        m.x1 = Var(bounds=(0, 3))
         m.x2 = Var(bounds=(1, 7))
         # Here's a cute paraboloid:
-        m.f = m.x1**2 + m.x2**2
+        def f(x, y):
+            return x**2 + y**2
+        m.f = f
+
+        m.pw = PiecewiseLinearFunction(points=[(0, 1), (0, 4), (0, 7),
+                                               (3, 1), (3, 4), (3, 7)],
+                                       function=m.f)
+        m.c = Constraint(expr=m.pw(m.x1, m.x2) <= 5)
+
+
+# def alternate_thing():
+#     def f(x, y):
+#         return x**2 + y**2
+
+#     m.pw = PiecewiseLinearFunction(points=[...],
+#                                    function=f,
+#                                    #nargs=2,
+#     )
+#     m.c = Constraint(expr=m.pw(m.x, m.y) <= 5)
+
+#     # 
+
+#     PLF(points, function)
+#     PLF(simplices, function?)
+#     PLF(simplices, linear_expression_list)
+#     PLF(table_data) ... {point: val}
