72a3cfeb5da60d8fdf1b8fbc1df26c93884aba16
Robert Parker <robbybparker@gmail.com>
2023-01-28 21:31:58 -0700

    experiment with callback

--- pyomo/contrib/pynumero/algorithms/solvers/callbacks.py
+++ pyomo/contrib/pynumero/algorithms/solvers/callbacks.py
@@ -9,6 +9,8 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
+import numpy as np
+
 class CyIpoptCallback(object):
 
     def __init__(
@@ -22,6 +24,10 @@ class CyIpoptCallback(object):
         self._n_residuals = n_residuals
 
         self._callback_count = 0
+        self._prev_grad_obj = None
+        self._prev_con_resids = None
+        self._prev_jac_eq = None
+        # prev_primals?
 
     def __call__(
         self,
@@ -39,6 +45,11 @@ class CyIpoptCallback(object):
         ls_trials,
     ):
         self._callback_count += 1
+        if iter_count == 0:
+            self._prev_grad_obj = nlp.evaluate_grad_objective()
+            self._prev_con_resids = nlp.evaluate_constraints()
+            self._prev_jac_eq = nlp.evaluate_jacobian_eq()
+            return
         tab = "    "
         #if (
         #    inf_pr > self._primal_inf_threshold
@@ -47,37 +58,61 @@ class CyIpoptCallback(object):
         #    print()
         #else:
         #    return
-        print()
         print(
-            "%sCallback %s: inf_pr = %s, inf_du = %s"
-            % (tab, self._callback_count, inf_pr, inf_du)
+            "%sCallback at iter %s: inf_pr = %s, inf_du = %s"
+            % (tab, iter_count, inf_pr, inf_du)
         )
-        if inf_pr > self._primal_inf_threshold:
-            constraints = nlp.get_pyomo_constraints()
-            residuals = nlp.evaluate_constraints()
-            con_resids = list(zip(constraints, residuals))
-            sorted_resids = sorted(con_resids, key=lambda item: abs(item[1]), reverse=True)
-            top_n_resids = sorted_resids[:self._n_residuals]
-            print("%s  %s largest primal residuals:" % (tab, self._n_residuals))
-            for con, val in top_n_resids:
-                print("%s    %s: %s" % (tab, con.name, val))
+        #if inf_pr > self._primal_inf_threshold:
+        constraints = nlp.get_pyomo_constraints()
+        residuals = self._prev_con_resids
+        con_resids = list(zip(constraints, residuals))
+        sorted_resids = sorted(con_resids, key=lambda item: abs(item[1]), reverse=True)
+        top_n_resids = sorted_resids[:self._n_residuals]
+        print("%s  %s largest primal residuals:" % (tab, self._n_residuals))
+        for con, val in top_n_resids:
+            print("%s    %s: %s" % (tab, con.name, val))
 
-        grad_lag = get_gradient_lagrangian(nlp)
+        #grad_lag = get_gradient_lagrangian(nlp)
+        duals_eq = nlp.get_duals_eq()
+        grad_lag = (
+            self._prev_grad_obj
+            + self._prev_jac_eq.transpose()*duals_eq
+        )
         #if inf_du > self._dual_inf_threshold:
         variables = nlp.get_pyomo_variables()
         var_resids = list(zip(variables, grad_lag))
         sorted_resids = sorted(var_resids, key=lambda item: abs(item[1]), reverse=True)
         top_n_resids = sorted_resids[:self._n_residuals]
         print("%s  %s largest dual residuals:" % (tab, self._n_residuals))
-        print("   ", max(nlp.get_duals_eq()))
+        print("   ", max(abs(nlp.get_duals_eq())))
         for var, val in top_n_resids:
             print("%s    %s: %s" % (tab, var.name, val))
 
+        #if self._prev_duals_eq is not None:
+        #    grad_lag = get_gradient_lagrangian(nlp, duals_eq=self._prev_duals_eq)
+        #    var_resids = list(zip(variables, np.abs(grad_lag)))
+        #    sorted_resids = sorted(var_resids, key=lambda item: abs(item[1]), reverse=True)
+        #    top_n_resids = sorted_resids[:self._n_residuals]
+        #    print("%s  %s largest dual residuals (with previous duals):" % (tab, self._n_residuals))
+        #    print("   ", max(abs(nlp.get_duals_eq())))
+        #    for var, val in top_n_resids:
+        #        print("%s    %s: %s" % (tab, var.name, val))
 
-def get_gradient_lagrangian(nlp):
+        self._prev_grad_obj = nlp.evaluate_grad_objective()
+        self._prev_con_resids = nlp.evaluate_constraints()
+        self._prev_jac_eq = nlp.evaluate_jacobian_eq()
+        print()
+
+
+def get_gradient_lagrangian(nlp, duals_eq=None):
+    # Could primals and duals be getting out of sync here?
+    # Primals are "correct" in the sense that they produce the inf_pr that
+    # I expect. But duals are not necessarily coming from the same iteration.
     grad_obj = nlp.evaluate_grad_objective()
     jac_eq = nlp.evaluate_jacobian_eq()
-    duals_eq = nlp.get_duals_eq()
+
+    if duals_eq is None:
+        duals_eq = nlp.get_duals_eq()
 
     grad_lag = (
         grad_obj
