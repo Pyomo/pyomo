2e9f92f1aba6f67b656792ceb717bdbe3b1ddd48
Robert Parker <robbybparker@gmail.com>
2023-01-31 15:01:48 -0700

    update maximum_matching to accept a bipartite graph as well as matrix

--- pyomo/contrib/incidence_analysis/matching.py
+++ pyomo/contrib/incidence_analysis/matching.py
@@ -12,7 +12,7 @@
 from pyomo.common.dependencies import networkx as nx
 
 
-def maximum_matching(matrix):
+def maximum_matching(matrix_or_graph, top_nodes=None):
     """
     Returns a maximum matching of the rows and columns of the
     matrix as a dict from row indices to column indices.
@@ -21,8 +21,18 @@ def maximum_matching(matrix):
     nxc = nx.algorithms.components
     from_biadjacency_matrix = nxb.matrix.from_biadjacency_matrix
 
-    M, N = matrix.shape
-    bg = from_biadjacency_matrix(matrix)
+    if isinstance(matrix_or_graph, nx.Graph):
+        M = len(top_nodes)
+        N = len(matrix_or_graph.nodes) - M
+        bg = matrix_or_graph
+    else:
+        # Assume something SciPy-sparse compatible was provided.
+        if top_nodes is not None:
+            raise RuntimeError(
+                "top_nodes argument cannot be used if a matrix is provided"
+            )
+        M, N = matrix_or_graph.shape
+        bg = from_biadjacency_matrix(matrix_or_graph)
 
     # Check assumptions regarding from_biadjacency_matrix function:
     for i in range(M):
@@ -37,6 +47,8 @@ def maximum_matching(matrix):
     # This is fine, but we need to separate into connected components
     # for NetworkX to not complain.
     conn_comp = [bg.subgraph(c) for c in nxc.connected_components(bg)]
+    # NOTE: We could also provide top_nodes to maximum_matching to avoid
+    # ambiguity.
     matchings = [nxb.maximum_matching(c) for c in conn_comp]
     # If n0 < M, then n1 >= M. n0 is the row index, n1-M is the column index
     max_matching = {n0: n1 - M for m in matchings for n0, n1 in m.items() if n0 < M}
