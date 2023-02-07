bbc492a8f88a09868ba24ef99b8550cfb700d563
Miranda Mundt <mmundt@sandia.gov>
2023-02-07 10:55:31 -0700

    Apply black to pyomo/version directory py files

--- pyomo/version/tests/check.py
+++ pyomo/version/tests/check.py
@@ -15,7 +15,8 @@ try:
     import pyomo
     import pyomo.environ
     import pyomo.core
+
     print("OK")
 except Exception:
     e = sys.exc_info()[1]
-    print("Pyomo package error: "+str(e))
+    print("Pyomo package error: " + str(e))
