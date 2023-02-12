bf8ea0aeb8341fbd01c3bc24ebc38f7ed0210216
jialuw96 <jwang44@nd.edu>
2023-02-12 14:31:33 -0500

    changed measurement input style. used itertools to iterate all index list.

--- pyomo/contrib/doe/measurements.py
+++ pyomo/contrib/doe/measurements.py
@@ -25,8 +25,11 @@
 #  publicly, and to permit other to do so.
 #  ___________________________________________________________________________
 
+import warnings
+import itertools
+
 class Measurements:
-    def __init__(self, self_define_res=None, measurement_index_time=None, variance=None, ind_string='_index_'):
+    def __init__(self, self_define_res=None,  measurement_var=None, variance=None, ind_string='_index_'):
         """
         This class stores information on which algebraic and differential variables in the Pyomo model are considered measurements. 
         This includes the functionality to specify indices for these measurement variables. 
@@ -36,14 +39,21 @@ class Measurements:
 
         Parameters
         ----------
-        measurement_index_time:
-            a ``dict``, keys are measurement variable names, 
-                if there are extra index, for e.g., Var[scenario, extra_index, time]:
-                    values are a dictionary, keys are its extra index, values are its measuring time points. 
-                if there are no extra index, for e.g., Var[scenario, time]:
-                    values are a list of measuring time point.
-            For e.g., for the kinetics illustrative example, it should be {'C':{'CA':[0,1,..], 'CB':[0,2,...]}, 'k':[0,4,..]},
-            so the measurements are C[scenario, 'CA', 0]..., k[scenario, 0]....
+        self_define_res: a ``list`` of ``string``, containing the measurement variable names with indexs, 
+            for e.g. "C['CA', 23, 0]". 
+            If this is defined, no need to define ``measurement_var``, ``extra_idx``, ``time_idx``.
+        measurement_var: if not given self_define_res, this needs to be given to provide measurement names.
+             a ``dict``. Keys are measurement Var name, values are a list of two lists. 
+             The first list contains extra index. 
+                extra_idx: a ``list`` of ``list``. Each list contains an extra index for the measurement variables. 
+                For e.g., [['CA', 'CB', 'CC'], [10, 23, 28]] means there are two extra indexes for measurement variables
+                besides the time index.
+            The second list contains time index. 
+                time_idx: a ``list`` containing timepoints. 
+
+            For e.g., {'C':[['CA', 'CB', 'CC'], [10, 23, 28]], [1,2,3]} is a valid input. The measurement names will be 
+            from "C['CA', 10, 1]" to "C['CB',28,3]".
+
         variance:
             a ``dict``, keys are measurement variable names, values are a dictionary, keys are its extra index,
             values are its variance (a scalar number), values are its variance if there is no extra index for this measurement.
@@ -55,55 +65,74 @@ class Measurements:
         """
         if self_define_res:
             self.measurement_name = self_define_res
+        elif measurement_var:
+            self.measure_varname = measurement_var
+            self._check_names()
+            self._measure_name()
         else:
-            if not measurement_index_time:
-                raise AttributeError("If self-defined response names are not given, measurement time and indexes need to be defined.")
+            raise AttributeError("No measurement names provided. Define either self_define_res or measurement_var.")
 
-    def _model_measure_name(self):
+    def _measure_name(self):
         """Return pyomo string name
         """
-        # store pyomo string name
-        measurement_names = []
-        # loop over measurement name
-        for mname in self.flatten_measure_name:
-            # check if there is extra index
-            if self.ind_string in mname:
-                measure_name = mname.split(self.ind_string)[0]
-                measure_index = mname.split(self.ind_string)[1]
-                for tim in self.flatten_measure_timeset[mname]:
-                    # get the measurement name in the model
-                    measurement_name = measure_name + '[0,' + measure_index + ',' + str(tim) + ']'
-                    measurement_names.append(measurement_name)
-            else:
-                for tim in self.flatten_measure_timeset[mname]:
-                    # get the measurement name in the model
-                    measurement_name = mname + '[0,' + str(tim) + ']'
-                    measurement_names.append(measurement_name)
-        self.model_measure_name = measurement_names
+        names = list(self.measure_varname.keys())
+        name_list = []
+        for n in names:
+            name_data = str(n)
 
-    def check_subset(self,subset, throw_error=True, valid_subset=True):
-        """
-        Check if the subset is correctly defined with right name, index and time.
+            # first flatten all indexes into a list 
+            all_index_list = [] # contains all index lists
+            if self.measure_varname[n][0]:
+                for index_list in self.measure_varname[n][0]:
+                    all_index_list.append(index_list)
+            if self.measure_varname[n][1]:
+                all_index_list.append(self.measure_varname[n][1])
 
-        subset:
-            a ''dict'' where measurement name and index are involved in jacobian calculation
-        throw_error:
-            if the given subset is not a subset of the measurement set, throw error message
+            if not all_index_list: # there is no any index for this measurement 
+                name_list.append(name_data)
+
+            else: # add indexes to the variable name 
+
+                # all idnex list for one variable, such as ["CA", 10, 1]
+                all_index_for_var = list(itertools.product(*all_index_list))
+
+                for lst in all_index_for_var:
+                    name1 = name_data+"["
+                    for i, idx in enumerate(lst):
+                        #if idx is str:
+                        #    name1 += "'" + str(idx)+"'"
+                        #else:
+                        name1 += str(idx)
+
+                        if i==len(lst)-1:
+                            name1 += "]"
+                        else:
+                            name1 += ","
+
+                    name_list.append(name1)
+        print(name_list)
+
+        self.measurement_name = name_list
+        
+
+    def _check_names(self):
         """
-        flatten_subset = subset.flatten_measure_name
-        flatten_timeset = subset.flatten_measure_timeset
-        # loop over subset measurement names
-        for i in flatten_subset:
-            # check if subset measurement names are in the overall measurement names
-            if i not in self.flatten_measure_name:
-                valid_subset = False
-                if throw_error:
-                    raise ValueError('This is not a legal subset of the measurement overall set!')
-            else:
-                # check if subset measurement timepoints are in the overall measurement timepoints
-                for t in flatten_timeset[i]:
-                    if t not in self.flatten_measure_timeset[i]:
-                        valid_subset = False
-                        if throw_error:
-                            raise ValueError('The time of {} is not included as measurements before.'.format(t))
-        return valid_subset
+        Check if the measurement information provided are valid to use. 
+        """
+
+        assert type(self.measure_varname)==dict, "Measurement_var should be a dictionary."
+
+        for name in self.measure_varname:
+            if len(self.measure_varname[name]) != 2: 
+                warnings.warn("There should be two lists for each measurement. This warning indicates a potential modeling error.")
+
+            if self.measure_varname[name][0]:
+                for i in self.measure_varname[name][0]:
+                    print(i)
+                    if type(i) is not list:
+                        warnings.warn("Each extra index should form a seperate list. This warning indicates a potential modeling error.")
+
+            if self.measure_varname[name][1]:
+                for i in self.measure_varname[name][1]:
+                    if type(i) is list:
+                        warnings.warn("Each element in time index should be a number.")
