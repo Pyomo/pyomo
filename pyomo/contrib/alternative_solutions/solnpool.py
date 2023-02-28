15db08e20aceb087d585e55a6ed3ed7d2c8758e4
jlgearh <jlgearh@sandia.gov>
2023-02-27 21:17:26 -0700

    - Completed an initial version of the solnpool wrapper. - Updated the solution.py to create a solution class.

--- pyomo/contrib/alternative_solutions/solnpool.py
+++ pyomo/contrib/alternative_solutions/solnpool.py
@@ -1,33 +1,49 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2022
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
 from pyomo.contrib import appsi
 from pyomo.contrib.alternative_solutions import aos_utils, var_utils, solution
 
-def gurobi_generate_solutions(model, max_solutions=10, rel_opt_gap=0.0, 
-                              abs_opt_gap=0.0, search_mode=2, 
+def gurobi_generate_solutions(model, max_solutions=10, rel_opt_gap=None, 
+                              abs_opt_gap=None, search_mode=2, 
                               round_discrete_vars=True, solver_options={}):
     '''
     Finds alternative optimal solutions for discrete variables using Gurobi's 
     built-in Solution Pool capability. See the Gurobi Solution Pool
-    documentation for additional details. This function uses the Gurobi
-    Auto-Persistent Pyomo Solver interface (appsi).
+    documentation for additional details. This function requires the use of 
+    the Gurobi Auto-Persistent Pyomo Solver interface (appsi).
 
         Parameters
         ----------
         model : ConcreteModel
-            A concrete Pyomo model
+            A concrete Pyomo model.
         max_solutions : int or None
             The maximum number of solutions to generate. None indictes no upper
             limit. Note, using None could lead to a large number of solutions.
             This parameter maps to the PoolSolutions parameter in Gurobi.
-        rel_opt_gap : non-negative float
+        rel_opt_gap : non-negative float or None
             The relative optimality gap for allowable alternative solutions.
+            None implies that there is no limit on the relative optimality gap 
+            (i.e. that any feasible solution can be considered by Gurobi).
             This parameter maps to the PoolGap parameter in Gurobi.
-        abs_opt_gap : non-negative float
+        abs_opt_gap : non-negative float or None
             The absolute optimality gap for allowable alternative solutions.
+            None implies that there is no limit on the absolute optimality gap 
+            (i.e. that any feasible solution can be considered by Gurobi).
             This parameter maps to the PoolGapAbs parameter in Gurobi.
         search_mode : 0, 1, or 2
-            Indicates the mode that is used to generate alternative solutions.
-            Mode 2 should typically be used as it finds the best n solutions. 
-            Mode 0 finds a single optimal solution. Mode 1 will generate n 
+            Indicates the Solution Pool mode that is used to generate 
+            alternative solutions in Gurobi. Mode 2 should typically be used as 
+            it finds the best n solutions. Mode 0 finds a single optimal 
+            solution (i.e. the standard mode in Gurobi). Mode 1 will generate n 
             solutions without providing guarantees on their quality. This 
             parameter maps to the PoolSearchMode in Gurobi.
         round_discrete_vars : boolean
@@ -39,17 +55,19 @@ def gurobi_generate_solutions(model, max_solutions=10, rel_opt_gap=0.0,
         Returns
         -------
         solutions
-            A list of solution dictionaries.
-            [solution]
+            A list of Solution objects.
+            [Solution]
     '''
 
     # Validate inputs
     aos_utils._is_concrete_model(model)
     num_solutions = aos_utils._get_max_solutions(max_solutions)
-    assert isinstance(rel_opt_gap, float) and rel_opt_gap >= 0, \
-                          'rel_opt_gap must be a non-negative float'
-    assert isinstance(abs_opt_gap, float) and abs_opt_gap >= 0, \
-                          'abs_opt_gap must be a non-negative float'
+    assert (isinstance(rel_opt_gap, float) and rel_opt_gap >= 0) or \
+        isinstance(rel_opt_gap, type(None)), \
+            'rel_opt_gap must be a non-negative float or None'
+    assert (isinstance(abs_opt_gap, float) and abs_opt_gap >= 0) or \
+        isinstance(abs_opt_gap, type(None)), \
+            'abs_opt_gap must be a non-negative float or None'
     assert search_mode in [0, 1, 2], 'search_mode must be 0, 1, or 2'
     assert isinstance(round_discrete_vars, bool), \
         'round_discrete_vars must be a Boolean'
@@ -60,8 +78,10 @@ def gurobi_generate_solutions(model, max_solutions=10, rel_opt_gap=0.0,
     opt.set_instance(model)
     opt.set_gurobi_param('PoolSolutions', num_solutions)
     opt.set_gurobi_param('PoolSearchMode', search_mode)
-    opt.set_gurobi_param('PoolGap', rel_opt_gap)
-    opt.set_gurobi_param('PoolGapAbs', abs_opt_gap)
+    if rel_opt_gap is not None:
+        opt.set_gurobi_param('PoolGap', rel_opt_gap)
+    if abs_opt_gap is not None:
+        opt.set_gurobi_param('PoolGapAbs', abs_opt_gap)
     results = opt.solve(model)
     assert results.termination_condition == \
         appsi.base.TerminationCondition.optimal, \
@@ -75,6 +95,6 @@ def gurobi_generate_solutions(model, max_solutions=10, rel_opt_gap=0.0,
     solutions = []
     for i in range(solution_count):
         results.solution_loader.load_vars(solution_number=i)
-        solutions.append(solution.get_model_solution(model, variables))
+        solutions.append(solution.Solution(model, variables))
 
     return solutions
--- pyomo/contrib/alternative_solutions/solution.py
+++ pyomo/contrib/alternative_solutions/solution.py
@@ -1,28 +1,81 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2022
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
 import pyomo.environ as pe
 from pyomo.common.collections import ComponentMap, ComponentSet
 
-def get_model_solution(model, variable_list, ignore_fixed_vars=False, 
-                       round_discrete_vars=True):
-    solution = {}
-    variables = ComponentMap()
-    fixed_vars = ComponentSet()
-    for var in variable_list:
-        if ignore_fixed_vars and var.is_fixed():
-            continue
-        if var.is_continuous() or not round_discrete_vars:
-            variables[var] = pe.value(var)
-        else:
-            variables[var] = round(pe.value(var))
-        if var.is_fixed():
-            fixed_vars.add(var)
+class Solution:
+    """
+    A class to store solutions from a Pyomo model.
+    
+    Attributes
+    ----------
+    variables : ComponentMap
+        A map between Pyomo variable objects and their values for a solution.
+    fixed_vars : ComponentSet
+        The set of Pyomo variables that are fixed in a solution.
+    objectives : ComponentMap
+        A map between Pyomo objective objects and their values for a solution.
+    
+    Methods
+    -------
+    pprint():
+        Prints the solution.
+    """
+    
+    def __init__(self, model, variable_list, ignore_fixed_vars=False, 
+                 round_discrete_vars=True):
+        """
+        Constructs a Pyomo Solution object.
+
+        Parameters
+        ----------
+            model : ConcreteModel
+                A concrete Pyomo model.
+            variables: A collection of Pyomo _GenereralVarData variables
+                The variables for which the solution will be stored.
+            ignore_fixed_vars : boolean
+                Boolean indicating that fixed variables should not be added to 
+                the solution.
+            round_discrete_vars : boolean
+                Boolean indicating that discrete values should be rounded to 
+                the nearest integer in the solutions results.
+        """
+        
+        self.variables = ComponentMap()
+        self.fixed_vars = ComponentSet()
+        for var in variable_list:
+            if ignore_fixed_vars and var.is_fixed():
+                continue
+            if var.is_continuous() or not round_discrete_vars:
+                self.variables[var] = pe.value(var)
+            else:
+                self.variables[var] = round(pe.value(var))
+            if var.is_fixed():
+                self.fixed_vars.add(var)
+                
+        self.objectives = ComponentMap()
+        for obj in model.component_data_objects(pe.Objective, active=True):
+            self.objectives[obj] = pe.value(obj)
             
-    solution["variables"] = variables
-    solution["fixed_variables"] = fixed_vars
-
-    objectives = ComponentMap()
-    for obj in model.component_data_objects(pe.Objective, active=True):
-        objectives[obj] = pe.value(obj)
-    solution["objectives"] = objectives
-
-    return solution
-
+    def pprint(self):
+        '''Print the solution variable and objective values.'''
+        fixed_string = "(fixed)"
+        print("Variable: Value")
+        for variable, value in self.variables.items():
+            if variable in self.fixed_vars:
+                print("{}: {} {}".format(variable.name, value, fixed_string))
+            else:
+                print("{}: {}".format(variable.name, value))
+        print()
+        print("Objective: Value")
+        for objective, value in self.objectives.items():
+            print("{}: {}".format(objective.name, value))
