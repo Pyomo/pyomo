2b880065e7308f06a028089645b7b95407111b4a
Emma Johnson <emma58@users.noreply.github.com>
2023-04-16 13:39:40 -0600

    Fixing a lot of typo bugs, but the algorithm doesn't really exist yet

--- pyomo/gdp/plugins/bound_pretransformation.py
+++ pyomo/gdp/plugins/bound_pretransformation.py
@@ -9,8 +9,12 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
+from pyomo.common.collections import ComponentMap
 from pyomo.common.config import ConfigDict, ConfigValue
-from pyomo.core import Block, Constraint, SortComponents
+from pyomo.common.modeling import unique_component_name
+from pyomo.core import (
+    Block, Constraint, NonNegativeIntegers, SortComponents, value, Var
+)
 from pyomo.core.base import Transformation, TransformationFactory
 from pyomo.core.util import target_list
 from pyomo.gdp import Disjunct, Disjunction
@@ -32,6 +36,15 @@ class CommonLHSTransformation(Transformation):
     Implements the special transformation mentioned in [1], [2], and [3] for
     handling disjunctive constraints with common left-hand sides (i.e., 
     Constraint bodies).
+
+    TODO: example
+
+    NOTE: Because this transformation allows tighter bound values higher in
+    the GDP hierarchy to supercede looser ones that are lower, the transformed
+    model will not necessarily still be valid in the case that there are 
+    mutable Params in disjunctive variable bounds or in constraints setting
+    bounds or values for exactly one variable when those mutable Param values
+    are changed.
 
     [1] Egon Balas, "On the convex hull of the union of certain polyhedra,"
         Operations Research Letters, vol. 7, 1988, pp. 279-283
@@ -75,8 +88,9 @@ class CommonLHSTransformation(Transformation):
         if targets is None:
             targets = (instance,)
 
+        transformation_blocks = {}
         for t in targets:
-            gdp_forest = get_gdp_tree(t, instance)
+            gdp_forest = get_gdp_tree([t,], instance)
             # we have to go from leaf to root because we pass bound information
             # upwards--the innermost disjuncts should restrict it the most. If
             # that's not true, they're useless, and if there are contradictions,
@@ -84,24 +98,29 @@ class CommonLHSTransformation(Transformation):
             bound_dict = ComponentMap()
             for d in gdp_forest.reverse_topological_sort():
                 if d.ctype is Disjunct:
+                    # TODO: This is a mess because you're overwriting for all
+                    # the Disjuncts in a Disjunction. Need to keep a dict for
+                    # each root Disjunct, at least, but I need to think about it
+                    # a little more
                     self._update_bounds_from_constraints(d, bound_dict)
-                elif d.ctype is Disjunction and gdp_tree.parent(d) is None:
+                elif d.ctype is Disjunction and gdp_forest.parent(d) is None:
                     # we're at a root, finish the transformation
-                    self._transform_disjunction(bound_dict)
+                    self._transform_disjunction(d, bound_dict,
+                                                transformation_blocks)
 
     def _update_bounds_from_constraints(self, disjunct, bound_dict):
         for constraint in disjunct.component_data_objects(
                 Constraint,
                 active=True,
                 descend_into=Block,
-                order=SortComponents.deterministic):
+                sort=SortComponents.deterministic):
             if (hasattr(constraint.body, 'ctype')
                 and constraint.body.ctype is Var):
                 v = constraint.body
                 # Then this is a bound or an equality 
                 current_bounds = bound_dict.get(v)
                 if current_bounds is None:
-                    # TODO: These need to be values, I think. We can guarantee
+                    # TODO: These need to be values, I think. We can't guarantee
                     # this is right when mutable stuff changes...
                     bound_dict[v] = (constraint.lower, constraint.upper,
                                      disjunct.binary_indicator_var,
@@ -114,25 +133,33 @@ class CommonLHSTransformation(Transformation):
     def _update_bounds_dict(self, bound_dict, variable, cons, indicator_var,
                             current_bounds):
         (lb, ub, lb_indicator_var, ub_indicator_var) = current_bounds
-        if value(cons.lower) <= lb:
-            # This GDP is more constrained higher in the tree. This is a bit
-            # surprising since it means the descendent constraint is useless,
-            # but we just replace what we will put in the final constraint:
-            lb = cons.lower
-            lb_indicator_var = indicator_var
-        if value(cons.upper) <= ub:
-            # Same surprise as above in the UB
-            ub = current_ub
-            ub_indicator_var = indicator_var
+        lower = value(cons.lower)
+        if lower is not None:
+            if lb is None or (lb is not None and lower <= lb):
+                # This GDP is more constrained higher in the tree. If lb wasn't
+                # None, this is a bit surprising since it means the descendent
+                # constraint is useless, but we just replace what we will put in
+                # the final constraint. If lb was None, this is just the first
+                # time we've found a real bound.
+                lb = cons.lower
+                lb_indicator_var = indicator_var
+        upper = value(cons.upper)
+        if upper is not None:
+            if ub is None or (ub is not None and upper <= ub):
+                # Same case as above in the UB
+                ub = cons.upper
+                ub_indicator_var = indicator_var
         # In all other cases, there is nothing to do... The lower gives more
         # information and all the logical stuff will be filled in later by a
         # "full" transformation to MIP.
-        bounds_dict[v] = (lb, ub, lb_indicator_var, ub_indicator_var)
+        bound_dict[variable] = (lb, ub, lb_indicator_var, ub_indicator_var)
 
     def _transform_disjunction(self, disjunction, bound_dict,
                                transformation_blocks):
         trans_block = self._add_transformation_block(disjunction,
                                                      transformation_blocks)
+        for v, (lb, ub, lb_indicator_var, ub_indicator_var) in bound_dict.items():
+            set_trace()
 
     def _add_transformation_block(self, disjunction, transformation_blocks):
         to_block = disjunction.parent_block()
