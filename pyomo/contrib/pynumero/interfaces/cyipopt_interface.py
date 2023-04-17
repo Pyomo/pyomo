eba9b399965708933a555d5367c4d9435dd76c4f
Robert Parker <robbybparker@gmail.com>
2023-04-17 17:21:48 -0600

    fix indentation

--- pyomo/contrib/pynumero/interfaces/cyipopt_interface.py
+++ pyomo/contrib/pynumero/interfaces/cyipopt_interface.py
@@ -330,18 +330,18 @@ class CyIpoptNLP(CyIpoptProblemInterface):
         return data
 
     def intermediate(
-            self,
-            alg_mod,
-            iter_count,
-            obj_value,
-            inf_pr,
-            inf_du,
-            mu,
-            d_norm,
-            regularization_size,
-            alpha_du,
-            alpha_pr,
-            ls_trials
+        self,
+        alg_mod,
+        iter_count,
+        obj_value,
+        inf_pr,
+        inf_du,
+        mu,
+        d_norm,
+        regularization_size,
+        alpha_du,
+        alpha_pr,
+        ls_trials
     ):
         if self._intermediate_callback is not None:
             return self._intermediate_callback(
