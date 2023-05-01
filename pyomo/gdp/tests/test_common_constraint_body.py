aafba1610661e428f58d723047b2d5fc6b9013eb
Emma Johnson <emma58@users.noreply.github.com>
2023-05-01 12:48:47 -0600

    Adding tests for when we have to 'partially deactivate' constraints--that is, we transform the upper or lower part, but not both

--- pyomo/gdp/tests/test_common_constraint_body.py
+++ pyomo/gdp/tests/test_common_constraint_body.py
@@ -218,3 +218,87 @@ class TestCommonConstraintBodyTransformation(unittest.TestCase):
             Constraint,
             active=True,
             descend_into=(Block, Disjunct)))), 4)
+
+    def test_partially_deactivating_constraint_lb_transformed_but_ub_not(self):
+        m = ConcreteModel()
+        m.x = Var()
+        m.x.setlb(3.0)
+        m.d1 = Disjunct()
+        m.d1.c = Constraint(expr=(4.5, m.x, 6))
+        m.d2 = Disjunct()
+        m.d2.c = Constraint(expr=5.6 <= m.x)
+        m.disj = Disjunction(expr=[m.d1, m.d2])
+        
+        bt = TransformationFactory('gdp.common_constraint_body')
+        bt.apply_to(m)
+
+        # We expect: 4.5*y_1 + 5.6*y_2 <= x
+
+        cons = bt.get_transformed_constraints(m.x, m.disj)
+        self.assertEqual(len(cons), 1)
+        lb = cons[0]
+        assertExpressionsEqual(
+            self,
+            lb.expr,
+            4.5*m.d1.binary_indicator_var +
+            5.6*m.d2.binary_indicator_var <= m.x
+        )
+
+        self.assertFalse(m.d1.c.active)
+        self.assertFalse(m.d2.c.active)
+        c_ub = m.d1.component('c_ub')
+        self.assertIsInstance(c_ub, Constraint)
+        self.assertTrue(c_ub.active)
+        assertExpressionsEqual(
+            self,
+            c_ub.expr,
+            m.x <= 6.0
+        )
+
+        self.assertEqual(len(list(m.component_data_objects(
+            Constraint,
+            active=True,
+            descend_into=(Block, Disjunct)))), 2)
+
+    def test_partially_deactivating_constraint_ub_transformed_but_lb_not(self):
+        m = ConcreteModel()
+        m.w = Var()
+        m.d = Disjunct([1, 2, 3])
+        m.disjunction = Disjunction(expr=[m.d[1], m.d[2], m.d[3]])
+
+        m.d[1].c = Constraint(expr=m.w == 45)
+        m.d[2].c = Constraint(expr=m.w <= 36)
+        m.d[3].c = Constraint(expr=m.w <= 232)
+
+        bt = TransformationFactory('gdp.common_constraint_body')
+        bt.apply_to(m)
+
+        # We expect: 45*y_1 + 36y_2 + 232y_3 >= w
+
+        cons = bt.get_transformed_constraints(m.w, m.disjunction)
+        self.assertEqual(len(cons), 1)
+        ub = cons[0]
+        assertExpressionsEqual(
+            self,
+            ub.expr,
+            45.0*m.d[1].binary_indicator_var +
+            36.0*m.d[2].binary_indicator_var +
+            232.0*m.d[3].binary_indicator_var >= m.w
+        )
+
+        self.assertFalse(m.d[1].c.active)
+        self.assertFalse(m.d[2].c.active)
+        self.assertFalse(m.d[3].c.active)
+        c_lb = m.d[1].component('c_lb')
+        self.assertIsInstance(c_lb, Constraint)
+        self.assertTrue(c_lb.active)
+        assertExpressionsEqual(
+            self,
+            c_lb.expr,
+            m.w >= 45.0
+        )
+
+        self.assertEqual(len(list(m.component_data_objects(
+            Constraint,
+            active=True,
+            descend_into=(Block, Disjunct)))), 2)
