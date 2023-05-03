a2fd119b5bd08817d98de71445dfb8f67f47b259
John Siirola <jsiirola@users.noreply.github.com>
2023-05-02 19:36:09 -0600

    Deactivate Disjunct/Disjunctions so writers don't complain (in feasibility test)

--- pyomo/contrib/preprocessing/plugins/induced_linearity.py
+++ pyomo/contrib/preprocessing/plugins/induced_linearity.py
@@ -44,7 +44,7 @@ from pyomo.core import (
     value,
 )
 from pyomo.core.plugins.transform.hierarchy import IsomorphicTransformation
-from pyomo.gdp import Disjunct
+from pyomo.gdp import Disjunct, Disjunction
 from pyomo.opt import TerminationCondition as tc
 from pyomo.opt import SolverFactory
 from pyomo.repn import generate_standard_repn
@@ -219,6 +219,15 @@ def prune_possible_values(block_scope, possible_values, config):
         disj = tmp_clone_blk._tmp_block_scope[0]
         disj.indicator_var.fix(1)
         TransformationFactory('gdp.bigm').apply_to(model)
+    # FIXME: this whole transformation should be reworked to solve
+    # feasibility checks on independent models (using References) and
+    # NOT have to clone the model / deactivate components in place.
+    #
+    # Deactivate any Disjuncts / Disjunctions (so the writers don't complain)
+    for d in model.component_data_objects(Disjunction):
+        d.deactivate()
+    for d in model.component_data_objects(Disjunct):
+        d._deactivate_without_fixing_indicator()
     tmp_clone_blk.test_feasible = Constraint()
     tmp_clone_blk._obj = Objective(expr=1)
     for eff_discr_var, vals in tmp_clone_blk._possible_values.items():
