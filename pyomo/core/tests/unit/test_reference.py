c9c012980588c97781d78a25c0fcddf302fc4ff7
John Siirola <jsiirola@users.noreply.github.com>
2023-05-14 15:36:20 -0600

    Test ordered iteration of references

--- pyomo/core/tests/unit/test_reference.py
+++ pyomo/core/tests/unit/test_reference.py
@@ -159,6 +159,114 @@ class TestReferenceDict(unittest.TestCase):
             ],
         )
 
+    def test_ordered_iterators(self):
+        m = ConcreteModel()
+        m.I = Set(initialize=[3, 2])
+        m.b = Block([1, 0])
+        m.b[1].x = Var(m.I)
+        m.b[0].x = Var(m.I)
+        m.y = Reference(m.b[:].x[:])
+        self.assertEqual(list(m.y.index_set().subsets()), [m.b.index_set(), m.I])
+        self.assertEqual(list(m.y.keys()), [(1, 3), (1, 2), (0, 3), (0, 2)])
+        self.assertEqual(
+            list(m.y.values()), [m.b[1].x[3], m.b[1].x[2], m.b[0].x[3], m.b[0].x[2]]
+        )
+        self.assertEqual(
+            list(m.y.items()),
+            [
+                ((1, 3), m.b[1].x[3]),
+                ((1, 2), m.b[1].x[2]),
+                ((0, 3), m.b[0].x[3]),
+                ((0, 2), m.b[0].x[2]),
+            ],
+        )
+        self.assertEqual(list(m.y.keys(True)), [(0, 2), (0, 3), (1, 2), (1, 3)])
+        self.assertEqual(
+            list(m.y.values(True)), [m.b[0].x[2], m.b[0].x[3], m.b[1].x[2], m.b[1].x[3]]
+        )
+        self.assertEqual(
+            list(m.y.items(True)),
+            [
+                ((0, 2), m.b[0].x[2]),
+                ((0, 3), m.b[0].x[3]),
+                ((1, 2), m.b[1].x[2]),
+                ((1, 3), m.b[1].x[3]),
+            ],
+        )
+
+        m = ConcreteModel()
+        m.b = Block([1, 0])
+        m.b[1].x = Var([3, 2])
+        m.b[0].x = Var([5, 4])
+        m.y = Reference(m.b[:].x[:])
+        self.assertIs(type(m.y.index_set()), FiniteSetOf)
+        self.assertEqual(list(m.y.keys()), [(1, 3), (1, 2), (0, 5), (0, 4)])
+        self.assertEqual(
+            list(m.y.values()), [m.b[1].x[3], m.b[1].x[2], m.b[0].x[5], m.b[0].x[4]]
+        )
+        self.assertEqual(
+            list(m.y.items()),
+            [
+                ((1, 3), m.b[1].x[3]),
+                ((1, 2), m.b[1].x[2]),
+                ((0, 5), m.b[0].x[5]),
+                ((0, 4), m.b[0].x[4]),
+            ],
+        )
+        self.assertEqual(list(m.y.keys(True)), [(0, 4), (0, 5), (1, 2), (1, 3)])
+        self.assertEqual(
+            list(m.y.values(True)), [m.b[0].x[4], m.b[0].x[5], m.b[1].x[2], m.b[1].x[3]]
+        )
+        self.assertEqual(
+            list(m.y.items(True)),
+            [
+                ((0, 4), m.b[0].x[4]),
+                ((0, 5), m.b[0].x[5]),
+                ((1, 2), m.b[1].x[2]),
+                ((1, 3), m.b[1].x[3]),
+            ],
+        )
+
+        m = ConcreteModel()
+        m.b = Block([1, 0])
+        m.b[1].x = Var([3, 2])
+        m.b[0].x = Var([5, 4])
+        m.y = Reference(
+            {
+                (1, 3): m.b[1].x[3],
+                (0, 5): m.b[0].x[5],
+                (1, 2): m.b[1].x[2],
+                (0, 4): m.b[0].x[4],
+            }
+        )
+        self.assertIs(type(m.y.index_set()), FiniteSetOf)
+        self.assertEqual(list(m.y.keys()), [(1, 3), (0, 5), (1, 2), (0, 4)])
+        self.assertEqual(
+            list(m.y.values()), [m.b[1].x[3], m.b[0].x[5], m.b[1].x[2], m.b[0].x[4]]
+        )
+        self.assertEqual(
+            list(m.y.items()),
+            [
+                ((1, 3), m.b[1].x[3]),
+                ((0, 5), m.b[0].x[5]),
+                ((1, 2), m.b[1].x[2]),
+                ((0, 4), m.b[0].x[4]),
+            ],
+        )
+        self.assertEqual(list(m.y.keys(True)), [(0, 4), (0, 5), (1, 2), (1, 3)])
+        self.assertEqual(
+            list(m.y.values(True)), [m.b[0].x[4], m.b[0].x[5], m.b[1].x[2], m.b[1].x[3]]
+        )
+        self.assertEqual(
+            list(m.y.items(True)),
+            [
+                ((0, 4), m.b[0].x[4]),
+                ((0, 5), m.b[0].x[5]),
+                ((1, 2), m.b[1].x[2]),
+                ((1, 3), m.b[1].x[3]),
+            ],
+        )
+
     def test_nested_assignment(self):
         m = self.m
 
