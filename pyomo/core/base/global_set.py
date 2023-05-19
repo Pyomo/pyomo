37d00072f39949bd9a9a37cdb42bc59bb7a9ca2c
John Siirola <jsiirola@users.noreply.github.com>
2023-05-18 23:46:45 -0600

    Flushing out (and testing) more of the UnindexedComponent_set API

--- pyomo/core/base/global_set.py
+++ pyomo/core/base/global_set.py
@@ -10,6 +10,7 @@
 #  ___________________________________________________________________________
 
 from pyomo.core.pyomoobject import PyomoObject
+from pyomo.core.base.range import NonNumericRange
 
 GlobalSets = {}
 
@@ -94,11 +95,23 @@ class _UnindexedComponent_set(GlobalSetBase):
     def sorted_iter(self):
         return iter(self)
 
+    def data(self):
+        return tuple(self)
+
+    def ordered_data(self):
+        return tuple(self)
+
+    def sorted_data(self):
+        return tuple(self)
+
     def subsets(self, expand_all_set_operators=None):
         return [self]
 
     def construct(self):
         pass
+
+    def ranges(self):
+        yield NonNumericRange(None)
 
     def bounds(self):
         return (None, None)
@@ -128,6 +141,44 @@ class _UnindexedComponent_set(GlobalSetBase):
     def isordered(self):
         # As this set only has a single element, it is implicitly "ordered"
         return True
+
+    def at(self, index):
+        if index == 1:
+            return None
+        raise IndexError("%s index out of range" % (self.name,))
+
+    def ord(self, item):
+        if item is None:
+            return 1
+        raise IndexError(
+            "Cannot identify position of %s in Set %s: item not in Set"
+            % (item, self.name)
+        )
+
+    def first(self):
+        return None
+
+    def last(self):
+        return None
+
+    def next(self, item, step=1):
+        # make sure that item is None (and raise the standard error if it is not)
+        self.ord(item)
+        if step < 0:
+            raise IndexError("Cannot advance before the beginning of the Set")
+        else:
+            raise IndexError("Cannot advance past the end of the Set")
+
+    def nextw(self, item, step=1):
+        # make sure that item is None (and raise the standard error if it is not)
+        self.ord(item)
+        return None
+
+    def prev(self, item, step=1):
+        return self.next(item, -step)
+
+    def prevw(self, item, step=1):
+        return self.nextw(item, -step)
 
 
 UnindexedComponent_set = _UnindexedComponent_set('UnindexedComponent_set')
