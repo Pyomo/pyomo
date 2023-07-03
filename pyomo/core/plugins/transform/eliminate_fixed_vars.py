fa3aca8420e36ede1dc1257f7c88c865d5240b70
John Siirola <jsiirola@users.noreply.github.com>
2023-07-03 16:40:09 -0600

    Track move of pyomo.core.util module

--- pyomo/core/plugins/transform/eliminate_fixed_vars.py
+++ pyomo/core/plugins/transform/eliminate_fixed_vars.py
@@ -13,7 +13,7 @@ from pyomo.core.expr.current import ExpressionBase
 from pyomo.core.expr.numvalue import as_numeric
 from pyomo.core import Constraint, Objective, TransformationFactory
 from pyomo.core.base.var import Var, _VarData
-from pyomo.core.base.util import sequence
+from pyomo.core.util import sequence
 from pyomo.core.plugins.transform.hierarchy import IsomorphicTransformation
 
 
