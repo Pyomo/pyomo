ec5c83b1e8c9389f737a10f2d7c6879bc86c50e2
Robert Parker <robbybparker@gmail.com>
2023-07-07 17:25:36 -0600

    add blank line for black

--- pyomo/contrib/pynumero/exceptions.py
+++ pyomo/contrib/pynumero/exceptions.py
@@ -9,10 +9,12 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
+
 class PyNumeroEvaluationError(ArithmeticError):
     """An exception to be raised by PyNumero evaluation backends in the event
     of a failed function evaluation. This should be caught by solver interfaces
     and translated to the solver-specific evaluation error API.
 
     """
+
     pass
