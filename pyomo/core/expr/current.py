019213869b19345c9e3ccbbcf5a0d482a941bf0b
John Siirola <jsiirola@users.noreply.github.com>
2023-07-13 23:52:02 -0600

    deprecate pyomo.core.expr.current

--- pyomo/core/expr/current.py
+++ pyomo/core/expr/current.py
@@ -12,6 +12,13 @@
 import enum
 import math
 
+from pyomo.common.deprecation import deprecation_warning
+deprecation_warning(
+    "pyomo.common.expr.current is deprecated.  "
+    "Please import expression symbols from pyomo.core.expr",
+    version='6.6.2.dev0'
+)
+
 #
 # Common intrinsic functions
 #
