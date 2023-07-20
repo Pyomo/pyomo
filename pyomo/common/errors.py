d4ab77fb2a947b1ccae84efc49937672265d760d
John Siirola <jsiirola@users.noreply.github.com>
2023-07-20 07:31:29 -0600

    NFC: fix grammar

--- pyomo/common/errors.py
+++ pyomo/common/errors.py
@@ -128,9 +128,9 @@ class IterationLimitError(PyomoException, RuntimeError):
     """A subclass of :py:class:`RuntimeError`, raised by an iterative method
     when the iteration limit is reached.
 
-    TODO: currently solvers currently do not raise this exception, but
-    probably should (at least when non-normal termination conditions are
-    mapped to exceptions)
+    TODO: solvers currently do not raise this exception, but probably
+    should (at least when non-normal termination conditions are mapped
+    to exceptions)
 
     """
 
