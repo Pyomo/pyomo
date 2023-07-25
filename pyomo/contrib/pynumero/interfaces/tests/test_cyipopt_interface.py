ddc60d76881c31807929def48eda33ebc715f554
Robert Parker <robbybparker@gmail.com>
2023-07-25 13:14:00 -0600

    test raising CyIpoptEvaluationError from CyIpoptNLP

--- pyomo/contrib/pynumero/interfaces/tests/test_cyipopt_interface.py
+++ pyomo/contrib/pynumero/interfaces/tests/test_cyipopt_interface.py
@@ -10,6 +10,7 @@
 #  ___________________________________________________________________________
 
 import pyomo.common.unittest as unittest
+import pyomo.environ as pyo
 
 from pyomo.contrib.pynumero.dependencies import (
     numpy as np,
@@ -25,13 +26,17 @@ from pyomo.contrib.pynumero.asl import AmplInterface
 if not AmplInterface.available():
     raise unittest.SkipTest("Pynumero needs the ASL extension to run CyIpopt tests")
 
+from pyomo.contrib.pynumero.interfaces.pyomo_nlp import PyomoNLP
 from pyomo.contrib.pynumero.interfaces.cyipopt_interface import (
     cyipopt_available,
     CyIpoptProblemInterface,
+    CyIpoptNLP,
 )
 
 if not cyipopt_available:
     raise unittest.SkipTest("CyIpopt is not available")
+
+import cyipopt
 
 
 class TestSubclassCyIpoptInterface(unittest.TestCase):
@@ -86,6 +91,50 @@ class TestSubclassCyIpoptInterface(unittest.TestCase):
         msg = "__init__ has not been called"
         with self.assertRaisesRegex(RuntimeError, msg):
             problem.solve(x0)
+
+
+class TestCyIpoptEvaluationErrors(unittest.TestCase):
+    def _get_model_nlp_interface(self):
+        m = pyo.ConcreteModel()
+        m.x = pyo.Var([1, 2, 3], initialize=1.0)
+        m.obj = pyo.Objective(expr=m.x[1] * pyo.sqrt(m.x[2]) + m.x[1] * m.x[3])
+        m.eq1 = pyo.Constraint(expr=m.x[1] * pyo.sqrt(m.x[2]) == 1.0)
+        nlp = PyomoNLP(m)
+        interface = CyIpoptNLP(nlp)
+        bad_primals = np.array([1.0, -2.0, 3.0])
+        indices = nlp.get_primal_indices([m.x[1], m.x[2], m.x[3]])
+        bad_primals = bad_primals[indices]
+        return m, nlp, interface, bad_primals
+
+    def test_error_in_objective(self):
+        m, nlp, interface, bad_x = self._get_model_nlp_interface()
+        msg = "Error in objective function"
+        with self.assertRaisesRegex(cyipopt.CyIpoptEvaluationError, msg):
+            interface.objective(bad_x)
+
+    def test_error_in_gradient(self):
+        m, nlp, interface, bad_x = self._get_model_nlp_interface()
+        msg = "Error in objective gradient"
+        with self.assertRaisesRegex(cyipopt.CyIpoptEvaluationError, msg):
+            interface.gradient(bad_x)
+
+    def test_error_in_constraints(self):
+        m, nlp, interface, bad_x = self._get_model_nlp_interface()
+        msg = "Error in constraint evaluation"
+        with self.assertRaisesRegex(cyipopt.CyIpoptEvaluationError, msg):
+            interface.constraints(bad_x)
+
+    def test_error_in_jacobian(self):
+        m, nlp, interface, bad_x = self._get_model_nlp_interface()
+        msg = "Error in constraint Jacobian"
+        with self.assertRaisesRegex(cyipopt.CyIpoptEvaluationError, msg):
+            interface.jacobian(bad_x)
+
+    def test_error_in_hessian(self):
+        m, nlp, interface, bad_x = self._get_model_nlp_interface()
+        msg = "Error in Lagrangian Hessian"
+        with self.assertRaisesRegex(cyipopt.CyIpoptEvaluationError, msg):
+            interface.hessian(bad_x, [1.0], 0.0)
 
 
 if __name__ == "__main__":
