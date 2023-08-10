ee778cfb4ea7543af0121b80a4eff8b715197f32
ZedongPeng <peng_zedong@126.com>
2023-08-10 13:36:11 -0400

    fix bug

--- pyomo/contrib/mindtpy/algorithm_base_class.py
+++ pyomo/contrib/mindtpy/algorithm_base_class.py
@@ -2746,60 +2746,61 @@ class _MindtPyAlgorithm(object):
 
     def handle_main_mip_termination(self, main_mip, main_mip_results):
         should_terminate = False
-        if main_mip_results is not None and not self.config.single_tree:
-            if main_mip_results.solver.termination_condition is tc.optimal:
-                self.handle_main_optimal(main_mip)
-            elif main_mip_results.solver.termination_condition is tc.infeasible:
-                self.handle_main_infeasible()
-                self.last_iter_cuts = True
-                should_terminate = True
-            elif main_mip_results.solver.termination_condition is tc.unbounded:
-                temp_results = self.handle_main_unbounded(main_mip)
-            elif main_mip_results.solver.termination_condition is tc.infeasibleOrUnbounded:
-                temp_results = self.handle_main_unbounded(main_mip)
-                if temp_results.solver.termination_condition is tc.infeasible:
+        if main_mip_results is not None:
+            if not self.config.single_tree:
+                if main_mip_results.solver.termination_condition is tc.optimal:
+                    self.handle_main_optimal(main_mip)
+                elif main_mip_results.solver.termination_condition is tc.infeasible:
                     self.handle_main_infeasible()
-            elif main_mip_results.solver.termination_condition is tc.maxTimeLimit:
-                self.handle_main_max_timelimit(main_mip, main_mip_results)
-                self.results.solver.termination_condition = tc.maxTimeLimit
-            elif main_mip_results.solver.termination_condition is tc.feasible or (
-                main_mip_results.solver.termination_condition is tc.other
-                and main_mip_results.solution.status is SolutionStatus.feasible
-            ):
-                # load the solution and suppress the warning message by setting
-                # solver status to ok.
-                MindtPy = main_mip.MindtPy_utils
-                self.config.logger.info(
-                    'MILP solver reported feasible solution, '
-                    'but not guaranteed to be optimal.'
-                )
-                copy_var_list_values(
-                    main_mip.MindtPy_utils.variable_list,
-                    self.fixed_nlp.MindtPy_utils.variable_list,
-                    self.config,
-                    skip_fixed=False,
-                )
-                self.update_suboptimal_dual_bound(main_mip_results)
-                self.config.logger.info(
-                    self.log_formatter.format(
-                        self.mip_iter,
-                        'MILP',
-                        value(MindtPy.mip_obj.expr),
-                        self.primal_bound,
-                        self.dual_bound,
-                        self.rel_gap,
-                        get_main_elapsed_time(self.timing),
+                    self.last_iter_cuts = True
+                    should_terminate = True
+                elif main_mip_results.solver.termination_condition is tc.unbounded:
+                    temp_results = self.handle_main_unbounded(main_mip)
+                elif main_mip_results.solver.termination_condition is tc.infeasibleOrUnbounded:
+                    temp_results = self.handle_main_unbounded(main_mip)
+                    if temp_results.solver.termination_condition is tc.infeasible:
+                        self.handle_main_infeasible()
+                elif main_mip_results.solver.termination_condition is tc.maxTimeLimit:
+                    self.handle_main_max_timelimit(main_mip, main_mip_results)
+                    self.results.solver.termination_condition = tc.maxTimeLimit
+                elif main_mip_results.solver.termination_condition is tc.feasible or (
+                    main_mip_results.solver.termination_condition is tc.other
+                    and main_mip_results.solution.status is SolutionStatus.feasible
+                ):
+                    # load the solution and suppress the warning message by setting
+                    # solver status to ok.
+                    MindtPy = main_mip.MindtPy_utils
+                    self.config.logger.info(
+                        'MILP solver reported feasible solution, '
+                        'but not guaranteed to be optimal.'
                     )
-                )
-            else:
-                raise ValueError(
-                    'MindtPy unable to handle MILP main termination condition '
-                    'of %s. Solver message: %s'
-                    % (
-                        main_mip_results.solver.termination_condition,
-                        main_mip_results.solver.message,
+                    copy_var_list_values(
+                        main_mip.MindtPy_utils.variable_list,
+                        self.fixed_nlp.MindtPy_utils.variable_list,
+                        self.config,
+                        skip_fixed=False,
                     )
-                )
+                    self.update_suboptimal_dual_bound(main_mip_results)
+                    self.config.logger.info(
+                        self.log_formatter.format(
+                            self.mip_iter,
+                            'MILP',
+                            value(MindtPy.mip_obj.expr),
+                            self.primal_bound,
+                            self.dual_bound,
+                            self.rel_gap,
+                            get_main_elapsed_time(self.timing),
+                        )
+                    )
+                else:
+                    raise ValueError(
+                        'MindtPy unable to handle MILP main termination condition '
+                        'of %s. Solver message: %s'
+                        % (
+                            main_mip_results.solver.termination_condition,
+                            main_mip_results.solver.message,
+                        )
+                    )
         else:
             self.config.logger.info('Algorithm should terminate here.')
             should_terminate = True
