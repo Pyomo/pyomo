8b1997fd1a3604581937c50eb643d915b5b74bbb
Soren Davis <sorenadavis@gmail.com>
2023-08-23 17:03:43 -0400

    nested inner repn: remove non-working variable identification code

--- pyomo/contrib/piecewise/transform/nested_inner_repn.py
+++ pyomo/contrib/piecewise/transform/nested_inner_repn.py
@@ -2,27 +2,25 @@ from pyomo.contrib.fbbt.fbbt import compute_bounds_on_expr
 from pyomo.contrib.piecewise.transform.piecewise_to_gdp_transformation import (
     PiecewiseLinearToGDP,
 )
-from pyomo.core import Constraint, Binary, NonNegativeIntegers, Suffix, Var
+from pyomo.core import Constraint, NonNegativeIntegers, Suffix, Var
 from pyomo.core.base import TransformationFactory
-from pyomo.gdp import Disjunct, Disjunction
+from pyomo.gdp import Disjunction
 from pyomo.common.errors import DeveloperError
-from pyomo.core.expr.visitor import SimpleExpressionVisitor
-from pyomo.core.expr.current import identify_components
 
 @TransformationFactory.register(
     'contrib.piecewise.nested_inner_repn_gdp',
-    doc="TODO document",
+    doc="TODO document", # TODO
 )
 class NestedInnerRepresentationGDPTransformation(PiecewiseLinearToGDP):
     """
-    Represent a piecewise linear function "logarithmically" by using a nested
-    GDP to determine which polytope a point is in, then representing it as a
-    convex combination of extreme points, with multipliers "local" to that 
-    particular polytope, i.e., not shared with neighbors. This method of 
-    logarithmically formulating the piecewise linear function imposes no 
-    restrictions on the family of polytopes. We rely on the identification of 
-    variables to make this logarithmic in the number of binaries. This method 
-    is due to Vielma et al., 2010.
+    Represent a piecewise linear function by using a nested GDP to determine 
+    which polytope a point is in, then representing it as a convex combination 
+    of extreme points, with multipliers "local" to that particular polytope, 
+    i.e., not shared with neighbors. This method of formulating the piecewise 
+    linear function imposes no restrictions on the family of polytopes. Note 
+    that this is NOT a logarithmic formulation - it has linearly many binaries. 
+    This method was, however, inspired by the disagreggated logarithmic 
+    formulation of Vielma et al., 2010.
     """
     CONFIG = PiecewiseLinearToGDP.CONFIG()
     _transformation_name = 'pw_linear_nested_inner_repn'
@@ -31,7 +29,6 @@ class NestedInnerRepresentationGDPTransformation(PiecewiseLinearToGDP):
     # that replaces the transformed piecewise linear expr
     def _transform_pw_linear_expr(self, pw_expr, pw_linear_func, transformation_block):
         self.DEBUG = False
-        identify_vars = False
         # Get a new Block() in transformation_block.transformed_functions, which
         # is a Block(Any)
         transBlock = transformation_block.transformed_functions[
@@ -41,8 +38,6 @@ class NestedInnerRepresentationGDPTransformation(PiecewiseLinearToGDP):
         # these copy-pasted lines (from inner_representation_gdp) seem useful
         # adding some of this stuff to self so I don't have to pass it around
         self.pw_linear_func = pw_linear_func
-        # map number -> list of Disjuncts which contain Disjunctions at that level
-        self.disjunct_levels = {}
         self.dimension = pw_expr.nargs()
         substitute_var = transBlock.substitute_var = Var()
         pw_linear_func.map_transformation_var(pw_expr, substitute_var)
@@ -64,7 +59,7 @@ class NestedInnerRepresentationGDPTransformation(PiecewiseLinearToGDP):
             (self.substitute_var_lb, self.substitute_var_ub) = compute_bounds_on_expr(linear_func_expr)
         else:
             # Add the disjunction
-            transBlock.disj = self._get_disjunction(choices, transBlock, pw_expr, transBlock, 1)
+            transBlock.disj = self._get_disjunction(choices, transBlock, pw_expr, transBlock)
 
         # Widen bounds as determined when setting up the disjunction
         if self.substitute_var_lb < float('inf'):
@@ -75,21 +70,6 @@ class NestedInnerRepresentationGDPTransformation(PiecewiseLinearToGDP):
         if self.DEBUG:
             print(f"lb is {self.substitute_var_lb}, ub is {self.substitute_var_ub}")
         
-        # NOTE - This functionality does not work. Even when we can choose the indicator
-        # variables, it seems that infeasibilities will always be generated. We may need
-        # to just directly transform to mip :(
-        if identify_vars:
-            if self.DEBUG:
-                print("Now identifying variables")
-                for i in self.disjunct_levels.keys():
-                    print(f"level {i}: {len(self.disjunct_levels[i])} disjuncts")
-            transBlock.var_identifications_l = Constraint(NonNegativeIntegers, NonNegativeIntegers)
-            transBlock.var_identifications_r = Constraint(NonNegativeIntegers, NonNegativeIntegers)
-            for k in self.disjunct_levels.keys():
-                disj_0 = self.disjunct_levels[k][0]
-                for i, disj in enumerate(self.disjunct_levels[k][1:]):
-                    transBlock.var_identifications_l[k, i] = disj.d_l.binary_indicator_var == disj_0.d_l.binary_indicator_var
-                    transBlock.var_identifications_r[k, i] = disj.d_r.binary_indicator_var == disj_0.d_r.binary_indicator_var
         return substitute_var
 
     # Recursively form the Disjunctions and Disjuncts. This shouldn't blow up
@@ -111,14 +91,10 @@ class NestedInnerRepresentationGDPTransformation(PiecewiseLinearToGDP):
             # Is this valid Pyomo?
             @parent_block.Disjunct()
             def d_l(b):
-                b.inner_disjunction_l = self._get_disjunction(choices_l, b, pw_expr, root_block, level + 1)
+                b.inner_disjunction_l = self._get_disjunction(choices_l, b, pw_expr, root_block)
             @parent_block.Disjunct()
             def d_r(b):
-                b.inner_disjunction_r = self._get_disjunction(choices_r, b, pw_expr, root_block, level + 1)
-            if level not in self.disjunct_levels.keys():
-                self.disjunct_levels[level] = []
-            self.disjunct_levels[level].append(parent_block.d_l)
-            self.disjunct_levels[level].append(parent_block.d_r)
+                b.inner_disjunction_r = self._get_disjunction(choices_r, b, pw_expr, root_block)
             return Disjunction(expr=[parent_block.d_l, parent_block.d_r])
         elif size == 3:
             # Let's stay heavier on the right side for consistency. So the left
@@ -131,9 +107,6 @@ class NestedInnerRepresentationGDPTransformation(PiecewiseLinearToGDP):
             @parent_block.Disjunct()
             def d_r(b):
                 b.inner_disjunction_r = self._get_disjunction(choices[1:], b, pw_expr, root_block, level + 1)
-            if level not in self.disjunct_levels.keys():
-                self.disjunct_levels[level] = []
-            self.disjunct_levels[level].append(parent_block.d_r)
             return Disjunction(expr=[parent_block.d_l, parent_block.d_r])
         elif size == 2:
             # In this case both sides are regular Disjuncts
