0e1c8c750981f76828d5cb3283b1468c9f439541
Miranda Mundt <mmundt@sandia.gov>
2023-08-29 13:16:41 -0600

    Turning on examples test

--- pyomo/contrib/appsi/examples/tests/test_examples.py
+++ pyomo/contrib/appsi/examples/tests/test_examples.py
@@ -5,14 +5,14 @@ from pyomo.contrib.appsi.cmodel import cmodel_available
 from pyomo.contrib import appsi
 
 
-@unittest.skip('Currently turning off cplex support')
+@unittest.skipUnless(cmodel_available, 'appsi extensions are not available')
 class TestExamples(unittest.TestCase):
     def test_getting_started(self):
         try:
             import numpy as np
         except:
             raise unittest.SkipTest('numpy is not available')
-        opt = appsi.solvers.Cplex()
+        opt = appsi.solvers.Ipopt()
         if not opt.available():
-            raise unittest.SkipTest('cplex is not available')
+            raise unittest.SkipTest('ipopt is not available')
         getting_started.main(plot=False, n_points=10)
