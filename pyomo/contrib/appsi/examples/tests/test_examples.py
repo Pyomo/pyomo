d2b91264275b5611d7b577dc91d2b6c38e5b7db1
Miranda Mundt <mmundt@sandia.gov>
2023-08-29 14:37:13 -0600

    Change test_examples skipping

--- pyomo/contrib/appsi/examples/tests/test_examples.py
+++ pyomo/contrib/appsi/examples/tests/test_examples.py
@@ -1,17 +1,16 @@
 from pyomo.contrib.appsi.examples import getting_started
 from pyomo.common import unittest
-import pyomo.environ as pe
+from pyomo.common.dependencies import attempt_import
 from pyomo.contrib.appsi.cmodel import cmodel_available
 from pyomo.contrib import appsi
 
+numpy, numpy_available = attempt_import('numpy')
+
 
 @unittest.skipUnless(cmodel_available, 'appsi extensions are not available')
+@unittest.skipUnless(numpy_available, 'numpy is not available')
 class TestExamples(unittest.TestCase):
     def test_getting_started(self):
-        try:
-            import numpy as np
-        except:
-            raise unittest.SkipTest('numpy is not available')
         opt = appsi.solvers.Ipopt()
         if not opt.available():
             raise unittest.SkipTest('ipopt is not available')
