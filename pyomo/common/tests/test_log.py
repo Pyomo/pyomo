1e4bb2f7b5b6fe1124ab5e5a0adcafcacb1615ad
John Siirola <jsiirola@users.noreply.github.com>
2023-09-18 16:01:32 -0600

    Add Preformatted() API tests

--- pyomo/common/tests/test_log.py
+++ pyomo/common/tests/test_log.py
@@ -544,3 +544,18 @@ class TestLogStream(unittest.TestCase):
                 self.assertEqual(OUT.getvalue(), "INFO: line 1\n")
             # Exiting the context manager flushes the LogStream
             self.assertEqual(OUT.getvalue(), "INFO: line 1\nINFO: line 2\n")
+
+
+class TestPreformatted(unittest.TestCase):
+    def test_preformatted_api(self):
+        ref = 'a message'
+        msg = Preformatted(ref)
+        self.assertIs(msg.msg, ref)
+        self.assertEqual(str(msg), ref)
+        self.assertEqual(repr(msg), "Preformatted('a message')")
+
+        ref = 2
+        msg = Preformatted(ref)
+        self.assertIs(msg.msg, ref)
+        self.assertEqual(str(msg), '2')
+        self.assertEqual(repr(msg), "Preformatted(2)")
