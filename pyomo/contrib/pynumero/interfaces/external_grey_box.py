30771b332f866bd861abf339c042114a88e7e0c1
ZedongPeng <peng_zedong@126.com>
2023-09-20 14:35:29 -0400

    recover numpy and scipy import

--- pyomo/contrib/pynumero/interfaces/external_grey_box.py
+++ pyomo/contrib/pynumero/interfaces/external_grey_box.py
@@ -11,6 +11,8 @@
 
 import abc
 import logging
+import numpy as np
+from scipy.sparse import coo_matrix
 
 from pyomo.common.deprecation import RenamedClass
 from pyomo.common.log import is_debug_set
