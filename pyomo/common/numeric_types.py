63aba93f3906a7d875e4558931d95d911555cbe9
John Siirola <jsiirola@users.noreply.github.com>
2023-10-12 14:25:53 -0600

    Apply black

--- pyomo/common/numeric_types.py
+++ pyomo/common/numeric_types.py
@@ -148,6 +148,7 @@ def RegisterBooleanType(new_type: type):
     native_types.add(new_type)
     nonpyomo_leaf_types.add(new_type)
 
+
 def RegisterComplexType(new_type: type):
     """Register the specified type as an "complex type".
 
@@ -243,25 +244,25 @@ numeric types using RegisterNumericType() or RegisterIntegerType()."""
 
 def value(obj, exception=True):
     """
-        A utility function that returns the value of a Pyomo object or
-        expression.
+    A utility function that returns the value of a Pyomo object or
+    expression.
 
-        Args:
-            obj: The argument to evaluate. If it is None, a
-                string, or any other primitive numeric type,
-                then this function simply returns the argument.
-                Otherwise, if the argument is a NumericValue
-                then the __call__ method is executed.
-            exception (bool): If :const:`True`, then an exception should
-                be raised when instances of NumericValue fail to
-                evaluate due to one or more objects not being
-                initialized to a numeric value (e.g, one or more
-                variables in an algebraic expression having the
-                value None). If :const:`False`, then the function
-                returns :const:`None` when an exception occurs.
-                Default is True.
+    Args:
+        obj: The argument to evaluate. If it is None, a
+            string, or any other primitive numeric type,
+            then this function simply returns the argument.
+            Otherwise, if the argument is a NumericValue
+            then the __call__ method is executed.
+        exception (bool): If :const:`True`, then an exception should
+            be raised when instances of NumericValue fail to
+            evaluate due to one or more objects not being
+            initialized to a numeric value (e.g, one or more
+            variables in an algebraic expression having the
+            value None). If :const:`False`, then the function
+            returns :const:`None` when an exception occurs.
+            Default is True.
 
-        Returns: A numeric value or None.
+    Returns: A numeric value or None.
     """
     if obj.__class__ in native_types:
         return obj
