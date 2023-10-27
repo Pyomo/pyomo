db0cb67696342e6aee56c2eac5264bc99cfd19aa
Emma Johnson <emma58@users.noreply.github.com>
2023-10-27 16:35:09 -0600

    NFC: Adding a doc string with a word to the wise

--- pyomo/contrib/fbbt/expression_bounds_walker.py
+++ pyomo/contrib/fbbt/expression_bounds_walker.py
@@ -240,6 +240,22 @@ class ExpressionBoundsVisitor(StreamBasedExpressionVisitor):
     """
     Walker to calculate bounds on an expression, from leaf to root, with
     caching of terminal node bounds (Vars and Expressions)
+
+    NOTE: If anything changes on the model (e.g., Var bounds, fixing, mutable
+    Param values, etc), then you need to either create a new instance of this
+    walker, or clear self.leaf_bounds!
+
+    Parameters
+    ----------
+    leaf_bounds: ComponentMap in which to cache bounds at leaves of the expression
+        tree
+    feasibility_tol: float, feasibility tolerance for interval arithmetic
+        calculations
+    use_fixed_var_values_as_bounds: bool, whether or not to use the values of
+        fixed Vars as the upper and lower bounds for those Vars or to instead
+        ignore fixed status and use the bounds. Set to 'True' if you do not
+        anticipate the fixed status of Variables to change for the duration that
+        the computed bounds should be valid.
     """
 
     def __init__(
