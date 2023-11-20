208a5dac01ca7dab7830f70119eeb0e1ba94918e
Michael Bynum <mlbynum@sandia.gov>
2023-11-20 13:27:31 -0700

    simplification interface

--- pyomo/contrib/simplification/simplify.py
+++ pyomo/contrib/simplification/simplify.py
@@ -1,6 +1,17 @@
 from pyomo.core.expr.sympy_tools import sympy2pyomo_expression, sympyify_expression
 from pyomo.core.expr.numeric_expr import NumericExpression
 from pyomo.core.expr.numvalue import is_fixed, value
+import logging
+import warnings
+try:
+    from pyomo.contrib.simplification.ginac_interface import GinacInterface
+    ginac_available = True
+except:
+    GinacInterface = None
+    ginac_available = False
+
+
+logger = logging.getLogger(__name__)
 
 
 def simplify_with_sympy(expr: NumericExpression):
@@ -10,3 +21,25 @@ def simplify_with_sympy(expr: NumericExpression):
     if is_fixed(new_expr):
         new_expr = value(new_expr)
     return new_expr
+
+
+def simplify_with_ginac(expr: NumericExpression, ginac_interface):
+    gi = ginac_interface
+    return gi.from_ginac(gi.to_ginac(expr).normal())
+
+
+class Simplifier(object):
+    def __init__(self, supress_no_ginac_warnings: bool = False) -> None:
+        if ginac_available:
+            self.gi = GinacInterface()
+        self.suppress_no_ginac_warnings = supress_no_ginac_warnings
+
+    def simplify(self, expr: NumericExpression):
+        if ginac_available:
+            return simplify_with_ginac(expr, self.gi)
+        else:
+            if not self.suppress_no_ginac_warnings:
+                msg = f"GiNaC does not seem to be available. Using SymPy. Note that the GiNac interface is significantly faster."
+                logger.warning(msg)
+                warnings.warn(msg)
+            return simplify_with_sympy(expr)
