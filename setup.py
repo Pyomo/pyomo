88e95b76c42ff8c4e23235bde3910970233e9932
Miranda Mundt <mmundt@sandia.gov>
2024-01-10 09:59:36 -0700

    Mark pyqt6 as only pypi

--- setup.py
+++ setup.py
@@ -278,7 +278,7 @@ setup_kwargs = dict(
             #'pathos',   # requested for #963, but PR currently closed
             'pint',  # units
             'plotly',  # incidence_analysis
-            'PyQt5',  # contrib.viewer
+            'PyQt6',  # contrib.viewer
             'python-louvain',  # community_detection
             'pyyaml',  # core
             'qtconsole',  # contrib.viewer
