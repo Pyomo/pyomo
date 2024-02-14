51dccea8e8835b5150abc68ef29429413a733554
Miranda Mundt <mmundt@sandia.gov>
2024-02-14 13:28:36 -0700

    Add unit tests for solution module

--- pyomo/contrib/solver/tests/unit/test_solution.py
+++ pyomo/contrib/solver/tests/unit/test_solution.py
@@ -19,6 +19,15 @@ class TestSolutionLoaderBase(unittest.TestCase):
         member_list = list(SolutionLoaderBase.__abstractmethods__)
         self.assertEqual(sorted(expected_list), sorted(member_list))
 
+    def test_member_list(self):
+        expected_list = ['load_vars', 'get_primals', 'get_duals', 'get_reduced_costs']
+        method_list = [
+            method
+            for method in dir(SolutionLoaderBase)
+            if method.startswith('_') is False
+        ]
+        self.assertEqual(sorted(expected_list), sorted(method_list))
+
     @unittest.mock.patch.multiple(SolutionLoaderBase, __abstractmethods__=set())
     def test_solution_loader_base(self):
         self.instance = SolutionLoaderBase()
@@ -29,9 +38,45 @@ class TestSolutionLoaderBase(unittest.TestCase):
             self.instance.get_reduced_costs()
 
 
+class TestSolSolutionLoader(unittest.TestCase):
+    # I am currently unsure how to test this further because it relies heavily on
+    # SolFileData and NLWriterInfo
+    def test_member_list(self):
+        expected_list = ['load_vars', 'get_primals', 'get_duals', 'get_reduced_costs']
+        method_list = [
+            method
+            for method in dir(SolutionLoaderBase)
+            if method.startswith('_') is False
+        ]
+        self.assertEqual(sorted(expected_list), sorted(method_list))
+
+
 class TestPersistentSolutionLoader(unittest.TestCase):
     def test_abstract_member_list(self):
         # We expect no abstract members at this point because it's a real-life
         # instantiation of SolutionLoaderBase
         member_list = list(PersistentSolutionLoader('ipopt').__abstractmethods__)
         self.assertEqual(member_list, [])
+
+    def test_member_list(self):
+        expected_list = [
+            'load_vars',
+            'get_primals',
+            'get_duals',
+            'get_reduced_costs',
+            'invalidate',
+        ]
+        method_list = [
+            method
+            for method in dir(PersistentSolutionLoader)
+            if method.startswith('_') is False
+        ]
+        self.assertEqual(sorted(expected_list), sorted(method_list))
+
+    def test_default_initialization(self):
+        # Realistically, a solver object should be passed into this.
+        # However, it works with a string. It'll just error loudly if you
+        # try to run get_primals, etc.
+        self.instance = PersistentSolutionLoader('ipopt')
+        self.assertTrue(self.instance._valid)
+        self.assertEqual(self.instance._solver, 'ipopt')
