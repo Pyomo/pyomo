5aa9670c1af9f2e7eee517a0f48cacf840b31822
Emma Johnson <emma58@users.noreply.github.com>
2024-02-14 11:01:28 -0700

    Adding copyright statement

--- pyomo/gdp/plugins/binary_multiplication.py
+++ pyomo/gdp/plugins/binary_multiplication.py
@@ -1,3 +1,14 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2022
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
 from .gdp_to_mip_transformation import GDP_to_MIP_Transformation
 from pyomo.common.config import ConfigDict, ConfigValue
 from pyomo.core.base import TransformationFactory
