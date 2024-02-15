eb9b2532cd4045ca0b674a6c5b73503258731b9a
Miranda Mundt <mmundt@sandia.gov>
2024-02-15 15:49:19 -0700

    Underscore instead of dash

--- setup.py
+++ setup.py
@@ -253,7 +253,7 @@ setup_kwargs = dict(
             'sphinx_rtd_theme>0.5',
             'sphinxcontrib-jsmath',
             'sphinxcontrib-napoleon',
-            'enum-tools',
+            'enum_tools',
             'numpy',  # Needed by autodoc for pynumero
             'scipy',  # Needed by autodoc for pynumero
         ],
