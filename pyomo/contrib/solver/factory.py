316fb3faa4c0faa37a095eceddd60b36957e9ee4
John Siirola <jsiirola@users.noreply.github.com>
2024-02-15 13:45:37 -0700

    Add __future__ mechanism for switching solver factories

--- pyomo/contrib/solver/factory.py
+++ pyomo/contrib/solver/factory.py
@@ -10,7 +10,7 @@
 #  ___________________________________________________________________________
 
 
-from pyomo.opt.base import SolverFactory as LegacySolverFactory
+from pyomo.opt.base import LegacySolverFactory
 from pyomo.common.factory import Factory
 from pyomo.contrib.solver.base import LegacySolverWrapper
 
