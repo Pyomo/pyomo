843c0f416b591cfc33a92a290428b3ab7522fe00
Miranda Mundt <mmundt@sandia.gov>
2024-02-15 13:17:50 -0700

    Add copyright statement to all source files

--- pyomo/contrib/pyros/tests/test_grcs.py
+++ pyomo/contrib/pyros/tests/test_grcs.py
@@ -1,3 +1,14 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2022
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
 '''
 Unit tests for the grcs API
 One class per function being tested, minimum one test per class
