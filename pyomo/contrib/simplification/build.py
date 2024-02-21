92163f2989dc99d4e14f63c658c0cc77e3d9dc7a
Michael Bynum <michaelbynum@users.noreply.github.com>
2024-02-21 09:15:41 -0700

    cleanup

--- pyomo/contrib/simplification/build.py
+++ pyomo/contrib/simplification/build.py
@@ -1,3 +1,14 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2022
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from pyomo.common.fileutils import this_file_dir, find_library
 import os
