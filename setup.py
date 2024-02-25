7f62934e8c16b04f6a82c59020b951898efc96b7
Michael Bynum <michaelbynum@users.noreply.github.com>
2024-02-24 22:47:34 -0700

    update GHA

--- setup.py
+++ setup.py
@@ -285,7 +285,6 @@ setup_kwargs = dict(
             'sympy',  # differentiation
             'xlrd',  # dataportals
             'z3-solver',  # community_detection
-            'pybnb',  # coramin
             #
             # subprocess output is merged more reliably if
             # 'PeekNamedPipe' is available from pywin32
