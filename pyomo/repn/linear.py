9d9c98ad02699535f0a446fb90683f8a3356f3b4
John Siirola <jsiirola@users.noreply.github.com>
2024-03-05 20:05:43 -0700

    simplify initialization of exit node dispatchers

--- pyomo/repn/linear.py
+++ pyomo/repn/linear.py
@@ -208,9 +208,8 @@ def _handle_negation_ANY(visitor, node, arg):
 
 
 _exit_node_handlers[NegationExpression] = {
+    None: _handle_negation_ANY,
     (_CONSTANT,): _handle_negation_constant,
-    (_LINEAR,): _handle_negation_ANY,
-    (_GENERAL,): _handle_negation_ANY,
 }
 
 #
@@ -284,15 +283,12 @@ def _handle_product_nonlinear(visitor, node, arg1, arg2):
 
 
 _exit_node_handlers[ProductExpression] = {
+    None: _handle_product_nonlinear,
     (_CONSTANT, _CONSTANT): _handle_product_constant_constant,
     (_CONSTANT, _LINEAR): _handle_product_constant_ANY,
     (_CONSTANT, _GENERAL): _handle_product_constant_ANY,
     (_LINEAR, _CONSTANT): _handle_product_ANY_constant,
-    (_LINEAR, _LINEAR): _handle_product_nonlinear,
-    (_LINEAR, _GENERAL): _handle_product_nonlinear,
     (_GENERAL, _CONSTANT): _handle_product_ANY_constant,
-    (_GENERAL, _LINEAR): _handle_product_nonlinear,
-    (_GENERAL, _GENERAL): _handle_product_nonlinear,
 }
 _exit_node_handlers[MonomialTermExpression] = _exit_node_handlers[ProductExpression]
 
@@ -317,15 +313,10 @@ def _handle_division_nonlinear(visitor, node, arg1, arg2):
 
 
 _exit_node_handlers[DivisionExpression] = {
+    None: _handle_division_nonlinear,
     (_CONSTANT, _CONSTANT): _handle_division_constant_constant,
-    (_CONSTANT, _LINEAR): _handle_division_nonlinear,
-    (_CONSTANT, _GENERAL): _handle_division_nonlinear,
     (_LINEAR, _CONSTANT): _handle_division_ANY_constant,
-    (_LINEAR, _LINEAR): _handle_division_nonlinear,
-    (_LINEAR, _GENERAL): _handle_division_nonlinear,
     (_GENERAL, _CONSTANT): _handle_division_ANY_constant,
-    (_GENERAL, _LINEAR): _handle_division_nonlinear,
-    (_GENERAL, _GENERAL): _handle_division_nonlinear,
 }
 
 #
@@ -366,15 +357,10 @@ def _handle_pow_nonlinear(visitor, node, arg1, arg2):
 
 
 _exit_node_handlers[PowExpression] = {
+    None: _handle_pow_nonlinear,
     (_CONSTANT, _CONSTANT): _handle_pow_constant_constant,
-    (_CONSTANT, _LINEAR): _handle_pow_nonlinear,
-    (_CONSTANT, _GENERAL): _handle_pow_nonlinear,
     (_LINEAR, _CONSTANT): _handle_pow_ANY_constant,
-    (_LINEAR, _LINEAR): _handle_pow_nonlinear,
-    (_LINEAR, _GENERAL): _handle_pow_nonlinear,
     (_GENERAL, _CONSTANT): _handle_pow_ANY_constant,
-    (_GENERAL, _LINEAR): _handle_pow_nonlinear,
-    (_GENERAL, _GENERAL): _handle_pow_nonlinear,
 }
 
 #
@@ -397,9 +383,8 @@ def _handle_unary_nonlinear(visitor, node, arg):
 
 
 _exit_node_handlers[UnaryFunctionExpression] = {
+    None: _handle_unary_nonlinear,
     (_CONSTANT,): _handle_unary_constant,
-    (_LINEAR,): _handle_unary_nonlinear,
-    (_GENERAL,): _handle_unary_nonlinear,
 }
 _exit_node_handlers[AbsExpression] = _exit_node_handlers[UnaryFunctionExpression]
 
@@ -422,9 +407,8 @@ def _handle_named_ANY(visitor, node, arg1):
 
 
 _exit_node_handlers[Expression] = {
+    None: _handle_named_ANY,
     (_CONSTANT,): _handle_named_constant,
-    (_LINEAR,): _handle_named_ANY,
-    (_GENERAL,): _handle_named_ANY,
 }
 
 #
@@ -457,12 +441,7 @@ def _handle_expr_if_nonlinear(visitor, node, arg1, arg2, arg3):
     return _GENERAL, ans
 
 
-_exit_node_handlers[Expr_ifExpression] = {
-    (i, j, k): _handle_expr_if_nonlinear
-    for i in (_LINEAR, _GENERAL)
-    for j in (_CONSTANT, _LINEAR, _GENERAL)
-    for k in (_CONSTANT, _LINEAR, _GENERAL)
-}
+_exit_node_handlers[Expr_ifExpression] = {None: _handle_expr_if_nonlinear}
 for j in (_CONSTANT, _LINEAR, _GENERAL):
     for k in (_CONSTANT, _LINEAR, _GENERAL):
         _exit_node_handlers[Expr_ifExpression][_CONSTANT, j, k] = _handle_expr_if_const
@@ -495,11 +474,9 @@ def _handle_equality_general(visitor, node, arg1, arg2):
 
 
 _exit_node_handlers[EqualityExpression] = {
-    (i, j): _handle_equality_general
-    for i in (_CONSTANT, _LINEAR, _GENERAL)
-    for j in (_CONSTANT, _LINEAR, _GENERAL)
+    None: _handle_equality_general,
+    (_CONSTANT, _CONSTANT): _handle_equality_const,
 }
-_exit_node_handlers[EqualityExpression][_CONSTANT, _CONSTANT] = _handle_equality_const
 
 
 def _handle_inequality_const(visitor, node, arg1, arg2):
@@ -525,13 +502,9 @@ def _handle_inequality_general(visitor, node, arg1, arg2):
 
 
 _exit_node_handlers[InequalityExpression] = {
-    (i, j): _handle_inequality_general
-    for i in (_CONSTANT, _LINEAR, _GENERAL)
-    for j in (_CONSTANT, _LINEAR, _GENERAL)
+    None: _handle_inequality_general,
+    (_CONSTANT, _CONSTANT): _handle_inequality_const,
 }
-_exit_node_handlers[InequalityExpression][
-    _CONSTANT, _CONSTANT
-] = _handle_inequality_const
 
 
 def _handle_ranged_const(visitor, node, arg1, arg2, arg3):
@@ -562,14 +535,9 @@ def _handle_ranged_general(visitor, node, arg1, arg2, arg3):
 
 
 _exit_node_handlers[RangedExpression] = {
-    (i, j, k): _handle_ranged_general
-    for i in (_CONSTANT, _LINEAR, _GENERAL)
-    for j in (_CONSTANT, _LINEAR, _GENERAL)
-    for k in (_CONSTANT, _LINEAR, _GENERAL)
+    None: _handle_ranged_general,
+    (_CONSTANT, _CONSTANT, _CONSTANT): _handle_ranged_const,
 }
-_exit_node_handlers[RangedExpression][
-    _CONSTANT, _CONSTANT, _CONSTANT
-] = _handle_ranged_const
 
 
 class LinearBeforeChildDispatcher(BeforeChildDispatcher):
@@ -750,7 +718,10 @@ def _initialize_exit_node_dispatcher(exit_handlers):
     exit_dispatcher = {}
     for cls, handlers in exit_handlers.items():
         for args, fcn in handlers.items():
-            exit_dispatcher[(cls, *args)] = fcn
+            if args is None:
+                exit_dispatcher[cls] = fcn
+            else:
+                exit_dispatcher[(cls, *args)] = fcn
     return exit_dispatcher
 
 
