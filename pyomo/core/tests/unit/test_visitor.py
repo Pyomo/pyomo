07f5234575aeaf6905827fd7d89842e80200cdbd
John Siirola <jsiirola@users.noreply.github.com>
2024-03-07 12:58:56 -0700

    Update tests to track change in LinearExpression arg types

--- pyomo/core/tests/unit/test_visitor.py
+++ pyomo/core/tests/unit/test_visitor.py
@@ -437,9 +437,7 @@ class WalkerTests(unittest.TestCase):
         sub_map = dict()
         sub_map[id(m.x)] = 5
         e2 = replace_expressions(e, sub_map)
-        assertExpressionsEqual(
-            self, e2, LinearExpression([10, MonomialTermExpression((1, m.y))])
-        )
+        assertExpressionsEqual(self, e2, LinearExpression([10, m.y]))
 
         e = LinearExpression(linear_coefs=[2, 3], linear_vars=[m.x, m.y])
         sub_map = dict()
@@ -886,20 +884,7 @@ class WalkerTests_ReplaceInternal(unittest.TestCase):
         assertExpressionsEqual(
             self,
             SumExpression(
-                [
-                    LinearExpression(
-                        [
-                            MonomialTermExpression((1, m.y[1])),
-                            MonomialTermExpression((1, m.y[2])),
-                        ]
-                    ),
-                    LinearExpression(
-                        [
-                            MonomialTermExpression((1, m.y[2])),
-                            MonomialTermExpression((1, m.y[3])),
-                        ]
-                    ),
-                ]
+                [LinearExpression([m.y[1], m.y[2]]), LinearExpression([m.y[2], m.y[3]])]
             )
             == 0,
             f,
@@ -930,9 +915,7 @@ class TestReplacementWithNPV(unittest.TestCase):
         e3 = replace_expressions(e1, {id(m.p1): m.x})
 
         assertExpressionsEqual(self, e2, m.p2 + 2)
-        assertExpressionsEqual(
-            self, e3, LinearExpression([MonomialTermExpression((1, m.x)), 2])
-        )
+        assertExpressionsEqual(self, e3, LinearExpression([m.x, 2]))
 
     def test_npv_negation(self):
         m = ConcreteModel()
