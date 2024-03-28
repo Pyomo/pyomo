5cd0e653c2386e3e4436769c1464f98645091c6a
Miranda Mundt <mmundt@sandia.gov>
2024-03-28 14:48:57 -0600

    Accidentally removed solver_io check

--- pyomo/contrib/solver/base.py
+++ pyomo/contrib/solver/base.py
@@ -349,6 +349,8 @@ class LegacySolverWrapper:
     """
 
     def __init__(self, **kwargs):
+        if 'solver_io' in kwargs:
+            raise NotImplementedError('Still working on this')
         # There is no reason for a user to be trying to mix both old
         # and new options. That is silly. So we will yell at them.
         if 'options' in kwargs and 'solver_options' in kwargs:
