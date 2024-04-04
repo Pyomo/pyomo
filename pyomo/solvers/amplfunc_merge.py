11d11e0672ef963631d9c75fb5946dc683e6592c
Eslick <John.Eslick@carrier.com>
2024-04-04 09:29:56 -0400

    Add tests

--- pyomo/solvers/amplfunc_merge.py
+++ pyomo/solvers/amplfunc_merge.py
@@ -12,10 +12,16 @@
 
 def amplfunc_merge(amplfunc, pyomo_amplfunc):
     """Merge two AMPLFUNC variable strings eliminating duplicate lines"""
+    # Assume that the strings amplfunc and pyomo_amplfunc don't contain duplicates
+    # Assume that the path separator is correct for the OS so we don't need to
+    # worry about comparing Unix and Windows paths.
     amplfunc_lines = amplfunc.split("\n")
     existing = set(amplfunc_lines)
     for line in pyomo_amplfunc.split("\n"):
         # Skip lines we already have
         if line not in existing:
             amplfunc_lines.append(line)
+    # Remove empty lines which could happen if one or both of the strings is
+    # empty or there are two new lines in a row for whatever reason.
+    amplfunc_lines = [s for s in amplfunc_lines if s != ""]
     return "\n".join(amplfunc_lines)
