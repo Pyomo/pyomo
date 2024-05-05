c2c63bc52acedace36de75d6ce09c0d062d1ef84
John Siirola <jsiirola@users.noreply.github.com>
2024-05-05 10:09:09 -0600

    Run sympy tests any time sympy is installed

--- pyomo/contrib/simplification/tests/test_simplification.py
+++ pyomo/contrib/simplification/tests/test_simplification.py
@@ -100,12 +100,12 @@ class SimplificationMixin:
             assertExpressionsEqual(self, e, e2)
 
 
-@unittest.skipIf((not sympy_available) or (ginac_available), 'sympy is not available')
+@unittest.skipUnless(sympy_available, 'sympy is not available')
 class TestSimplificationSympy(TestCase, SimplificationMixin):
     pass
 
 
-@unittest.skipIf(not ginac_available, 'GiNaC is not available')
+@unittest.skipUnless(ginac_available, 'GiNaC is not available')
 class TestSimplificationGiNaC(TestCase, SimplificationMixin):
     def test_param(self):
         m = pe.ConcreteModel()
