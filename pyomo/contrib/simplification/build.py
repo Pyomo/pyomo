8a37c8b1e0a4a54702eb81996d5730a4e55c0da5
John Siirola <jsiirola@users.noreply.github.com>
2024-05-05 10:08:50 -0600

    Update builder to use argparse, clean up output

--- pyomo/contrib/simplification/build.py
+++ pyomo/contrib/simplification/build.py
@@ -21,18 +21,23 @@ from pyomo.common.envvar import PYOMO_CONFIG_DIR
 from pyomo.common.fileutils import find_library, this_file_dir
 from pyomo.common.tempfiles import TempfileManager
 
+logger = logging.getLogger(__name__ if __name__ != '__main__' else 'pyomo')
 
-logger = logging.getLogger(__name__)
 
-
-def build_ginac_library(parallel=None, argv=None):
-    sys.stdout.write("\n**** Building GiNaC library ****")
+def build_ginac_library(parallel=None, argv=None, env=None):
+    sys.stdout.write("\n**** Building GiNaC library ****\n")
 
     configure_cmd = ['configure', '--prefix=' + PYOMO_CONFIG_DIR, '--disable-static']
     make_cmd = ['make']
     if parallel:
         make_cmd.append(f'-j{parallel}')
     install_cmd = ['make', 'install']
+
+    env = dict(os.environ)
+    pcdir = os.path.join(PYOMO_CONFIG_DIR, 'lib', 'pkgconfig')
+    if 'PKG_CONFIG_PATH' in env:
+        pcdir += os.pathsep + env['PKG_CONFIG_PATH']
+    env['PKG_CONFIG_PATH'] = pcdir
 
     with TempfileManager.new_context() as tempfile:
         tmpdir = tempfile.mkdtemp()
@@ -49,10 +54,10 @@ def build_ginac_library(parallel=None, argv=None):
             % (url, downloader.destination())
         )
         downloader.get_tar_archive(url, dirOffset=1)
-        assert subprocess.run(configure_cmd, cwd=cln_dir).returncode == 0
+        assert subprocess.run(configure_cmd, cwd=cln_dir, env=env).returncode == 0
         logger.info("\nBuilding CLN\n")
-        assert subprocess.run(make_cmd, cwd=cln_dir).returncode == 0
-        assert subprocess.run(install_cmd, cwd=cln_dir).returncode == 0
+        assert subprocess.run(make_cmd, cwd=cln_dir, env=env).returncode == 0
+        assert subprocess.run(install_cmd, cwd=cln_dir, env=env).returncode == 0
 
         url = 'https://www.ginac.de/ginac-1.8.7.tar.bz2'
         ginac_dir = os.path.join(tmpdir, 'ginac')
@@ -62,10 +67,10 @@ def build_ginac_library(parallel=None, argv=None):
             % (url, downloader.destination())
         )
         downloader.get_tar_archive(url, dirOffset=1)
-        assert subprocess.run(configure_cmd, cwd=ginac_dir).returncode == 0
+        assert subprocess.run(configure_cmd, cwd=ginac_dir, env=env).returncode == 0
         logger.info("\nBuilding GiNaC\n")
-        assert subprocess.run(make_cmd, cwd=ginac_dir).returncode == 0
-        assert subprocess.run(install_cmd, cwd=ginac_dir).returncode == 0
+        assert subprocess.run(make_cmd, cwd=ginac_dir, env=env).returncode == 0
+        assert subprocess.run(install_cmd, cwd=ginac_dir, env=env).returncode == 0
 
 
 def _find_include(libdir, incpaths):
@@ -84,7 +89,7 @@ def build_ginac_interface(parallel=None, args=None):
     from pybind11.setup_helpers import Pybind11Extension, build_ext
     from pyomo.common.cmake_builder import handleReadonly
 
-    sys.stdout.write("\n**** Building GiNaC interface ****")
+    sys.stdout.write("\n**** Building GiNaC interface ****\n")
 
     if args is None:
         args = []
@@ -98,7 +103,7 @@ def build_ginac_interface(parallel=None, args=None):
         raise RuntimeError(
             'could not find the GiNaC library; please make sure either to install '
             'the library and development headers system-wide, or include the '
-            'path tt the library in the LD_LIBRARY_PATH environment variable'
+            'path to the library in the LD_LIBRARY_PATH environment variable'
         )
     ginac_lib_dir = os.path.dirname(ginac_lib)
     ginac_include_dir = _find_include(ginac_lib_dir, ('ginac', 'ginac.h'))
@@ -110,7 +115,7 @@ def build_ginac_interface(parallel=None, args=None):
         raise RuntimeError(
             'could not find the CLN library; please make sure either to install '
             'the library and development headers system-wide, or include the '
-            'path tt the library in the LD_LIBRARY_PATH environment variable'
+            'path to the library in the LD_LIBRARY_PATH environment variable'
         )
     cln_lib_dir = os.path.dirname(cln_lib)
     cln_include_dir = _find_include(cln_lib_dir, ('cln', 'cln.h'))
@@ -136,7 +141,7 @@ def build_ginac_interface(parallel=None, args=None):
                     tmpdir = os.path.join(this_file_dir(), 'ginac')
                 else:
                     tmpdir = os.path.abspath(tempfile.mkdtemp())
-                sys.stdout.write("Building in '%s'" % tmpdir)
+                sys.stdout.write("Building in '%s'\n" % tmpdir)
                 os.chdir(tmpdir)
                 super(ginacBuildExt, self).run()
                 if not self.inplace:
@@ -174,21 +179,25 @@ class GiNaCInterfaceBuilder(object):
 
 
 if __name__ == '__main__':
-    logging.getLogger('pyomo').setLevel(logging.DEBUG)
-    parallel = None
-    for i, arg in enumerate(sys.argv):
-        if arg == '-j':
-            parallel = int(sys.argv.pop(i + 1))
-            sys.argv.pop(i)
-            break
-        if arg.startswith('-j'):
-            if '=' in arg:
-                parallel = int(arg.split('=')[1])
-            else:
-                parallel = int(arg[2:])
-            sys.argv.pop(i)
-            break
-    if '--build-deps' in sys.argv:
-        sys.argv.remove('--build-deps')
-        build_ginac_library(parallel, [])
-    build_ginac_interface(parallel, sys.argv[1:])
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-j",
+        dest='parallel',
+        type=int,
+        default=None,
+        help="Enable parallel build with PARALLEL cores",
+    )
+    parser.add_argument(
+        "--build-deps",
+        dest='build_deps',
+        action='store_true',
+        default=False,
+        help="Download and build the CLN/GiNaC libraries",
+    )
+    options, argv = parser.parse_known_args(sys.argv)
+    logging.getLogger('pyomo').setLevel(logging.INFO)
+    if options.build_deps:
+        build_ginac_library(options.parallel, [])
+    build_ginac_interface(options.parallel, argv[1:])
