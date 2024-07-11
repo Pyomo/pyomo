793bf57cfc9f35d0f802995af07e9141d9ed3a46
jasherma <jasherma@andrew.cmu.edu>
2024-07-11 19:39:14 -0400

    Remove unused subproblem data containers

--- pyomo/contrib/pyros/solve_data.py
+++ pyomo/contrib/pyros/solve_data.py
@@ -10,7 +10,7 @@
 #  ___________________________________________________________________________
 
 """
-Objects to contain all model data and solve results for the ROSolver
+Containers for PyROS subproblem solve results.
 """
 
 
@@ -81,32 +81,6 @@ class ROSolveResults(object):
             lines.append(f" {attr_desc:<{attr_desc_pad_length}s} : {val_str}")
 
         return "\n".join(lines)
-
-
-class MasterProblemData(object):
-    """
-    Container for the grcs master problem
-
-    Attributes:
-        :master_model: master problem model object
-        :base_model: block representing the original model object
-        :iteration: current iteration of the algorithm
-    """
-
-
-class SeparationProblemData(object):
-    """
-    Container for the grcs separation problem
-
-    Attributes:
-        :separation_model: separation problem model object
-        :points_added_to_master: list of parameter violations added to the master problem over the course of the algorithm
-        :separation_problem_subsolver_statuses: list of subordinate sub-solver statuses throughout separations
-        :total_global_separation_solvers: Counter for number of times global solvers were employed in separation
-        :constraint_violations: List of constraint violations identified in separation
-    """
-
-    pass
 
 
 class MasterResult(object):
