ea528ebd999a199cf7e497f34e25466181bbf92b
Daniel Laky <29078718+djlaky@users.noreply.github.com>
2024-07-12 09:52:56 -0400

    Added user initialization check, Ran Black

--- pyomo/contrib/doe/tests/test_doe_build.py
+++ pyomo/contrib/doe/tests/test_doe_build.py
@@ -20,8 +20,68 @@ data_ex = json.load(f)
 data_ex["control_points"] = {float(k): v for k, v in data_ex["control_points"].items()}
 
 
+def get_FIM_FIMPrior_Q_L(
+    doe_obj=None,
+):
+    """
+    Helper function to retreive results to compare.
+
+    """
+    model = doe_obj.model
+
+    n_param = doe_obj.n_parameters
+    n_y = doe_obj.n_experiment_outputs
+
+    FIM_vals = [
+        pyo.value(model.fim[i, j])
+        for i in model.parameter_names
+        for j in model.parameter_names
+    ]
+    FIM_prior_vals = [
+        pyo.value(model.priorFIM[i, j])
+        for i in model.parameter_names
+        for j in model.parameter_names
+    ]
+    if hasattr(model, "L"):
+        L_vals = [
+            pyo.value(model.L[i, j])
+            for i in model.parameter_names
+            for j in model.parameter_names
+        ]
+    else:
+        L_vals = [[0] * n_param] * n_param
+    Q_vals = [
+        pyo.value(model.sensitivity_jacobian[i, j])
+        for i in model.output_names
+        for j in model.parameter_names
+    ]
+    sigma_inv = [1 / v for k, v in model.scenario_blocks[0].measurement_error.items()]
+    param_vals = np.array(
+        [
+            [v for k, v in model.scenario_blocks[0].unknown_parameters.items()],
+        ]
+    )
+
+    FIM_vals_np = np.array(FIM_vals).reshape((n_param, n_param))
+    FIM_prior_vals_np = np.array(FIM_prior_vals).reshape((n_param, n_param))
+
+    for i in range(n_param):
+        for j in range(n_param):
+            if j < i:
+                FIM_vals_np[j, i] = FIM_vals_np[i, j]
+
+    L_vals_np = np.array(L_vals).reshape((n_param, n_param))
+    Q_vals_np = np.array(Q_vals).reshape((n_y, n_param))
+
+    sigma_inv_np = np.zeros((n_y, n_y))
+
+    for ind, v in enumerate(sigma_inv):
+        sigma_inv_np[ind, ind] = v
+
+    return FIM_vals_np, FIM_prior_vals_np, Q_vals_np, L_vals_np, sigma_inv_np
+
+
 class TestReactorExampleBuild(unittest.TestCase):
-    @unittest.skipIf(not ipopt_available, "The 'ipopt' command is not available")
     @unittest.skipIf(not numpy_available, "Numpy is not available")
     def test_reactor_fd_central_check_fd_eqns(self):
         fd_method = "central"
@@ -77,7 +137,6 @@ class TestReactorExampleBuild(unittest.TestCase):
 
             assert np.isclose(param_val, param_val_from_step)
 
-    @unittest.skipIf(not ipopt_available, "The 'ipopt' command is not available")
     @unittest.skipIf(not numpy_available, "Numpy is not available")
     def test_reactor_fd_backward_check_fd_eqns(self):
         fd_method = "backward"
@@ -135,7 +194,6 @@ class TestReactorExampleBuild(unittest.TestCase):
                 other_param_val = pyo.value(k)
                 assert np.isclose(other_param_val, v)
 
-    @unittest.skipIf(not ipopt_available, "The 'ipopt' command is not available")
     @unittest.skipIf(not numpy_available, "Numpy is not available")
     def test_reactor_fd_forward_check_fd_eqns(self):
         fd_method = "forward"
@@ -193,7 +251,6 @@ class TestReactorExampleBuild(unittest.TestCase):
                 other_param_val = pyo.value(k)
                 assert np.isclose(other_param_val, v)
 
-    @unittest.skipIf(not ipopt_available, "The 'ipopt' command is not available")
     @unittest.skipIf(not numpy_available, "Numpy is not available")
     def test_reactor_fd_central_design_fixing(self):
         fd_method = "central"
@@ -244,7 +301,6 @@ class TestReactorExampleBuild(unittest.TestCase):
         # Should not have any constraints sets beyond the length of design_vars - 1 (started with index 0)
         assert not hasattr(model, con_name_base + str(len(design_vars)))
 
-    @unittest.skipIf(not ipopt_available, "The 'ipopt' command is not available")
     @unittest.skipIf(not numpy_available, "Numpy is not available")
     def test_reactor_fd_backward_design_fixing(self):
         fd_method = "backward"
@@ -295,7 +351,6 @@ class TestReactorExampleBuild(unittest.TestCase):
         # Should not have any constraints sets beyond the length of design_vars - 1 (started with index 0)
         assert not hasattr(model, con_name_base + str(len(design_vars)))
 
-    @unittest.skipIf(not ipopt_available, "The 'ipopt' command is not available")
     @unittest.skipIf(not numpy_available, "Numpy is not available")
     def test_reactor_fd_forward_design_fixing(self):
         fd_method = "forward"
@@ -345,6 +400,51 @@ class TestReactorExampleBuild(unittest.TestCase):
 
         # Should not have any constraints sets beyond the length of design_vars - 1 (started with index 0)
         assert not hasattr(model, con_name_base + str(len(design_vars)))
+
+    @unittest.skipIf(not ipopt_available, "The 'ipopt' command is not available")
+    @unittest.skipIf(not numpy_available, "Numpy is not available")
+    def test_reactor_check_user_initialization(self):
+        fd_method = "central"
+        obj_used = "det"
+
+        experiment = FullReactorExperiment(data_ex, 10, 3)
+
+        FIM_prior = np.ones((4, 4))
+        FIM_initial = np.eye(4) + FIM_prior
+        JAC_initial = np.ones((27, 4)) * 2
+        L_initial = np.tril(
+            np.ones((4, 4)) * 3
+        )  # Must input lower triangular to get equality
+
+        doe_obj = DesignOfExperiments(
+            experiment,
+            fd_formula=fd_method,
+            step=1e-3,
+            objective_option=obj_used,
+            scale_constant_value=1,
+            scale_nominal_param_value=True,
+            prior_FIM=FIM_prior,
+            jac_initial=JAC_initial,
+            fim_initial=FIM_initial,
+            L_initial=L_initial,
+            L_LB=1e-7,
+            solver=None,
+            tee=False,
+            args=None,
+            _Cholesky_option=True,
+            _only_compute_fim_lower=True,
+        )
+
+        doe_obj.create_doe_model()
+
+        # Grab the matrix values on the model
+        FIM, FIM_prior_model, Q, L, sigma = get_FIM_FIMPrior_Q_L(doe_obj)
+
+        # Make sure they match the inputs we gave
+        assert np.array_equal(FIM, FIM_initial)
+        assert np.array_equal(FIM_prior, FIM_prior_model)
+        assert np.array_equal(L_initial, L)
+        assert np.array_equal(JAC_initial, Q)
 
 
 if __name__ == "__main__":
