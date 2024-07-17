57203c62187820637fe3514caf02dc9b55b4f5b7
Pedro L. Magalhães <pmlpm@posteo.de>
2024-07-17 22:01:35 +0200

    Nothing.

--- pyomo/core/tests/unit/test_sets.py
+++ pyomo/core/tests/unit/test_sets.py
@@ -2908,7 +2908,6 @@ class TestSetArgs2(PyomoModel):
         with self.assertRaisesRegex(ValueError, ".*Cannot add value "):
             self.instance = self.model.create_instance()
 
-
 class TestMisc(PyomoModel):
     def setUp(self):
         #
