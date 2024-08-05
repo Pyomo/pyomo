96b0a99277e81a22cacdb5526a1e41c55dfbea1b
Daniel Laky <29078718+djlaky@users.noreply.github.com>
2024-08-05 12:33:15 -0400

    Update import order

--- pyomo/contrib/doe/doe.py
+++ pyomo/contrib/doe/doe.py
@@ -25,14 +25,12 @@
 #  publicly, and to permit other to do so.
 #  ___________________________________________________________________________
 
-import pyomo.environ as pyo
-from pyomo.opt import SolverStatus
-from pyomo.common.timing import TicTocTimer
-from pyomo.contrib.sensitivity_toolbox.sens import get_dsdp
-
-from pyomo.contrib.parmest.utils.model_utils import convert_params_to_vars
-
-from pyomo.contrib.doe.utils import get_parameters_from_suffix
+from enum import Enum
+from itertools import permutations, product
+import json
+import logging
+import math
+from pathlib import Path
 
 from pyomo.common.dependencies import (
     numpy as np,
@@ -41,13 +39,13 @@ from pyomo.common.dependencies import (
     matplotlib as plt,
 )
 
-from itertools import permutations, product
-from enum import Enum
-from pathlib import Path
+from pyomo.common.timing import TicTocTimer
 
-import logging
-import json
-import math
+from pyomo.contrib.sensitivity_toolbox.sens import get_dsdp
+
+import pyomo.environ as pyo
+
+from pyomo.opt import SolverStatus
 
 
 class CalculationMode(Enum):
@@ -1018,25 +1016,7 @@ class DesignOfExperiments:
 
         # TODO: Allow Params for `unknown_parameters` and `experiment_inputs`
         #       May need to make a new converter Param to Var that allows non-string names/references to be passed
-
-        # # Search for unknown parameters that are Params, or Vars. Params are updated to be fixed Vars
-        # unknown_parameter_Params = get_parameters_from_suffix(model.base_model.unknown_parameters, fix_vars=True)
-
-        # # # Remove ``base_model`` precursor name on parameters
-        # # for ind, val in enumerate(unknown_parameter_Params):
-        # #     base_model_ind = val.split(".").index("base_model")
-        # #     unknown_parameter_Params[ind] = ".".join(val.split(".")[(base_model_ind + 1) :])
-
-        # print(unknown_parameter_Params)
-
-        # # Change the unknown parameters that are Params to be Vars and fix them
-        # if len(unknown_parameter_Params) > 0:
-        #     model.base_model = convert_params_to_vars(model.base_model, unknown_parameter_Params, fix_vars=True)
-
-        # model.base_model.unknown_parameters.pprint()
-
-        # # Search for experiment inputs that are Params, or Vars. Params are updated to be unfixed vars
-        # experiment_inputs_Params = get_parameters_from_suffix(model.base_model.experiment_inputs, fix_vars=False)
+        #       Waiting on updates to the parmest params_to_vars utility function.....
 
         # Run base model to get initialized model and check model function
         for comp in model.base_model.experiment_inputs:
