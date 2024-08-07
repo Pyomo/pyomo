c28d103fefc7937d22c48203722749eb5ed54106
Daniel Laky <daniellaky@Daniels-MacBook-Pro.local>
2024-08-06 20:35:45 -0500

    Clean up init file

--- pyomo/contrib/doe/__init__.py
+++ pyomo/contrib/doe/__init__.py
@@ -9,7 +9,4 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 from .doe import DesignOfExperiments, ObjectiveLib, FiniteDifferenceStep
-from .tests import experiment_class_example, experiment_class_example_flags
 from .utils import rescale_FIM, get_parameters_from_suffix
-from .examples import reactor_experiment, reactor_example, reactor_compute_factorial_FIM
-from .experiment import Experiment
