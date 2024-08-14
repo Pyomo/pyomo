f4ff8b7086b6c6675a10afb6bb25d42d2efe1a30
Daniel Laky <29078718+djlaky@users.noreply.github.com>
2024-08-14 09:30:40 -0400

    Ran Black

--- pyomo/contrib/doe/tests/test_doe_solve.py
+++ pyomo/contrib/doe/tests/test_doe_solve.py
@@ -230,7 +230,7 @@ class TestReactorExampleSolving(unittest.TestCase):
         # Make sure FIM and Q.T @ sigma_inv @ Q are close (alternate definition of FIM)
         self.assertTrue(np.all(np.isclose(FIM, Q.T @ sigma_inv @ Q)))
 
-    # This test ensure that compute FIM runs without error using the 
+    # This test ensure that compute FIM runs without error using the
     # `sequential` option with central finite differences
     def test_compute_FIM_seq_centr(self):
         fd_method = "central"
@@ -244,7 +244,7 @@ class TestReactorExampleSolving(unittest.TestCase):
 
         doe_obj.compute_FIM(method="sequential")
 
-    # This test ensure that compute FIM runs without error using the 
+    # This test ensure that compute FIM runs without error using the
     # `sequential` option with forward finite differences
     def test_compute_FIM_seq_forward(self):
         fd_method = "forward"
@@ -258,7 +258,7 @@ class TestReactorExampleSolving(unittest.TestCase):
 
         doe_obj.compute_FIM(method="sequential")
 
-    # This test ensure that compute FIM runs without error using the 
+    # This test ensure that compute FIM runs without error using the
     # `kaug` option. kaug computes the FIM directly so no finite difference
     # scheme is needed.
     @unittest.skipIf(not scipy_available, "Scipy is not available")
@@ -277,7 +277,7 @@ class TestReactorExampleSolving(unittest.TestCase):
 
         doe_obj.compute_FIM(method="kaug")
 
-    # This test ensure that compute FIM runs without error using the 
+    # This test ensure that compute FIM runs without error using the
     # `sequential` option with backward finite differences
     def test_compute_FIM_seq_backward(self):
         fd_method = "backward"
