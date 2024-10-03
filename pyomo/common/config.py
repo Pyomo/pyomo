f08ebadf11a77483e3c3c24a52b62adac19a4ce4
John Siirola <jsiirola@users.noreply.github.com>
2024-10-03 09:32:28 -0600

    Output str instead of repr for Config default values

--- pyomo/common/config.py
+++ pyomo/common/config.py
@@ -1495,11 +1495,7 @@ class numpydoc_ConfigFormatter(ConfigFormatter):
                 None,
                 [
                     'dict' if isinstance(obj, ConfigDict) else obj.domain_name(),
-                    (
-                        'optional'
-                        if obj._default is None
-                        else f'default={repr(obj._default)}'
-                    ),
+                    ('optional' if obj._default is None else f'default={obj._default}'),
                 ],
             )
         )
