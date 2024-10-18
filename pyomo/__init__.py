c2c77837cbbb9b2a80dbaa05c808e21435ac0ced
John Siirola <jsiirola@users.noreply.github.com>
2024-10-17 18:02:19 -0600

    Remove pysp stub module

--- pyomo/__init__.py
+++ pyomo/__init__.py
@@ -11,3 +11,17 @@
 
 from . import common
 from .version import __version__
+
+
+from pyomo.common.deprecation import moved_module
+
+moved_module(
+    'pyomo.pysp',
+    'pysp',
+    version='6.0',
+    msg="PySP has been removed from the pyomo.pysp namespace.  "
+    "Beginning in Pyomo 6.0, PySP is distributed as a separate "
+    "package.  Please see https://github.com/Pyomo/pysp for "
+    "information on downloading and installing PySP",
+)
+del moved_module
