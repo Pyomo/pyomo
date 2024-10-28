9fe85a7208496ca0d9271807b365772fbeffb352
John Siirola <jsiirola@users.noreply.github.com>
2024-10-28 16:58:14 -0600

    Update Sphinx version

--- setup.py
+++ setup.py
@@ -252,7 +252,7 @@ setup_kwargs = dict(
         # the dependencies.
         'tests': ['coverage', 'parameterized', 'pybind11', 'pytest', 'pytest-parallel'],
         'docs': [
-            'Sphinx>4',
+            'Sphinx>7.2',  # version requirement set by our autosummary plugin
             'sphinx-copybutton',
             'sphinx_rtd_theme>0.5',
             'sphinxcontrib-jsmath',
