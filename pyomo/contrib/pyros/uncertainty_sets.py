bdbd0cb85eb9d7034bc986f60029409299dc54f3
jasherma <jasherma@andrew.cmu.edu>
2025-02-26 13:29:54 -0500

    Tweak documentation of `BoxSet`

--- pyomo/contrib/pyros/uncertainty_sets.py
+++ pyomo/contrib/pyros/uncertainty_sets.py
@@ -972,10 +972,10 @@ class BoxSet(UncertaintySet):
 
     in which
     :math:`q^\\text{L} \\in \\mathbb{R}^n` refers to
-    ``np.array(bounds)[:, 0]``,
+    ``numpy.array(bounds)[:, 0]``,
     and
     :math:`q^\\text{U} \\in \\mathbb{R}^n` refers to
-    ``np.array(bounds)[:, 1]``.
+    ``numpy.array(bounds)[:, 1]``.
 
     Examples
     --------
