1c4207d1f291a08385bceb0c1ebdf6edf225f040
Miranda Mundt <55767766+mrmundt@users.noreply.github.com>
2025-03-10 11:57:55 -0600

    Remove duplicate license

--- pyomo/contrib/latex_printer/tests/test_latex_printer_vartypes.py
+++ pyomo/contrib/latex_printer/tests/test_latex_printer_vartypes.py
@@ -9,17 +9,6 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
-#  ___________________________________________________________________________
-#
-#  Pyomo: Python Optimization Modeling Objects
-#  Copyright (c) 2008-2023
-#  National Technology and Engineering Solutions of Sandia, LLC
-#  Under the terms of Contract DE-NA0003525 with National Technology and
-#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
-#  rights in this software.
-#  This software is distributed under the 3-clause BSD License.
-#  ___________________________________________________________________________
-
 import pyomo.common.unittest as unittest
 from pyomo.contrib.latex_printer import latex_printer
 import pyomo.environ as pyo
