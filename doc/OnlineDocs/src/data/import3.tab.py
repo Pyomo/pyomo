6ca4b4599e08999a32d6811049ad54f96b2dd3e9
Miranda Mundt <mmundt@sandia.gov>
2025-03-27 16:00:18 -0600

    Update src file imports

--- doc/OnlineDocs/src/data/import3.tab.py
+++ doc/OnlineDocs/src/data/import3.tab.py
@@ -9,11 +9,11 @@
 #  This software is distributed under the 3-clause BSD License.
 #  ___________________________________________________________________________
 
-from pyomo.environ import *
+import pyomo.environ as pyo
 
-model = AbstractModel()
+model = pyo.AbstractModel()
 
-model.A = Set()
+model.A = pyo.Set()
 
 instance = model.create_instance('import3.tab.dat')
 
