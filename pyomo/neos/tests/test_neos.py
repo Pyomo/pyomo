e5c6e478cad1197251c54e01da635a6dbb2352e9
Miranda Mundt <mmundt@sandia.gov>
2025-04-24 13:34:03 -0600

    Turn 'on' test but it just skips

--- pyomo/neos/tests/test_neos.py
+++ pyomo/neos/tests/test_neos.py
@@ -164,8 +164,9 @@ class RunAllNEOSSolvers(object):
     # (and testing) until we have time to resolve #3321
     # [20 Sep 24]: and appears to have been removed from NEOS
     # [24 Apr 25]: it appears to be there but causes timeouts
-    # def test_octeract(self):
-    #     self._run('octeract')
+    def test_octeract(self):
+        pass
+        # self._run('octeract')
 
     def test_ooqp(self):
         if self.sense == pyo.maximize:
