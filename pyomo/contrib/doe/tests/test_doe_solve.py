4a2a7f4d0e898f549b1569a034f26a74a9acc057
Daniel Laky <29078718+djlaky@users.noreply.github.com>
2025-05-23 09:04:27 -0400

    Removed incorrect skip?
    Unsure if this needs kaug? I don't know why it would need kaug.

--- pyomo/contrib/doe/tests/test_doe_solve.py
+++ pyomo/contrib/doe/tests/test_doe_solve.py
@@ -188,9 +188,7 @@ class TestReactorExampleSolving(unittest.TestCase):
         # Make sure FIM and Q.T @ sigma_inv @ Q are close (alternate definition of FIM)
         self.assertTrue(np.all(np.isclose(FIM, Q.T @ sigma_inv @ Q)))
 
-    @unittest.skipIf(
-        not k_aug_available.available(False), "The 'k_aug' command is not available"
-    )
+
     def test_reactor_obj_det_solve(self):
         fd_method = "central"
         obj_used = "determinant"
