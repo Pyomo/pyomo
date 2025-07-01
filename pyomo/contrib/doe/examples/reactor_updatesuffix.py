403eae785fd1e2d9a16720b7c1681ca2a8798ee6
Stephen Cini <114932899+sscini@users.noreply.github.com>
2025-07-01 16:36:25 -0400

    Updated example and added tests, still debugging

--- pyomo/contrib/doe/examples/reactor_updatesuffix.py
+++ pyomo/contrib/doe/examples/reactor_updatesuffix.py
@@ -43,29 +43,26 @@ def run_reactor_update_suffix_items():
     # Update the model to change the values of the desired component
     # Here we will update the unknown parameters of the reactor model
     example_suffix = "unknown_parameters"
-    if example_suffix not in reactor_model.component_map():
-        raise ValueError(
-            f"The suffix '{example_suffix}' is not present in the reactor model."
-        )
+    suffix_obj = reactor_model.unknown_parameters
 
     # Original values
     print(f"Original values of {example_suffix}: \n")
-    for v in reactor_model.unknown_parameters:
+    for v in suffix_obj:
         v.display()  # prints “v : <value>”
 
     # Update the suffix with new values
     # Here we are updating the values of the unknown parameters
     # You must know the length of the list and order of the suffix items to update them correctly
-    utils.update_model_from_suffix(reactor_model, example_suffix, [1, 0.5, 0.1, 1])
+    utils.update_model_from_suffix(suffix_obj, [1, 0.5, 0.1, 1])
 
     # Updated values
     print(f"\nUpdated values of {example_suffix}: \n")
-    for v in reactor_model.unknown_parameters:
+    for v in suffix_obj:
         v.display()  # prints “v : <value>”
 
     # Show suffix is unchanged
     print(f"\nSuffix '{example_suffix}' is unchanged: \n")
-    print({comp.name: tag for comp, tag in reactor_model.unknown_parameters.items()})
+    print({comp.name: tag for comp, tag in suffix_obj.items()})
 
 
 if __name__ == "__main__":
