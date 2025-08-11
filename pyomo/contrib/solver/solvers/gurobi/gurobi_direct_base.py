5b1d3f9cfb551598599a8f2ecf99313517bf06f7
Michael Bynum <mlbynum@sandia.gov>
2025-08-10 20:43:45 -0600

    refactoring gurobi interfaces

---
+++ pyomo/contrib/solver/solvers/gurobi/gurobi_direct_base.py
@@ -0,0 +1,328 @@
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2025
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
+
+import datetime
+import io
+import math
+import operator
+import os
+
+from pyomo.common.collections import ComponentMap, ComponentSet
+from pyomo.common.config import ConfigValue
+from pyomo.common.dependencies import attempt_import
+from pyomo.common.enums import ObjectiveSense
+from pyomo.common.errors import MouseTrap, ApplicationError
+from pyomo.common.shutdown import python_is_shutting_down
+from pyomo.common.tee import capture_output, TeeStream
+from pyomo.common.timing import HierarchicalTimer
+from pyomo.core.staleflag import StaleFlagManager
+
+from pyomo.contrib.solver.common.base import SolverBase, Availability
+from pyomo.contrib.solver.common.config import BranchAndBoundConfig
+from pyomo.contrib.solver.common.util import (
+    NoFeasibleSolutionError,
+    NoOptimalSolutionError,
+    NoDualsError,
+    NoReducedCostsError,
+    NoSolutionError,
+    IncompatibleModelError,
+)
+from pyomo.contrib.solver.common.results import (
+    Results,
+    SolutionStatus,
+    TerminationCondition,
+)
+from pyomo.contrib.solver.common.solution_loader import SolutionLoaderBase
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+gurobipy, gurobipy_available = attempt_import('gurobipy')
+
+
+class GurobiConfig(BranchAndBoundConfig):
+    def __init__(
+        self,
+        description=None,
+        doc=None,
+        implicit=False,
+        implicit_domain=None,
+        visibility=0,
+    ):
+        BranchAndBoundConfig.__init__(
+            self,
+            description=description,
+            doc=doc,
+            implicit=implicit,
+            implicit_domain=implicit_domain,
+            visibility=visibility,
+        )
+        self.use_mipstart: bool = self.declare(
+            'use_mipstart',
+            ConfigValue(
+                default=False,
+                domain=bool,
+                description="If True, the current values of the integer variables "
+                "will be passed to Gurobi.",
+            ),
+        )
+
+
+class GurobiDirectBase(SolverBase):
+
+    _num_gurobipy_env_clients = 0
+    _gurobipy_env = None
+    _available = None
+    _gurobipy_available = gurobipy_available
+    _tc_map = None
+
+    CONFIG = GurobiConfig()
+
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+        self._register_env_client()
+
+    def __del__(self):
+        if not python_is_shutting_down():
+            self._release_env_client()
+
+    def available(self):
+        if self._available is None:
+            # this triggers the deferred import, and for the persistent
+            # interface, may update the _available flag
+            #
+            # Note that we set the _available flag on the *most derived
+            # class* and not on the instance, or on the base class.  That
+            # allows different derived interfaces to have different
+            # availability (e.g., persistent has a minimum version
+            # requirement that the direct interface doesn't - is that true?)
+            if not self._gurobipy_available:
+                if self._available is None:
+                    self.__class__._available = Availability.NotFound
+            else:
+                self.__class__._available = self._check_license()
+        return self._available
+
+    @staticmethod
+    def release_license():
+        if GurobiDirectBase._gurobipy_env is None:
+            return
+        if GurobiDirectBase._num_gurobipy_env_clients:
+            logger.warning(
+                "Call to GurobiDirectBase.release_license() with %s remaining "
+                "environment clients." % (GurobiDirectBase._num_gurobipy_env_clients,)
+            )
+        GurobiDirectBase._gurobipy_env.close()
+        GurobiDirectBase._gurobipy_env = None
+
+    @staticmethod
+    def env():
+        if GurobiDirectBase._gurobipy_env is None:
+            with capture_output(capture_fd=True):
+                GurobiDirectBase._gurobipy_env = gurobipy.Env()
+        return GurobiDirectBase._gurobipy_env
+
+    @staticmethod
+    def _register_env_client():
+        GurobiDirectBase._num_gurobipy_env_clients += 1
+
+    @staticmethod
+    def _release_env_client():
+        GurobiDirectBase._num_gurobipy_env_clients -= 1
+        if GurobiDirectBase._num_gurobipy_env_clients <= 0:
+            # Note that _num_gurobipy_env_clients should never be <0,
+            # but if it is, release_license will issue a warning (that
+            # we want to know about)
+            GurobiDirectBase.release_license()
+
+    def _check_license(self):
+        try:
+            model = gurobipy.Model(env=self.env())
+        except gurobipy.GurobiError:
+            return Availability.BadLicense
+
+        model.setParam('OutputFlag', 0)
+        try:
+            model.addVars(range(2001))
+            model.optimize()
+            return Availability.FullLicense
+        except gurobipy.GurobiError:
+            return Availability.LimitedLicense
+        finally:
+            model.dispose()
+
+    def version(self):
+        version = (
+            gurobipy.GRB.VERSION_MAJOR,
+            gurobipy.GRB.VERSION_MINOR,
+            gurobipy.GRB.VERSION_TECHNICAL,
+        )
+        return version
+
+    def _create_solver_model(self, pyomo_model, config):
+        # should return gurobi_model, solution_loader, has_objective
+        raise NotImplementedError('should be implemented by derived classes')
+
+    def _pyomo_gurobi_var_iter(self):
+        # generator of tuples (pyomo_var, gurobi_var)
+        raise NotImplementedError('should be implemented by derived classes')
+    
+    def _mipstart(self):
+        for pyomo_var, gurobi_var in self._pyomo_gurobi_var_iter():
+            if pyomo_var.is_integer() and pyomo_var.value is not None:
+                gurobi_var.setAttr('Start', pyomo_var.value)
+
+    def solve(self, model, **kwds) -> Results:
+        start_timestamp = datetime.datetime.now(datetime.timezone.utc)
+        config: GurobiConfig = self.config(
+            value=kwds, 
+            preserve_implicit=True,
+        )
+        if not self.available():
+            c = self.__class__
+            raise ApplicationError(
+                f'Solver {c.__module__}.{c.__qualname__} is not available '
+                f'({self.available()}).'
+            )
+        if config.timer is None:
+            config.timer = HierarchicalTimer()
+        timer = config.timer
+
+        StaleFlagManager.mark_all_as_stale()
+        ostreams = [io.StringIO()] + config.tee
+
+        orig_cwd = os.getcwd()
+        try:
+            if config.working_dir:
+                os.chdir(config.working_dir)
+            with capture_output(TeeStream(*ostreams), capture_fd=False):
+                gurobi_model, solution_loader, has_obj = self._create_solver_model(model, config)
+                options = config.solver_options
+
+                gurobi_model.setParam('LogToConsole', 1)
+
+                if config.threads is not None:
+                    gurobi_model.setParam('Threads', config.threads)
+                if config.time_limit is not None:
+                    gurobi_model.setParam('TimeLimit', config.time_limit)
+                if config.rel_gap is not None:
+                    gurobi_model.setParam('MIPGap', config.rel_gap)
+                if config.abs_gap is not None:
+                    gurobi_model.setParam('MIPGapAbs', config.abs_gap)
+
+                if config.use_mipstart:
+                    self._mipstart()
+
+                for key, option in options.items():
+                    gurobi_model.setParam(key, option)
+
+                timer.start('optimize')
+                gurobi_model.optimize()
+                timer.stop('optimize')
+        finally:
+            os.chdir(orig_cwd)
+
+        res = self._postsolve(
+            grb_model=gurobi_model, 
+            config=config,
+        )
+
+        res.solution_loader = solution_loader
+        res.solver_log = ostreams[0].getvalue()
+        end_timestamp = datetime.datetime.now(datetime.timezone.utc)
+        res.timing_info.start_timestamp = start_timestamp
+        res.timing_info.wall_time = (end_timestamp - start_timestamp).total_seconds()
+        res.timing_info.timer = timer
+        return res
+
+    def _get_tc_map(self):
+        if GurobiDirectBase._tc_map is None:
+            grb = gurobipy.GRB
+            tc = TerminationCondition
+            GurobiDirectBase._tc_map = {
+                grb.LOADED: tc.unknown,  # problem is loaded, but no solution
+                grb.OPTIMAL: tc.convergenceCriteriaSatisfied,
+                grb.INFEASIBLE: tc.provenInfeasible,
+                grb.INF_OR_UNBD: tc.infeasibleOrUnbounded,
+                grb.UNBOUNDED: tc.unbounded,
+                grb.CUTOFF: tc.objectiveLimit,
+                grb.ITERATION_LIMIT: tc.iterationLimit,
+                grb.NODE_LIMIT: tc.iterationLimit,
+                grb.TIME_LIMIT: tc.maxTimeLimit,
+                grb.SOLUTION_LIMIT: tc.unknown,
+                grb.INTERRUPTED: tc.interrupted,
+                grb.NUMERIC: tc.unknown,
+                grb.SUBOPTIMAL: tc.unknown,
+                grb.USER_OBJ_LIMIT: tc.objectiveLimit,
+            }
+        return GurobiDirectBase._tc_map
+
+    def _postsolve(self, grb_model, config, has_obj):
+        status = grb_model.Status
+
+        results = Results()
+        results.timing_info.gurobi_time = grb_model.Runtime
+
+        if grb_model.SolCount > 0:
+            if status == gurobipy.GRB.OPTIMAL:
+                results.solution_status = SolutionStatus.optimal
+            else:
+                results.solution_status = SolutionStatus.feasible
+        else:
+            results.solution_status = SolutionStatus.noSolution
+
+        results.termination_condition = self._get_tc_map().get(
+            status, TerminationCondition.unknown
+        )
+
+        if (
+            results.termination_condition
+            != TerminationCondition.convergenceCriteriaSatisfied
+            and config.raise_exception_on_nonoptimal_result
+        ):
+            raise NoOptimalSolutionError()
+
+        if has_obj:
+            try:
+                if math.isfinite(grb_model.ObjVal):
+                    results.incumbent_objective = grb_model.ObjVal
+                else:
+                    results.incumbent_objective = None
+            except (gurobipy.GurobiError, AttributeError):
+                results.incumbent_objective = None
+            try:
+                results.objective_bound = grb_model.ObjBound
+            except (gurobipy.GurobiError, AttributeError):
+                if grb_model.ModelSense == ObjectiveSense.minimize:
+                    results.objective_bound = -math.inf
+                else:
+                    results.objective_bound = math.inf
+        else:
+            results.incumbent_objective = None
+            results.objective_bound = None
+
+        results.iteration_count = grb_model.getAttr('IterCount')
+
+        config.timer.start('load solution')
+        if config.load_solutions:
+            if grb_model.SolCount > 0:
+                results.solution_loader.load_vars()
+            else:
+                raise NoFeasibleSolutionError()
+        config.timer.stop('load solution')
+
+        results.solver_config = config
+        results.solver_name = self.name
+        results.solver_version = self.version()
+
+        return results
