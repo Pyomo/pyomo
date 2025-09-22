fdf856855a127ac5983c04dfb900712427455102
jasherma <jasherma@andrew.cmu.edu>
2025-09-22 04:06:42 -0400

    Log master feasibility and DR polishing failure msgs at DEBUG level

--- pyomo/contrib/pyros/master_problem_methods.py
+++ pyomo/contrib/pyros/master_problem_methods.py
@@ -296,7 +296,7 @@ def solve_master_feasibility_problem(master_data):
             f" Solve time: {getattr(results.solver, TIC_TOC_SOLVE_TIME_ATTR)}s"
         )
     else:
-        config.progress_logger.warning(
+        config.progress_logger.debug(
             "Could not successfully solve master feasibility problem "
             f"of iteration {master_data.iteration} with primary subordinate "
             f"{'global' if config.solve_master_globally else 'local'} solver "
@@ -548,7 +548,7 @@ def minimize_dr_vars(master_data):
     acceptable = {tc.globallyOptimal, tc.optimal, tc.locallyOptimal}
     if results.solver.termination_condition not in acceptable:
         # continue with "unpolished" master model solution
-        config.progress_logger.warning(
+        config.progress_logger.debug(
             "Could not successfully solve DR polishing problem "
             f"of iteration {master_data.iteration} with primary subordinate "
             f"{'global' if config.solve_master_globally else 'local'} solver "
