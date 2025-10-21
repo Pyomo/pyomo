036a320bb559e827060bc955c766df0726598d95
Michael Bynum <mlbynum@sandia.gov>
2025-10-21 00:48:22 -0600

    observer typos

--- pyomo/contrib/observer/model_observer.py
+++ pyomo/contrib/observer/model_observer.py
@@ -1314,10 +1314,16 @@ class ModelChangeDetector:
             self._updates.run()
 
     def get_variables_impacted_by_param(self, p: ParamData):
-        return [self._vars[vid][0] for vid in self._referenced_params[id(p)][3]]
+        return list(self._referenced_params[p][3])
 
     def get_constraints_impacted_by_param(self, p: ParamData):
-        return list(self._referenced_params[id(p)][0])
+        return list(self._referenced_params[p][0])
 
     def get_constraints_impacted_by_var(self, v: VarData):
-        return list(self._referenced_variables[id(v)][0])
+        return list(self._referenced_variables[v][0])
+
+    def get_objectives_impacted_by_param(self, p: ParamData):
+        return list(self._referenced_params[p][2])
+
+    def get_objectives_impacted_by_var(self, v: VarData):
+        return list(self._referenced_variables[v][2])
