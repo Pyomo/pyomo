3957dc91ed1b9c0bbda5978b81bb4a8e9000ddb6
Stephen Cini <114932899+sscini@users.noreply.github.com>
2026-01-21 17:58:55 -0500

    Added Shammah fix for exp count

--- pyomo/contrib/parmest/parmest.py
+++ pyomo/contrib/parmest/parmest.py
@@ -367,7 +367,17 @@ def _count_total_experiments(experiment_list):
     """
     total_number_data = 0
     for experiment in experiment_list:
-        total_number_data += len(experiment.get_labeled_model().experiment_outputs)
+        # get the experiment outputs
+        output_variables = experiment.get_labeled_model().experiment_outputs
+
+        # get the parent component of the first output variable
+        parent = list(output_variables.keys())[0].parent_component()
+
+        # check if there is only one unique experiment output, e.g., dynamic output variable
+        if all(v.parent_component() is parent for v in output_variables):
+            total_number_data += len(output_variables)
+        else:
+            total_number_data += 1
 
     return total_number_data
 
