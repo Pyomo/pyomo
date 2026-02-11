6532978c64d956407602134d52c57ca83f65d47d
John Siirola <jsiirola@users.noreply.github.com>
2026-02-10 21:15:54 -0700

    Update copyright statements, add docstrings

--- pyomo/tpl/ply/__init__.py
+++ pyomo/tpl/ply/__init__.py
@@ -1,5 +1,27 @@
-# PLY package
-# Author: David Beazley (dave@dabeaz.com)
+#  ___________________________________________________________________________
+#
+#  Pyomo: Python Optimization Modeling Objects
+#  Copyright (c) 2008-2025
+#  National Technology and Engineering Solutions of Sandia, LLC
+#  Under the terms of Contract DE-NA0003525 with National Technology and
+#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
+#  rights in this software.
+#  This software is distributed under the 3-clause BSD License.
+#  ___________________________________________________________________________
 
-__version__ = '3.11'
+"""PLY
+===
+
+This module is derived from the PLY package (3.11) by David Beazley.
+
+It has been modified to disallow loading parse tables from arbitrary
+files or pickles.  This simplifies management of the parse table module
+and avoids potential security concerns.
+
+The original PLY project (https://github.com/dabeaz/ply) was archived on
+December 21, 2025 and is no longer developed or maintained.
+
+"""
+
+__version__ = '3.11.post1'
 __all__ = ['lex','yacc']
