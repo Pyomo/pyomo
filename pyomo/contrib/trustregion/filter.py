126707e09017c6918624c78b928be47ab7482163
John Siirola <jsiirola@users.noreply.github.com>
2026-02-11 16:57:50 -0700

    Reformat copyright statement

--- pyomo/contrib/trustregion/filter.py
+++ pyomo/contrib/trustregion/filter.py
@@ -1,20 +1,17 @@
-#  ___________________________________________________________________________
+# ____________________________________________________________________________________
 #
-#  Pyomo: Python Optimization Modeling Objects
-#  Copyright (c) 2008-2026
-#  National Technology and Engineering Solutions of Sandia, LLC
-#  Under the terms of Contract DE-NA0003525 with National Technology and
-#  Engineering Solutions of Sandia, LLC, the U.S. Government retains certain
-#  rights in this software.
-#  This software is distributed under the 3-clause BSD License.
-#  ___________________________________________________________________________
+# Pyomo: Python Optimization Modeling Objects
+# Copyright (c) 2008-2026 National Technology and Engineering Solutions of Sandia, LLC
+# Under the terms of Contract DE-NA0003525 with National Technology and Engineering
+# Solutions of Sandia, LLC, the U.S. Government retains certain rights in this
+# software.  This software is distributed under the 3-clause BSD License.
+# ____________________________________________________________________________________
 #
-#  Development of this module was conducted as part of the Institute for
-#  the Design of Advanced Energy Systems (IDAES) with support through the
-#  Simulation-Based Engineering, Crosscutting Research Program within the
-#  U.S. Department of Energy's Office of Fossil Energy and Carbon Management.
-#  ___________________________________________________________________________
-
+# Development of this module was conducted as part of the Institute for
+# the Design of Advanced Energy Systems (IDAES) with support through the
+# Simulation-Based Engineering, Crosscutting Research Program within the
+# U.S. Department of Energy's Office of Fossil Energy and Carbon Management.
+# ____________________________________________________________________________________
 
 class FilterElement:
     def __init__(self, objective, feasible):
