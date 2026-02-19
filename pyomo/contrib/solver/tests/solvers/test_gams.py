5adcffb12ba56cbca841de7575794ccabeea7d69
John Siirola <jsiirola@users.noreply.github.com>
2026-02-18 23:11:35 -0700

    gams: expand testing

--- pyomo/contrib/solver/tests/solvers/test_gams.py
+++ pyomo/contrib/solver/tests/solvers/test_gams.py
@@ -9,19 +9,28 @@
 
 import os
 import subprocess
+import sys
 
-from pyomo.common.tempfiles import TempfileManager
-from pyomo.opt.base import SolverFactory
+import pyomo.environ as pyo
+
 from pyomo.common import unittest, Executable
-from pyomo.repn.plugins.gams_writer_v2 import GAMSWriter
+from pyomo.common.config import ConfigDict
+from pyomo.common.errors import ApplicationError
+from pyomo.common.fileutils import ExecutableData
+from pyomo.common.log import LoggingIntercept
+from pyomo.common.tempfiles import TempfileManager
 from pyomo.core.base import SymbolMap
 from pyomo.core.base.label import NumericLabeler
-import pyomo.environ as pyo
-from pyomo.common.fileutils import ExecutableData
-from pyomo.common.config import ConfigDict
-import pyomo.contrib.solver.solvers.gams as gams
-from pyomo.contrib.solver.common.util import NoDualsError, NoReducedCostsError
+from pyomo.opt.base import SolverFactory
+from pyomo.repn.plugins.gams_writer_v2 import GAMSWriter
+from pyomo.contrib.solver.common.util import (
+    NoDualsError,
+    NoOptimalSolutionError,
+    NoReducedCostsError,
+)
 from pyomo.contrib.solver.common.base import Availability
+from pyomo.contrib.solver.common.results import TerminationCondition, SolutionStatus
+import pyomo.contrib.solver.solvers.gams as gams
 
 """
 Formatted after pyomo/pyomo/contrib/solver/test/solvers/test_ipopt.py
@@ -176,16 +185,102 @@ class TestGAMSInterface(unittest.TestCase):
         opt.config = config
         self.assertTrue(opt.available() == Availability.NotFound)
 
-    def test_version_cache(self):
+    def test_version(self):
+        # GAMS should be available...
         opt = gams.GAMS()
-        opt.version()
-        self.assertTrue(opt.version())
+        ver = opt.version()
+        self.assertIsInstance(ver, tuple)
+        self.assertEqual(len(ver), 3)
+        self.assertIsInstance(ver[0], int)
+        self.assertIsInstance(ver[1], int)
+        self.assertIsInstance(ver[2], int)
+        self.assertGreater(ver[0], 0)
+        self.assertGreaterEqual(ver[1], 0)
+        self.assertGreaterEqual(ver[2], 0)
+
         # Now we will try with a custom config that has a fake path
         config = gams.GAMSConfig()
         config.executable = Executable('/a/bogus/path')
         opt = gams.GAMS()
         opt.config = config
         self.assertIsNone(opt.version())
+
+        # Now try pointing to "executables" that are not GAMS
+        with TempfileManager as tmp:
+            dname = tmp.mkdtemp()
+            fname = os.path.join(dname, 'test_rc.py')
+            with open(fname, 'w') as F:
+                F.write(f"""\
+#!{sys.executable}
+import sys
+sys.stderr.write('FAIL\\n')
+sys.stdout.write('HERE\\n')
+sys.exit(2)
+""")
+            os.chmod(fname, 0o755)
+            opt.config.executable = fname
+            with LoggingIntercept() as LOG:
+                self.assertIsNone(opt.version())
+                self.assertEqual(opt.available(), Availability.NotFound)
+            self.assertEqual(
+                "Failed running GAMS command to get version (non-zero returncode 2):\n"
+                "    FAIL\n    HERE\n",
+                LOG.getvalue(),
+            )
+
+            fname = os.path.join(dname, 'test_bad_ver.py')
+            with open(fname, 'w') as F:
+                F.write(f"""\
+#!{sys.executable}
+import sys
+sys.stderr.write('FAIL\\n')
+sys.stdout.write('GAMS Release : 26.1\\n')
+sys.stdout.write('HERE\\n')
+""")
+            os.chmod(fname, 0o755)
+            opt.config.executable = fname
+            with LoggingIntercept() as LOG:
+                self.assertIsNone(opt.version())
+                self.assertEqual(opt.available(), Availability.NotFound)
+            self.assertEqual(
+                "Failed parsing GAMS version (version not found while parsing):\n"
+                "    FAIL\n    GAMS Release : 26.1\n    HERE\n",
+                LOG.getvalue(),
+            )
+
+            fname = os.path.join(dname, 'test_demo.py')
+            with open(fname, 'w') as F:
+                F.write(f"""\
+#!{sys.executable}
+import sys
+sys.stderr.write('FAIL\\n')
+sys.stdout.write('*** GAMS Release     : 45.1.0 88bbff72\\n')
+sys.stdout.write('*** GAMS Demo, for EULA and demo limitations\\n')
+sys.stdout.write('HERE\\n')
+""")
+            os.chmod(fname, 0o755)
+            opt.config.executable = fname
+            with LoggingIntercept() as LOG:
+                self.assertEqual(opt.version(), (45, 1, 0))
+                self.assertEqual(opt.available(), Availability.LimitedLicense)
+            self.assertEqual("", LOG.getvalue())
+
+            fname = os.path.join(dname, 'test_full.py')
+            with open(fname, 'w') as F:
+                F.write(f"""\
+#!{sys.executable}
+import sys
+sys.stderr.write('FAIL\\n')
+sys.stdout.write('*** GAMS Release     : 45.1.0 88bbff72\\n')
+sys.stdout.write('*** Evaluation expiration date (GAMS base module)\\n')
+sys.stdout.write('HERE\\n')
+""")
+            os.chmod(fname, 0o755)
+            opt.config.executable = fname
+            with LoggingIntercept() as LOG:
+                self.assertEqual(opt.version(), (45, 1, 0))
+                self.assertEqual(opt.available(), Availability.FullLicense)
+            self.assertEqual("", LOG.getvalue())
 
     def test_write_gms_file(self):
         # We are creating a simple model with 1 variable to check for gams execution
@@ -218,15 +313,24 @@ class TestGAMSInterface(unittest.TestCase):
 @unittest.pytest.mark.solver("gams")
 class TestGAMS(unittest.TestCase):
     def create_model(self):
-        model = pyo.ConcreteModel('TestModel')
-        model.x = pyo.Var(initialize=1.5, bounds=(-5, 5))
-        model.y = pyo.Var(initialize=1.5, bounds=(-5, 5))
+        m = pyo.ConcreteModel('TestModel')
+        m.x = pyo.Var(initialize=1.5, bounds=(-5, 5))
+        m.y = pyo.Var(initialize=1.5, bounds=(-5, 5))
 
-        def dummy_equation(m):
-            return (1.0 - m.x) + 100.0 * (m.y - m.x)
+        m.obj = pyo.Objective(
+            expr=(1.0 - m.x) + 100.0 * (m.y - m.x), sense=pyo.minimize
+        )
+        return m
 
-        model.obj = pyo.Objective(rule=dummy_equation, sense=pyo.minimize)
-        return model
+    def create_infeasible_model(self):
+        m = pyo.ConcreteModel('TestModel')
+        m.x = pyo.Var(initialize=1.5, bounds=(-4, 4))
+        m.y = pyo.Var(initialize=1.5, bounds=(-4, 4))
+
+        m.c1 = pyo.Constraint(expr=m.x + m.y >= 5)
+        m.c2 = pyo.Constraint(expr=-m.x + m.y >= 5)
+        m.obj = pyo.Objective(rule=m.x + m.y, sense=pyo.minimize)
+        return m
 
     def test_gams_config(self):
         # Test default initialization
@@ -245,13 +349,287 @@ class TestGAMS(unittest.TestCase):
     def test_gams_solve_gdx(self):
         # Gut check - does it solve?
         model = self.create_model()
-        gams.GAMS().solve(model, writer_config={'put_results_format': 'gdx'})
+        result = gams.GAMS().solve(model, writer_config={'put_results_format': 'gdx'})
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, -1004)
         self.assertAlmostEqual(model.x.value, 5)
         self.assertAlmostEqual(model.y.value, -5)
+
+        model.obj.sense = pyo.maximize
+        result = gams.GAMS().solve(model, writer_config={'put_results_format': 'gdx'})
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, 1006)
+        self.assertAlmostEqual(model.x.value, -5)
+        self.assertAlmostEqual(model.y.value, 5)
 
     def test_gams_solve_dat(self):
         # Gut check - does it solve?
         model = self.create_model()
-        gams.GAMS().solve(model, writer_config={'put_results_format': 'dat'})
+        result = gams.GAMS().solve(model, writer_config={'put_results_format': 'dat'})
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, -1004)
         self.assertAlmostEqual(model.x.value, 5)
         self.assertAlmostEqual(model.y.value, -5)
+
+        model.obj.sense = pyo.maximize
+        result = gams.GAMS().solve(model, writer_config={'put_results_format': 'dat'})
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, 1006)
+        self.assertAlmostEqual(model.x.value, -5)
+        self.assertAlmostEqual(model.y.value, 5)
+
+    @unittest.skipIf(not gams.gdxcc_available, "'gdx' requires the gdx/gdxcc module")
+    def test_gams_solve_noload_gdx(self):
+        # Gut check - does it solve?
+        model = self.create_model()
+        result = gams.GAMS().solve(
+            model, load_solutions=False, writer_config={'put_results_format': 'gdx'}
+        )
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, -1004)
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+        model.obj.sense = pyo.maximize
+        result = gams.GAMS().solve(
+            model, load_solutions=False, writer_config={'put_results_format': 'gdx'}
+        )
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, 1006)
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+    def test_gams_solve_noload_dat(self):
+        # Gut check - does it solve?
+        model = self.create_model()
+        result = gams.GAMS().solve(
+            model, load_solutions=False, writer_config={'put_results_format': 'dat'}
+        )
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, -1004)
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+        model.obj.sense = pyo.maximize
+        result = gams.GAMS().solve(
+            model, load_solutions=False, writer_config={'put_results_format': 'dat'}
+        )
+        self.assertEqual(
+            result.termination_condition,
+            TerminationCondition.convergenceCriteriaSatisfied,
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.optimal)
+        self.assertAlmostEqual(result.incumbent_objective, 1006)
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+    @unittest.skipIf(not gams.gdxcc_available, "'gdx' requires the gdx/gdxcc module")
+    def test_gams_solve_infeasible_gdx(self):
+        # Gut check - does it solve?
+        model = self.create_infeasible_model()
+        result = gams.GAMS().solve(
+            model,
+            raise_exception_on_nonoptimal_result=False,
+            writer_config={'put_results_format': 'gdx'},
+        )
+        self.assertEqual(
+            result.termination_condition, TerminationCondition.provenInfeasible
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.infeasible)
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+        model.obj.sense = pyo.maximize
+        with self.assertRaisesRegex(
+            NoOptimalSolutionError, "Solver did not find the optimal solution."
+        ):
+            result = gams.GAMS().solve(
+                model, writer_config={'put_results_format': 'gdx'}
+            )
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+    def test_gams_solve_infeasible_dat(self):
+        # Gut check - does it solve?
+        model = self.create_infeasible_model()
+        result = gams.GAMS().solve(
+            model,
+            raise_exception_on_nonoptimal_result=False,
+            writer_config={'put_results_format': 'dat'},
+        )
+        self.assertEqual(
+            result.termination_condition, TerminationCondition.provenInfeasible
+        )
+        self.assertEqual(result.solution_status, SolutionStatus.infeasible)
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+        model.obj.sense = pyo.maximize
+        with self.assertRaisesRegex(
+            NoOptimalSolutionError, "Solver did not find the optimal solution."
+        ):
+            result = gams.GAMS().solve(
+                model, writer_config={'put_results_format': 'dat'}
+            )
+        self.assertAlmostEqual(model.x.value, 1.5)
+        self.assertAlmostEqual(model.y.value, 1.5)
+
+    def test_bad_gams_executable(self):
+        model = self.create_model()
+        solver = gams.GAMS()
+        solver.config.executable = "/path/to/bogus/gams"
+
+        with self.assertRaisesRegex(
+            ApplicationError, "GAMS: 'gams' executable not found"
+        ):
+            solver.solve(model)
+
+        # Now try pointing to "executables" that are not GAMS
+        with TempfileManager as tmp:
+            dname = tmp.mkdtemp()
+            solver.config.working_dir = dname
+
+            fname = os.path.join(dname, 'test_rc.py')
+            with open(fname, 'w') as F:
+                F.write(f"""\
+#!{sys.executable}
+import sys
+lst = sys.argv[1].replace('.gms', '.lst')
+for arg in sys.argv:
+    if arg.startswith('o='):
+        lst = arg[2:]
+with open(lst, 'w') as LST:
+    LST.write("Here is a multiline\\nlisting file.\\n")
+sys.stderr.write('FAIL\\n')
+sys.stdout.write('HERE\\n')
+sys.exit(2)
+""")
+            os.chmod(fname, 0o755)
+            solver.config.executable = fname
+            with LoggingIntercept() as LOG:
+                with self.assertRaisesRegex(
+                    RuntimeError,
+                    r"GAMS process encountered an error \(returncode=2\).\n"
+                    r"Check listing file for details.",
+                ):
+                    solver.solve(model)
+            self.assertEqual(
+                "\n".join(
+                    [
+                        "GAMS process encountered an error (returncode=2).",
+                        "Check listing file for details.",
+                        "",
+                        "FAIL",
+                        "HERE",
+                        "",
+                        "GAMS Listing file:",
+                        "",
+                        "Here is a multiline",
+                        "listing file.",
+                    ]
+                )
+                + "\n",
+                LOG.getvalue(),
+            )
+
+        with TempfileManager as tmp:
+            dname = tmp.mkdtemp()
+            solver.config.working_dir = dname
+
+            fname = os.path.join(dname, 'test_rc_no_lst.py')
+            with open(fname, 'w') as F:
+                F.write(f"""\
+#!{sys.executable}
+import sys
+sys.stderr.write('FAIL\\n')
+sys.stdout.write('HERE\\n')
+sys.exit(2)
+""")
+            os.chmod(fname, 0o755)
+            solver.config.executable = fname
+            with LoggingIntercept() as LOG:
+                with self.assertRaisesRegex(
+                    RuntimeError,
+                    r"GAMS process encountered an error \(returncode=2\).\n"
+                    r"Check listing file for details.",
+                ):
+                    solver.solve(model)
+            self.assertEqual(
+                "\n".join(
+                    [
+                        "GAMS process encountered an error (returncode=2).",
+                        "Check listing file for details.",
+                        "",
+                        "FAIL",
+                        "HERE",
+                    ]
+                )
+                + "\n",
+                LOG.getvalue(),
+            )
+
+        with TempfileManager as tmp:
+            dname = tmp.mkdtemp()
+            solver.config.working_dir = dname
+
+            fname = os.path.join(dname, 'test_rc3_no_lst.py')
+            with open(fname, 'w') as F:
+                F.write(f"""\
+#!{sys.executable}
+import sys
+sys.stderr.write('FAIL\\n')
+sys.stdout.write('HERE\\n')
+sys.exit(3)
+""")
+            os.chmod(fname, 0o755)
+            solver.config.executable = fname
+            with LoggingIntercept() as LOG:
+                with self.assertRaisesRegex(
+                    RuntimeError,
+                    r"GAMS process encountered an error \(returncode=3\).\n"
+                    r"Error rc=3 \(GAMS execution error\), to be determined later.\n"
+                    r"Check listing file for details.",
+                ):
+                    solver.solve(model)
+            self.assertEqual(
+                "\n".join(
+                    [
+                        "GAMS process encountered an error (returncode=3).",
+                        "Error rc=3 (GAMS execution error), to be determined later.",
+                        "Check listing file for details.",
+                        "",
+                        "FAIL",
+                        "HERE",
+                    ]
+                )
+                + "\n",
+                LOG.getvalue(),
+            )
