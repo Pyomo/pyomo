0338bcc359fa1970c6ac967b7af72b42afe7196b
Michael Bynum <mlbynum@sandia.gov>
2026-03-17 13:30:53 -0600

    update gams solution loader

--- pyomo/contrib/solver/solvers/gams.py
+++ pyomo/contrib/solver/solvers/gams.py
@@ -431,7 +431,7 @@ class GAMS(SolverBase):
         results.solution_status = solution_status
 
         # replaced below, if solution should be loaded
-        results.solution_loader = GMSSolutionLoader(None, None)
+        results.solution_loader = GMSSolutionLoader(model, None, None)
 
         if solvestat == 1:
             results.termination_condition = model_term
@@ -453,7 +453,7 @@ class GAMS(SolverBase):
 
         if results.solution_status in {SolutionStatus.feasible, SolutionStatus.optimal}:
             results.solution_loader = GMSSolutionLoader(
-                gdx_data=model_soln, gms_info=gms_info
+                pyomo_model=model, gdx_data=model_soln, gms_info=gms_info
             )
 
             if config.load_solutions:
