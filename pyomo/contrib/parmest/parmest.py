fe0627a58796bd30ac537bcc98f77a8ae3ab1543
sscini <scini@nd.edu>
2026-05-11 11:23:07 -0300

    Moved deprecated functions down

--- pyomo/contrib/parmest/parmest.py
+++ pyomo/contrib/parmest/parmest.py
@@ -81,159 +81,6 @@ inverse_reduced_hessian, inverse_reduced_hessian_available = attempt_import(
 
 logger = logging.getLogger(__name__)
 
-
-# Only used in the deprecatedEstimator class
-def ef_nonants(ef):
-    # Wrapper to call someone's ef_nonants
-    # (the function being called is very short, but it might be changed)
-    if use_mpisppy:
-        return sputils.ef_nonants(ef)
-    else:
-        return local_ef.ef_nonants(ef)
-
-
-# Only used in the deprecatedEstimator class
-def _experiment_instance_creation_callback(
-    scenario_name, node_names=None, cb_data=None
-):
-    """
-    This is going to be called by mpi-sppy or the local EF and it will call into
-    the user's model's callback.
-
-    Parameters:
-    -----------
-    scenario_name: `str` Scenario name should end with a number
-    node_names: `None` ( Not used here )
-    cb_data : dict with ["callback"], ["BootList"],
-              ["theta_names"], ["cb_data"], etc.
-              "cb_data" is passed through to user's callback function
-                        that is the "callback" value.
-              "BootList" is None or bootstrap experiment number list.
-                       (called cb_data by mpisppy)
-
-
-    Returns:
-    --------
-    instance: `ConcreteModel`
-        instantiated scenario
-
-    Note:
-    ----
-    There is flexibility both in how the function is passed and its signature.
-    """
-    assert cb_data is not None
-    outer_cb_data = cb_data
-    scen_num_str = re.compile(r'(\d+)$').search(scenario_name).group(1)
-    scen_num = int(scen_num_str)
-    basename = scenario_name[: -len(scen_num_str)]  # to reconstruct name
-
-    CallbackFunction = outer_cb_data["callback"]
-
-    if callable(CallbackFunction):
-        callback = CallbackFunction
-    else:
-        cb_name = CallbackFunction
-
-        if "CallbackModule" not in outer_cb_data:
-            raise RuntimeError(
-                "Internal Error: need CallbackModule in parmest callback"
-            )
-        else:
-            modname = outer_cb_data["CallbackModule"]
-
-        if isinstance(modname, str):
-            cb_module = im.import_module(modname, package=None)
-        elif isinstance(modname, types.ModuleType):
-            cb_module = modname
-        else:
-            print("Internal Error: bad CallbackModule")
-            raise
-
-        try:
-            callback = getattr(cb_module, cb_name)
-        except:
-            print("Error getting function=" + cb_name + " from module=" + str(modname))
-            raise
-
-    if "BootList" in outer_cb_data:
-        bootlist = outer_cb_data["BootList"]
-        # print("debug in callback: using bootlist=",str(bootlist))
-        # assuming bootlist itself is zero based
-        exp_num = bootlist[scen_num]
-    else:
-        exp_num = scen_num
-
-    scen_name = basename + str(exp_num)
-
-    cb_data = outer_cb_data["cb_data"]  # cb_data might be None.
-
-    # at least three signatures are supported. The first is preferred
-    try:
-        instance = callback(experiment_number=exp_num, cb_data=cb_data)
-    except TypeError:
-        raise RuntimeError(
-            "Only one callback signature is supported: "
-            "callback(experiment_number, cb_data) "
-        )
-        """
-        try:
-            instance = callback(scenario_tree_model, scen_name, node_names)
-        except TypeError:  # deprecated signature?
-            try:
-                instance = callback(scen_name, node_names)
-            except:
-                print("Failed to create instance using callback; TypeError+")
-                raise
-        except:
-            print("Failed to create instance using callback.")
-            raise
-        """
-    if hasattr(instance, "_mpisppy_node_list"):
-        raise RuntimeError(f"scenario for experiment {exp_num} has _mpisppy_node_list")
-    nonant_list = [
-        instance.find_component(vstr) for vstr in outer_cb_data["theta_names"]
-    ]
-    if use_mpisppy:
-        instance._mpisppy_node_list = [
-            scenario_tree.ScenarioNode(
-                name="ROOT",
-                cond_prob=1.0,
-                stage=1,
-                cost_expression=instance.FirstStageCost,
-                nonant_list=nonant_list,
-                scen_model=instance,
-            )
-        ]
-    else:
-        instance._mpisppy_node_list = [
-            scenario_tree.ScenarioNode(
-                name="ROOT",
-                cond_prob=1.0,
-                stage=1,
-                cost_expression=instance.FirstStageCost,
-                scen_name_list=None,
-                nonant_list=nonant_list,
-                scen_model=instance,
-            )
-        ]
-
-    if "ThetaVals" in outer_cb_data:
-        thetavals = outer_cb_data["ThetaVals"]
-
-        # dlw august 2018: see mea code for more general theta
-        for name, val in thetavals.items():
-            theta_cuid = ComponentUID(name)
-            theta_object = theta_cuid.find_component_on(instance)
-            if val is not None:
-                # print("Fixing",vstr,"at",str(thetavals[vstr]))
-                theta_object.fix(val)
-            else:
-                # print("Freeing",vstr)
-                theta_object.unfix()
-
-    return instance
-
-
 def SSE(model):
     """
     Returns an expression that is used to compute the sum of squared errors
@@ -2276,6 +2123,155 @@ class Estimator:
 # deprecated functions/classes #
 ################################
 
+# Only used in the deprecatedEstimator class after 6.10.1dev0
+def ef_nonants(ef):
+    # Wrapper to call someone's ef_nonants
+    # (the function being called is very short, but it might be changed)
+    if use_mpisppy:
+        return sputils.ef_nonants(ef)
+    else:
+        return local_ef.ef_nonants(ef)
+
+# Only used in the deprecatedEstimator class
+def _experiment_instance_creation_callback(
+    scenario_name, node_names=None, cb_data=None
+):
+    """
+    This is going to be called by mpi-sppy or the local EF and it will call into
+    the user's model's callback.
+
+    Parameters:
+    -----------
+    scenario_name: `str` Scenario name should end with a number
+    node_names: `None` ( Not used here )
+    cb_data : dict with ["callback"], ["BootList"],
+              ["theta_names"], ["cb_data"], etc.
+              "cb_data" is passed through to user's callback function
+                        that is the "callback" value.
+              "BootList" is None or bootstrap experiment number list.
+                       (called cb_data by mpisppy)
+
+
+    Returns:
+    --------
+    instance: `ConcreteModel`
+        instantiated scenario
+
+    Note:
+    ----
+    There is flexibility both in how the function is passed and its signature.
+    """
+    assert cb_data is not None
+    outer_cb_data = cb_data
+    scen_num_str = re.compile(r'(\d+)$').search(scenario_name).group(1)
+    scen_num = int(scen_num_str)
+    basename = scenario_name[: -len(scen_num_str)]  # to reconstruct name
+
+    CallbackFunction = outer_cb_data["callback"]
+
+    if callable(CallbackFunction):
+        callback = CallbackFunction
+    else:
+        cb_name = CallbackFunction
+
+        if "CallbackModule" not in outer_cb_data:
+            raise RuntimeError(
+                "Internal Error: need CallbackModule in parmest callback"
+            )
+        else:
+            modname = outer_cb_data["CallbackModule"]
+
+        if isinstance(modname, str):
+            cb_module = im.import_module(modname, package=None)
+        elif isinstance(modname, types.ModuleType):
+            cb_module = modname
+        else:
+            print("Internal Error: bad CallbackModule")
+            raise
+
+        try:
+            callback = getattr(cb_module, cb_name)
+        except:
+            print("Error getting function=" + cb_name + " from module=" + str(modname))
+            raise
+
+    if "BootList" in outer_cb_data:
+        bootlist = outer_cb_data["BootList"]
+        # print("debug in callback: using bootlist=",str(bootlist))
+        # assuming bootlist itself is zero based
+        exp_num = bootlist[scen_num]
+    else:
+        exp_num = scen_num
+
+    scen_name = basename + str(exp_num)
+
+    cb_data = outer_cb_data["cb_data"]  # cb_data might be None.
+
+    # at least three signatures are supported. The first is preferred
+    try:
+        instance = callback(experiment_number=exp_num, cb_data=cb_data)
+    except TypeError:
+        raise RuntimeError(
+            "Only one callback signature is supported: "
+            "callback(experiment_number, cb_data) "
+        )
+        """
+        try:
+            instance = callback(scenario_tree_model, scen_name, node_names)
+        except TypeError:  # deprecated signature?
+            try:
+                instance = callback(scen_name, node_names)
+            except:
+                print("Failed to create instance using callback; TypeError+")
+                raise
+        except:
+            print("Failed to create instance using callback.")
+            raise
+        """
+    if hasattr(instance, "_mpisppy_node_list"):
+        raise RuntimeError(f"scenario for experiment {exp_num} has _mpisppy_node_list")
+    nonant_list = [
+        instance.find_component(vstr) for vstr in outer_cb_data["theta_names"]
+    ]
+    if use_mpisppy:
+        instance._mpisppy_node_list = [
+            scenario_tree.ScenarioNode(
+                name="ROOT",
+                cond_prob=1.0,
+                stage=1,
+                cost_expression=instance.FirstStageCost,
+                nonant_list=nonant_list,
+                scen_model=instance,
+            )
+        ]
+    else:
+        instance._mpisppy_node_list = [
+            scenario_tree.ScenarioNode(
+                name="ROOT",
+                cond_prob=1.0,
+                stage=1,
+                cost_expression=instance.FirstStageCost,
+                scen_name_list=None,
+                nonant_list=nonant_list,
+                scen_model=instance,
+            )
+        ]
+
+    if "ThetaVals" in outer_cb_data:
+        thetavals = outer_cb_data["ThetaVals"]
+
+        # dlw august 2018: see mea code for more general theta
+        for name, val in thetavals.items():
+            theta_cuid = ComponentUID(name)
+            theta_object = theta_cuid.find_component_on(instance)
+            if val is not None:
+                # print("Fixing",vstr,"at",str(thetavals[vstr]))
+                theta_object.fix(val)
+            else:
+                # print("Freeing",vstr)
+                theta_object.unfix()
+
+    return instance
 
 @deprecated(version='6.7.2')
 def group_data(data, groupby_column_name, use_mean=None):
