3bd26e3559727eca9a6a13fec83b855fd1c5e909
Stephen Cini <114932899+sscini@users.noreply.github.com>
2026-05-11 08:08:10 -0300

    Ran black

--- pyomo/contrib/parmest/parmest.py
+++ pyomo/contrib/parmest/parmest.py
@@ -1287,12 +1287,12 @@ class Estimator:
             If fix_theta is False, this is the optimal value of the objective.
         theta_estimates : dict
             Dictionary of estimated theta values. If fix_theta is True, this will be the same as
-            the input theta_vals (or default values if theta_vals is None). If fix_theta is False, 
+            the input theta_vals (or default values if theta_vals is None). If fix_theta is False,
             this will be the estimated parameter values that optimize the objective.
         var_values : pd.DataFrame, optional
-            DataFrame of variable values for the variables specified in return_values. 
-            Only returned if return_values is not None and contains valid variable names. 
-            The DataFrame will have one row per scenario block (experiment) and columns 
+            DataFrame of variable values for the variables specified in return_values.
+            Only returned if return_values is not None and contains valid variable names.
+            The DataFrame will have one row per scenario block (experiment) and columns
             corresponding to the variable names in return_values.
 
         """
